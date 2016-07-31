c869191e6c677a113d41d951170423b95fac7e53
xdite <xuite.joke@gmail.com>
2016-07-31 20:49:50 +0800

    implement require_is_admin

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ProductsController < ApplicationController
   before_action :authenticatd_user!
+  before_action :admin_required
 
   def index
     @products = Product.all
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def admin_required
+    redirect_to root_path unless current_user.admin?
+  end
 end
