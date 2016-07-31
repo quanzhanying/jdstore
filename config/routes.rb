a1f956a225cbf8df12c7699d9d1c3aa6e02d2e52
xdite <xuite.joke@gmail.com>
2016-07-31 21:10:30 +0800

    products#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :products
+
   namespace :admin do
     resources :products
   end
 
-  root 'welcome#index'
+  root 'products#index'
 end
