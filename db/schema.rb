d47a13981fb350ba1a5fc5b8f6ba5fdbf297a578
chpwang <chpengwang@gmail.com>
2016-08-02 16:44:14 +0800

    add quantity and price column to Model items

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160802075457) do
+ActiveRecord::Schema.define(version: 20160802083721) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "quantity"
+    t.float    "price"
   end
 
   create_table "sellers", force: :cascade do |t|
