2abfd72ca7adfb6c09dcc32094c601794c85d34d
Jane <jane@JanedeMacBook-Air.local>
2016-08-03 15:17:57 +0800

    add crud to admin controller

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -8,18 +8,35 @@ class Admin::ProductsController < ApplicationController
     def new
       @product=Product.new
       @product.user=current_user
+
     end
 
     def create
       @product=Product.new(params_product)
+      @product.user=current_user
       @product.save
+      redirect_to admin_products_path
+    end
 
+    def edit
+      @product=Product.find(params[:id])
+    end
+
+    def update
+      @product=Product.update(params_product)
+      redirect_to admin_products_path
+    end
+
+    def destroy
+      @product=Product.find(params[:id])
+      @product.destroy
+      redirect_to admin_products_path
     end
 
     private
 
     def params_product
-      params_product.require(:product).permit(:title, :description, :quantity, :price, :image)
+      params.require(:product).permit(:title, :description, :quantity, :price, :image)
     end
 
 
