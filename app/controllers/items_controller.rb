aecc83e242f0f103ab51ba461977847b4b8fde56
chpwang <chpengwang@gmail.com>
2016-08-03 17:27:30 +0800

    minor decoration bug fixed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,7 @@ class ItemsController < ApplicationController
 	end
 
 	def show
-		
+		@item = Item.find(params[:id])
 	end
 	
 end
--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,11 @@
 module ItemsHelper
+
+	def render_image_link(item)
+
+		if item.image?
+			link_to(image_tag(item.image.thumb.url, class: "img-rounded"), item_path(item))
+		else
+			link_to(item.title, item_path(item))
+		end
+	end
 end
