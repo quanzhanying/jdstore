c3cfcf396a258af0fdb637930db3683440a9f712
chpwang <chpengwang@gmail.com>
2016-08-03 12:01:36 +0800

    implement admin panel CRUD

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,22 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  quantity    :integer
+#  price       :float
+#  user_id     :integer
+#
+
 class Item < ApplicationRecord
+	validates :title, presence: true
+	validates :description, presence: true
+	validates :quantity, numericality: { greater_or_equal_to: 0}
+	validates :price, numericality: { greater_or_equal_to: 0.0}
+
+	belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160802070847) do
+ActiveRecord::Schema.define(version: 20160803033312) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "quantity"
+    t.float    "price"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160802070847) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "is_admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
