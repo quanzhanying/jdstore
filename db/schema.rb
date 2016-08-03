fa9a472dcf85ee32e5dd1dc7fe20d7307a3be90d
shaojunda <shaojunda@gmail.com>
2016-08-03 13:30:35 +0800

    remove stock column from product

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803025828) do
+ActiveRecord::Schema.define(version: 20160803052742) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.integer  "quantity"
     t.integer  "price"
-    t.integer  "stock"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
