e8bccb353f36afa4bd4dd1a5f92f305401c7f1f5
shaojunda <shaojunda@gmail.com>
2016-08-03 17:21:09 +0800

    add sidebar to admin layout

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,7 @@
 class Admin::ProductsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_admin
+  layout "admin"
 
   def index
     @products = Product.all
