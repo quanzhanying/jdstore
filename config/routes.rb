009d90ae425954e985415ea0df6946fedbb095b7
chenyunli <chenyunli@chenyunlideMacBook-Pro.local>
2016-08-03 17:51:34 +0800

    admin can set user to admin or not

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :admin do
-    resources:products
+    resources :products
+
+    resources :users do
+      member do
+        post :be_admin
+      end
+    end
   end
 
   root 'products#index'
