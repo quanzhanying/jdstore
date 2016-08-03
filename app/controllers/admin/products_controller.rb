c359c19bb13d49d7bb097590f5efebbc617b28cd
Jane <jane@JanedeMacBook-Air.local>
2016-08-03 14:32:10 +0800

    add dropdown

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -2,7 +2,25 @@ class Admin::ProductsController < ApplicationController
 
     def index
       @products=Product.all
+      # @product.user=current_user
+    end
+
+    def new
+      @product=Product.new
+      @product.user=current_user
+    end
+
+    def create
+      @product=Product.new(params_product)
+      @product.save
 
     end
+
+    private
+
+    def params_product
+      params_product.require(:product).permit(:title, :description, :quantity, :price, :image)
+    end
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803033355) do
+ActiveRecord::Schema.define(version: 20160803050543) do
 
   create_table "products", force: :cascade do |t|
     t.datetime "created_at",  null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160803033355) do
     t.integer  "quantity"
     t.float    "price"
     t.string   "image"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
