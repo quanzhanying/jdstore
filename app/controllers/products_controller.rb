ccc785dceeda511edbc320191f84cf51a6a29958
nieyicheng <nieyicheng1990@gmail.com>
2016-08-03 16:18:16 +0800

    uploader

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -42,7 +42,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price)
+    params.require(:product).permit(:title, :description, :quantity, :price, :image)
   end
 
 end
