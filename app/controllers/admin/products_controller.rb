1a8474771032838699f47dc861a02ec2269c17c6
潘秀斌 <panxiubin@panxiubindeMacBook-Pro.local>
2016-08-04 15:11:04 +0800

    implement admin crud

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,13 @@
 class Admin::ProductsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_filter :require_is_admin
+
+  def require_is_admin
+    if !current_user.admin?
+      flash[:alert] = 'You are not admin'
+      redirect_to root_path
+    end
+  end
 
   def show
     @product = Product.find(params[:id])
