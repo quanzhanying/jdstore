20569d3915df59d465d21559317a2bb74d1c56b4
nfreeness <nfreeness@gmail.com>
2016-08-04 17:12:00 +0800

    implement admin_product#new/edit

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,12 @@
 class Admin::ProductsController < ApplicationController
+  before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
 
   def index
     @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
   end
 
   def new
@@ -10,11 +15,30 @@ class Admin::ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    if  @product.save
-      redirect_to admin_products_path, notice: "Create new product succsess!"
+    if @product.save
+      redirect_to admin_products_path, notice: "Product create success!"
     else
       render :new
     end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_products_path, notice: "Product update success!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to admin_products_path, alert: "Product deleted!"
   end
 
   private
