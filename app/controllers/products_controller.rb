c966638168a5e9ddb44cd09fe894c5d10000b6ab
xdite <xuite.joke@gmail.com>
2016-08-04 21:48:36 +0800

    implement add_to_cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,9 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def add_product_to_cart
+    @product = Product.find(params[:id])
+    redirect_to :back
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :products
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
 
   namespace :admin do
     resources :products
