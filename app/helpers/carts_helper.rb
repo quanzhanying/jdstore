8290a4bc945a489412dfd02fc5c57519bedaa902
xdite <xuite.joke@gmail.com>
2016-08-05 14:32:06 +0800

    cart total price

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,9 +1,5 @@
 module CartsHelper
   def render_cart_total_price(cart)
-    sum = 0
-    cart.cart_items.each do |cart_item|
-      sum += cart_item.quantity * cart_item.product.price
-    end
-    sum
+    cart.total_price
   end
 end
