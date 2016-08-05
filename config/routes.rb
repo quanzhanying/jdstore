d85e784d50dda17ef9fc90d26381588142081f69
xdite <xuite.joke@gmail.com>
2016-08-05 15:16:29 +0800

    order info

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,13 @@ Rails.application.routes.draw do
     resources :products
   end
 
-  resources :carts
+  resources :carts do
+    collection do
+      post :checkout
+    end
+  end
+
+  resources :orders
 
   root 'products#index'
 end
