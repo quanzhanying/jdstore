8ae6b270f449c388c5ee10d279dbb2e239ade853
xdite <xuite.joke@gmail.com>
2016-08-05 12:12:45 +0800

    Cart Summary

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
     resources :products
   end
 
+  resources :carts
+
   root 'products#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,9 @@ ActiveRecord::Schema.define(version: 20160804134251) do
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
     t.integer  "product_id"
-    t.integer  "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "quantity",   default: 1
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "carts", force: :cascade do |t|
