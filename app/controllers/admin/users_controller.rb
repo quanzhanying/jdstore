9e6dfeed21f55a7596ca545e432988272544b384
nfreeness <nfreeness@gmail.com>
2016-08-05 16:07:24 +0800

    implement image upload & show

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,5 @@
 class Admin::UsersController < ApplicationController
-  before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_filter :authenticate_user!, only: [:index, :promote, :demote,]
   before_filter :require_is_admin
 
   layout 'admin'
