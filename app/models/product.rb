769072b56e411c903b453f1eff0c4e8b2b35479e
nfreeness <nfreeness@gmail.com>
2016-08-05 09:30:08 +0800

    fix product validates & is_admin model

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,4 +12,8 @@
 #
 
 class Product < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :quantity, presence: true
+  validates :price, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   def admin?
-     email == 'admin@jd.com'
+     is_admin
   end
 
 end
