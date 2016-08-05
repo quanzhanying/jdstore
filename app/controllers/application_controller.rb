d87bff26537affd02e4eef2da3ee39c5a576186c
xdite <xuite.joke@gmail.com>
2016-08-05 12:03:04 +0800

    fix routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  helper_method :current_cart
+
   def admin_required
     redirect_to root_path unless current_user.admin?
+  end
+
+  def current_cart
+    @current_cart ||= find_cart
+  end
+
+  private
+
+  def find_cart
+    cart = Cart.find_by(id: session[:cart_id])
+
+    cart = Cart.create if cart.blank?
+
+    session[:cart_id] = cart.id
+
+    cart
   end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -10,4 +10,8 @@
 class Cart < ApplicationRecord
   has_many :cart_items
   has_many :products, through: :cart_items, source: :product
+
+  def add_product_to_cart(product)
+    products << product
+  end
 end
