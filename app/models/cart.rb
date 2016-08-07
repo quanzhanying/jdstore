56e2615d83f9aa54a9da3763d1d132625690797a
shaojunda <shaojunda@gmail.com>
2016-08-07 19:41:37 +0800

    add check quantity, only when quantity more than 1, the product can be move to cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -12,9 +12,7 @@ class Cart < ApplicationRecord
   has_many :products, :through => :cart_items, :source => :product
 
   def add_product_to_cart(product)
-    puts "+++++#{cart_items}"
     ci = cart_items.build
-    puts "#{ci}+++++#{cart_items}"
     ci.product = product
     ci.quantity = 1
     ci.save
