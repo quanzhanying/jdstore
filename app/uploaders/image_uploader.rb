82d1420424cc25d5930be0a2d4f16c1c18e3c429
shaojunda <shaojunda@gmail.com>
2016-08-07 16:14:59 +0800

    set default pic when don't upload a pic; set timezone to beijing

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -25,6 +25,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
+  def default_url
+    # "/images/fallback/" + [thumb, "avatar.jpg"].compact.join('_')
+    "/images/fallback/avatar.jpg"
+  end
+
   # Process files as they are uploaded:
   # process :scale => [200, 300]
   #
