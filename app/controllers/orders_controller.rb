4344abe7b61f3853d1a290cf2e52aaf121015557
shaojunda <shaojunda@gmail.com>
2016-08-08 13:33:46 +0800

    implement orderlist; impelement render_create_at make careted_at is format by (yyyy-mm-dd hh:mm:ss)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,10 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:create]
 
+  def index
+    @orders = Order.all
+  end
+
   def show
     @order = Order.find_by_token(params[:id])
   end
