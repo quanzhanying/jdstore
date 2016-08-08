00cf21707c01aef431c90970a189f4a90e7926c7
geophy <ligeophy@gmail.com>
2016-08-08 17:28:07 +0800

    add cart delete method

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-    root 'welcome#index'
+    root 'products#index'
 
     namespace :admin do
       resources :products
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
     end
 
     resources :carts do
-    end 
+      post :delete_one
+    end
 
 end
