f1fdd2341835a3cb57328e03098df738f6fc2646
geophy <ligeophy@gmail.com>
2016-08-08 16:10:50 +0800

    add carts

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,14 @@ Rails.application.routes.draw do
     namespace :admin do
       resources :products
     end
+
+    resources :products do
+      member do
+        post :add_to_cart
+      end
+    end
+
+    resources :carts do
+    end 
+
 end
