98621c7b5a58cdd45979e16446fb9433046b72c4
chenyunli <chenyunli@chenyunlideMacBook-Pro.local>
2016-08-08 16:18:14 +0800

    can't add to cart twice

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,9 +10,16 @@ class ProductsController < ApplicationController
   def add_to_cart
     @product = Product.find(params[:id])
 
-    if @product.quantity>0 
-      current_cart.add_product_to_cart(@product)
-      flash[:notice] = "加入购物车成功"
+    if @product.quantity>0
+
+      @cart_item = CartItem.find_by(product_id: @product.id,cart_id: current_cart.id)
+
+      if @cart_item
+        flash[:alert] = "不能重复加入购物车哦"
+      else
+        current_cart.add_product_to_cart(@product)
+        flash[:notice] =  "加入购物车成功"
+      end
     else
       flash[:alert] = "此商品被抢光了哦"
     end
