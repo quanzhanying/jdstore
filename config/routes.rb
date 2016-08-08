0773d570caa58a837ed5a23ae37c0afd1ffd241b
mayalin <mayalin2012@gmail.com>
2016-08-08 13:10:23 +0800

    implement story3

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :products
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
+  
+  resources :carts
+
   namespace :admin do
     resources :products
   end
+
   root 'products#index'
 end
