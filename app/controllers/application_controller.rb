e17daf081df069a43e88c4bb51448c2f6789c583
liumiao2023 <liumiao2023@hotmail.com>
2016-08-09 00:56:55 +0800

    add delete single product

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,5 @@ class ApplicationController < ActionController::Base
     session[:cart_id] = cart.id
     return cart
   end
+  
 end
--- app/controllers/shipping_cart_controller.rb
+++ app/controllers/shipping_cart_controller.rb
@@ -1,3 +1,9 @@
 class ShippingCartController < ApplicationController
 
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to :back
+  end
+  
 end
