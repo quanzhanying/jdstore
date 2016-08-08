63398457bae9533cc88b93a37a7d0ec95d16f43f
Rebecca Wang <wangmmqq@gmail.com>
2016-08-08 17:03:16 +0800

    product in cart can't be more than product quantity, and can't be less than 1

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,15 +2,24 @@ class CartsController < ApplicationController
 
   def up_product
       @cart_item = CartItem.find(params[:id])
-      @cart_item.quantity = @cart_item.quantity + 1
-      @cart_item.save
+      @product = @cart_item.product
+      if (@cart_item.quantity + 1) > @product.quantity
+        flash[:alert] = "抱歉，库存不够了"
+      else
+        @cart_item.quantity = @cart_item.quantity + 1
+        @cart_item.save
+      end
       redirect_to :back
   end
 
   def down_product
       @cart_item = CartItem.find(params[:id])
-      @cart_item.quantity = @cart_item.quantity - 1
-      @cart_item.save
+      if (@cart_item.quantity - 1) < 1
+        flash[:alert] = "最少要一个产品哦"
+      else
+        @cart_item.quantity = @cart_item.quantity - 1
+        @cart_item.save
+      end
       redirect_to :back
   end
 
