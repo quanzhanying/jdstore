99a9f4641588651f6cea219318cacb57f355b925
shaojunda <shaojunda@gmail.com>
2016-08-08 13:33:46 +0800

    implement orderlist; impelement render_create_at make careted_at is format by (yyyy-mm-dd hh:mm:ss)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def render_created_at(created_at)
+    created_at.strftime("%Y-%m-%d %H:%M:%S")
+  end
 end
--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,5 @@
 module OrdersHelper
+  def render_order_created_at(create_at)
+    create_at.strftime("%Y-%m-%d %H:%M:%S")
+  end
 end
