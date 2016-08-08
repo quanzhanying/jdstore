96b553ed4ab95d1992dbde829766e7dd072a68ee
liyanzeng <yanzengli@gmail.com>
2016-08-08 14:47:07 +0800

    delete cart item

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,14 @@
 class CartsController < ApplicationController
+
+  def index
+    @carts = Cart.all
+  end
+
+
+  def destroy
+    @cart = Cart.find(session[:cart_id])
+    @cart.destroy
+    redirect_to root_path
+  end
+
 end
