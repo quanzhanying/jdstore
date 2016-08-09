9c70f7d049029ca78a83a68813be53a3a178495c
shaojunda <shaojunda@gmail.com>
2016-08-10 01:07:24 +0800

    use default carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,12 +2,17 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage :fog
     config.fog_credentials = {
-      provider: "AWS",
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: EVN["AWS_SECRET_ACCESS_KEY"],
-      region: 'US Standard'
+      provider:              'AWS',
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],      # 你的 key
+
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],      # 你的 secret key
+
+      region:                'US Standard' # 你的 S3 bucket 的 Region 位置
+
     }
-    config.fog_directory = ENV["AWS_BUCKET_NAME"]
+    config.fog_directory  = ENV["AWS_BUCKET_NAME"] # 你設定的 bucket name
+
+
   else
     config.storage :file
   end
