a47e32d9bd73b898fa8429f40f51b31e84fcb059
shaojunda <shaojunda@gmail.com>
2016-08-09 11:55:12 +0800

    implement mail to user after they paid.

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'file_validators'
 gem 'mini_magick'
 gem "font-awesome-rails"
 gem 'rubocop'
+gem 'letter_opener', group: :development
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,9 @@
+class OrderMailer < ApplicationMailer
+  def notify_order_placed(order)
+    @order = order
+    @user = order.user
+    @product_lists = @order.product_lists
+
+    mail(to: @user.email, subject: "[JDStore] Thanks for you ordering, this si your order information #{order.token}")
+  end
+end
