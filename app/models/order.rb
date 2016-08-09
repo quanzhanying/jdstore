7c91d0a434b04ee8c4662345ff47ac00fdfd819f
nfreeness <nfreeness@gmail.com>
2016-08-09 16:01:07 +0800

    use token url

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
+  before_create :generate_token
   belongs_to :user
   has_many :product_lists
 
@@ -7,4 +8,7 @@ class Order < ApplicationRecord
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
 
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
 end
