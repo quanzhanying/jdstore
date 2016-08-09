9ffbedb7808658815fbfc619c8f122021f672059
nfreeness <nfreeness@gmail.com>
2016-08-09 18:54:02 +0800

    fix product_list relationship

---
+++ db/migrate/20160809103905_add_product_id_to_prouduct_lists.rb
@@ -0,0 +1,5 @@
+class AddProductIdToProuductLists < ActiveRecord::Migration[5.0]
+  def change
+    add_column :product_lists, :product_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809073923) do
+ActiveRecord::Schema.define(version: 20160809103905) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20160809073923) do
     t.integer  "quantity"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "product_id"
   end
 
   create_table "products", force: :cascade do |t|
