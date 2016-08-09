53657d7c0da3c5f698bf2cd3d83d422dde5b80c0
shaojunda <shaojunda@gmail.com>
2016-08-09 23:47:42 +0800

    deal confilict

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: "AWS",
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: EVN["AWS_SECRET_ACCESS_KEY"]
-      region: "US Standard"
+      aws_secret_access_key: EVN["AWS_SECRET_ACCESS_KEY"],
+      region: 'US Standard'
     }
     config.fog_directory = ENV["AWS_BUCKET_NAME"]
   else
