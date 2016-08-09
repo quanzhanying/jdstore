afd2d6ce73e5db775f9ebb2072465841b301b157
shaojunda <shaojunda@gmail.com>
2016-08-09 12:57:09 +0800

    edit eviroment/development.rb and production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.after_initialize do
+    Pay2go.integratioin_mode = :production
+  end
 end
