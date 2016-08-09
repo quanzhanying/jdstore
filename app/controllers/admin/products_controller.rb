d09fa9e383244eb9d4ba1df7a849a0fb21b1e3fc
liyanzeng <yanzengli@gmail.com>
2016-08-09 18:19:38 +0800

    order history

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -2,7 +2,7 @@ class Admin::ProductsController < ApplicationController
 
 before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
 before_filter :require_is_admin
-
+layout "admin"
   def index
     @products = Product.all
   end
