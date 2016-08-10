27e770c2e94a9054c129f36416036d52ae82e9f0
geophy <ligeophy@gmail.com>
2016-08-10 09:24:55 +0800

    生成订单

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
 
   def create
     @order = Order.new(order_params)
@@ -6,10 +7,24 @@ class OrdersController < ApplicationController
     @order.total = current_cart.total_price
 
     if @order.save
-      redirect_to order_path(@order)
+      current_cart.cart_items.each do |cart_item|
+        product_list = ProductList.new
+        product_list.order = @order
+        product_list.product_name = cart_item.product.title
+        product_list.product_price = cart_item.product.price
+        product_list.quantity = cart_item.product.quantity
+      end
+
+      redirect_to order_path(@order.token)
     else
       render 'carts/checkout'
     end
+  end
+
+  def show
+    @order = Order.find_by_token(params[:id])
+    @product_lists = @order.product_lists
+
   end
 
   private
