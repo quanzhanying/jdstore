73b38656fccbb3a1abfa37b627232e2907ee14a2
chpwang <chpengwang@gmail.com>
2016-08-11 10:10:56 +0800

    fix a bug in config/initializers/carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
       :provider               => 'AWS',                        # required
       :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],                        # required
       :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],
-      :region  => 'AWS_BUCKET_REGION'                        # required
+      :region  => ENV['AWS_BUCKET_REGION']                        # required
     }
     config.fog_directory  = ENV["AWS_BUCKET_NAME"]                     # required
   else
