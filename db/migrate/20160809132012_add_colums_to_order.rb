1e5de021b83082caddcc9d3a6377059c18a48ac0
Rebecca Wang <wangmmqq@gmail.com>
2016-08-11 10:41:07 +0800

    change db migration 2012 again

--- db/migrate/20160809132012_add_colums_to_order.rb
+++ db/migrate/20160809132012_add_colums_to_order.rb
@@ -1,9 +1,4 @@
 class AddColumsToOrder < ActiveRecord::Migration[5.0]
   def change
-    add_column :orders, :total, :integer, default: 0
-    add_column :orders, :billing_name, :string
-    add_column :orders, :billing_address, :string
-    add_column :orders, :shipping_name, :string
-    add_column :orders, :shipping_address, :string
   end
 end
