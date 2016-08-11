373298e74d0e899982f311dc06dc4ad96b9529e2
mayalin <mayalin2012@gmail.com>
2016-08-11 15:39:48 +0800

    add product uploader

---
+++ db/migrate/20160811050349_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.integer :quantity
+      t.float :price
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811043751) do
+ActiveRecord::Schema.define(version: 20160811050349) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "quantity"
+    t.float    "price"
+    t.string   "image"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
