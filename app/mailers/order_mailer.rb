94b1b7518dbf24111133d4085d69d4b336df5d2d
Jone Doe <johndoe@example.com>
2016-08-12 19:43:39 +0800

    add ordermail

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,7 +1,7 @@
 class OrderMailer < ApplicationMailer
   def notify_order_placed(order)
-    @order       = order
-    @users       = order.user
+    @order      = order
+    @user       = order.user
     @product_lists = @order.product_lists
 
     mail(to: @user.email, subject: "[JDStore] 感谢您完成本次的下单，以下是您这次购买明细 #{order.token}")
