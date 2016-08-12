ed02fe02f5561ab04749a17515dacd8743c82fdb
mayalin <mayalin2012@gmail.com>
2016-08-12 14:53:14 +0800

    add some new functions

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,4 +2,11 @@ class CartsController < ApplicationController
   def checkout
     @order = Order.new
   end
+
+  def destroy
+   @cart = Cart.find(params[:id])
+
+   @cart.destroy
+   redirect_to :back
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   end
 
   resources :orders
+  resources :cart_items
 
   root 'products#index'
 end
