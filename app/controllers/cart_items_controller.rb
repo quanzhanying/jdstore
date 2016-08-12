11089e9bf2ffb12d6b40754e1af097966e6664eb
mayalin <mayalin2012@gmail.com>
2016-08-12 15:50:53 +0800

    在購物車新增數量時，不能更新超過原有庫存的數量

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,14 +2,18 @@ class CartItemsController < ApplicationController
 
   def increment
     @cart_item = current_cart.cart_items.find(params[:id])
+    if @cart_item.quantity < @cart_item.product.quantity
     @cart_item.quantity += 1
+    end
     @cart_item.save
     redirect_to :back
   end
 
-  def decrement
+def decrement
     @cart_item = current_cart.cart_items.find(params[:id])
+    if @cart_item.quantity > 0
     @cart_item.quantity -= 1
+    end
     @cart_item.save
     redirect_to :back
   end
