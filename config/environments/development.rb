72de8f863d5aa4a3ced41bc4b9e3d2ad443c5477
Jone Doe <johndoe@example.com>
2016-08-14 14:23:10 +0800

    add figaro

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,9 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :letter_opener
+
+
+  config.after_initialize do
+    Pay2go.integration_mode = :development
+  end
 end
