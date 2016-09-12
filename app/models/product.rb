65e9317abb66b54fa98e22b3a86fda5020bab41e
yanzeng li <yanzengli@gmail.com>
2016-09-12 15:15:21 +0800

    liyanzeng-modify-image-size

--- app/models/product.rb
+++ app/models/product.rb
@@ -15,4 +15,6 @@
 class Product < ApplicationRecord
   mount_uploader :image, ImageUploaderUploader
   validates :title,presence: true
+  validates :price,numericality: { greater_than: 0 }
+  validates :quantity,numericality: { greater_than: 0 }
 end
--- app/uploaders/image_uploader_uploader.rb
+++ app/uploaders/image_uploader_uploader.rb
@@ -16,10 +16,13 @@ class ImageUploaderUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+  def default_url
+    "/images/default.gif"
+  end
   process resize_to_fit: [800, 800]
 
   version :thumb do
-    process resize_to_fill: [20, 20]
+    process resize_to_fill: [60, 60]
   end
 
   version :medium do
