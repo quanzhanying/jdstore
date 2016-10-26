d1419cc994afda191c9ba493ebb6fc763c3fdf88
xbthh <hanhan_smith@hotmail.com>
2016-10-26 15:48:21 +0800

    implement is_admin

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ProductsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
-#  before_filter :require_is_admin
+  before_filter :require_is_admin
 #  before_action :require_is_hidden,only: [:show]
 #  layout "admin"
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :products
+  resources :welcome
 
   namespace :admin do
     resources :products
