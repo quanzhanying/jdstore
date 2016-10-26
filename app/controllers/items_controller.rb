5a9fdae8c6a6184caeec708453fd659b1a72b2df
yzha3917 <boston.zhang@gmail.com>
2016-10-26 14:48:20 +0800

    items CRUD index & show only

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,36 +5,9 @@ class ItemsController < ApplicationController
     @items = Items.all
   end
 
-  def new
-    @item = Item.new
-  end
-
-  def create
-    @item = Item.new(job_params)
-
-    if @item.save
-      redirect_to items_path
-    else
-      render :new
-    end
-  end
 
   def show
     @item = Item.find(params[:id])
-  end
-
-  def edit
-    @item = Item.find(params[:id])
-  end
-
-  def updated
-    @item = Item.find(params[:id])
-
-    if @item.update(job.params)
-      redirect_to jobs_path
-    else
-      render :edit
-    end
   end
 
 
