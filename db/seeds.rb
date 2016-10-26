0315290efd2fcd473ba38f49119c6b9f47b1eb61
chiyaki <66chiyaki@gmail.com>
2016-10-26 17:06:02 +0800

    implement admin/products/index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# puts "這個種子檔會自動建立一個admin帳號, 並且創建 10 個 public jobs, 以及10個hidden jobs"
-#
-# create_account = User.create([email: 'example@gmail.com', password: '12345678', password_confirmation: '12345678', is_admin: 'true'])
-# puts "Admin account created."
-#
-# create_jos = for i in 1..10 do
-#   Job.create!([title: "Job no.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false"])
-# end
-# puts "10 Public jobs created."
-#
-# create_jos = for i in 1..10 do
-#   Job.create!([title: "Job no.#{i+10}", description: "這是用種子建立的第 #{i+10} 個Hidden工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "true"])
-# end
-# puts "10 Hidden jobs created."
+puts "這個種子檔會自動建立一個admin帳號, 並且創建 10 個 on sale products, 以及10個not on sale products"
+
+create_account = User.create([email: 'example@gmail.com', password: '12345678', password_confirmation: '12345678', is_admin: 'true'])
+puts "Admin account created."
+
+create_product = for i in 1..10 do
+  Product.create!([title: "Product no.#{i}", description: "這是用種子建立的第 #{i} 個on sale product", price: rand(50..99)*100, quantity: rand(10..49)*100, onsale: "true"])
+end
+puts "10 on sale products created."
+
+create_product = for i in 1..10 do
+  Product.create!([title: "Product no.#{i+10}", description: "這是用種子建立的第 #{i+10} 個not on sale product", price: rand(50..99)*100, quantity: rand(10..49)*100, onsale: "false"])
+end
+puts "10 not on sale products created."
