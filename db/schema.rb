ef7849d1cb8d24a240e1c2d929b4b5ed384c5344
chiyaki <66chiyaki@gmail.com>
2016-10-26 14:20:13 +0800

    generate controller products & controller admin/products & model product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161026051630) do
+ActiveRecord::Schema.define(version: 20161026061415) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.decimal  "price"
+    t.integer  "quantity"
+    t.boolean  "onsale"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
