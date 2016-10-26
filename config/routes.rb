0e69e7e572ca41237469416a7110db987deeeb0e
xbthh <hanhan_smith@hotmail.com>
2016-10-26 14:19:08 +0800

    implemement step1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
 
+
   devise_for :users
-  root 'welcome#index'
+
+  resources :products
+
+  namespace :admin do
+    resources :products
+  end
+
+  root 'admin/products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
