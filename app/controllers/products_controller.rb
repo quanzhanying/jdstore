b9e63778ba7a07c29cc99b480ff205ab79fbc31e
chiyaki <66chiyaki@gmail.com>
2016-10-26 17:56:06 +0800

    implement dropdown & add a product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,44 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to admin_products_path
+  end
+
+  private
+  def job_params
+    params.require(:product).permit(:title, :description, :price, :quantity, :onsale)
+  end
+
+
 end
