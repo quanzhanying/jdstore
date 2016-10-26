9253c87d3147cc571b82fe69188421b3036f5d3c
chiyaki <66chiyaki@gmail.com>
2016-10-26 18:12:06 +0800

    implement validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :title, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :quantity, numericality: { greater_than: 0 }
 end
