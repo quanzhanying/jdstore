fbd27a0873b858279328b90f6332ede056f8ae6b
xbthh <hanhan_smith@hotmail.com>
2016-10-26 16:34:23 +0800

    implement 首页展示

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,22 @@
 class ProductsController < ApplicationController
+
+    def index
+      @products = Product.all
+    end
+
+    def show
+      @product = Product.find(params[:id])
+    end
+
+
+    def new
+      @product = Product.new
+    end
+
+    private
+
+    def product_params
+      params.require(:product).permit(:title, :description, :quantity, :price, :image)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
     resources :products
   end
 
-  root 'admin/products#index'
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
