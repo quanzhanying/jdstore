384553db3a09a23f08f4289a60e6ecac58bd83ea
Ray Gao <atlantis.ray@gmail.com>
2016-10-26 12:05:44 +0800

    STORY-1

---
+++ db/migrate/20161026031711_add_admin_column_to_user.rb
@@ -0,0 +1,5 @@
+class AddAdminColumnToUser < ActiveRecord::Migration[5.0]
+  def change
+  	add_column :users, :is_admin, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161026030415) do
+ActiveRecord::Schema.define(version: 20161026032427) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "quantity"
+    t.string   "integer"
+    t.float    "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -25,6 +35,7 @@ ActiveRecord::Schema.define(version: 20161026030415) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "is_admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
