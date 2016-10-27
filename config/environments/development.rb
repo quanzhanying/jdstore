b667b9ede364ab1bc66d86a6cc1494eae108c87c
Ray Gao <atlantis.ray@gmail.com>
2016-10-27 10:09:35 +0800

    Oct.27

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,8 +5,8 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
 
-  require 'rubygems'
-  require "mini_magick"
+  # require 'rubygems'
+  # require "mini_magick"
 
   config.cache_classes = false
 
