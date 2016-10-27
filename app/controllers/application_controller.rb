b46770278352cfa1a2c8bbd464b80ce33906d3c0
孙雁飞 <sunyanfei@sunyanfeideMacBook.local>
2016-10-27 13:34:04 +0800

    add image uploader

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def admin_required
     if !current_user.admin?
-      redirect_to root_path
+      redirect_to "/"
     end
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   validates :title,presence:true
-  # belongs_to :users
+  mount_uploader :image,ImageUploader
+
 end
