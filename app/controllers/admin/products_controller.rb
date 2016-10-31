abfb7c4bfec5ddaa68e77f67c931d3c1aa4eb5c4
Zhou ZENG <zhouzeng@ZhoudeMacBook-Air.local>
2016-10-31 11:27:29 +0800

    implement admin/product#new #create

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,21 @@
 class Admin::ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    
+    if @product.save
+      redirect_to admin_prodcuts_path
+    else
+      render :back
+    end
+
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:title, :description, :quantity, :price)
+  end
 end
