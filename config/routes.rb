7eb069863ba7f7ec66ad968646870a6db5973334
mike562661672 <mike562661672@gmail.com>
2016-10-31 12:16:46 +0800

    implement story3

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  root 'products#index'
   devise_for :users
 
   namespace :admin do
     resources :products
-
   end
   #root "admin/products#index"
-  root "welcome#index"
+  resources :products
 end
