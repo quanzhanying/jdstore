2692c662e59b33aab4659804a32f499e85446662
William <william@WilliamtekiMacBook-Pro.local>
2016-10-31 18:31:07 +0800

    cart_item_function

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
 
+  resources :carts
+
   namespace :admin do
     resources :products
   end
 
-  resources :products
-
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
 end
