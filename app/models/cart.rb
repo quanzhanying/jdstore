284c8493e0c358d002848b58b00e2b701e4c897b
qinfeng <q884822@gmail.com>
2016-10-31 17:11:34 +0800

    计算总价

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -18,4 +18,12 @@ class Cart < ApplicationRecord
     ci.save
   end
 
+  def total_price
+    sum=0
+    cart_items.each do |cart_item|
+      sum += cart_item.quantity * cart_item.product.price
+    end
+    sum
+  end
+
 end
