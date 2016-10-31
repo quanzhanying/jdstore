5bc9657749f81ecef9a20e4d6b7c11ffaf90a3a4
mike562661672 <mike562661672@gmail.com>
2016-10-31 16:05:27 +0800

    购物车明细

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   root 'products#index'
   devise_for :users
-
+  resources :carts
+  
   namespace :admin do
     resources :products
   end
   #root "admin/products#index"
   resources :products do
+
      member do
        post :add_to_cart
      end
