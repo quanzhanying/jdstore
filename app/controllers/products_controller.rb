9e75a813a6e7b294e56472d3ba6a26cfa4e38894
mapuying <mapuying@gmail.com>
2016-10-31 19:18:38 +0800

    one key to clear cart and delete product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,10 @@
 class ProductsController < ApplicationController
+  def add_to_cart
+    @product = Product.find(params[:id])
+    current_cart.add_product_to_cart(@product)
+    redirect_to :back
+  end
+
   def index
     @products = Product.all
   end
@@ -6,5 +12,5 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
-  
+
 end
