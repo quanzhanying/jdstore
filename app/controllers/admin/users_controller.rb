894a3319a1df4e6afbc6700db3df3e51ac19700d
mapuying <mapuying@gmail.com>
2016-10-31 11:12:40 +0800

    update privilecy

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -18,5 +18,4 @@ class Admin::UsersController < ApplicationController
     @users = User.all.except_user(current_user)
   end
 
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
+
   def admin?
     is_admin
   end
