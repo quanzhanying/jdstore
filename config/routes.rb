ee7c574029906ac6cab3675163daf5d8bd69a1d0
孙雁飞 <sunyanfei@sunyanfeideMacBook.local>
2016-10-31 15:06:13 +0800

    rake db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-root 'products#index'  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+root 'welcome#index'  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161026035000) do
+ActiveRecord::Schema.define(version: 20161031064635) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
