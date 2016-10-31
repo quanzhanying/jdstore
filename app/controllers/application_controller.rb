30035bf56d9d18a0d3e4151748f36db560676df0
孙雁飞 <sunyanfei@sunyanfeideMacBook.local>
2016-10-31 16:09:26 +0800

    only admin can acess backend panel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def admin_required
+    if !current_user.admin?
+      redirect_to "/"
+    end
+  end 
 end
