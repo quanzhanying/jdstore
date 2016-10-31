60000d59d055dd876e4bf66047a70ef5f494aab1
liangchaob <liangchaob@163.com>
2016-10-31 12:02:54 +0800

    change cart&cartitem modle

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,15 @@
 class Cart < ApplicationRecord
+
+  has_many :cart_items
+  has_many :products, through: :cart_items, source: :product
+
+  def add_product_to_cart(product)
+    ci = cart_items.build
+    ci.product = product
+    ci.quantity = 1
+    ci.save
+  end
+
 end
 
 # == Schema Information
@@ -9,3 +20,9 @@ end
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
+
+
+
+
+
+
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,9 @@
 class CartItem < ApplicationRecord
+
+  belongs_to :cart
+  belongs_to :product
+
+
 end
 
 # == Schema Information
