46304cf4a53c6fbff9623847c792c956611ea2d5
张硕 <zhangshuo@zhangshuodeMacBook-Pro.local>
2016-10-31 14:31:24 +0800

    noting

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,10 @@
 class CartsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def delete
+    @cart = current_cart
+    @cart.destroy
+    session[:cart_id] = nil
+    redirect_to carts_path
+  end
 end
