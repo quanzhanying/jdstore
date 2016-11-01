dab4dcbfb74a0164e01edc6931ff3b6f6d4b4955
valery0510 <valery0510@gmail.com>
2016-11-01 19:24:41 +0800

    hw git

--- app/controllers/account/orders_controller.rb
+++ app/controllers/account/orders_controller.rb
@@ -2,4 +2,8 @@ class Account::OrdersController < ApplicationController
   def index
     @orders = Order.current_order(current_user).recent
   end
+
+  def show
+    @order = Order.find(params[:id])
+  end
 end
