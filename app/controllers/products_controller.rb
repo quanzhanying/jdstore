97664dcd4d454def68ae2bc53d2f8f5e379c974b
David <dyz2102@gmail.com>
2016-11-01 15:08:07 +0800

    completed can't buy if stock is 0; can't buy more than stock quantity

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,13 +10,21 @@ class ProductsController < ApplicationController
 
 	def add_to_cart
     @product = Product.find(params[:id])
+		if @product.quantity > 0
     cart_item = current_cart.cart_items.where(product_id: @product.id).first
 			if cart_item
 				cart_item.quantity = cart_item.quantity + 1
-				cart_item.save
+				if cart_item.quantity <= @product.quantity
+					cart_item.save
+				else
+					flash[:alert] = "We're sorry but you have already selected the maximum number of items in stock."
+				end
 			else
 				current_cart.add_product_to_cart(@product)
 			end
+		else
+			flash[:alert] = "We're sorry but the selected item has been sold out."
+		end
     redirect_to :back
   end
 
