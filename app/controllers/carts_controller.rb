12a633d7209322a9a8d0a6bb5d66cb749b5ddf65
孙雁飞 <sunyanfei@sunyanfeideMacBook.local>
2016-11-01 09:21:57 +0800

    add destroy cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,47 @@
 class CartsController < ApplicationController
+  def index
+    @carts = Cart.all
+  end
+
+  def new
+    @cart = Cart.new
+  end
+
+  def create
+    @cart = Cart.new(cart_params)
+    if @cart.save
+      redirect_to carts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @cart = Cart.find(params[:id])
+  end
+  def show
+    @cart = Cart.find(params[:id])
+  end
+
+  def update
+    @cart = Cart.find(params[:id])
+    if @cart.update(cart_params)
+      redirect_to carts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @cart= current_cart 
+    @cart.destroy
+    redirect_to carts_path
+  end
+
+ private
+ def cart_params
+   params.require(:cart).permit(:created_at,:updated_at)
+ end
+
+
 end
