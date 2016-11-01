a13737bc145f549ca6e06d61d60a0f97b151c616
qinfeng <q884822@gmail.com>
2016-11-01 12:54:27 +0800

    清除购物车中的某个商品

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,18 +1,16 @@
 class CartsController < ApplicationController
     def index
       @cart=current_cart
-
       @cartitem=CartItem.where(:cart_id => @cart.id)
     end
+
     def destroy
       @cart=current_cart
       @cart.destroy
-
       @cart_item=CartItem.where(:cart_id => @cart.id)
       @cart_item.each do |d|
         d.destroy
-      end
-
+    end
       redirect_to carts_path
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :carts
-  
+  resources :cart_items
+
   resources :products do
     member do
       post :add_to_cart
