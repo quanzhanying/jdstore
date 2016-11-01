63ac0d0c2285920c82e81d21c08c08d1c232b4f8
Zhou ZENG <zhouzeng@ZhoudeMacBook-Air.local>
2016-11-01 13:57:44 +0800

    cant add too much

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -8,15 +8,27 @@ class CartItemsController < ApplicationController
 
   def add_quantity
     @cart_item = CartItem.find(params[:id])
-    @cart_item.quantity += 1
-    @cart_item.save
-    redirect_to carts_path
+    @product = @cart_item.product
+    if @cart_item.quantity < @product.quantity
+      @cart_item.quantity += 1
+      @cart_item.save
+      redirect_to carts_path
+    else
+      flash[:warning] = "已经买空了哟"
+      redirect_to carts_path
+    end
   end
 
   def minus_quantity
     @cart_item = CartItem.find(params[:id])
-    @cart_item.quantity -= 1
-    @cart_item.save
-    redirect_to carts_path
+
+    if @cart_item.quantity > 1
+      @cart_item.quantity -= 1
+      @cart_item.save
+      redirect_to carts_path
+    else
+      flash[:alert] = "大爷你至少买一个吧"
+      redirect_to carts_path
+    end
   end
 end
