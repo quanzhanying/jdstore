53063d27a872eaa23b4bed29cd2fbeff7860d591
qinfeng <q884822@gmail.com>
2016-11-02 11:58:42 +0800

    已加购物车，不会被重复加入

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,11 +1,16 @@
 class CartItemsController < ApplicationController
+  def create
+    @cart_item=CartItem.new(params[:id])
+    @cart_item.save
+
+
+
+    
+  end
 
   def destroy
-
     @cart_item=CartItem.find(params[:id])
     @cart_item.destroy
-
     redirect_to carts_path
-
   end
 end
