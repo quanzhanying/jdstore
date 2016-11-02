c1157a652ecf6e14b0304649632fe2803c1e9259
qinfeng <q884822@gmail.com>
2016-11-02 17:20:57 +0800

    购物车不能为0

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -13,14 +13,17 @@ class CartItemsController < ApplicationController
 
     @cart = current_cart
     @cart_item =@cart.cart_items.find_by(product_id: params[:id])
-    
-    if @cart_item.product.quantity >= quantity_params[:quantity].to_i
-      @cart_item.update(quantity_params)
-      flash[:notice]="ok "
-    else
-      flash[:warning]="已经超出库存数量"
-    end
+    if quantity_params[:quantity].to_i > 0
 
+     if @cart_item.product.quantity >= quantity_params[:quantity].to_i
+       @cart_item.update(quantity_params)
+      flash[:notice]="ok "
+     else
+      flash[:warning]="已经超出库存数量"
+     end
+   else
+     flash[:warning]="不能为0"
+   end
     redirect_to carts_path
 
   end
