c4985070e1ab19248d76b6367c1a04ac09f0a55e
mapuying <mapuying@gmail.com>
2016-11-02 23:06:24 +0800

    add cancel order function

--- app/controllers/account/orders_controller.rb
+++ app/controllers/account/orders_controller.rb
@@ -4,4 +4,11 @@ class Account::OrdersController < ApplicationController
     @orders = current_user.orders
   end
 
+  def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+    OrderMailer.notify_order_placed(Order.last).deliver!
+    redirect_to :back
+  end
+
 end
