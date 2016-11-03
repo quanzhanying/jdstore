46021388203568ba1e8e088517deb1fdf951c651
xbthh <hanhan_smith@hotmail.com>
2016-11-03 15:17:24 +0800

    安装figaro2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,10 +3,10 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV["S3_key_id"],      # 你的 key
+      aws_access_key_id:  "S3_key_id",      # 你的 key
 
 
-      aws_secret_access_key: ENV["S3_key_secret"],      # 你的 secret key
+      aws_secret_access_key: "S3_key_secret",      # 你的 secret key
 
 
       region:                'ap-northeast-1' # 你的 S3 bucket 的 Region 位置
