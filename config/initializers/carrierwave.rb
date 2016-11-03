5a82ddb68bef6b335fb202e27e1c7b7fa765eb3d
xbthh <hanhan_smith@hotmail.com>
2016-11-03 14:42:54 +0800

    carrierwave production implement

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,23 @@
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV["AKIAIOEH6JYFUHV4Y2ZA"],      # 你的 key
+
+
+      aws_secret_access_key: ENV["natquebua8MJsLqqrRB35KFghvqh50VrW"],      # 你的 secret key
+
+
+      region:                'ap-northeast-1' # 你的 S3 bucket 的 Region 位置
+
+
+    }
+    config.fog_directory  = ENV["hanhan_test"] # 你设定的 bucket name
+
+
+
+  else
+    config.storage :file
+  end
+end
