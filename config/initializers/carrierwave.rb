e408d5b7aaa6afb7d3c5e2a7774aa0c8174a830c
yzha3917 <boston.zhang@gmail.com>
2016-11-03 17:15:00 +0800

    Cred

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,17 +4,17 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     "S3_key_id",      # 你的 key
+      aws_access_key_id:     ENV["S3_key_id"],      # 你的 key
 
 
-      aws_secret_access_key: "S3_key_secret",      # 你的 secret key
+      aws_secret_access_key: ENV["S3_key_secret"],      # 你的 secret key
 
 
       region:                'ap-northeast-2' # 你的 S3 bucket 的 Region 位置
 
 
     }
-    config.fog_directory  = ENV["jdonline-bucket"] # 你设定的 bucket name
+    config.fog_directory  = "jdonline-bucket" # 你设定的 bucket name
 
 
 
