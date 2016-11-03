87892599203c773ffc9086fb551ccddd9158e6c6
Ray Gao <atlantis.ray@gmail.com>
2016-11-03 11:30:38 +0800

    day 4

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -24,4 +24,14 @@ class OrderMailer < ApplicationMailer
     	mail(to: @user.email, subject: "Order #{@order.token}已取消")
   	end
 
+    def mailgun_email(user)
+       @user = user
+       mg_client = Mailgun::Client.new ENV['api_key']
+       message_params = {:from    => ENV['gmail_username'],
+                         :to      => @user.email,
+                         :subject => 'Sample Mail using Mailgun API',
+                         :text    => 'This mail is sent using Mailgun API via mailgun-ruby'}
+       mg_client.send_message ENV['domain'], message_params
+  end
+
 end
