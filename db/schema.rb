5c5cf7b13541ceeddc61f480387e488a4eaad59a
qinfeng <q884822@gmail.com>
2016-11-03 15:04:08 +0800

    为网址加一些随机字符

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102114914) do
+ActiveRecord::Schema.define(version: 20161103064949) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20161102114914) do
     t.string   "shipping_address"
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
+    t.string   "token"
+    t.index ["token"], name: "index_orders_on_token"
   end
 
   create_table "product_lists", force: :cascade do |t|
