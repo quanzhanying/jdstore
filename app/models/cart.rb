f949dff97306ef715c8e7122e240cc22e3474c35
孙雁飞 <sunyanfei@sunyanfeideMacBook.local>
2016-11-04 20:18:34 +0800

    all the store

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -15,8 +15,7 @@ class Cart < ApplicationRecord
 
     # current_item = @items.find {|item| item.product == product}
     #判断是否存在，存在当前的item，则购物车数量增加1，否则把CartItem放进items里头
-      cart_item = self.cart_items.find_by_product_id(product.id)
-      if 
+    if cart_item = self.cart_items.find_by_product_id(product.id)
        cart_item.quantity = cart_item.quantity+1
        cart_item.save
 
