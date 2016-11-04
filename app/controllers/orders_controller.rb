71c70327458d946c50868c9954a68742c5f3f07e
William <william@WilliamtekiMacBook-Pro.local>
2016-11-04 12:46:42 +0800

    change aasm status

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,10 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:create]
 
+  def index
+    @orders = Order.all
+  end
+
   def create
     @order = Order.new(order_params)
     @order.user = current_user
@@ -31,22 +35,47 @@ class OrdersController < ApplicationController
   def notify_order_cancel
     @order = Order.find_by_token(params[:id])
     @product_lists = @order.product_lists
-    @order.cancell_order
-    OrderMailer.notify_order_cancel(@order).deliver
+    if @order.aasm_state == "order_cancelled"
+      flash[:warning] = "订单之前已经被取消"
+      redirect_to account_orders_path
+    else
+      @order.aasm_state == "paid"
+      @order.cancell_order!
+      flash[:warning] = "订单取消中"
+      OrderMailer.notify_order_cancel(@order).deliver
+      redirect_to account_orders_path
+    end
+
   end
 
   def notify_order_cancel_by_admin
     @order = Order.find_by_token(params[:id])
     @product_lists = @order.product_lists
-    @order.cancell_order
-    OrderMailer.notify_order_cancel_by_admin(@order).deliver
+    if @order.aasm_state == "order_cancelled"
+      flash[:warning] = "订单之前已经被取消"
+      redirect_to admin_orders_path
+    else
+      @order.cancell_order!
+      flash[:warning] = "取消订单"
+      OrderMailer.notify_order_cancel_by_admin(@order).deliver
+      redirect_to admin_orders_path
+    end
+
   end
 
   def ship
     @order = Order.find_by_token(params[:id])
     @product_lists = @order.product_lists
+    if @order.aasm_state == "paid"
+       flash[:warning] = "打包中"
+       @order.ship!
+       OrderMailer.notify_order_ship(@order).deliver
+       redirect_to admin_orders_path
+    else
+      redirect_to admin_orders_path
+      flash[:warning] = "请联系客服"
+    end
 
-    OrderMailer.notify_order_ship(@order).deliver
   end
 
   def pay_with_alipay
