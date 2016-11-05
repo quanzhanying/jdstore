302858182763ef464283b2d2bc3c1d9053fc6805
孙雁飞 <sunyanfei@sunyanfeideMacBook.local>
2016-11-05 11:49:46 +0800

    add destroy product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,8 +14,8 @@ class ProductsController < ApplicationController
       # if current_cart.cart_items >= @product.quantity
       #   flash[:alert] = '超出库存'
       # else
-        current_cart.add_product_to_cart(@product)
-        flash[:alert] = '已加入购物车！'
+      current_cart.add_product_to_cart(@product)
+      flash[:alert] = '已加入购物车！'
       # end
     else
       flash[:alert] = '库存为零'
@@ -23,9 +23,21 @@ class ProductsController < ApplicationController
     redirect_to :back
   end
 
+  def destroy
+    @product = Product.find_by(params[:id])
+    @product.destroy
+    redirect_to :back
+  end
+
   def destroy_to_cart
     @product = Product.find(params[:id])
     current_cart.destroy_product_to_cart(@product)
     redirect_to :back
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :quantity, :price, :image)
   end
 end
