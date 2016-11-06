215b9c8fcfcb5c9b308e8c6e7fbeedc4d917c842
张硕 <zhangshuo@zhangshuodeMacBook-Pro.local>
2016-11-06 18:18:40 +0800

    debug

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,11 +12,11 @@ class ProductsController < ApplicationController
 
   def add_to_cart
     @product = Product.find(params[:id])
-    @cart_item = CartItem.find_by(params[:id])
-    # if current_cart.products.include?(@product)
-      @cart_item.quantity += 1
-  #   flash[:notice] = "你已经加过了"
-  # else
+  #   @cart_item = CartItem.find_by(params[:id])
+  #   # if current_cart.products.include?(@product)
+  #     @cart_item.quantity += 1
+  # #   flash[:notice] = "你已经加过了"
+  # # else
     current_cart.add_product_to_cart(@product)
     flash[:notice] = "成功加入购物车！"
   # end
