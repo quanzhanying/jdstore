79c6bf83dc2c8b4cb625c434b0d3c33e49fdb68b
megy <megy@megys-MacBook-Pro.local>
2017-01-18 14:52:32 +0800

    implement admin/products CRUD

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,47 @@
+class Admin::ProductsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_products_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = "Update Success"
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:alert] = "Product deleted"
+    redirect_to admin_products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :quantity)
+  end
+end
