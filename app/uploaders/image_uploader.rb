a8ca14141294d0905f4dc429358a903b894d81f8
megy <megy@megys-MacBook-Pro.local>
2017-01-18 17:34:15 +0800

    implement products#index & show

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,8 +1,8 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+
+    include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -25,15 +25,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
-   process resize_to_fit: [800, 800]
+  process :resize_to_fit => [800, 800]
 
   # Create different versions of your uploaded files:
   version :thumb do
-     process resize_to_fit: [200, 200]
+     process :resize_to_fit => [200, 200]
   end
 
   version :medium do
-     process resize_to_fit: [400, 400]
+     process :resize_to_fit => [400, 400]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
