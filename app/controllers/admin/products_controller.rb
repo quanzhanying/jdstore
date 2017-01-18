1bb7fa1d7cd2288b909e9dbe6b95877e726d167c
milesmao233 <milesmao233@gmail.com>
2017-01-18 15:49:05 +0800

    only admin can acess backend panel

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,47 +1,51 @@
 class Admin::ProductsController < ApplicationController
-  def index
-    @products = Product.all
-  end
+    before_action :authenticate_user!
+    before_action :admin_required
+    layout "admin"
 
-  def show
-    @product = Product.find(params[:id])
-  end
-
-  def new
-    @product = Product.new
-  end
-
-  def create
-    @product = Product.new(product_params)
-    if @product.save
-      redirect_to admin_products_path
-    else
-      render :new
+    def index
+        @products = Product.all
     end
-  end
 
-  def edit
-    @product = Product.find(params[:id])
-  end
-
-  def update
-    @product = Product.find(params[:id])
-    if @product.update(product_params)
-      redirect_to admin_products_path, notice: "update success"
-    else
-      render :edit
+    def show
+        @product = Product.find(params[:id])
     end
-  end
 
+    def new
+        @product = Product.new
+    end
 
-  def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
-    redirect_to admin_products_path, alert: "product delete"
-  end
+    def create
+        @product = Product.new(product_params)
+        if @product.save
+            redirect_to admin_products_path
+        else
+            render :new
+        end
+    end
 
-  private
-  def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price)
-  end
+    def edit
+        @product = Product.find(params[:id])
+    end
+
+    def update
+        @product = Product.find(params[:id])
+        if @product.update(product_params)
+            redirect_to admin_products_path, notice: 'update success'
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @product = Product.find(params[:id])
+        @product.destroy
+        redirect_to admin_products_path, alert: 'product delete'
+    end
+
+    private
+
+    def product_params
+        params.require(:product).permit(:title, :description, :quantity, :price)
+    end
 end
