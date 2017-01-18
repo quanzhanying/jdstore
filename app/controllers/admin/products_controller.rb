f91f732cbbc8ed7207f9b86a73306a6597817fe0
fengchi <c2h20406@gmail.com>
2017-01-18 20:29:29 +0800

    add something

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -36,6 +36,12 @@ class Admin::ProductsController < ApplicationController
 		end
 	end
 
+	def destroy
+		@product = Product.find(params[:id])
+		@product.destroy
+		redirect_to admin_products_path	
+	end
+
 	def admin_required
 		if !current_user.admin?
 			redirect_to "/"
