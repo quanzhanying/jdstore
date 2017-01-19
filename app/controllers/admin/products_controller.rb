c18a55ee0031476d3d0fe51d5e33d325eda5bab3
raimonfuns <raimonfuns@163.com>
2017-01-19 13:10:43 +0800

    implement product#create

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -6,4 +6,19 @@ class Admin::ProductsController < ApplicationController
 	def new
 		@product = Product.new
 	end
+
+	def create
+		@product = Product.create(product_params)
+		if @product.save
+			redirect_to admin_products_path
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def product_params
+		params.require(:product).permit(:title, :description, :quantity, :price)
+	end
 end
