6c80795141e16ea2c02cfd772ae9471dc84950cd
Yammy <yammyxing@foxmail.com>
2017-01-19 23:04:29 +0800

    fix code spell mistake

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -21,9 +21,9 @@ layout 'admin'
     end
   end
 
-  def show
-    @product = Product.find(params[:id])
-  end
+  # def show
+  #   @product = Product.find(params[:id])
+  # end
 
   def edit
     @product = Product.find(params[:id])
