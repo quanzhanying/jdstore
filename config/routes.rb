c14289ae460c2697f32d4fd24b6ddeb53c7a462d
bruceqiao <bruceq619@gmail.com>
2017-01-20 20:12:35 +0800

    add product model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :users
+
+    resources :products
+    root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+
 end
