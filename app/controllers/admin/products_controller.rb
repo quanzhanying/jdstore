449819788c34aae7ba5c015a7dd9bf34301059c1
runpee <992142460@qq.com>
2017-01-20 10:01:44 +0800

    implement backend CRUD

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,9 @@
 class Admin::ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
   def new
     @product = Product.new
   end
@@ -12,6 +17,19 @@ class Admin::ProductsController < ApplicationController
     end
   end
 
+  def edit
+     @product = Product.find(params[:id])
+  end
+
+  def update
+   @product = Product.find(params[:id])
+
+   if @product.update(product_params)
+     redirect_to admin_products_path
+   else
+     render :edit
+   end
+  end
 
   private
 
