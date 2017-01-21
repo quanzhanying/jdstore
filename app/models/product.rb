0a6e2dcbb6c6230ba6560ff7271162226b462a24
Cheng Ge <chengge@MBP.local>
2017-01-20 20:31:15 -0500

    implemented limitation to product title, quantity, price when creating

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+    validates :title, presence: true
+    validates :quantity, numericality: { greater_than_or_equal_to: 0 }
+    validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
