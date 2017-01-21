228ab16f9a8e30cdc1c893ac3e891f03dab4f4bb
xieyanlin <xieyanlin820@gmail.com>
2017-01-21 14:50:47 +0800

    product show

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
    resources :products
  end
 
-  root 'welcome#index'
+  root 'products#index'
   devise_for :users
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
