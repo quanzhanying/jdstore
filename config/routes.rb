85f8c1596b4b14c20f1f6592e339636baecc1958
suilulu <m13869841793@gmail.com>
2017-01-21 18:00:19 +0800

    implement product image

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  
+  root 'products#index'
+  devise_for :users
+
   namespace :admin do
     resources :products
   end
 
-  devise_for :users
-  root 'welcome#index'
+resources :products
+
+end
 
 
 end
