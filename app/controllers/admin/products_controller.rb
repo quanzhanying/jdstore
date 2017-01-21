45a1493a38590b5dce6e9996090917de6eeef9af
cuishirui <344494024@qq.com>
2017-01-21 09:26:30 +0800

    让顾客看到主页的商品

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,7 @@
 class Admin::ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   before_action :admin_required
+  layout "admin"
 
   def index
       @products = Product.all
@@ -44,7 +45,7 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price)
+    params.require(:product).permit(:title, :description, :quantity, :price, :image)
 
   end
 end
