60cdd40ad50f0b5f8d00872eb13ad0ec20973c42
linfgit <linfgg1@gmail.com>
2017-01-21 16:30:34 +0800

    implement backend crud

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -2,6 +2,10 @@ class Admin::ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
   def new
     @product = Product.new
   end
