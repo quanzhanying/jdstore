b20ff841e4a556e26974cfa3222493c81c37b093
你的名字 <ksalsqzhbl@aliyun.com>
2017-01-21 18:07:05 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'products#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
@@ -6,4 +7,6 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
  end
+
+  resources :products
 end
