24f07befec09ea855dc76f8458d74689e45bce18
chentianwei <ctw.3@163.com>
2017-01-22 20:27:21 +0800

    step1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def admin_required
+    if !current_user.admin?
+      flash[:alert] = "你不是管理员"
+      redirect_to products_path
+    end
+  end
 end
