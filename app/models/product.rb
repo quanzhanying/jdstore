2c2d9adae6bfb2dcac19c66217de2639f24f2767
leichaofan <leichaofan2024@gmail.com>
2017-01-23 00:09:09 +0800

    implement image upload

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,5 @@ class Product < ApplicationRecord
   validates :description, presence: true
   validates :quantity, presence: true
   validates :price, presence: true
+  mount_uploader :image, ImageUploader
 end
