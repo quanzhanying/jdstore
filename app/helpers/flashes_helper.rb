33736ec77451410b66e8b39b46ac69361f30cfe3
陈扬帆 <cyf3211@qq.com>
2017-01-22 10:41:01 +0800

    user can login/logout/signup

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -2,10 +2,10 @@ module FlashesHelper
   FLASH_CLASSES = {alert:"danger",notice:"success",warning:"warning"}.freeze
 
   def flash_class(key)
-    FLSAH_CLASSES.fetch key to_sym, key
+    FLASH_CLASSES.fetch key. to_sym, key
   end
 
   def user_facing_flashes
-    flash.to_hash,slice "alert","notice","warning"
+    flash.to_hash.slice "alert","notice","warning"
   end
 end
