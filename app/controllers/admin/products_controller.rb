1b46cc6ce261b0fc7adec1eb67f8a1153b5e2420
raimonfuns <raimonfuns@163.com>
2017-01-22 11:39:16 +0800

    implement product#destroy

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -35,6 +35,12 @@ class Admin::ProductsController < ApplicationController
 		end
 	end
 
+	def destroy
+		@product = Product.find(params[:id])
+		@product.destroy
+		redirect_to admin_products_path, alert: "Product deleted"
+	end
+
 	private
 
 	def product_params
