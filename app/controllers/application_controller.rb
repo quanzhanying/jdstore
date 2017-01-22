5a1670fdb291ccf46b13083baf381568c8a32414
Johnson Liang <johnson.liangxinzhong@gmail.com>
2017-01-22 21:30:00 +0800

    only admin can access backend panel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def admin_required
-    redirect_to '/' unless current.user.admin?
+    if !current_user.admin?
+    redirect_to '/'
+  end
   end
 end
