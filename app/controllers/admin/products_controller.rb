c71d9a05f088b41f60410e29fbfd9086ec30917a
Kevin161111 <hwha2008@hotmail.com>
2017-01-23 11:04:34 +0800

    homework for Jdstore 1st week

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -40,6 +40,11 @@ class Admin::ProductsController < ApplicationController
     end
   end
 
+   def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to admin_products_path
+  end
 
   private
 
