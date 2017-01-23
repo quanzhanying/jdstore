0585e86696353d9fbd13103426d4f1921dc17b29
charvine <crossovermeister@gmail.com>
2017-01-23 22:34:24 +0800

    signed out error

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,22 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 u = User.new
-u.email = "admin@test.com"           # 可以改成自己的 email
+u.email = "admin@gmail.com"           # 可以改成自己的 email
 
 u.password = "111111"                # 最少要六码
 
 u.password_confirmation = "111111"   # 最少要六码
 
 u.is_admin = true
+u.save
+
+
+u = User.new
+u.email = "test@gmail.com"           # 可以改成自己的 email
+
+u.password = "111111"                # 最少要六码
+
+u.password_confirmation = "111111"   # 最少要六码
+
+u.is_admin = false 
 u.save
