7e387907ff51fbbb6549f5638fff52021caede19
megy <megy@megys-MacBook-Pro.local>
2017-01-23 12:46:51 +0800

    implement backend admin permission

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def admin_required
+    unless current_user.admin?
+      flash[:warning] = "You have no permission"
+      redirect_to root_path
+    end
+  end
 end
