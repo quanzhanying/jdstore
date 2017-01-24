6680fef5864bbb1b0ea35711051745ef2fd60a94
yifipro <yifiplus@139.com>
2017-01-24 21:27:09 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+    root 'product#index'
     devise_for :users
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     namespace :admin do
         resources :products
     end
 
-    root 'welcome#index'
-end
+    resources :products
+
+    end
