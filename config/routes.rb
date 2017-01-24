e6a8c13b4821bfe723bc08279a291ba969e96641
Caven <xll290016537@gmail.com>
2017-01-24 22:25:01 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root 'products#index'
   namespace :admin do
     resources :products
   end
-
+  resources :products
   devise_for :users
-root 'welcome#index'
+
 end
