cb38cc894b042705f582f5776c84735729f75750
candy <candycan@163.com>
2017-01-25 22:36:50 +0800

    change admin/product backend panel

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -14,10 +14,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_fit: [800, 800]
+  process resize_to_fit: [800,800]
 
   version :thumb do
-    process resize_to_fill: [200, 200]
+    process resize_to_fill: [200,200]
   end
 
 version :medium do
