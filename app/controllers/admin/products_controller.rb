ff72a64bd1b50df9d34c953b88a46a8f1d9b6bb7
candy <candycan@163.com>
2017-01-25 20:33:27 +0800

    implement product show

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -7,10 +7,6 @@ class Admin::ProductsController < ApplicationController
     @products = Product.all
   end
 
-  def show
-    @product = Product.find(params[:id])
-  end
-
   def new
     @product = Product.new
   end
