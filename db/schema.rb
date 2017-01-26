1e1633e303b7148c343a697eff226967c29d2d2c
escray <zescray@gmail.com>
2017-01-26 08:18:52 +0800

    fixed rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125235506) do
+ActiveRecord::Schema.define(version: 20170126000731) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.text     "descritption"
-    t.integer  "quanlity"
+    t.integer  "quantity"
     t.integer  "price"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
