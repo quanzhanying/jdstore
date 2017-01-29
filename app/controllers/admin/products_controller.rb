749481b5281e93863579996fba624d0ad72a9424
Learnor <learnor@gmail.com>
2017-01-29 12:06:11 -0600

    add admin require to backend panel

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :admin_required
   before_action :set_product, only: [:show, :edit, :update, :destroy]
   def index
     @products = Product.all
@@ -42,5 +44,9 @@ class Admin::ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:title, :description, :quantity, :price)
+  end
+
+  def admin_required
+    redirect_to root_path, notice: 'You are not admin!' unless current_user.admin?
   end
 end
