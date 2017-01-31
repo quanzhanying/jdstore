9de633a800be459554d49b3eb92945fe2336420e
shaheming <hemingsha@gmail.com>
2017-01-31 08:49:02 +0800

    add is_admin to ouser

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+create_account = User.create([email: "a@a.com", password:"123456",password_confirmation: "123456",is_admin:"true"])
+puts "Admin account created."
+create_account_user = User.create([email: "b@b.com", password:"123456",password_confirmation: "123456",is_admin:"false"])
+puts "User account created."
+
+# create_products = for i in 1..15  do
+# 	product_title
+
+# end
