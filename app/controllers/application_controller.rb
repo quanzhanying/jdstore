d4fd28ac77ebc313cee85b256bd2c689bf8b8080
kuangbenzhuzhu <kuangbenzhuzhu@gmail.com>
2017-01-31 17:02:28 +0800

    only admin can access backend panel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   def index
     flash[:notice] = "Hello~"
   end
+
+  def admin_required
+    if !current_user.admin?
+      redirect_to "/", alert: "You are not admin."
+    end
+  end
 end
