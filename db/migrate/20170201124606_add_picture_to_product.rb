4b803e1d866dae42c693cccf29a627f1910f942d
janice <janicejiang.d@gmail.com>
2017-02-01 21:33:00 +0800

    implement picture upload

---
+++ db/migrate/20170201124606_add_picture_to_product.rb
@@ -0,0 +1,5 @@
+class AddPictureToProduct < ActiveRecord::Migration[5.0]
+  def change
+    add_column :products, :picture, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170120084331) do
+ActiveRecord::Schema.define(version: 20170201124606) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20170120084331) do
     t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
+    t.string   "picture"
   end
 
   create_table "users", force: :cascade do |t|
