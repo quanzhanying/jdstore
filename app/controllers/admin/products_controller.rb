f7636f6914990c6c5df5532ecb96c00ad35656d6
zhoujin <jin100chow@hotmail.com>
2017-02-01 19:08:00 +0800

    add is_admin? to user

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,15 +1,16 @@
 class Admin::ProductsController < ApplicationController
-  def index
-  @products = Product.all
-  end
+before_action :authenticate_user!
+before_action :admin_required
 
-  def show
-    @product = Product.find(params[:id])
-  end
+  def index
+  @products = Prouduct.all
+end
 
   def new
-  @product = Product.new
+  @products = Product.new
   end
+
+
 
   def create
   @product = Product.new(product_params)
@@ -21,28 +22,9 @@ class Admin::ProductsController < ApplicationController
   end
 end
 
-  def edit
-    @product = Product.find(params[:id])
-  end
-
-  def update
-    @product = Product.find(params[:id])
-    if @product.update(product_params)
-      redirect_to admin_products_path
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
-    redirect_to product_params
-  end
+  private
 
   def product_params
-    params.require(:product).permit(:title,:description,:quantity,:price)
+    params.require(:product).permit(:title, :description, :quantity, :price)
   end
-
-
 end
