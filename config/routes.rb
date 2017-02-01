3ca269f5dcddf844ef6fa8456a07a39ba5c3b056
sharkofecuis <sharkofecuis@gmail.com>
2017-02-02 00:00:53 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  root 'products#index'
+  devise_for :users
+  namespace :admin do
+    resources :products
+  end
+
+ resources :products
+
   devise_for :users
 
   namespace :admin do
