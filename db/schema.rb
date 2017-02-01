cb4788ea8ee5837a60aea9f43ce2c033c506e616
RsMacAir <51xiuxian@sina.com>
2017-02-01 12:00:04 +0800

    implement products#index/show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170131131538) do
+ActiveRecord::Schema.define(version: 20170201032205) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "price"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
