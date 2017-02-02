0d22fe87c8830214247c1d3b8ec7a8393c2f9b14
youying0356 <youying0356@qq.com>
2017-02-02 18:12:29 +0800

    amd products/index.html.erb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @product = Product.all
+    @products = Product.all
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'products#index'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    namespace :admin do
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
 
     resources :products
 
+root 'products#index'
 end
