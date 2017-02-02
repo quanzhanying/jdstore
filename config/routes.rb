7a71833dbb7282757c4141975c2e4e348943ca45
littlefat <littlefat.liujingli@gmail.com>
2017-02-02 09:31:47 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-    namespace :admin do
-        resources :products
-    end
+  root 'products#index'
   devise_for :users
+  namespace :admin do
+    resources :products
+  end
+
+  resources :products
   root 'welcome#index'
 end
