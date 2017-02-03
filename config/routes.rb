8630baadad5201df7ccf85bfa72c7d439dd962eb
hukui <tckuikui1234>
2017-02-03 15:11:24 +0800

    implement product#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "welcome#index"
+
+  root "products#index"
+
+  resources :products
+
   namespace :admin do
     resources :products
   end
+
 end
