b714dd4564acfba5cf65d4504ea1de87f7a9aa73
hukui <tckuikui1234>
2017-02-03 13:30:56 +0800

    implement admin/products new

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,20 @@
 class Admin::ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_products_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :quantity, :price)
+  end
 end
