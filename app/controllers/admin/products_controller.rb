1206d7bd9a21150066c8823ae6fda681bd5ba53c
RsMacAir <51xiuxian@sina.com>
2017-02-03 12:05:41 +0800

    can not admin#action unless login

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  before_action :authenticate_user!, only:[:new, :create, :edit, :update, :destroy]
+  
   def index
     @products = Product.all
   end
