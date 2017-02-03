9349ddee0f2935a4ab1320a34c28a924f2e82880
hukui <tckuikui1234>
2017-02-03 13:38:11 +0800

    implement admin/products show

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -2,10 +2,15 @@ class Admin::ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
   def new
     @product = Product.new
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+  
   def create
     @product = Product.new(product_params)
     if @product.save
