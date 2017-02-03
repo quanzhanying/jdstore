b91cfb248b5c61fa2f96e817ad9fedb47bbaa6e0
sarah <smile-wu@hotmail.com>
2017-02-03 17:47:14 +0800

    change addmin index and show

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -31,11 +31,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-     process resize_to_fit: [200, 200]
+     process resize_to_fill: [200, 200]
    end
 
    version :medium do
-     process resize_to_fit: [400, 400]
+     process resize_to_fill: [400, 400]
    end
 
   # Add a white list of extensions which are allowed to be uploaded.
