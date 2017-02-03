0fec5f3146f48a5e921856ced1b56f0cfef9879f
RsMacAir <51xiuxian@sina.com>
2017-02-03 11:01:12 +0800

    implement admin#product actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170201032205) do
+ActiveRecord::Schema.define(version: 20170201054208) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170201032205) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "price"
+    t.integer  "quantity"
   end
 
   create_table "users", force: :cascade do |t|
