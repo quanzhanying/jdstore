905feaf21f5e98c2322945fa963c90f972583b31
RsMacAir <51xiuxian@sina.com>
2017-02-04 10:05:37 +0800

    route products#index

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
-  
+
   resources :products
 
-  root 'welcome#index'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'products#index'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
