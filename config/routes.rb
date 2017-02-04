3af258ca07c5cb09f3c72ca001f68591915ffa53
Yammy <yammyxing@foxmail.com>
2017-02-04 12:00:22 +0800

    implement backend CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
+
+  namespace :admin do
+    resources :products
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
