6d397a4ca18c4dcce6b874fd62b9a83da21d553f
yuyang <18516569687@163.com>
2017-02-04 15:40:18 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  root 'product#index'
   devise_for :users
   root 'welcome#index'
 
   namespace :admin do
     resources :products
   end
+
+  resources :products
 end
