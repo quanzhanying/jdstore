f1640b452a3ce167610dfeeb121731c5a4d2db22
xulingxian <xulingxian001@gmail.com>
2017-02-04 22:15:59 +0800

    only admin can acess backend panel

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
         resources :products
     end
     devise_for :users
-    root 'welcome#index'
+    root 'product#index'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
