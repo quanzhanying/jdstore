a6b2a9de5fe58d97f1b8e5fb3088f79be426842f
hyacinth0906 <doris_0906@126.com>
2017-02-04 23:23:43 +0800

    change admin/product backed panel

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -21,9 +21,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :medium do
-    process resize_to_fill:[400,400]
+    process resize_to_fill: [400,400]
   end
-  
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
