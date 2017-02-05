0677ed25285fb22cc8bb6979f20b31bd80ff49c9
shaheming <hemingsha@gmail.com>
2017-02-05 17:04:57 +0800

    implement backend crud and limit the input

---
+++ app/models/product.rb
@@ -0,0 +1,5 @@
+class Product < ApplicationRecord
+	validates :title, presence:true
+	validates :price, numericality:{:greater_than_or_equal_to => 0}
+	validates :quantity, numericality:{:greater_than_or_equal_to => 0}
+end
