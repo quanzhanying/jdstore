7f8bfb7e786a25182af7c5c4aba10649bdfb3f91
jay.g <jkland927@hotmail.com>
2017-02-05 23:27:20 +0800

    debug in admin/new.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,4 @@ Rails.application.routes.draw do
   resources :products
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
 end
