1742d13696393f189281f857ace6fcd7377221ea
zhanglin <a18646564596@163.com>
2017-02-05 22:58:26 +0800

    carrierwave mini_magick admin::product new edit index、product index show

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :products
   namespace :admin do
     resources :products
   end
-  root 'welcome#index'
+  root 'products#index'
 end
