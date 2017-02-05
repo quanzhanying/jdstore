6052f95f1c0683477ca66044e7a6f40576b703f9
shaheming <hemingsha@gmail.com>
2017-02-05 19:45:01 +0800

    implement iamge upload

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,7 +3,7 @@ class Admin::ProductsController < ApplicationController
 	before_action :admin_required
 
 	layout "admin"
-	
+
 	def index
 		@products = Product.all
 	end
@@ -44,6 +44,6 @@ class Admin::ProductsController < ApplicationController
 
 private
 	def product_params
-		params.require(:product).permit(:title,:description,:quantity,:price)
+		params.require(:product).permit(:title,:description,:quantity,:price,:image)
 	end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
 	validates :title, presence:true
 	validates :price, numericality:{:greater_than_or_equal_to => 0}
 	validates :quantity, numericality:{:greater_than_or_equal_to => 0}
+	mount_uploader :image, ImageUploader
 end
