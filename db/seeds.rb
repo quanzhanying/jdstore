2f04cc2d4c78e00c857f1efb3b55c42a6a25670d
shaheming <hemingsha@gmail.com>
2017-02-05 19:28:53 +0800

    only admin can acess backend panel

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+u = User.create([ email: 'a@a.com' ,  password: '123456' ,password_confirmation:'123456',is_admin:true])
+puts "create a adminer"
+
+p1 = Product.create([title:"phone",description:"this is a phone",quantity:20,price:30])
+p2 = Product.create([title:"watch",description:"this is a watch",quantity:20,price:10])
+puts "create products"
