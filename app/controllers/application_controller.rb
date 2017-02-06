8dd398d2fa069936f6bc20b89e27de64caa1aef7
zhangyan <tongkusat123@gmail.com>
2017-02-06 20:38:21 +0800

    implement current_cart function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,27 @@ if !current_user.is_admin?
 end
 end
 
+
+def current_cart
+@current_cart ||= find_cart
+end
+
+
+private
+
+def find_cart
+cart = Cart.find_by(id: session[:cart_id])
+if cart.blank?
+  cart = Cart.create_table
+end
+session[:cart_id]=cart.id
+return cart
+end 
+
+end
+
+
+
+
+
 end
