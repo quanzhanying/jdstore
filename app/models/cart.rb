b52efcc5bbee94ed4923715a42a3055c01aa4c7c
Yammy <yammyxing@foxmail.com>
2017-02-06 23:28:45 +0800

    delete an item from the cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -8,4 +8,16 @@ class Cart < ApplicationRecord
     ci.quantity = 1
     ci.save
   end
+
+  def total_price
+    sum = 0
+      cart_items.each do |cart_item|
+        sum += cart_item.quantity * cart_item.product.price
+      end
+    sum
+  end
+
+  def clean!
+    cart_items.destroy_all
+  end
 end
