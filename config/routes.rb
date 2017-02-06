b1c4374f7fdd67980a19cd822367f2332905d3c2
kai <328458211@qq.com>
2017-02-06 19:06:52 +0800

    implement user product show

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root 'welcome#index'
+  resources :products
+  root 'products#index'
 #root 'admin/products#index'
 end
