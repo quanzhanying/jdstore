2a3eed80e2efe0d9d9de83cee4847a672a0665ac
wuyinghe <wuyinghe1925@gmail.com>
2017-02-06 23:40:24 +0800

    empty cart

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,9 @@ root 'products#index'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-  resources :carts
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end
 end
