68214a2ebc443bcaab166bf99b1ab11bbe177338
nirvana <wiltonwung@gmail.com>
2017-02-06 21:02:29 +0800

    add add_to_cart button

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
     resources :products
   end
 
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
+
   resources :products
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
