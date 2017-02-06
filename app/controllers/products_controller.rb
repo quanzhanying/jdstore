6890f9bde0cf817d8a7ec59eedb29239e7c8e8c5
freedomsky <hanwei_1989@126.com>
2017-02-06 23:17:04 +0800

    finish part3

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,11 @@
 class ProductsController < ApplicationController
 
   def index
-    @products =Product.all
+    @products = Product.all
   end
-  
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
 end
