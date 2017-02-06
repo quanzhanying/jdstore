dc2e3f497e96b04f9f7d5ac380ef31d6cd896d5c
bob <bao309@163.com>
2017-02-06 21:19:45 +0800

    implement add_product_to_cart function

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ root 'products#index'
 namespace :admin do
   resources :products
 end
+resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
 resources :products
 root 'welcome#index'
 end
