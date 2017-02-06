20aada61dd662ff65d151d25967b5aee3bef0f46
freedomsky <hanwei_1989@126.com>
2017-02-06 10:38:20 +0800

    Finish Part1

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
-    resource :products
+    resources :products
   end
   root 'welcome#index'
 end
