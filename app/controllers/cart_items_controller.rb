01260c2ad20c6e3ef7787175068744142027ff50
Yammy <yammyxing@foxmail.com>
2017-02-07 00:09:50 +0800

    cannot add product with quantity 0

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -12,7 +12,12 @@ class CartItemsController < ApplicationController
   def update
     @cart = current_cart
     @cart_item = @cart.cart_items.find_by(product_id: params[:id])
-    @cart_item.update(cart_item_params)
+    if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
+      @cart_item.update(cart_item_params)
+      flash[:notice] = "Successfully revise number!"
+    else
+      flash[:warning] = "The number is less than what you want so can't be added in cart!"
+    end
 
     redirect_to carts_path
   end
