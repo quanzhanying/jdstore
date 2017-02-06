e3e24e0099012f33ddbd96a6949c84d6732665bd
Kevin161111 <hwha2008@hotmail.com>
2017-02-06 22:46:41 +0800

    empty cart

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,11 @@ Rails.application.routes.draw do
       post :add_to_cart
     end
   end
- resources :carts
-  root 'welcome#index'
+ resources :carts do
+   collection do
+     delete :clean
+   end
+ end
+
+  root 'products#index'
 end
