60ce6911854a152e9208363ec7d85ee04cd16919
bob <bao309@163.com>
2017-02-06 22:20:32 +0800

    implement cart_total_price function

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,16 @@ root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 namespace :admin do
   resources :products
+
 end
 resources :products do
     member do
       post :add_to_cart
+
     end
   end
+
 resources :products
+ resources :carts
 root 'welcome#index'
 end
