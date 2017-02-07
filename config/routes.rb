c58f10eccaab4492697fbde11753214346224905
AnattaGuo <mengjunkwok@gmail.com>
2017-02-07 21:17:43 +0800

    update add_to_cart button

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'products#index'
+
   devise_for :users
+
   namespace :admin do
     resources :products
  end
@@ -8,6 +10,7 @@ Rails.application.routes.draw do
    resources :products do
      member do
        post :add_to_cart
-     end
    end
+ end
+
 end
