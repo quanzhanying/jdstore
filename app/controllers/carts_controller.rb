6383497cb2a6d4ece5782d4825f92ce3e77e021b
苗志良 <13585824737@163.com>
2017-02-07 20:31:58 +0800

    empty cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,6 @@
 class CartsController < ApplicationController
+  def clean_cart.clean!
+    flash[:warning] = "已清空购物车"
+    redirect_to carts_path
+  end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -16,4 +16,8 @@ class Cart < ApplicationRecord
     end
     sum
   end
+
+  def clean!
+    cart_items.destroy_all
+  end
 end
