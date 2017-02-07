2e4cf9c34e78d5883d040b1bf33c03dc2143eefe
lu <lubiao.zc@gmail.com>
2017-02-07 17:46:17 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root 'products#index'
   namespace :admin do
     resources :products
   end
+
+  resources :products
   
   devise_for :users
   root 'welcome#index'
