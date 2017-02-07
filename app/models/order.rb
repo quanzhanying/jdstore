4e3b48cd58afc3b859991aade4e26c8b8efe4752
andy2000xyz <andy2000xyz@hotmail.com>
2017-02-07 16:10:34 +0800

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,3 +8,11 @@ class Order < ApplicationRecord
 
   has_many :product_lists
 end
+
+class Order < ApplicationRecord
+  before_create :generate_token
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
+end
