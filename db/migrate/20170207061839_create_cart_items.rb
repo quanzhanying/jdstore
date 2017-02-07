e91d4f3ef94a3e42f0ecf693d8beefd742d47ac9
chenzhiduo <zhiduochen@gmail.com>
2017-02-07 17:03:24 +0800

    implement current_cart function

--- db/migrate/20170207061839_create_cart_items.rb
+++ db/migrate/20170207061839_create_cart_items.rb
@@ -4,7 +4,6 @@ class CreateCartItems < ActiveRecord::Migration[5.0]
       t.integer :cart_id
       t.integer :product_id
       t.integer :quantity, default: 1
-
       t.timestamps
     end
   end
