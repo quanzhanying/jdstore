03d32e1dae0b6f18b556625116bd5518a8899167
cheng df <cheng.010021@gmail.com>
2017-02-07 19:30:12 +0800

    implement cart_total_price function

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     member do
       post :add_to_cart
     end
-  end 
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :carts
 end
