419a670e227266468bbbde359160ce50fc92f565
os <kqkq1212@aliyun.com>
2017-02-07 10:52:51 +0800

    implement add_product_to_cart function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,20 +12,20 @@ class ProductsController < ApplicationController
     flash[:notice] = "成功加入购车"
     redirect_to :back
   end
-  helper_method :current_cart
-
-  def current_cart
-     @current_cart ||= find_cart
-   end
-
-   private
-
-   def find_cart
-     cart = Cart.find_by(id: session[:cart_id])
-     if cart.blank?
-       cart = Cart.create
-     end
-     session[:cart_id] = cart.id
-     return cart
-   end
+  # helper_method :current_cart
+  #
+  # def current_cart
+  #    @current_cart ||= find_cart
+  #  end
+  #
+  #  private
+  #
+  #  def find_cart
+  #    cart = Cart.find_by(id: session[:cart_id])
+  #    if cart.blank?
+  #      cart = Cart.create
+  #    end
+  #    session[:cart_id] = cart.id
+  #    return cart
+  #  end
 end
