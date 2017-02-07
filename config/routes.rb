2167d8794efc9d026d8418aa8d7db62a3bf59c7c
janice <janicejiang.d@gmail.com>
2017-02-07 15:32:45 +0800

    implement cart_total_price function

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
 
+  resources :carts
+
   resources :products do
     member do
       post :add_to_cart
