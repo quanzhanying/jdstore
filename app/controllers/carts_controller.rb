e0378ac6ef88cb8972cbd2914e6a4d3ebf75dff2
吴海铭 <freehuntman@gmail.com>
2017-02-07 21:13:44 +0800

    fix empty cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,6 +2,6 @@ class CartsController < ApplicationController
   def clean
     current_cart.clean!
     flash[:warning] = "已清空购物车"
-    redirect_to clean_carts_path
+    redirect_to carts_path
   end
 end
