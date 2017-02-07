591be4bac2a0028b0e6cce13e071fc109ba3284c
candy <candycan@163.com>
2017-02-07 15:01:59 +0800

    add add_to_cart button

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 root 'products#index'
 devise_for :users
   namespace :admin do
-    resources :products
+    resources :products do
+      member do
+        post :add_to_cart
+      end 
   end
 
   resources :products
