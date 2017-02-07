23450c68fd563dcc9f9eb758d9eddb549aaa9fa7
kai <328458211@qq.com>
2017-02-07 20:10:44 +0800

    clean cart FUNC.

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,12 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :carts
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end
+
   resources :cart_items
 
   root 'products#index'
