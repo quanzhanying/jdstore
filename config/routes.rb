fe27f08139f0618a3f2924fdf8d5d5a62e76096f
yoyo <yuziyao2007@163.com>
2017-02-08 00:02:10 +0800

    add add_to_cart button

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ root 'products#index'
      resources :products
    end
 
-  resources :products
-  root 'welcome#index'
+  resources :products do
+    member do
+
+      post :add_to_cart
+
+    end
+  end
+  
 end
