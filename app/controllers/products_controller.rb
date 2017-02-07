6ed9c3452fabd7497d5ea660a744d8a87f198f43
apple <chgpgf@gamil.com>
2017-02-07 21:04:33 +0800

    add_to_cart button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,10 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    redirect_to :back
+    flash[:notice] = "测试加入购物车"
+  end
 end
