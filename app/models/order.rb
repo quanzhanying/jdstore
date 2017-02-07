c68145092456dedb03fdd12c09a04192287a6913
linfgit <linfgg1@gmail.com>
2017-02-08 00:14:32 +0800

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,10 @@
 class Order < ApplicationRecord
+  before_create :generate_token
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
+  
   belongs_to :user
   validates :billing_name, presence: true
   validates :billing_address, presence: true
@@ -6,4 +12,5 @@ class Order < ApplicationRecord
   validates :shipping_address, presence: true
 
   has_many :product_lists
+
 end
