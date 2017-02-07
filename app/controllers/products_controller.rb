ebe8e5431862ddeb309d543884008e97a31c5b79
王巍 <wei.wang90@foxmail.com>
2017-02-07 23:10:55 +0800

    实现购物车不能有重复商品的功能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,8 +11,13 @@ class ProductsController < ApplicationController
 
   def add_to_cart
     @product = Product.find(params[:id])
-    current_cart.add_product_to_cart(@product)
-    flash[:notice] = "成功加入购物车"
-    redirect_to :back
+    if current_cart.products.include?(@product)
+      flash[:alert] = "该商品已经在购物车内"
+      redirect_to :back
+    else
+      current_cart.add_product_to_cart(@product)
+      flash[:notice] = "成功加入购物车"
+      redirect_to :back
+    end
   end
 end
