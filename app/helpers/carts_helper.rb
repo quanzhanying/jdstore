3a8b77a98e65597a6cbcdac29f2071d352443818
hyacinth0906 <doris_0906@126.com>
2017-02-07 22:07:32 +0800

    empty cart

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -2,4 +2,10 @@ module CartsHelper
   def render_cart_total_price(cart)
     cart.total_price
  end
+
+ def clean
+   current_cart.clean!
+   flash[:warning] = "已清空购物车"
+   redirect_to carts_path
+ end
 end
