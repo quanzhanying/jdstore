1bcaeaf880f50dcf3bc382eadd2378c2bea6b221
sunan <sunanchn@126.com>
2017-02-07 20:53:59 +0800

    debug:

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -9,14 +9,14 @@ class Cart < ApplicationRecord
     ci.save
   end
 
-  def total_price
-    sum = 0
-    cart_items.each do |cart_item|
-      if cart_item.product.price.present?
-        sum = cart_item.quantity * cart_item.product.price
+    def total_price
+      sum = 0
+      cart_items.each do |cart_item|
+        if cart_item.product.price.present?
+          sum += cart_item.quantity * cart_item.product.price
+        end
       end
+      sum
     end
-    sum
-  end
 
 end
