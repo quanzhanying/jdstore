51b28149e2069b22b60a338aaeebf16d342f28f4
apple <apple@appledeMacBook-Pro.local>
2017-02-07 15:10:54 +0800

    implement current_cart function & add_product_to_cart function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,14 +8,17 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def add_to_cart
+    @product = Product.find(params[:id])
+    current_cart.add_product_to_cart(@product)
+    redirect_to :back
+    flash[:notice] = "成功加入购物车"
+  end
+
   private
   def product_params
     params.require(:product).permit(:title,:description,:quantity,:price,:image)
   end
 
-  def add_to_cart
-    @product = Product.find(params[:id])
-    redirect_to :back
-    flash[:notice] = "测试加入购物车"
-  end
+
 end
