ba06b5512ce4942b2aa0d4092bbf6052d491b24e
linfgit <linfgg1@gmail.com>
2017-02-07 19:53:30 +0800

    add order show function

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,9 @@
 class Order < ApplicationRecord
   belongs_to :user
   validates :billing_name, presence: true
-  vaildates :billing_address, presence: true
+  validates :billing_address, presence: true
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
 
-  had_mant :product_lists
+  has_many :product_lists
 end
