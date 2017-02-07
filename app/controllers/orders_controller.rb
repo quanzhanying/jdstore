0b789230ae1326b4c6bc1ba6d7dfcf586cd4a165
linfgit <linfgg1@gmail.com>
2017-02-08 00:03:32 +0800

    debug @order = Order.new(order_params)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
   def create
-    @order = Order.new(order.params)
+    @order = Order.new(order_params)
     @order.user = current_user
     @order.total = current_cart.total_price
 
