e8d5b1b5cb86a2c5db4444adb0c6bb3fb774154c
baohua <113983633@qq.com>
2017-02-07 20:29:55 +0800

    user can see all orders

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,8 @@ resources :carts do
 end
 resources :cart_items
 resources :orders
+namespace :account do
+  resources :orders
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
