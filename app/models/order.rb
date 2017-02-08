2bc57ca3c36ff6eaea1743de105d88b4ce01a1e7
Learnor <learnor@gmail.com>
2017-02-08 10:01:37 -0600

    add order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :user
 
-  validates :billing_name, billing_address, shipping_name, shipping_address, presence: true
+  validates :billing_name, :billing_address, :shipping_name, :shipping_address, presence: true
 end
