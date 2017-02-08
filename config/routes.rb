5d3c4c60c654a6550d7c7b8484bafe823d38c908
陶梦迪 <taomengdi99@163.com>
2017-02-08 19:11:11 +0800

    add add_to_cart button

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
-  resources :products
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
 end
