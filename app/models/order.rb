2c69fef3e03d777f03479e76ff58b952658c7565
“nataliu” <13636424061@163.com>
2017-02-08 21:59:13 +0800

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,10 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :product_lists
 
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
+
   validates :billing_name, presence: true
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
