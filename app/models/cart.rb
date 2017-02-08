995224b92522ffc539dcdc532b0b8d5ede13e50c
bob <bao309@163.com>
2017-02-08 21:16:58 +0800

    empty cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -7,9 +7,13 @@ class Cart < ApplicationRecord
       ci.quantity = 1
       ci.save
   end
+  def clean!
+  cart_items.destroy_all
+ end
   def total_price
       sum = 0
       cart_items.each do |cart_item|
+        
         sum += cart_item.quantity * cart_item.product.price
       end
       sum
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,18 @@ namespace :admin do
   resources :products
 
 end
+namespace :account do
+  resources :orders
+end
 resources :products do
     member do
       post :add_to_cart
 
+    end
+  end
+  resources :carts do
+    collection do
+      delete :clean
     end
   end
   resources :carts do
