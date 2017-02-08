3739c61ac89086dedce7042bd35d4f13bde19a4a
lichen <lichen19844@gmail.com>
2017-02-08 22:07:44 +0800

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
+  before_create :generate_token
   has_many :product_lists
   belongs_to :user
 
@@ -6,5 +7,9 @@ class Order < ApplicationRecord
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
 
 end
