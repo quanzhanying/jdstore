83545d46b3e2a79870063b8aa963232fadd4ccce
zengxiangrong <zengxiangrong5261435@gmail.com>
2017-02-08 11:32:26 +0800

    add add_to_cart button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,5 +6,10 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
-  
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    redirect_to :back
+    flash[:notice] = "test add to cart..."
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,10 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :products
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
+
 end
