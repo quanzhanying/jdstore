cace5f09819a196da3a7761aa5f59e350fcf2119
tuantuan <lisuji1986@gmail.com>
2017-02-08 23:19:42 +0800

    delete an item from cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,9 +1,13 @@
 class CartItemsController < ApplicationController
+  before_action :authenticate_user!
 
   def destroy
-
-    @cart_item = Cart_item.find(params[:id])
+    @cart = current_cart
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    @product = @cart_item.product
     @cart_item.destroy
-    redirect_to carts_path
+
+    flash[:warning] = "成功将 #{@product.title} 从购物车删除！"
+    redirect_to :back
   end
 end
