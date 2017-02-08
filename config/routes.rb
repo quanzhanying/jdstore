917d4ac2e188d672d5289b76038b05bc557aecd7
Jimmylee <Jimmylee180180@outlook.com>
2017-02-08 15:45:50 +0800

    delete an item from cart

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
     root 'products#index'
     devise_for :users
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+    resources :cart_items
     namespace :admin do
         resources :products
     end
