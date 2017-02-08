737e69be25b45e645ccdd2382ef6b649c31367c0
sunrise <luoruijunblue@hotmail.com>
2017-02-08 21:13:02 +0800

    fix total_price

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -13,7 +13,7 @@ class Cart < ApplicationRecord
     sum = 0
     cart_items.each do |cart_item|
       if cart_item.product.price.present?
-        sum = cart_item.quantity * cart_item.product.price
+        sum += cart_item.quantity * cart_item.product.price
       end
     end
     sum
