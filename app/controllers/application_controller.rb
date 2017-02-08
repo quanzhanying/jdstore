fafa0cd4e7a5680d483c3c974563f5eb52505a51
zin <romandozin@gmail.com>
2017-02-08 19:27:42 +0100

    implement current_cart function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,18 @@ class ApplicationController < ActionController::Base
       redirect_to root_path
     end
   end
+
+  helper_method :current_cart
+  def current_cart
+    @current_cart ||= find_cart
+  end
+
+  private
+
+  def find_cart
+    cart = Cart.find_by(id: session[:cart_id])
+    cart = Cart.create if cart.blank?
+    session[:cart_id] = cart.id
+    cart
+  end
 end
