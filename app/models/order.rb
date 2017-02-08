98a8980170088f95d61b61334931d235be1b0c8e
xyy601chenyan <xyy545688506@gmail.com>
2017-02-08 08:31:40 +0800

    add product_list model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
 belongs_to :user
+has_many :product_lists
 validates :billing_name, presence:{message: "请填写购买人姓名"}
 validates :billing_address, presence: {message: "请填写购买人地址"}
  validates :shipping_name, presence: {message: "请填写收件人姓名"}
