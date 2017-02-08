e6b09b050b3583a071bccf7749bc2ba818c91292
apple <chgpgf@gamil.com>
2017-02-08 22:33:46 +0800

    connot add item quanlity 0

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -13,9 +13,12 @@ class CartItemsController < ApplicationController
   def update
     @cart = current_cart
     @cart_item = @cart.cart_items.find_by(product_id: params[:id])
-    @cart_item.update(cart_item_params)
-
+    if @cart_item.product.quanlity >= cart_item_params[:quanlity].to_i
+      @cart_item.update(cart_item_params)
+      flash[:notice] = "成功变更数量"
+    else
     redirect_to carts_path
+    end
   end
 
 
