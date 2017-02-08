2fa5dedd0e5e66386fd412b58d214b5f18dd1478
“nataliu” <13636424061@163.com>
2017-02-08 16:36:47 +0800

    delete one item from cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,9 +15,5 @@ class ProductsController < ApplicationController
     redirect_to :back
   end
 
-  def clean
-    current_cart.clean!
-    flash[:warning] = "已清空购物车"
-    redirect_to carts_path
-  end
+
 end
