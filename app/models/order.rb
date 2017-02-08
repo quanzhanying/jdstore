06cf5e00b1b5c3b0346eb036afb8248b7fd6df24
Cindy <cindyjiang88@163.com>
2017-02-08 16:35:30 +0800

    add product_list model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,9 @@
 class Order < ApplicationRecord
   belongs_to :users
-  
+
   validates :billing_name, presence: true
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
+  has_many :product_lists
 end
