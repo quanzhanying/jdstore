791b1c7ec927ecc156a816855e53568807e3881e
maviszhu <maviszhu1987@gmail.com>
2017-02-08 22:04:26 +0800

    change quantity in cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,15 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    if !current_cart.products.include?(@product)
+      current_cart.add_product_to_cart(@product)
+      flash[:notice] = "成功将#{@product.title}加入购物车!"
+    else
+      flash[:warning] = "该物品已在购物车中。"
+    end
+    redirect_to :back
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :products
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end
+  resources :cart_items
   namespace :admin do
     resources :products
   end
