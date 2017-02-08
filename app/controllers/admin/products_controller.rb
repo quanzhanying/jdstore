8263205df2c1e4a2ad82a669dc1fb2b0f9083e09
hyacinth0906 <doris_0906@126.com>
2017-02-08 21:23:59 +0800

    change quantity in cart

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -40,6 +40,6 @@ class Admin::ProductsController < ApplicationController
     private
 
     def product_params
-      params.require(:product).permit(:title, :description, :quatity, :price, :image)
+      params.require(:product).permit(:title, :description, :quantity, :price, :image)
     end
 end
