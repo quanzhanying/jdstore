84c885c472ed0452d0adcc261f08c79b150cccbc
apple <chgpgf@gamil.com>
2017-02-08 22:27:43 +0800

    change quanlity in cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,11 +2,26 @@ class CartItemsController < ApplicationController
 
   def destroy
     @cart = current_cart
-    @cart_itme = @cart.cart_itmes.find_by(product_id: params[:id])
-    @product = @cart_itme.product
-    @cart_itme.destroy
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    @product = @cart_item.product
+    @cart_item.destroy
 
     flahs[:warning] = "成功将 #{@product.title} 从购物车删除！"
     redirect_to :back
+  end
+
+  def update
+    @cart = current_cart
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    @cart_item.update(cart_item_params)
+
+    redirect_to carts_path
+  end
+
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:quanlity)
   end
 end
