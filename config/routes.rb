d451ed77acd3ba12cc6c9565ecaf2c970bbbb5ca
Caven <xll290016537@gmail.com>
2017-02-08 16:03:44 +0800

    empty cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,25 @@
 Rails.application.routes.draw do
   root 'products#index'
+  devise_for :users
+
+
   namespace :admin do
     resources :products
   end
+
   resources :products do
     member do
       post :add_to_cart
     end
   end
-  devise_for :users
 
-  resources :carts
+
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end 
+
+
+
 end
