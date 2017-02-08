a77b14625307cbd9a91a8ef8bf947a01e7ca7ec5
Caven <xll290016537@gmail.com>
2017-02-08 21:42:52 +0800

    cannot add item with quantity 0

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -14,7 +14,14 @@ class CartItemsController < ApplicationController
   def update
     @cart = current_cart
     @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+
+  if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
     @cart_item.update(cart_item_params)
+    flash[:notice] = "成功变更数量"
+
+  else
+    flash[:warning] = "库存不足"
+  end
 
     redirect_to carts_path
   end
@@ -22,5 +29,5 @@ class CartItemsController < ApplicationController
   private
   def cart_item_params
     params.require(:cart_item).permit(:quantity)
-  end 
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207062554) do
+ActiveRecord::Schema.define(version: 20170208133055) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -28,10 +28,10 @@ ActiveRecord::Schema.define(version: 20170207062554) do
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.string   "quantity"
+    t.integer  "quantity",    default: 1
     t.integer  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
     t.string   "image"
   end
 
