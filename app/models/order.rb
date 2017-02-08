39f108473e4f85ed7fca51d0085791c5dea88ba6
nirvana <wiltonwung@gmail.com>
2017-02-08 14:57:21 +0800

    add product_list model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :product_lists
 
-  validates :billig_name, presence: true
+  validates :billing_name, presence: true
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
