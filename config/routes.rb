b4f241c5ba8efca320ecf3d5ca91e0f9b5492cbd
RealAnalysis <detective_boys@hotmail.com>
2017-02-08 23:25:20 +0800

    empty cart

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
 
-  resources :carts
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end
 end
