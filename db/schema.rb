7d87bc6fc7d60b6d4cbbb08ee710bac948395c00
shiyongheng <ziyou@shiyonghengdeMacBook-Air.local>
2017-02-08 14:49:37 +0800

    add token to arder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207141538) do
+ActiveRecord::Schema.define(version: 20170208063857) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20170207141538) do
     t.string   "shipping_address"
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
+    t.string   "tiken"
+    t.string   "token"
   end
 
   create_table "product_lists", force: :cascade do |t|
