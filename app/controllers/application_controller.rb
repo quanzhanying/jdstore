8b4b6ab83ce7797c2f6cb579e06beddb33aed305
zsmhy <zsm@example.com>
2017-02-08 23:20:01 +0800

    only admin can access backend panel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+   # protect_from_forgery with: :exception
+   #  For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
+
+  def admin_required
+    if !current_user.admin?
+      redirect_to "/", alert: "You are not admin."
+    end
+  end
 end
