457cc26f25a16c6054cceab3d0eacdcfe24fab60
juliusyoung <yangzuohui@gmail.com>
2017-02-08 21:57:46 +0800

    add orders show function

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class OrderControllerTest < ActionDispatch::IntegrationTest
+class OrdersControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
