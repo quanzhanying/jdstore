818c948a9f26defa181163e10b910cabbc592d04
maple <datongde@gmail.com>
2017-02-08 20:13:21 +0800

    add add_to_cart button

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   end
   # root 'welcome#index'
   root 'products#index'
-  resources :products
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
 end
