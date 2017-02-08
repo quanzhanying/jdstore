6a64b64dfc7ff47f93cae34b662ee0fdcf37d0d5
liwei <liwei5917@gmail.com>
2017-02-08 21:54:56 +0800

    add product_list model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :product_lists
 
   validates :billing_name, presence: true
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
-  
+
 end
