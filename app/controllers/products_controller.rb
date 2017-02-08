92796441e2ca0affb93d8d5e50efc2e106c529c4
taketimeasafriend <taketimeasafriend@gmail.com>
2017-02-09 02:29:21 +0800

    add put_to_cart button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,17 @@
 class ProductsController < ApplicationController
+    # 首页 R
+    def index
+        @products = Product.all
+    end
 
-  # 首页 R
-  def index
-    @products = Product.all
-  end
+    # 查看 R
+    def show
+        @product = Product.find(params[:id])
+    end
 
-  # 查看 R
-  def show
-    @product = Product.find(params[:id])
-  end
-  
+    def add_to_cart
+        @product = Product.find(params[:id])
+        redirect_to :back
+        flash[:notice] = '测试加入购物车'
+    end
 end
