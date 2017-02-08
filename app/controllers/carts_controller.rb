e41fedc91156fe378b667d96cb4931d40dd491a7
cheng df <cheng.010021@gmail.com>
2017-02-08 22:04:28 +0800

    empty cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,8 @@
 class CartsController < ApplicationController
+  def clean
+     current_cart.clean!
+    # current_cart.cart_items.destroy_all
+    flash[:warning] = "已清空购物车"
+    redirect_to carts_path
+  end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -19,5 +19,9 @@ class Cart < ApplicationRecord
     sum
   end
 
+  def clean!
+    cart_items.destroy_all
+  end
+
 
 end
