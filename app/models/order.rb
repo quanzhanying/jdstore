3b65b1e491ca1c2e591342da0105aa21aec75781
liwei <liwei5917@gmail.com>
2017-02-08 22:16:17 +0800

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,10 @@
 class Order < ApplicationRecord
+  before_create :generate_token
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
+
   belongs_to :user
   has_many :product_lists
 
@@ -6,5 +12,6 @@ class Order < ApplicationRecord
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
+
 
 end
