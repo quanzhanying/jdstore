e0e204df24c07ced058813366f5894c732390c2d
yandean <yandean018@163.com>
2017-02-08 21:04:28 +0800

    implement cart_total_price function

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :carts
+
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
