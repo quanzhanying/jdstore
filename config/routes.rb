4af8550aa3351194220703e24a268df525865026
YukiFeng0923 <fcx0923@163.com>
2017-02-09 21:55:21 +0800

    finish story5

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,11 @@ end
 
 resources :cart_items
 resources :orders
+
+namespace :account do
+  resources :orders
+end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
