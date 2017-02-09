c293d7d5bdb6d4fcb7e886c33e43862709b698c7
kai <328458211@qq.com>
2017-02-09 15:50:55 +0800

    my orders in nav

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,10 @@ Rails.application.routes.draw do
   resources :cart_items
   resources :orders
 
+  namespace :account do
+    resources :orders
+  end
+
   root 'products#index'
 #root 'admin/products#index'
 end
