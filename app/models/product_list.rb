0321e22799ea98f58dafd0522b13ad57ab8db49c
shaoxueqian <sxq101@icloud.com>
2017-02-09 15:40:01 +0800

    users can see past order in account/orders panel

--- app/models/product_list.rb
+++ app/models/product_list.rb
@@ -1,2 +1,3 @@
 class ProductList < ApplicationRecord
+  belongs_to :order
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
   root 'products#index'
 
+
+  namespace :account do
+    resources :orders
+  end
+
+  
   namespace :admin do
     resources :products
   end
@@ -27,5 +33,7 @@ Rails.application.routes.draw do
   resources :cart_items
 
   resources :orders
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
