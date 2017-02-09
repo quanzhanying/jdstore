33a3b084ff0e373205db5207839df4e07f49948b
jacksparrow <chenqiong0623@qq.com>
2017-02-09 14:47:54 +0800

    fix something

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
     resources :products
   end
 
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
