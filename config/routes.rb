77db4577d817ba32b42137f56af1774aad383a1b
wangyanli <wylhope365@163.com>
2017-02-09 21:10:45 +0800

    implement image upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+root 'products#index'
   devise_for:users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
   resources :products
   end
+resources :products
 
 root 'welcome#index'
 end
