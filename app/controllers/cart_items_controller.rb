0a0b59fa259e6236b3607eef3a695c150614be14
王巍 <wei.wang90@foxmail.com>
2017-02-09 22:46:30 +0800

    修改选择商品数量的逻辑

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,6 +1,13 @@
 class CartItemsController < ApplicationController
   before_action :authenticate_user!
 
+  def update
+    @cart = current_cart
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to carts_path
+  end
+
   def destroy
     @cart = current_cart
     @cart_item = @cart.cart_items.find_by(product_id: params[:id])
@@ -27,5 +34,11 @@ class CartItemsController < ApplicationController
       @cart_item.save
       redirect_to carts_path
     end
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity)
   end
 end
