9a37e94fd4238d5091d38c9704ed35e07f469e1f
kai <328458211@qq.com>
2017-02-09 15:52:35 +0800

    my order DESC

--- app/controllers/account/orders_controller.rb
+++ app/controllers/account/orders_controller.rb
@@ -1,6 +1,6 @@
 class Account::OrdersController < ApplicationController
   before_action :authenticate_user!
   def index
-    @orders = current_user.orders
+    @orders = current_user.orders.("id DESC")
   end
 end
