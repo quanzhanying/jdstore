7115b834fb15cb54b2adae266d2b0a4404f37a15
你的名字 <email>
2017-02-09 10:49:46 +0800

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,11 @@
 class Order < ApplicationRecord
+  before_create :generate_token
   belongs_to :user
   has_many :product_lists
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
 
   validates :billing_name, presence: true
   validates :billing_address, presence: true
