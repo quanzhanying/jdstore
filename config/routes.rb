87b222efe6958a855f9fd83f52a4cf2a187ae0b7
Qingqing <renwq8@126.com>
2017-02-09 11:11:04 +0800

    add account/orders

--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,9 @@ resources :carts do
   resources :cart_items
 
   resources :orders
+
+  namespace :account do
+    resources :orders
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
