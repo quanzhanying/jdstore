a187ac3d1bc6fb541b94912b54e116523c4027e2
Kevin161111 <hwha2008@hotmail.com>
2017-02-09 22:39:42 +0800

    add checkout routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,17 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
+
   resources :products do
     member do
       post :add_to_cart
     end
   end
+
  resources :carts do
    collection do
      delete :clean
+     post :checkout
    end
  end
 
