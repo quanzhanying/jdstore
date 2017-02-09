6e3495c609d271fc969d4d02d4000c055bcf0c56
wayne <lightww@me.com>
2017-02-09 22:35:26 +0800

    imiplement backend CRUD

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,51 +1,49 @@
 class Admin::ProductsController < ApplicationController
+    def index
+        @products = Product.all
+    end
 
-def index
-  @products = Product.all
-end
+    def new
+        @product = Product.new
+    end
 
-def new
-  @product = Product.new
-end
+    def create
+        @product = Product.new(product_params)
 
-def create
-  @product = Product.new(product_params)
+        if @product.save
+            redirect_to admin_products_path
+        else
+            render :new
+        end
+    end
 
-  if @product.save
-    redirect_to admin_products_path
-  else
-    render :new
-  end
-end
+    def show
+        @product = Product.find(params[:id])
+    end
 
-def show
-  @product = Product.find(params[:id])
-end
+    def edit
+        @product = Product.find(params[:id])
+    end
 
-def edit
-  @product = Product.find(params[:id])
-end
+    def update
+        @product = Product.find(params[:id])
 
-def update
-  @product = Product.find(params[:id])
+        if @product.update(product_params)
+            redirect_to admin_products_path
+        else
+            render :edit
+        end
+    end
 
-  if @product.update(product_params)
-    redirect_to admin_products_path
-  else
-    render :edit
-  end
-end
+    def destroy
+        @product = Product.find(param[:id])
+        @product.destroy
+        redirect_to admin_products_path
+    end
 
-def destroy
-  @product = Product.find(param[:id])
-  @product.destroy
-  redirect_to admin_products_path
-end
+    private
 
-
-private
-def product_params
-  params.require(:product).permit(:title, :description,:quantity, :price)
-end
-
+    def product_params
+        params.require(:product).permit(:title, :description, :quantity, :price)
+    end
 end
