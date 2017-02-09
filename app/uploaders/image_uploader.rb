f5c35d1c952d9179d2dab2dbf5a898dec026b38f
mayue <mayue@mayuedeMacBook-Air.local>
2017-02-09 23:18:14 +0800

    change admin/product backend panel

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,11 +2,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
 
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
+  
   storage :file
   # storage :fog
 
