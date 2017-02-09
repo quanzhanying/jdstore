2b67ab9d2481c9741650fe1d696e323307c59159
pitter liu <pitterliu@pitters-MacBook-Pro.local>
2017-02-09 09:42:49 +0800

    add add_to_cart button2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,4 +10,12 @@ before_action :authenticate_user! , only: [:new]
   def show
     @product=Product.find(params[:id])
   end
+
+
+  def add_to_cart
+    @product=Product.find(params[:id])
+    redirect_to :back
+    flash[:notice]="加入购物车"
+  end
+  
 end
