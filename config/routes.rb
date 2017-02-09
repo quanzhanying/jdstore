74642129d2a09385c4115b4c32cdfaad49851909
NicknameYi <nicknameyi@gmail.com>
2017-02-10 00:55:25 +0800

    implement add_product_to_cart function

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,14 @@ Rails.application.routes.draw do
   
   devise_for :users
   namespace :admin do
-	resources :products
+	  resources :products
   end
 
-  resources :products
+  resources :products do 
+  	member do
+  		post :add_to_cart
+  	end
+  end
 
 
 
