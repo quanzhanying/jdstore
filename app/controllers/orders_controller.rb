9b24903af0682a1e5a86ef7047d1bbf82c0b6dbf
yoyo <yuziyao2007@163.com>
2017-02-09 22:53:18 +0800

    alter token order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -23,7 +23,7 @@ class OrdersController < ApplicationController
   end
 
   def show
-    @order = Order.find_by_token(params[:id])
+   @order = Order.find_by_token(params[:id])
     @product_lists = @order.product_lists
   end
 
--- db/migrate/20170209141049_add_token_to_order.rb
+++ db/migrate/20170209141049_add_token_to_order.rb
@@ -1,5 +1,5 @@
 class AddTokenToOrder < ActiveRecord::Migration[5.0]
   def change
-    add_column :orders, :token, :string 
+  add_column :orders, :token, :string
   end
 end
