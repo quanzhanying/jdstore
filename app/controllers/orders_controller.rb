f7ba8b36d9b07f95c28cd3b01d017111d5af8ec9
你的名字 <email>
2017-02-09 10:18:30 +0800

    add orders show function2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,7 +9,7 @@ class OrdersController < ApplicationController
     if @order.save
 
       current_cart.cart_items.each do |cart_item|
-        product_list = Product.new
+        product_list = ProductList.new
         product_list.order = @order
         product_list.product_name = cart_item.product.title
         product_list.product_price = cart_item.product.price
