1a612070d4270307ab5ab9c280e0afffe4c208f6
huangqiaoming <carolinehuang.aiesec@gmail.com>
2017-02-10 18:52:08 +0800

    add token to order

--- db/migrate/20170210072142_create_product_lists.rb
+++ db/migrate/20170210072142_create_product_lists.rb
@@ -2,7 +2,7 @@ class CreateProductLists < ActiveRecord::Migration[5.0]
   def change
     create_table :product_lists do |t|
       t.integer :order_id
-      t.integer :product_name
+      t.string :product_name
       t.integer :product_price
       t.integer :quantity
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170210072142) do
+ActiveRecord::Schema.define(version: 20170210103857) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -34,11 +34,12 @@ ActiveRecord::Schema.define(version: 20170210072142) do
     t.string   "shipping_address"
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
+    t.string   "token"
   end
 
   create_table "product_lists", force: :cascade do |t|
     t.integer  "order_id"
-    t.integer  "product_name"
+    t.string   "product_name"
     t.integer  "product_price"
     t.integer  "quantity"
     t.datetime "created_at",    null: false
