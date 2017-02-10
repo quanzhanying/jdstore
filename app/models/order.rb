eeabf23dafa3d6ceffecf4939b0c6e95f495654b
yanlinx <xieyanlin820@gmail.com>
2017-02-10 14:11:49 +0800

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :user
+  before_create :generate_token
+
+     def generate_token
+       self.token = SecureRandom.uuid
+     end
   has_many :product_lists
      validates :billing_name, presence: true
      validates :billing_address, presence: true
