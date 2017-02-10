1e63da39a6ea5195681a40f360feb3107b1d0a3c
shaheming <hemingsha@gmail.com>
2017-02-10 14:20:53 +0800

    implement carts index function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,4 +20,15 @@ def find_cart
 	session[:cart_id] = cart.id
 	return cart
 end
+
+def show_product_image(product)
+    link_to product_path(product) do
+    if product.image.present?
+      image_tag(product.image.thumb.url, class: "thumbnail")
+    else
+      image_tag("http://placehold.it/200x200&text=No Pic", class: "thumbnail")
+    end
+  end
+end
+
 end
