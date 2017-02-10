d930520879d9ee72c4c118a6fe0c9e73e3a9090a
Mason <q791862937@126.com>
2017-02-10 18:30:33 +0800

    implement current cart function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,22 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  helper_method :current_cart
+
+  def current_cart
+    @current_cart ||= find_cart
+  end
+
+  private
+
+  def find_cart
+    cart = Cart.find_by(id: session[:cart_id])
+    if cart.blank?
+      cart = Cart.create
+    end
+    session[:cart_id] = cart.id
+    return cart
+  end
   # def admin_required
   #   redirect_to '/', alert: 'You are not admin.' unless current_user.admin?
   # end
