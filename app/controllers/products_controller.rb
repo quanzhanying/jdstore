a46ca9f6b271e561756824bf9c00498e049e42bf
shaheming <hemingsha@gmail.com>
2017-02-10 10:23:23 +0800

    implement add_prodcut_to_cart function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,7 @@ class ProductsController < ApplicationController
 	def add_to_cart
 		@product = Product.find(params[:id])
 		flash[:notice] = "Add to cart"
+		current_cart.add_product_to_cart(@product)
 		redirect_to :back
 	end
 end
