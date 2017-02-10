ad8b7675abc75780a093d96e221e22d38c414df9
shaheming <hemingsha@gmail.com>
2017-02-10 14:57:24 +0800

    cannot add same item in cart

---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,10 @@
+class CartItemsController < ApplicationController
+ 	before_action :authenticate_user!
+ 	def destroy
+ 		@cart = current_cart
+ 		@cart_item = @cart.cart_items.find_by(product_id: params[:id])
+ 		@product = @cart_item.product
+ 		@cart_item.destroy
+ 		redirect_to :back, warning:"#{@product.title} was deleted!"
+ 	end
+end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,9 +7,12 @@ class ProductsController < ApplicationController
 	end
 	def add_to_cart
 		@product = Product.find(params[:id])
-		
+		if !current_cart.products.include?(@product)
 		current_cart.add_product_to_cart(@product)
 		flash[:notice] = "Add to cart"
+		else
+		flash[:warning] = "#{@product.title} is already in cart"
+		end
 		redirect_to :back
 	end
 end
