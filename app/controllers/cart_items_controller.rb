9f1dd1844657d057350f0a2312dbb4d3c9e9d065
shaheming <hemingsha@gmail.com>
2017-02-10 15:03:53 +0800

    change quantity in cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -7,4 +7,16 @@ class CartItemsController < ApplicationController
  		@cart_item.destroy
  		redirect_to :back, warning:"#{@product.title} was deleted!"
  	end
+
+ 	def update
+ 		@cart = current_cart
+ 		@cart_item = @cart.cart_items.find_by(product_id: params[:id])
+ 		@cart_item.update(cart_item_params)
+ 		redirect_to carts_path
+ 	end
+
+ 	private
+ 	def cart_item_params
+ 		params.require(:cart_item).permit(:quantity)
+ 	end
 end
