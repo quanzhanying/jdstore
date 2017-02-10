9b0a65ee9c64e872af98aebe4dbe61d1a1b82c08
bianhaiming <bianhaiming@gmail.com>
2017-02-10 18:17:46 +0800

    only admin can access backend pannel

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+u = User.new
+u.email = "qq@qq.com"
+
+u.password = "qazwsx"
+
+u.password_confirmation = "qazwsx"
+
+u.is_admin = true
+u.save
