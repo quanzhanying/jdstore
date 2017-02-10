c56b1436f52598711f7e8844769f6d516d946471
isaac <237957459@qq.com>
2017-02-10 21:32:08 +0800

    add checkout routing

--- config/routes.rb
+++ config/routes.rb
@@ -14,10 +14,11 @@ Rails.application.routes.draw do
   resources :carts do
     collection do
       delete :clean
+      post :checkout 
     end
   end
 
-  resources :cart_items 
+  resources :cart_items
 
   root 'welcome#index'
 end
