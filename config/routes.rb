213bb163234d12f3e4843c03a9235e83669b01fd
zhongjie <zhongjie10@gmail.com>
2017-02-11 14:45:49 +0800

    add order model

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,10 @@ Rails.application.routes.draw do
   resources :carts do
     collection do
       delete :clean
+      post :checkout
     end
   end
 
-  resources :cart_items 
+  resources :cart_items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
