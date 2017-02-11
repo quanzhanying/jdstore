0af31e1950140f780fd1f342bf2f26970a29cc8f
RsMacAir <51xiuxian@sina.com>
2017-02-11 14:58:53 +0800

    add admin in rails console

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ProductsController < ApplicationController
-
+before_action :authenticate_user!
+before_action :admin_required
   def index
     @products = Product.all
   end
@@ -33,7 +34,7 @@ class Admin::ProductsController < ApplicationController
       render :edit
     end
   end
-  
+
 
   private
 
