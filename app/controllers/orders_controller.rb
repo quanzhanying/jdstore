5e1b4dbb4e13d063c0ba86892701269c3d944753
zhanyouxin <zhanyouxin@gmail.com>
2017-02-11 23:39:50 +0800

    add orders show function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,6 +15,6 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order).permit(:billing_name, :billing_address, shipping_name, :shipping_address)
+    params.require(:order).permit(:billing_name, :billing_address, :shipping_name, :shipping_address)
   end
 end
