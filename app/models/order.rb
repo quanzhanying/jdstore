b748a5caa4b9e77a1446bea29179a543f410b9e0
isaac <237957459@qq.com>
2017-02-11 11:25:46 +0800

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
+  before_create :generate_token
 
   validates :billing_name, presence: true
   validates :billing_address, presence: true
@@ -7,4 +8,9 @@ class Order < ApplicationRecord
   validates :shipping_address, presence: true
 
   has_many :product_lists
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
+  
 end
