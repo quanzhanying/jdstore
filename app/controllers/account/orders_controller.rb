b42bad912afb937e9633d0154b1d513ede401891
王巍 <wei.wang90@foxmail.com>
2017-02-11 22:35:05 +0800

    user can see their orders

--- app/controllers/account/orders_controller.rb
+++ app/controllers/account/orders_controller.rb
@@ -2,11 +2,6 @@ class Account::OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @orders = current_user.orders
-  end
-
-  def show
-    @order = Order.find(params[:id])
-    @product_lists = @order.product_lists
+    @orders = current_user.orders.order("id DESC")
   end
 end
