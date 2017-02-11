57dbc9a8877b3ec7981c174ca21d8ab174689acf
Cheng Ge <chengge@MBP.local>
2017-02-10 20:43:51 -0500

    changed root to welcome, implemented slideshow

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,4 @@
 class WelcomeController < ApplicationController
     def index
-        flash[:notice] = "你好！"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
         end
     end
 
-    root 'products#index'
+    root 'welcome#index'
 
     resources :carts do
         collection do
