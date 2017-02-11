293d42b9bb633d71d791c2caeab62c9c762269a2
zengxiangrong <zengxiangrong5261435@gmail.com>
2017-02-11 10:34:00 +0800

    fix bug

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -27,7 +27,7 @@ class OrdersController < ApplicationController
   end
 
   def show
-    @order = Order.find(params[:id])
+    @order = Order.find_by_token(params[:id])
     @product_lists = @order.product_lists
   end
 end
