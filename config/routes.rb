a51e76f22327db69c2d7ef82f3fecd673d998f02
罗心 <xiaoxindihua@hotmail.com>
2017-02-12 21:55:35 +0800

    add add_to_cart button

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
       resources :products
   end
   root 'products#index'
-  resources :products
+  resources :products do
+      member do
+        post :add_to_cart
+      end
+  end
 end
