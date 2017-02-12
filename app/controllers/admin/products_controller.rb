ead85a4bbb1d176e9e642163b75d708eefa5d1bf
escray <zescray@gmail.com>
2017-02-12 22:20:05 +0800

    add show page

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,6 +3,10 @@ class Admin::ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def new
     @product = Product.new
   end
