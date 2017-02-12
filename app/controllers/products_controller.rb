57f2b8902522e3195c9a21507c6eb7d46eb4293d
tianshuai <157058166@qq.com>
2017-02-12 21:26:51 +0800

    implement product show

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,10 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
     resources :products
   end
 
-  root 'welcome#index'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'products#index' # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :products
+
 end
