be9f1ffe0f2861f01eed67c939915ebba5756d17
junoliang <junoliang007@gmail.com>
2017-02-12 21:31:49 +0800

    debug

--- db/migrate/20170205115416_add_is_admin_to_user.rb
+++ db/migrate/20170205115416_add_is_admin_to_user.rb
@@ -1,5 +1,5 @@
 class AddIsAdminToUser < ActiveRecord::Migration[5.0]
     def change
-        add_column :users, :is_admin, :boolean, default: fal
+        add_column :users, :is_admin, :boolean, default: false
     end
 end
