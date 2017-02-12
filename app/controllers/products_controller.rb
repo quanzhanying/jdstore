fcf3e918204cc3387391c87238958b462f902057
kaihe1989@gmail.com <hekai@HekaitekiMacBook-Pro.local>
2017-02-12 10:27:40 -0500

    add add_to_cart button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,12 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    flash[:notice] = "测试加入购物车"
+    redirect_to :back
+    
+  end
+
 end
