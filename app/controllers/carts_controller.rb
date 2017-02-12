683744b81590cef4504524ce2acb567aa89f57c4
RsMacAir <51xiuxian@sina.com>
2017-02-12 17:06:46 +0800

    delete an item from cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,5 +3,6 @@ class CartsController < ApplicationController
     current_cart.clean!
     flash[:warning] = "已清空购物车"
     redirect_to carts_path
-  end 
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,7 @@ resources :carts do
   end
 end
 
+resources :cart_items
+
 root 'products#index'  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
