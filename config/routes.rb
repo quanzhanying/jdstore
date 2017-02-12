105ee9419b41dd36604ac299db32309161005ec9
Gh72 <cao.wang.work@foxmail.com>
2017-02-12 10:11:23 +0800

    empty cart

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,11 @@ Rails.application.routes.draw do
       post :add_to_cart
     end
   end
-  resources :carts
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end
   #root 'welcome#index'
   root 'products#index'
 end
