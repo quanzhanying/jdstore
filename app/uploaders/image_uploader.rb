c448306810ff26ed194f95031bdfb65d3dd63fbc
linfgit <linfgg1@gmail.com>
2017-02-13 00:38:24 +0800

    修改导航栏

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -21,7 +21,7 @@ class ImageUploader < CarrierWave::Uploader::Base
    end
 
    version :medium do
-     process resize_to_fill: [400,400]
+     process resize_to_fill: [600,800]
    end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
