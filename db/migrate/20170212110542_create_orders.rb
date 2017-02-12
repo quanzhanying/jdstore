f817ba96e91a91ce29f8ef4a1f489b962d488b6f
bianhaiming <bianhaiming@gmail.com>
2017-02-12 19:46:56 +0800

    it is ok for migrate of order

--- db/migrate/20170212110542_create_orders.rb
+++ db/migrate/20170212110542_create_orders.rb
@@ -6,7 +6,7 @@ t.integer :user_id
 t.string :billing_name
 t.string :billing_address
 t.string :shipping_name
-t.string :shipp_address
+t.string :shipping_address
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -26,14 +26,14 @@ ActiveRecord::Schema.define(version: 20170212110542) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer  "total",           default: 0
+    t.integer  "total",            default: 0
     t.integer  "user_id"
-    t.integer  "billing_name"
-    t.integer  "billing_address"
-    t.integer  "shipping_name"
-    t.integer  "shipp_address"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.string   "billing_name"
+    t.string   "billing_address"
+    t.string   "shipping_name"
+    t.string   "shipping_address"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
   end
 
   create_table "products", force: :cascade do |t|
