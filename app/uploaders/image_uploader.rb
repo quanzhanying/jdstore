fd618c2274d6f40826fbf619c7c4af1e5d05b85a
sujiang <jimmysu1991@gmail.com>
2017-02-12 18:25:03 +0800

    fixed image upload problem

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -14,17 +14,17 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_fit: (800,800)
+  process resize_to_fit: [800, 800]
 
   version :thumb do
-    process resize_to_fill：（200，200）
+    process resize_to_fill: [200, 200]
   end
 
   version :medium do
-    process resize_to_fill：（400，400）
+    process resize_to_fill: [400, 400]
   end
 
-  end
+
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
