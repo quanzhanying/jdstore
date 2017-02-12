def55c1ec9264684e3f13a6b3936f62d8b49e9d4
aliceguo2016reborn <aliceguo2016reborn@gmail.com>
2017-02-12 20:49:19 +0800

    user can add product to cart & sum

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,11 @@ def show
   @product = Product.find(params[:id])
 end
 
+def add_to_cart
+  @product = Product.find(params[:id])
+  current_cart.add_product_to_cart(@product)
+  redirect_to :back
+  flash[:notice] = "成功加入购物车"
+end
+
 end
