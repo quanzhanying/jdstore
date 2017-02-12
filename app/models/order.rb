7c233749570fd9bc9b2568b5f59d799ff355ee9a
An Li <anli@Ans-Mac-mini.local>
2017-02-12 09:42:35 +0800

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
+  before_create :generate_token
   belongs_to :user
   has_many :product_lists
 
@@ -6,5 +7,9 @@ class Order < ApplicationRecord
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
 
 end
