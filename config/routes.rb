8e26158c9f5602f2bb4a987b13d77f20e916679f
邹正敏 <zouzhengmin@gmail.com>
2017-02-12 20:20:22 +0800

    cannot add same item in cart

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,7 @@ Rails.application.routes.draw do
 
     resources :cart_items
 
+    resources :orders
+
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
