e8357ac53f040e439087bc709b83d280fcce9ac9
hexiuwei <abchxw@gmail.com>
2017-02-13 22:54:29 +0800

    add OrderMailer function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,16 +15,37 @@ class OrdersController < ApplicationController
           product_list.quantity = cart_item.quantity
           product_list.save
         end
+        current_cart.clean!
+        OrderMailer.notify_order_placed(@order).deliver!
+
         redirect_to order_path(@order.token)
       else
         render 'carts/checkout'
       end
     end
 
-        def show
+      def show
           @order = Order.find_by_token(params[:id])
           @product_lists = @order.product_lists
-        end
+      end
+
+    def pay_with_alipay
+       @order = Order.find_by_token(params[:id])
+       @order.set_payment_with!("alipay")
+       @order.pay!
+
+       redirect_to order_path(@order.token), notice:"使用支付宝成功付款"
+    end
+
+
+    def pay_with_wechat
+       @order = Order.find_by_token(params[:id])
+       @order.set_payment_with!("wechat")
+       @order.pay!
+
+       redirect_to order_path(@order.token), notice:"使用微信成功付款"
+    end
+
     private
 
     def order_params
