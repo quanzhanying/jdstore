5b6d85b85cbf8c650a20b2d65a37a55333080fa6
bob <bao309@163.com>
2017-02-13 22:09:04 +0800

    add payment_method fuction

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,4 +11,11 @@ has_many :product_lists
   def generate_token
     self.token = SecureRandom.uuid
   end
+  def set_payment_with!(method)
+   self.update_columns(payment_method: method )
+ end
+
+ def pay!
+   self.update_columns(is_paid: true )
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,11 @@ resources :products do
 resources :products
  resources :carts
  resources :cart_items
- resources :orders
+ resources :orders do
+     member do
+       post :pay_with_alipay
+       post :pay_with_wechat
+     end
+   end
 root 'welcome#index'
 end
