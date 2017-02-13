f0122f54f21c23615da52580cf0cbb8862380614
runpee <992142460@qq.com>
2017-02-13 23:09:26 +0800

    add add_to_cart button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,9 +3,15 @@ class ProductsController < ApplicationController
   def index
    @products = Product.all
   end
-  
+
   def show
     @product = Product.find(params[:id])
+  end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    redirect_to :back
+    flash[:notice] = "该商品已经加入购物车"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
   devise_for :users
   root "products#index"
 
-  resources :products
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
 end
