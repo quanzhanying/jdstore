589ec16ecc45afd0c069a5b114a18ea86a256b82
wangyanli <wylhope365@163.com>
2017-02-13 21:21:50 +0800

    cannot add same item in cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,4 +4,18 @@ class CartsController < ApplicationController
     flash[:warning] = "已清空购物车"
     redirect_to carts_path
   end
+
+  def update
+    @cart = current_cart
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    @cart_item.update(cart_item_params)
+
+    redirect_to carts_path
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity)
+  end
 end
