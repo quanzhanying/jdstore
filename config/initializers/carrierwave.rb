07d11dbaf8ef97ad3951df717b82593e040403c2
Learnor <learnor@gmail.com>
2017-02-13 17:05:42 -0600

    config figaro

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
       provider:              'AWS',
       aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'], # 你的 key
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 你的 secret key
-      region:                'eu-west-1' # 你的 S3 bucket 的 Region 位置
+      region:                '' # 你的 S3 bucket 的 Region 位置
     }
     config.storage :fog
     config.fog_directory = ENV['AWS_BUCKET_NAME'] # 你设定的 bucket name
