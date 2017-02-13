3f422fd27a36114647426d75abfca0423480f74c
linfgit <linfgg1@gmail.com>
2017-02-13 23:57:21 +0800

    结合aws s3存放上传图片

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,11 +1,17 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
    include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  #storage :file
+  if Rails.env.production?
+   storage :fog
+   else
+   storage :file
+   end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
