aad8618515e20b8474b0527381f4ee833078dd76
zhanglin <a18646564596@163.com>
2017-02-13 10:16:05 +0800

    delete intem from cart

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,18 @@ Rails.application.routes.draw do
       post :add_to_cart
     end
   end
+
   namespace :admin do
     resources :products
   end
+
   root 'products#index'
+
   resources :carts do
     collection do
       delete :clean
     end
   end
+
+  resources :cart_items
 end
