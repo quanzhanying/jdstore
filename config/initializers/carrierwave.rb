3aec10743f109fe95e949991683a3039659c029a
Learnor <learnor@gmail.com>
2017-02-13 17:52:00 -0600

    .

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
       provider:              'AWS',
       aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'], # 你的 key
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 你的 secret key
-      region:                '' # 你的 S3 bucket 的 Region 位置
+      region:                'us-east-1' # 你的 S3 bucket 的 Region 位置
     }
     config.storage :fog
     config.fog_directory = ENV['AWS_BUCKET_NAME'] # 你设定的 bucket name
