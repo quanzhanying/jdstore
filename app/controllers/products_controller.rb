7f1060a60ab6bb822218c28127c9b8218f933a91
sujiang <jimmysu1991@gmail.com>
2017-02-13 14:44:08 +0800

    fixed def add product to cart problem

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,7 +9,7 @@ class ProductsController < ApplicationController
 
   def add_to_cart
     @product = Product.find(params[:id])
-    current_cart.add_product_cart(@product)
+    current_cart.add_product_to_cart(@product)
     flash[:notice] = "Added to Cart Successfully!"
     redirect_to :back
 
