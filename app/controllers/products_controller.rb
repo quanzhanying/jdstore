901a0b7252f7cd91fda332a3c8a41981406c39d9
“将叫我将军” <d2194499406@gmail.com>
2017-02-13 15:59:33 +0800

    add add_to_cart button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,5 +7,10 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def add_to_cart
+    @product = Product.find(params[:id])
+    redirect_to :back
+    flash[:notice] = "测试"
+  end
   
 end
