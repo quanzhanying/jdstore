bf126a6b63fc2193f90212f2fbc7137d5cc07153
wangyanli <wylhope365@163.com>
2017-02-13 17:11:24 +0800

    implement cart_total_price function

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -9,4 +9,14 @@ def add_product_to_cart(product)
   ci.save
 end
 
+def total_price
+  sum = 0
+  cart_items.each do |cart_item|
+    if cart_item.product.price.present?
+      sum += cart_item.quantity * cart_item.product.price
+    end
+  end
+  sum
+end
+
 end
