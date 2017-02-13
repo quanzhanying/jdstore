073e30e54b7ca629aa962fd81fa0f2412066b5f9
subinhuang <806519191@qq.com>
2017-02-13 09:20:08 +0800

    add orders show function

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,10 @@
 class Order < ApplicationRecord
-  belongs_to :users
+  belongs_to :user
+
   validates :billing_name, presence: true
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
+
+  has_many :product_lists
 end
