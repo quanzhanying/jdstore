6ff231a588bca46e4c8eec29c8509ed9bd67e774
Learnor <learnor@gmail.com>
2017-02-13 13:48:16 -0600

    add notify_cancel/ship funtion

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -14,4 +14,20 @@ class OrderMailer < ApplicationMailer
 
     mail(to: "admin@test.com", subject: "[JDStore] User#{order.user.email} applied to cancel order #{order.token}")
   end
+
+  def notify_ship(order)
+    @order        = order
+    @user         = order.user
+    @product_lists = @order.product_lists
+
+    mail(to: @user.email, subject: "[JDStore] Your order #{order.token} has already shipped")
+  end
+
+  def notify_cancel(order)
+    @order = Order
+    @user = order.user
+    @product_lists = @order.product_lists
+
+    mail(to: @user.eamil, subject:"[JDStore] Your order #{order.token} already cancelled.")
+  end
 end
