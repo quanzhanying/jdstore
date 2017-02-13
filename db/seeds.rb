36eb52927f9bd16a1051fb7fd0343271474e2404
Cheng Ge <chengge@MBP.local>
2017-02-12 21:15:16 -0500

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,18 @@ u.password = "123456"
 u.password_confirmation = "123456"
 u.is_admin = true
 u.save
+
+puts "1 admin account created."
+
+products = ["全棉针织条纹四件套 新款", "可水洗舒柔丝羽绒枕", "日式记忆绵坐垫"]
+
+products_description = ["裸睡享受，柔软透气有弹性", "柔软舒适深睡眠", "活性炭记忆绵，缓解压力"]
+
+products_prices = [299, 59, 59]
+
+products_images = ["/images/bed.png", "/images/pillow.png", "/images/sitting.png"]
+
+create_jobs = for i in 1..3 do
+  Job.create!([title: products[i - 1], description: products_description[i - 1], quantity: rand(0..1), price: products_prices[i - 1], img: products_images[rand(0..2)])
+end
+puts "3 products created."
