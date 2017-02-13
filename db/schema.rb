8dddce13777c2e4931bc2e8f5cef9d85c9b207cf
meganho <bbeeaanns@gmail.com>
2017-02-13 22:50:02 +0100

    add payment_method fuction

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208215318) do
+ActiveRecord::Schema.define(version: 20170213213927) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20170208215318) do
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.string   "token"
+    t.string   "payment_method"
   end
 
   create_table "product_lists", force: :cascade do |t|
