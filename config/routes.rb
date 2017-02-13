522015b3f7e60e72b303351047320fe160ef29c3
meiputi <meiputi@gmail.com>
2017-02-13 23:08:28 +0800

    add checkout page

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
 
   resources :cart_items
 
+  resources :orders
+
   devise_for :users
   root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
