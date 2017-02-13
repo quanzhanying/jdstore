af4e08801fa34cc84df140d69d27a5b6c2a37c6e
junoliang <junoliang007@gmail.com>
2017-02-13 22:07:50 +0800

    modify login and sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
     root 'welcome#index'
-    devise_for :users
+    devise_for :users, controllers: { sessions: 'users/sessions' }
 
     namespace :admin do
         resources :products
