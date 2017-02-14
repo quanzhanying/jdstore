926567a4dae2c30aca51648337054673d86217d5
runpee <992142460@qq.com>
2017-02-14 22:21:48 +0800

    add payment_method fuction

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,12 @@ Rails.application.routes.draw do
     resources :orders
   end
 
-  resources :orders
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+    end
+  end
   resources :cart_items
 
 end
