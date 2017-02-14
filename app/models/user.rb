7aa669b448256bb7012c15b1d25a955962907c0b
chris1203 <liuhuiling1203@gmail.com>
2017-02-15 01:45:51 +0800

    add favorite function

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,10 @@ class User < ApplicationRecord
   has_attached_file :avatar, :styles => { :medium => "100x100>", :thumb => "30x30#" }
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   has_many :reviews, dependent: :destroy
+  has_many :favorites
+  has_many :favorite_products, :through => :favorites, :source => :product
+  def is_fan_of?(group)
+    favorite_products.include?(group)
+  end
+  
 end
