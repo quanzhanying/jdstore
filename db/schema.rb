b46458951d8418c551726857bf5efb42d43c4ba3
chris1203 <liuhuiling1203@gmail.com>
2017-02-14 17:59:05 +0800

    change config/database.yml.example to config/database.yml and chris's admin user account

--- db/schema.rb
+++ db/schema.rb
@@ -84,7 +84,6 @@ ActiveRecord::Schema.define(version: 20170214004609) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "is_admin",               default: false
-    t.integer  "user_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
