95371ca86c53fa409910e762419a22b07c742d74
apple <apple@appledeMacBook-Pro.local>
2017-02-14 11:23:57 +0800

    implement admin/orders show function

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,4 +8,9 @@ class Admin::OrdersController < ApplicationController
   def index
     @orders = Order.order("id DESC")
   end
+
+  def show
+    @order = Order.find(params[:id])
+    @product_lists = @order.product_lists
+  end
 end
