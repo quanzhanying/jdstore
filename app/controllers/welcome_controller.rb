89de9a1ce056c20fea0cb17749495da54d98c13f
junoliang <junoliang007@gmail.com>
2017-02-15 00:21:31 +0800

    add head info to html files

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-    def index
-        flash[:notice] = "Welcome to LOVE&GIFT.  Happy Valentine's Day."
-    end
+    # def index
+    #     flash[:notice] = "Welcome to LOVE&GIFT.  Happy Valentine's Day."
+    # end
 end
