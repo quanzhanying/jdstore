b26338956d3c365c2139cc0b67147cf076d5a3be
os <kqkq1212@aliyun.com>
2017-02-14 10:05:11 +0800

    nice

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'products#index'
+  root 'welcome#index'
   resources :products do
     member do
       post :add_to_cart
