2be1e8d2e3f55f5c73615e56988157447ea6933e
“将叫我将军” <d2194499406@gmail.com>
2017-02-14 22:26:39 +0800

    install fog

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -14,7 +14,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_fill: [800,800]  
+  process resize_to_fill: [800,800]
 
   version :thumb do
     process resize_to_fill: [200,200]
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,23 @@
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV["AKIAJXMYA22FVX6OLASQ"],      # 你的 key
+
+
+      aws_secret_access_key: ENV["r0+tD8b+fJQr8c3UC7GoJVyrX058Hoq32wXVSmMY"],      # 你的 secret key
+
+
+      region:                'ap-northeast-1' # 你的 S3 bucket 的 Region 位置
+
+
+    }
+    config.fog_directory  = ENV["fullstack-dong"] # 你设定的 bucket name
+
+
+
+  else
+    config.storage :file
+  end
+end
