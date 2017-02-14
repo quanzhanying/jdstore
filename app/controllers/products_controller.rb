46f6791873be19966a9f6b2cc39a18b1f4cc6a3a
shijunfeng <johnshiluck@gmail.comgit>
2017-02-14 20:42:07 +0800

    implement current_cart function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,6 @@ class ProductsController < ApplicationController
     def add_to_cart
       @product = Product.find(params[:id])
       current_cart.add_product_to_cart(@product)
-      redirect_to :back
-      
+      redirect_to :back      
     end
 end
