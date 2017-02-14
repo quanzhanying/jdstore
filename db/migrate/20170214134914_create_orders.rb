b98ec9eec8e55995b0c69c2bb93178560e6151e7
罗心 <xiaoxindihua@hotmail.com>
2017-02-14 22:21:05 +0800

    add product_list model

--- db/migrate/20170214134914_create_orders.rb
+++ db/migrate/20170214134914_create_orders.rb
@@ -1,14 +1,12 @@
 class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
-        t.integer :total, default: 0
-        t.integer :user_id
-        t.integer :billing_name
-        t.integer :billing_adress
-        t.integer :shipping_name
-        t.integer :shipping_adress
-        t.timestamps
-
+       t.integer :total, default: 0
+       t.integer :user_id
+       t.string :billing_name
+       t.string :billing_address
+       t.string :shipping_name
+       t.string :shipping_address
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214134914) do
+ActiveRecord::Schema.define(version: 20170214141839) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -26,14 +26,23 @@ ActiveRecord::Schema.define(version: 20170214134914) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer  "total",           default: 0
+    t.integer  "total",            default: 0
     t.integer  "user_id"
-    t.integer  "billing_name"
-    t.integer  "billing_adress"
-    t.integer  "shipping_name"
-    t.integer  "shipping_adress"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.string   "billing_name"
+    t.string   "billing_address"
+    t.string   "shipping_name"
+    t.string   "shipping_address"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+  end
+
+  create_table "product_lists", force: :cascade do |t|
+    t.integer  "order_id"
+    t.string   "product_name"
+    t.integer  "product_price"
+    t.integer  "quantity"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "products", force: :cascade do |t|
