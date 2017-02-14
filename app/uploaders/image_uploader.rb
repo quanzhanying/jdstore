bb590e0b9766340cc8af5fa3bdb360649998988d
linfgit <linfgg1@gmail.com>
2017-02-14 12:20:47 +0800

    debug

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,11 +7,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   #storage :file
-  if Rails.env.production?
-   storage :fog
-   else
+
    storage :file
-   end
+
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
