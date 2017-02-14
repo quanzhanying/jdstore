7341841d3982f0444b86409a587a46a743d07d1c
zhongjie <zhongjie10@gmail.com>
2017-02-14 23:05:50 +0800

    add aasm_state to order

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'awesome_rails_console'
 gem 'mini_magick'
 gem "rails-erd"
 gem 'letter_opener', group: :development
+gem 'aasm'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,7 +16,7 @@ class OrdersController < ApplicationController
         product_list.save
       end
       current_cart.clean!
-      OrderMailer.notify_order_placed(@order).deliver!
+      #OrderMailer.notify_order_placed(@order).deliver!
 
       redirect_to order_path(@order.token)
     else
@@ -32,7 +32,7 @@ class OrdersController < ApplicationController
   def pay_with_alipay
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("alipay")
-    @order.pay!
+    @order.make_payment!
 
     redirect_to order_path(@order.token), notice: "使用支付宝成功完成付款"
   end
@@ -40,7 +40,7 @@ class OrdersController < ApplicationController
   def pay_with_wechat
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("wechat")
-    @order.pay!
+    @order.make_payment!
 
     redirect_to order_path(@order.token), notice: "使用微信支付功完成付款"
   end
