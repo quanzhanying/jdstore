2b8437d3c2f4ceca603b3d3b6b7dd89677bd1ad2
你的名字 <email>
2017-02-14 14:54:23 +0800

    add OrderMailer function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,6 +16,8 @@ class OrdersController < ApplicationController
         product_list.quantity = cart_item.quantity
         product_list.save
       end
+      current_cart.clean!
+      OrderMailer.notify_order_placed(@order).deliver!
 
       redirect_to order_path(@order.token)
     else
