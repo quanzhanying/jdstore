00a8957978847e475582bd295259e74cc3d9bf88
linjiaxin <linjiaxin405@gmail.com>
2017-02-15 15:32:49 +0800

    add aasm_state to order

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'will_paginate'
 gem 'ransack'
 gem 'seo_helper'
 gem 'letter_opener', group: :development
+gem 'aasm'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -19,9 +19,8 @@ class OrdersController < ApplicationController
         product_list.quantity = cart_item.quantity
         product_list.save
       end
-      current_cart.clean!
-      OrderMailer.notify_order_placed(@order).deliver!
-      
+
+
       redirect_to order_path(@order.token)
     else
       render 'carts/checkout'
@@ -31,16 +30,18 @@ class OrdersController < ApplicationController
   def pay_with_alipay
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("alipay")
-    @order.pay!
-
+    @order.make_payment!
+    current_cart.clean!
+    OrderMailer.notify_order_placed(@order).deliver!
     redirect_to order_path(@order.token), notice: "使用支付宝成功完成付款"
   end
 
   def pay_with_wechat
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("Wechat")
-    @order.pay!
-
+    @order.make_payment!
+    current_cart.clean!
+    OrderMailer.notify_order_placed(@order).deliver!
     redirect_to order_path(@order.token), notice: "使用微信支付成功完成付款"
   end
   private
