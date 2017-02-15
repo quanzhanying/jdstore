5098f03d224107a92caf3bf248d496e48003f1d8
罗心 <xiaoxindihua@hotmail.com>
2017-02-15 12:17:44 +0800

    install fog

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -22,7 +22,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   version :medium do
       process resize_to_fill: [400, 400]
   end
-  
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
