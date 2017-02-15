bc2e7a4d227390f2e498a3852a26e86622eb56b2
kaihe1989@gmail.com <hekai@HekaitekiMacBook-Pro.local>
2017-02-15 13:10:07 -0500

    touch app/views/carts/checkout.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -15,10 +15,10 @@ Rails.application.routes.draw do
   resources :carts do
     collection do
       delete :clean
-        post :checkout 
+        post :checkout
     end
   end
 
   resources :cart_items
-
+  resources :orders 
 end
