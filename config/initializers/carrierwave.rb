fad2974eddbf12f30564253d7aa3bbccbabf6f74
andy2000xyz <andy2000xyz@hotmail.com>
2017-02-15 14:37:01 +0800

    modify carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,17 +6,17 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV["AKIAIIRDV4223L5RC3RQ"],      # 你的 key
+      aws_access_key_id:     ENV["S3_KEY"],      # 你的 key
 
 
-      aws_secret_access_key: ENV["UnN5/lDKPPdQNy1CMURf+m4VOCcpBaJXrzFnCsIj"],      # 你的 secret key
+      aws_secret_access_key: ENV["S3_SECRET"],      # 你的 secret key
 
 
-      region:                'ap-northeast-1' # 你的 S3 bucket 的 Region 位置
+      region:                ENV['S3_REGION'] # 你的 S3 bucket 的 Region 位置
 
 
     }
-    config.fog_directory  = ENV["fullstack-wx"] # 你设定的 bucket name
+    config.fog_directory  = ENV["S3_BUCKET_NAME"] # 你设定的 bucket name
 
 
 
