f2532bc90113dba45b17d1987684b72f436aa285
charvine <crossovermeister@gmail.com>
2017-02-15 22:16:00 +0800

    add OrderMailer function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -19,6 +19,10 @@ class OrdersController < ApplicationController
       product_list.quantity = cart_item.quantity
       product_list.save
       end
+
+      current_cart.clean!
+      OrdermailerMailer.notify_order_placed(@order).deliver!
+
       redirect_to order_path(@order.token)
     else
     render 'carts/checkout'
---
+++ test/mailers/previews/ordermailer_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/ordermailer_mailer
+class OrdermailerMailerPreview < ActionMailer::Preview
+
+end
