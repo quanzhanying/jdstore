43916c0ba13f68a2a019a9ab5362b2aabbcd92b7
os <kqkq1212@aliyun.com>
2017-02-15 14:04:33 +0800

    add apply_cancel function

--- app/controllers/account/orders_controller.rb
+++ app/controllers/account/orders_controller.rb
@@ -2,11 +2,11 @@ class Account::OrdersController < ApplicationController
   before_action :authenticate_user!
 
      def index
-       if !current_user.admin?
+      #  if !current_user.admin?
          @orders = current_user.orders.order("id DESC")
 
-       else
-          @orders = Order.order("id DESC")
-        end
+      #  else
+          # @orders = Order.order("id DESC")
+        # end
      end
    end
