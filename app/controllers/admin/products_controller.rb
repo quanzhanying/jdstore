2a8dcf081b3d4a80a5e33da3b4fbf6119eb2e340
chris1203 <liuhuiling1203@gmail.com>
2017-02-15 08:33:03 +0800

    product show image

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -42,8 +42,15 @@ class Admin::ProductsController < ApplicationController
 
   def update
     @product = Product.find(params[:id])
-    if @product.update(product_params)
-      redirect_to admin_products_path, notice: "Update Success"
+    if params[:photos] != nil
+      @product.photos.destroy_all
+      params[:photos]['image'].each do |a|
+        @picture = @product.photos.create(:image => a)
+      end
+      @product.update(product_params)
+      redirect_to admin_products_path
+    elsif @product.update(product_params)
+      redirect_to admin_products_path
     else
       render :edit
     end
