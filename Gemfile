cc9908e2398899b8dec35b054d46c40578525575
bianhaiming <bianhaiming@gmail.com>
2017-02-16 05:42:07 +0800

    add admin/orders

--- Gemfile
+++ Gemfile
@@ -34,7 +34,9 @@ gem 'simple_form'
 gem 'font-awesome-rails'
 gem 'carrierwave'
 gem 'mini_magick'
-gem 'letter_opener', group: :development 
+gem 'letter_opener', group: :development
+gem 'aasm'
+
 
 
 group :development, :test do
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'products#index'
   namespace :admin do
     resources :products
+    resources :orders
   end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
