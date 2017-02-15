1ef0ee03e9128cff19c2337db5197ead37f5a0a2
Gh72 <cao.wang.work@foxmail.com>
2017-02-15 21:31:23 +0800

    add aasm_state to order

--- db/migrate/20170215122955_add_aasm_state_to_order.rb
+++ db/migrate/20170215122955_add_aasm_state_to_order.rb
@@ -1,6 +1,6 @@
 class AddAasmStateToOrder < ActiveRecord::Migration[5.0]
   def change
-    add_column :orders, :assm_state, :string, default: "order_placed"
-    add_index :orders, :assm_state
+    add_column :orders, :aasm_state, :string, default: "order_placed"
+    add_index :orders, :aasm_state
   end
 end
