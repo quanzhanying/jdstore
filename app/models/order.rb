8ebbb1f71030ec9e10ffcccf8c234374fb052109
tuantuan <lisuji1986@gmail.com>
2017-02-15 17:25:03 +0800

    add aasm_state to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :user
   before_create :generate_token
+  include AASM
 
   def generate_token
     self.token = SecureRandom.uuid
@@ -21,4 +22,31 @@ class Order < ApplicationRecord
      self.update_columns(is_paid: true )
    end
 
+   aasm do
+     state :order_placed, initial: true
+     state :paid
+     state :shipping
+     state :shipped
+     state :order_cancelled
+     state :good_returned
+
+     event :make_payment, after_commit: :pay! do
+       transitions from: :order_placed, to: :paid
+     end
+
+     event :ship do
+       transitions from: :shipping, to: :shipping
+     end
+     event :deliver do
+      transitions from: :shipping,     to: :shipped
+    end
+
+    event :return_good do
+      transitions from: :shipped,      to: :good_returned
+    end
+
+    event :cancell_order do
+      transitions from: [:order_placed, :paid], to: :order_cancelled
+    end
+   end
 end
