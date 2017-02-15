36dc50dc1bb6d4a02415ac1b5243d3129d920219
apple <apple@appledeMacBook-Pro.local>
2017-02-15 15:46:57 +0800

    change s3 region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,14 +9,12 @@ CarrierWave.configure do |config|
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],      # 你的 secret key
 
 
-      region:                'eu-west-1' # 你的 S3 bucket 的 Region 位置
+      region:                'ap-northeast-1' # 你的 S3 bucket 的 Region 位置
 
 
     }
     config.storage :fog
     config.fog_directory  = ENV["AWS_BUCKET_NAME"] # 你设定的 bucket name
-
-
 
   else
     config.storage :file
