6bb7c58606962402ad49335d71fd74eba60021a5
“将叫我将军” <d2194499406@gmail.com>
2017-02-15 15:05:46 +0800

    the ninth required arguments debug

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,13 +5,13 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.root = Rails.root.join('tmp') # adding these...
     config.cache_dir = 'carrierwave' # ...two lines
-    
+
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV["AKIAJXMYA22FVX6OLASQ"],      # 你的 key
+      aws_access_key_id:     ENV['AKIAJXMYA22FVX6OLASQ'],      # 你的 key
 
 
-      aws_secret_access_key: ENV["r0+tD8b+fJQr8c3UC7GoJVyrX058Hoq32wXVSmMY"],      # 你的 secret key
+      aws_secret_access_key: ENV['r0+tD8b+fJQr8c3UC7GoJVyrX058Hoq32wXVSmMY'],      # 你的 secret key
 
 
       region:                'ap-northeast-1' # 你的 S3 bucket 的 Region 位置
@@ -19,7 +19,7 @@ CarrierWave.configure do |config|
 
     }
 
-    config.fog_directory  = ENV["fullstack-dong"] # 你设定的 bucket name
+    config.fog_directory  = ENV['fullstack-dong'] # 你设定的 bucket name
     config.storage :fog
 
 
