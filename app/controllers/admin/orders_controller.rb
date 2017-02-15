b8193646c857b0e1d1b09a876a9483d92bc5bcc5
taketimeasafriend <taketimeasafriend@gmail.com>
2017-02-15 14:10:25 +0800

    add ship/shipped/cancel/return action to admin/orders_controller

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,7 +2,7 @@ class Admin::OrdersController < ApplicationController
     layout 'admin'
 
     before_action :authenticate_user!
-    before_action :admin_required
+    before_action :require_is_admin
 
     def index
         @orders = Order.order('id DESC')
@@ -11,5 +11,29 @@ class Admin::OrdersController < ApplicationController
     def show
         @order = Order.find(params[:id])
         @product_lists = @order.product_lists
+    end
+
+    def ship
+        @order = Order.find(params[:id])
+        @order.ship!
+        redirect_to :back
+    end
+
+    def shipped
+        @order = Order.find(params[:id])
+        @order.deliver!
+        redirect_to :back
+    end
+
+    def cancel
+        @order = Order.find(params[:id])
+        @order.cancell_order!
+        redirect_to :back
+    end
+
+    def return
+        @order = Order.find(params[:id])
+        @order.return_good!
+        redirect_to :back
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,44 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #root 'products#index'
-  root 'welcome#index'
+    devise_for :users
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # root 'products#index'
+    root 'welcome#index'
 
-
-  namespace :admin do
-    resources :products
-    resources :orders
-  end
-
-  resources :products do
-    member do
-      post :add_to_cart
+    namespace :admin do
+        resources :products
+        resources :orders do
+            member do
+                post :cancel
+                post :ship
+                post :shipped
+                post :return
+            end
+        end
     end
-  end
 
-  resources :carts do
-    collection do
-      delete :clean
-      post :checkout
+    resources :products do
+        member do
+            post :add_to_cart
+        end
     end
-  end
 
-  resources :cart_items
-
-  resources :orders do
-    member do
-        post :pay_with_alipay
-        post :pay_with_wechat
+    resources :carts do
+        collection do
+            delete :clean
+            post :checkout
+        end
     end
-  end
 
-  namespace :account do
-    resources :orders
-  end
+    resources :cart_items
+
+    resources :orders do
+        member do
+            post :pay_with_alipay
+            post :pay_with_wechat
+        end
+    end
+
+    namespace :account do
+        resources :orders
+    end
 end
