2d66d81116a7045a60cfb8bf5c934d1f7b3aee4e
os <kqkq1212@aliyun.com>
2017-02-15 13:47:15 +0800

    implement admin/orders show function

--- app/controllers/account/orders_controller.rb
+++ app/controllers/account/orders_controller.rb
@@ -2,6 +2,11 @@ class Account::OrdersController < ApplicationController
   before_action :authenticate_user!
 
      def index
-        @orders = Order.order("id DESC")
+       if !current_user.admin?
+         @orders = current_user.orders.order("id DESC")
+
+       else
+          @orders = Order.order("id DESC")
+        end
      end
    end
