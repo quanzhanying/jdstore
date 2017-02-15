2d13b8af7b831a721d12c33562194e987f00723a
bob <bao309@163.com>
2017-02-15 20:52:17 +0800

    add admin/orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,43 @@
 class Order < ApplicationRecord
   before_create :generate_token
   belongs_to :user
-has_many :product_lists
+  has_many :product_lists
 
     validates :billing_name, presence: true
     validates :billing_address, presence: true
     validates :shipping_name, presence: true
    validates :shipping_address, presence: true
+   include AASM
+
+     aasm do
+       state :order_placed, initial: true
+       state :paid
+       state :shipping
+       state :shipped
+       state :order_cancelled
+       state :good_returned
+
+
+         event :make_payment, after_commit: :pay! do
+         transitions from: :order_placed, to: :paid
+       end
+
+       event :ship do
+         transitions from: :paid,         to: :shipping
+       end
+
+       event :deliver do
+         transitions from: :shipping,     to: :shipped
+       end
+
+       event :return_good do
+         transitions from: :shipped,      to: :good_returned
+       end
+
+       event :cancell_order do
+         transitions from: [:order_placed, :paid], to: :order_cancelled
+       end
+     end
 
   def generate_token
     self.token = SecureRandom.uuid
@@ -19,34 +50,3 @@ has_many :product_lists
    self.update_columns(is_paid: true )
  end
 end
-include AASM
-
-  aasm do
-    state :order_placed, initial: true
-    state :paid
-    state :shipping
-    state :shipped
-    state :order_cancelled
-    state :good_returned
-
-
-      event :make_payment, after_commit: :pay! do
-      transitions from: :order_placed, to: :paid
-    end
-
-    event :ship do
-      transitions from: :paid,         to: :shipping
-    end
-
-    event :deliver do
-      transitions from: :shipping,     to: :shipped
-    end
-
-    event :return_good do
-      transitions from: :shipped,      to: :good_returned
-    end
-
-    event :cancell_order do
-      transitions from: [:order_placed, :paid], to: :order_cancelled
-    end
-  end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 namespace :admin do
   resources :products
+  resources :orders
 
 end
 namespace :account do
