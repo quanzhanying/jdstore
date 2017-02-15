2e1c112f36b8ba93b74544db305bef38182547b5
mayue <mayue@mayuedeMacBook-Air.local>
2017-02-15 15:33:42 +0800

    add add_to_cart button

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   root 'products#index'
   devise_for :users
   namespace :admin do
-    resources :products
+  end
+    resources :products do
+      member do
+        post :add_to_cart
+      end
   end
 
   resources :products
+
+
 end
