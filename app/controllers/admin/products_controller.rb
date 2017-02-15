8820fb965cd942951d2990de1eb5c48651108f10
kai <328458211@qq.com>
2017-02-15 16:04:32 +0800

    revision product index

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -36,11 +36,11 @@ class Admin::ProductsController < ApplicationController
 
   end
 
-#  def destroy
-#    @product = Product.find(params[:id])
-#    @product.destroy
-#    redirect_to admin_products_path, alert: "Product Deleted!"
-#  end
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to admin_products_path, alert: "Product Deleted!"
+  end
 
 
 
