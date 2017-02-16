3e03fefb62d4b52a38b20860f966f48eedcaff1a
huangqiaoming <carolinehuang.aiesec@gmail.com>
2017-02-16 14:40:46 +0800

    add admin/orders

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ root 'products#index'
 devise_for :users
   namespace :admin do
     resources :products
+    resources :orders
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
