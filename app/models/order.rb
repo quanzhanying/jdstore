c30dd2544eaec12db6069720848d88a2780b80a8
leichaofan <leichaofan2024@gmail.com>
2017-02-16 23:22:07 +0800

    支付方式，订单确认信

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,13 @@ class Order < ApplicationRecord
   def generate_token
     self.token = SecureRandom.uuid
   end
+  def set_payment_with!(method)
+    self.update_columns(payment_method: method)
+  end
+  def pay!
+    self.update_columns(is_paid: true)
+
+  end
 
     belongs_to :user
 
