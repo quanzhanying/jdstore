3985d100b32535800a696881baf08f8b33d89945
apple <apple@appledeMacBook-Pro.local>
2017-02-16 22:19:29 +0800

    add banner

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,10 @@
 //= require turbolinks
 //= require bootstrap/dropdown
 //= require bootstrap/alert
+//= require bootstrap/carousel
 //= require_tree .
+
+// 首页轮播
+$(document).ready(function() {
+    $('#myCarousel').carousel({interval: 3000})
+});
