330396b30eca18069c551707baa1123eeeb982c5
Johnson Liang <johnson.liangxinzhong@gmail.com>
2017-02-16 18:18:48 +0800

    add aasm_state to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,11 +5,11 @@ class Order < ActiveRecord::Base
   end
 
   def set_payment_with!(method)
-    self.update_columns(payment_method: method )
+    update_columns(payment_method: method)
   end
 
   def pay!
-    self.update_columns(is_paid: true)
+    update_columns(is_paid: true)
   end
   belongs_to :user
   validates :billing_name, presence: true
@@ -17,4 +17,30 @@ class Order < ActiveRecord::Base
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
   has_many :product_lists
+  include AASM
+
+  aasm do
+    state :order_placed, initial: true
+    state :paid
+    state :shipping
+    state :shipped
+    state :order_cancelled
+    state :good_returned
+
+    event :make_payment, after_commit: :pay! do
+      transitions from: :order_placed, to: :paid
+    end
+    event :ship do
+      transitions from: :paid, to: :shipping
+    end
+    event :deliver do
+      transitions from: :shipping, to: :shipped
+    end
+    event :return_good do
+      transitions from: :shipped, to: :returned
+    end
+    event :cancell_order do
+      transitions from: [:order_placed, :paid], to: :order_cancelled
+    end
+  end
 end
