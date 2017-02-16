79a161ba174c67dc39a8d324489840efec07b576
daijianlin <daijianlin@163.com>
2017-02-16 15:07:33 +0800

    change the orders_controller end

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user! only: [:create]
+  before_action :authenticate_user!, only: [:create]
   def create
     @order = Order.new(order_params)
     @order.user = current_user
