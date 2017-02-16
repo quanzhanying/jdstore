aa56e3885cbeecc18b6f3ef2c039de843e163eb5
yangjitang77 <yangjitang77@gmail.com>
2017-02-16 11:35:10 +0800

    add add_to_cart button

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
 root 'products#index'
    namespace :admin do
-   resources :products
+
+      resources :products do
+         member do
+           post :add_to_cart
+         end
+       end
+
+
  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
