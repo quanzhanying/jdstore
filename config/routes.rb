d3b014efaaa7029a185e22255e0bc3492f847e06
megy <megy@megys-MacBook-Pro.local>
2017-02-16 13:22:57 +0800

    add payment_method fuction

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,12 @@ Rails.application.routes.draw do
 
   resources :cart_items
 
-  resources :orders
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+    end
+  end
 
   namespace :account do
     resources :orders
