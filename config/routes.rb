97bcff56d6f00afba75af3b8920742332f31ec28
linfgit <linfgg1@gmail.com>
2017-02-16 09:57:44 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+  resources :products
+
   namespace :admin do
     resources :products
   end
-  
+
   devise_for :users
-  root 'welcome#index'
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
