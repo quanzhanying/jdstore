4f7d173d3e9e5f7652b51d8200f296ec6258ecaf
huazai <470021389@qq.com>
2017-02-16 17:34:25 +0800

    add mailer to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_mailer.default_url_options = { host: 'desolate-caverns-98949.herokuapp.com'}
+
 end
