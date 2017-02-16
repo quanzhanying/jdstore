af2e31bcf340c9fab09564b6e9cbf3b623567787
charvine <crossovermeister@gmail.com>
2017-02-16 21:27:10 +0800

    add admin/orders

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
 
  root 'products#index'
   devise_for :users
+
   namespace :admin do
     resources :products
+    resources :orders
   end
 
   resources :products do
