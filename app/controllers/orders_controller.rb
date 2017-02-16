576afec0948baa5f07b774e334efb2c8253781a4
donggegithub <shxd_2000@163.com>
2017-02-16 19:51:22 +0800

    modify wechat fuction

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -35,7 +35,7 @@ class OrdersController < ApplicationController
     redirect_to order_path(@order.token), notice: "使用支付宝完成付款"
   end
 
-  def pay_with_webchat
+  def pay_with_wechat
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("wechat")
     @order.pay!
