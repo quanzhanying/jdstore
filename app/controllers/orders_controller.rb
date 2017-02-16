6c14e518f7d32e6c4c2deca19414ffb647766733
huangqiaoming <carolinehuang.aiesec@gmail.com>
2017-02-16 14:27:37 +0800

    add aasm_state to order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -30,7 +30,8 @@ end
 def pay_with_alipay
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("alipay")
-    @order.pay!
+    @order.make_payment!
+
 
     redirect_to order_path(@order.token), notice: "使用支付宝成功完成付款"
   end
@@ -38,7 +39,8 @@ def pay_with_alipay
   def pay_with_wechat
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("wechat")
-    @order.pay!
+    @order.make_payment!
+
 
     redirect_to order_path(@order.token), notice: "使用微信支付成功完成付款"
   end
