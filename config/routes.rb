581d18da4f5a129fe6db61d1ec67b32e34683ba8
isaac <237957459@qq.com>
2017-02-16 22:04:25 +0800

    add payment_method fuction

--- config/routes.rb
+++ config/routes.rb
@@ -22,9 +22,14 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  resources :orders
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+    end
+  end
 
   namespace :account do
-    resources :orders 
+    resources :orders
   end
 end
