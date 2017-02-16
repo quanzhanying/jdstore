921e8526079db14a12370eab9e1d5def5357e927
tuantuan <lisuji1986@gmail.com>
2017-02-16 19:20:23 +0800

    add aasm_state to order/ update database and fixed 12.12 bug

--- app/models/order.rb
+++ app/models/order.rb
@@ -23,21 +23,23 @@ class Order < ApplicationRecord
    end
 
    aasm do
-     state :order_placed, initial: true
-     state :paid
-     state :shipping
-     state :shipped
-     state :order_cancelled
-     state :good_returned
+    state :order_placed, initial: true
+    state :paid
+    state :shipping
+    state :shipped
+    state :order_cancelled
+    state :good_returned
 
-     event :make_payment, after_commit: :pay! do
-       transitions from: :order_placed, to: :paid
-     end
 
-     event :ship do
-       transitions from: :shipping, to: :shipping
-     end
-     event :deliver do
+    event :make_payment, after_commit: :pay! do
+      transitions from: :order_placed, to: :paid
+    end
+
+    event :ship do
+      transitions from: :paid,         to: :shipping
+    end
+
+    event :deliver do
       transitions from: :shipping,     to: :shipped
     end
 
@@ -48,5 +50,6 @@ class Order < ApplicationRecord
     event :cancell_order do
       transitions from: [:order_placed, :paid], to: :order_cancelled
     end
-   end
+  end
+
 end
