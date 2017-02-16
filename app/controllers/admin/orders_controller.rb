81a497d5fafe4a1dafe2373c70adbc3ba1142559
sujiang <jimmysu1991@gmail.com>
2017-02-16 10:55:25 +0800

    add ship/shipped/cancel/return action to admin/orders_controller and add admin/orders/state_option button

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,7 +8,7 @@ class Admin::OrdersController < ApplicationController
     @orders = Order.order("id DESC")
   end
 
-  def Show
+  def show
     @order = Order.find(params[:id])
     @product_lists = @order.product_lists
   end
