4b150722c17d59481a2d82ff3ce1048ac5ede38c
kai <328458211@qq.com>
2017-02-16 18:52:23 +0800

    implement delete one cart_item Function

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,10 @@ Rails.application.routes.draw do
       post :add_to_cart
     end
   end
-  resources :carts
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end
+  resources :cart_items
 end
