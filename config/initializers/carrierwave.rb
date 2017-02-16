1241af74ab824aced6b8542685bca21a11124105
weiyueyan <xiaoguaishou0809@gmail.com>
2017-02-16 22:36:17 +0800

    install fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,14 +3,14 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV["AKIAI2UKHWBJLVXFRMSQ"],
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
 
-      aws_secret_access_key: ENV["LWPMCElZ3b/hDa3C3aANgbnltazjWM/mVNcsHIVa"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
 
-      region:                ENV["ap-northeast-1"]
+      region:                ENV["AWS_REGION"]
 
     }
-    config.fog_directory  = ENV["jdstore2"] 
+    config.fog_directory  = ENV["AWS_BUCKET_NAME"]
 
 
   else
