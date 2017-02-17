8722fddd68b1453117f327e589e5b1e91ef31a45
mayue <mayue@mayuedeMacBook-Air.local>
2017-02-17 10:09:23 +0800

    touch app/views/carts/checkout.html.erb

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,6 +2,10 @@ class CartsController < ApplicationController
   def clean
      current_cart.clean!
      flash[:warning] = "已清空购物车"
-     redirect_to carts_path    
+     redirect_to carts_path
+  end
+
+  def checkout
+     @order = Order.new    
   end
 end
