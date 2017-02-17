9c7276b3e91aa86ee893326b02cb11161140638d
你的名字 <ksalsqzhbl@aliyun.com>
2017-02-17 11:36:57 +0800

    set config.assets

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+config.assets.initialize_on_precompile = false
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
