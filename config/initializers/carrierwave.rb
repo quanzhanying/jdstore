7b4b014bc811305c711f44d03df06d95feae3276
Qingqing <renwq8@126.com>
2017-02-17 12:40:58 +0800

    delete lib/fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,3 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   if Rails.env.production?
--- lib/fog.rb
+++
@@ -1,3 +0,0 @@
-Module Fog
-
-end
