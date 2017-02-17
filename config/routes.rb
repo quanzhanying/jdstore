ec7f74dc73ecf4c31441fdc0bad84c9074d76fb2
jay.g <jkland927@hotmail.com>
2017-02-17 22:37:18 +0800

    add apply_cancel function

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
          post :shipped
          post :return
        end
-   end  
+   end
   end
 
   resources :products do
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
     member do
       post :pay_with_alipay
       post :pay_with_wechat
+      post :apply_to_cancel
     end
   end
 
