577cf0b858adc1f500e5cc2d0cf98daad8bfe8ae
Johnson Liang <johnson.liangxinzhong@gmail.com>
2017-02-17 20:55:20 +0800

    add notify_cancel/ship function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery 
 
   def admin_required
-    redirect_to '/' unless current_user.admin?
+    if !current_user.admin?
+      redirect_to "/", alert: "You are not admin."
   end
+end
 
   helper_method :current_cart
   def current_cart
