e24a6cc98dbbc9efb493521ffc00798a57c948b9
YannaMA <helenmayanna@gmail.com>
2017-02-17 18:02:13 +0800

    can change quantity

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -13,7 +13,7 @@ class CartItemsController < ApplicationController
 
   def update
     @cart = current_cart
-    @cart_item = @cart.cart_item.find_by(product_id: params[:id])
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
     @cart_item.update(cart_item_params)
 
     redirect_to carts_path
