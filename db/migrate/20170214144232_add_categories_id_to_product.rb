b5c3f23b723fe9de607ba97d41db1081c182a6dd
你的名字 <ksalsqzhbl@aliyun.com>
2017-02-17 12:45:33 +0800

    delete categories db

--- db/migrate/20170214144232_add_categories_id_to_product.rb
+++
@@ -1,6 +0,0 @@
-class AddCategoriesIdToProduct < ActiveRecord::Migration[5.0]
-  def change
-    add_column :products, :categories, :string
-    add_column :products, :stars_number, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214144232) do
+ActiveRecord::Schema.define(version: 20170215134901) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 20170214144232) do
   end
 
   create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -55,11 +60,10 @@ ActiveRecord::Schema.define(version: 20170214144232) do
     t.text     "description"
     t.integer  "quantity"
     t.integer  "price"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "image"
     t.string   "categories"
-    t.integer  "stars_number"
   end
 
   create_table "users", force: :cascade do |t|
