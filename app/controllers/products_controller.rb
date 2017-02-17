573e84e6e5f0e135fa93e167bb4098664a051d7a
bruceqiao <bruceq619@gmail.com>
2017-02-17 11:37:51 +0800

    implement can't add_to_cart under 0 quantity function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,9 +11,15 @@ class ProductsController < ApplicationController
   def add_to_cart
     @product = Product.find(params[:id])
 
-    current_cart.add_product_to_cart(@product)
-    flash[:notice] = "成功加入购物车！"
-    redirect_to :back
+    if @product.quantity != 0
+      current_cart.add_product_to_cart(@product)
+      flash[:notice] = "成功加入购物车！"
+      redirect_to :back
+    else
+      flash[:warning] = "对不起，该商品已经没有库存了。"
+      redirect_to :back
+    end
+
   end
 
 
