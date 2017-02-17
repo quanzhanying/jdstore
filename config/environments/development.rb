e23897e98b2f7073d38ce3a2ebcddffad72d0c3d
你的名字 <ksalsqzhbl@aliyun.com>
2017-02-17 20:09:50 +0800

    sendcloud settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,7 +53,9 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
   # config.action_mailer.delivery_method = :letter_opener
+
   config.action_mailer.delivery_method = :smtp
     ActionMailer::Base.smtp_settings = {
       address: "smtpcloud.sohu.com",
