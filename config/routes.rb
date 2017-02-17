7d9402ce295fe9bb8fa8a23432ebe25c411dc360
noodlescnliu@gmail.com <noodlescnliu@gmail.com>
2017-02-17 19:52:43 +0800

    add add_to_cart button

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :products
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
 end
