162f5b071983125a2bb29d7213f2b1936cec40d7
candy <candycan@163.com>
2017-02-17 21:48:43 +0800

    touch app/views/carts/checkout.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ devise_for :users
   end
 
   resources :cart_items
+  resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
