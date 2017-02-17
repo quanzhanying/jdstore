495002021e9396477b4cf7fe2eddb9bf1b9eca2a
noodlescnliu@gmail.com <noodlescnliu@gmail.com>
2017-02-17 19:31:30 +0800

    fix edit bug && change the footer name

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -20,7 +20,7 @@ class Admin::ProductsController < ApplicationController
   def update
     @product = Product.find(params[:id])
 
-    if product.update(product_params)
+    if @product.update(product_params)
       redirect_to admin_products_path
     else
       render :edit
