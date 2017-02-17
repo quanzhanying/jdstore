675a7d2773274308e7ab54381c645cc3cbd5cd1c
kai <328458211@qq.com>
2017-02-17 16:10:12 +0800

    add order/show

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,6 @@
 class Admin::OrdersController < ApplicationController
   layout "admin"
-  
+
   before_action :authenticate_user!
   before_action :admin_required
 
@@ -8,4 +8,8 @@ class Admin::OrdersController < ApplicationController
     @orders = Order.order("id DESC")
   end
 
+  def show
+    @order = Order.find(params[:id])
+    @product_lists = @order.product_lists
+  end
 end
