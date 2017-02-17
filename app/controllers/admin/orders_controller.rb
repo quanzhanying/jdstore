ef0f1b82ca3fd35fc189a929079cfd662420aec2
os <kqkq1212@aliyun.com>
2017-02-17 16:55:05 +0800

    shaixuan

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,6 +6,32 @@ class Admin::OrdersController < ApplicationController
 
     def index
       @orders = Order.order("id DESC")
+      if params[:total].present?
+        @orders = @orders.where( "total > ?", params[:total])
+      end
+
+
+      if params[:ids].present?
+        @orders = @orders.where(:id => params[:ids].split(","))
+      end
+
+      if params[:date].present?
+        d = Date.parse(params[:date])
+        @orders = @orders.where(:created_at => d.beginning_of_day..d.end_of_day)
+      end
+
+      # if params[:status].present?
+      #   @orders = @orders.where( :aasm_state => params[:status])
+      # end
+
+      if params[:status] == "pending"
+        @orders = @orders.where( :aasm_state => ["paid", "order_placed"])
+      elsif params[:status] == "done"
+        @orders = @orders.where( :aasm_state => ["order_cancelled", "shipping", "shipped"])
+      end
+
+
+    # @orders.paginate(:page => params[:page])
     end
     def show
       @order = Order.find(params[:id])
