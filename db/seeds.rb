d90780fc1cbc6d24c87406085b5fdea41dea9428
littlefat <littlefat.liujingli@gmail.com>
2017-02-17 11:34:20 +0800

    only admin can access backend panel

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u = User.new
+u.email = "littlefat.liujingli@aliyun.com"           # 可以改成自己的 email
+
+u.password = "880812"                # 最少要六码
+
+u.password_confirmation = "880812"   # 最少要六码
+
+u.is_admin = true
+u.save
