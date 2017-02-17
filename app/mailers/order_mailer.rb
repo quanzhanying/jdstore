d5836c604eca0031e6e40d0f28542ec94f6a72e5
Jimmylee <Jimmylee180180@outlook.com>
2017-02-17 15:42:42 +0800

    change an error 'order' to 'user'

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,7 +1,7 @@
 class OrderMailer < ApplicationMailer
     def notify_order_placed(order)
       @order       = order
-      @order       = order.user
+      @user       = order.user
       @product_lists = @order.product_lists
 
       mail(to: @user.email , subject: "[JDstore] 感谢您完成本次的下单，以下是您这次购物明细 #{order.token}")
