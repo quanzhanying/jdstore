81d1827c7c8c9699d0cc00a80cad458a00f0370f
你的名字 <ksalsqzhbl@aliyun.com>
2017-02-17 12:30:17 +0800

    move fog after credantials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.storage :fog
+
     config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
@@ -11,7 +11,7 @@ CarrierWave.configure do |config|
 
     }
     config.fog_directory  = ENV["AWS_BUCKET_NAME"]
-
+   config.storage :fog
 
   else
     config.storage :file
