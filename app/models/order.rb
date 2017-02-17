36244a1248519d81bc7e02394ee1225cf85b3493
tinklexjane <tinklexjane@gmail.com>
2017-02-17 20:52:01 +0800

    fix some bugs

--- app/models/order.rb
+++ app/models/order.rb
@@ -36,7 +36,7 @@ class Order < ApplicationRecord
         end
 
         event :deliver do
-            transitions from: shipping, to: :shipped
+            transitions from: :shipping, to: :shipped
         end
 
         event :return_good do
