17416603f7393c597c7fbce871d3321031b3c344
tinklexjane <tinklexjane@gmail.com>
2017-02-18 00:20:43 +0800

    add admin/orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     namespace :admin do
         resources :products
+        resources :orders
     end
     devise_for :users
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
