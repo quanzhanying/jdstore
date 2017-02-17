a198a6adfafb32387a7775a67a0a8d54dc13feef
你的名字 <email@xxx.xxx>
2017-02-18 01:12:11 +0800

    ‘addall他和things“

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 u = User.new
-u.email = 'admin@test.com'
+u.email = 'huqian19900504@163.com'
 
 u.password = '123456'
 
