41410e80d6615832bd4961d9044d7feb5fb6e9de
yoyo <yuziyao2007@163.com>
2017-02-18 22:09:23 +0800

    add product_list model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+ has_many :orders
          def admin?
     is_admin
-  end 
+  end
 end
