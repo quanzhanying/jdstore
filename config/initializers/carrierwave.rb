274e0152c0b7658d4871e0fe73c3e4ba7df2736c
liulupeng <liulupeng3385@163.com>
2017-02-18 22:50:15 +0800

    fog2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,14 +3,14 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog'
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV["AKIAJILWJQK5CKFNLAGQ"],
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
 
-      aws_secret_access_key: ENV["S2RNiR98ju19a6c33zPQQRRuTIpLdovvS6bjSnio"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
 
-      region:                ENV["ap-northeast-1"]
+      region:                ENV["AWS_REGION"]
 
     }
-    config.fog_directory  = ENV["liulupeng"]
+    config.fog_directory  = ENV["AWS_BUCKET_NAME"]
 
 
   else
