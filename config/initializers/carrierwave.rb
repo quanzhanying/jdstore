6b2e8fac38946aab3ee44b973d40ad37f2c1766a
Gh72 <cao.wang.work@foxmail.com>
2017-02-18 12:30:50 +0800

    fix  storage config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.fog_provider = 'fog'
+    config.storage :fog
     config.fog_credentials = {
         provider:              'AWS',
         aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
