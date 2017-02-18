3bc6aabd528cab4b38dd6081d7871800411c233a
liulupeng <liulupeng3385@163.com>
2017-02-18 15:10:40 +0800

    add apply_cancel function

--- app/controllers/account/orders_controller.rb
+++ app/controllers/account/orders_controller.rb
@@ -4,4 +4,12 @@ class Account::OrdersController < ApplicationController
   def index
     @orders = current_user.orders.order("id DESC")
   end
+
+  def apply_to_cancel
+    @order = Order.find(params[:id])
+    OrderMailer.apply_cancel(@order).deliver!
+    flash[:notice] = "已提交申请"
+    redirect_to :back
+  end
+  
 end
