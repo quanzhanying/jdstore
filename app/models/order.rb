ff3cc7e3f77f5a3e8ff368f796c5f059b53d2be9
AnattaGuo <mengjunkwok@gmail.com>
2017-02-18 11:02:13 +0800

    update payment_method function

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,15 @@
 class Order < ApplicationRecord
   before_create :generate_token
 
+  belongs_to :user
+
+  validates :billing_name, presence: true
+  validates :billing_address, presence: true
+  validates :shipping_name, presence: true
+  validates :shipping_address, presence: true
+
+  has_many :product_lists
+
   def generate_token
     self.token = SecureRandom.uuid
   end
@@ -12,14 +21,5 @@ class Order < ApplicationRecord
   def pay!
     self.update_columns(is_paid: true )
   end
-
-  belongs_to :user
-
-  validates :billing_name, presence: true
-  validates :billing_address, presence: true
-  validates :shipping_name, presence: true
-  validates :shipping_address, presence: true
-
-  has_many :product_lists
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218013749) do
+ActiveRecord::Schema.define(version: 20170218025458) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20170218013749) do
     t.datetime "updated_at",                       null: false
     t.string   "token"
     t.boolean  "is_paid",          default: false
+    t.string   "payment_method"
   end
 
   create_table "product_lists", force: :cascade do |t|
