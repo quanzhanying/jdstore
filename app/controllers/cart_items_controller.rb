d5c87db063bc87840c8984d13510b89f273df13a
bruceqiao <bruceq619@gmail.com>
2017-02-18 23:59:25 +0800

    implement delete cart_item function

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,2 +1,12 @@
 class CartItemsController < ApplicationController
+
+  def destroy
+    @cart_item=current_cart.cart_items.find_by(product_id: params[:id])
+    @product=@cart_item.product
+    @cart_item.destroy
+
+    flash[:warning]="已经将#{@product.title}从购物车中删除！"
+    redirect_to :back
+  end
+
 end
