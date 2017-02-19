ae6b8d924351b67288a26860af72d060331fe265
sunrise <luoruijunblue@hotmail.com>
2017-02-19 19:25:49 +0800

    fix something

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170215143707) do
+ActiveRecord::Schema.define(version: 20170219085047) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20170215143707) do
     t.datetime "updated_at",                                null: false
     t.string   "token"
     t.boolean  "is_paid",          default: false
-    t.         "payment_method"
+    t.string   "payment_method"
     t.string   "aasm_state",       default: "order_placed"
     t.index ["aasm_state"], name: "index_orders_on_aasm_state"
   end
