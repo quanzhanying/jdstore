0c4796e33852dc2467707fcb34dcb70a9a3cb687
shijunfeng <johnshiluck@gmail.comgit>
2017-02-19 21:59:06 +0800

    add ship/shipped/cancel/return action to admin/orders_controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -48,9 +48,35 @@ class OrdersController < ApplicationController
     end
 
 
+
  private
 
   def order_params
     params.require(:order).permit(:billing_name, :billing_address, :shipping_name, :shipping_address)
+  end
+
+
+  def ship
+    @order = Order.find(params[:id])
+    @order.ship!
+    redirect_to :back
+  end
+
+  def shipped
+    @order = Order.find(params[:id])
+    @order.deliver!
+    redirect_to :back
+  end
+
+  def cancel
+    @order = Order.find(params[:id])
+    @order.cancel_order!
+    redirect_to :back
+  end
+
+  def return
+    @order = Order.find(params[:id])
+    @order.return_good!
+    redirect_to :back
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,16 @@ Rails.application.routes.draw do
   root 'products#index'
 
   namespace :admin do
-   resources :products
-   resources :orders
- end
+    resources :products
+    resources :orders do
+    member do
+      post :cancel
+      post :ship
+      post :shipped
+      post :return
+    end
+    end
+  end
 
 
  resources :products do
