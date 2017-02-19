2b486c543963376a9aafc13e448b0edd76173c73
zqz <71264737@qq.com>
2017-02-19 17:02:56 +0800

    add apply_cancel function

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -13,7 +13,7 @@ class OrderMailer < ApplicationMailer
 	@user		= order.user
 	@product_lists    = @order.product_lists
 	
-	 mail(to: "admin@test.com" , subject: "[JDStore] 用户#{order.user.email}申请取消订单 #{order.token}"
+	 mail(to: "admin@test.com" , subject: "[JDStore] 用户#{order.user.email}申请取消订单 #{order.token}")
   end
 
 end
