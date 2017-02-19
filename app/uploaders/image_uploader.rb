85d63d1e0854c737771c71fa332320c95ba6fddd
zhongjie <zhongjie10@gmail.com>
2017-02-19 16:09:28 +0800

    add styles for welcome page

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,7 +17,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [800, 800]
 
   version :thumb do
-    process resize_to_fill: [200, 200]
+    process resize_to_fill: [200, 150]
   end
 
   version :medium do
