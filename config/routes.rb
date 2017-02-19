5b939705ecfaaa425c19b6a19e585aa1029f8e6d
noodlescnliu@gmail.com <noodlescnliu@gmail.com>
2017-02-19 23:41:06 +0800

    add apply_cancel function && add ship/shipped/cancel/return action to admin/orders_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   root 'products#index'
   namespace :admin do
     resources :products
-    resources :orders
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
   end
 
   devise_for :users
@@ -23,6 +30,7 @@ Rails.application.routes.draw do
     member do
       post :pay_with_alipay
       post :pay_with_wechat
+      post :apply_to_cancel
     end
   end
 
