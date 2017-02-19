e9743751e0ed4add9fc0da21a42e80a2c73ee13a
donggegithub <shxd_2000@163.com>
2017-02-19 18:25:29 +0800

    add admin/orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -30,7 +30,8 @@ class OrdersController < ApplicationController
   def pay_with_alipay
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("alipay")
-    @order.pay!
+#    @order.pay!
+    @order.make_payment!
 
     redirect_to order_path(@order.token), notice: "使用支付宝完成付款"
   end
@@ -38,8 +39,9 @@ class OrdersController < ApplicationController
   def pay_with_wechat
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("wechat")
-    @order.pay!
-
+#    @order.pay!
+    @order.make_payment!
+    
     redirect_to order_path(@order.token), notice: "使用微信支付完成付款"
   end
 
