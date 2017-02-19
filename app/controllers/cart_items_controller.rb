f68e96bc05020e2f291214775815d00aa223e341
bruceqiao <bruceq619@gmail.com>
2017-02-19 10:07:43 +0800

    cannot change quantity which exceed stock

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,11 +2,16 @@ class CartItemsController < ApplicationController
 
   def update
     @cart_item=current_cart.cart_items.find_by(product_id: params[:id])
-    if @cart_item.update(cart_item_params)
-      redirect_to carts_path
+
+    if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
+      @cart_item.update(cart_item_params)
+      flash[:notice]="成功更新数量！"
     else
-      redirect_to :back
+      flash[:warning]="数量不足！"
     end
+
+    redirect_to carts_path
+
   end
 
   def destroy
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,12 +11,17 @@ class ProductsController < ApplicationController
   def add_to_cart
     @product = Product.find(params[:id])
 
-    if !current_cart.products.include?(@product)
-      current_cart.add_product_to_cart(@product)
-      flash[:notice] = "成功将#{@product.title}加入购物车！"
-      redirect_to :back
+    if @product.quantity.present? && @product.quantity > 0
+      if !current_cart.products.include?(@product)
+        current_cart.add_product_to_cart(@product)
+        flash[:notice] = "成功将#{@product.title}加入购物车！"
+        redirect_to :back
+      else
+        flash[:notice] = "该商品已经加入购物车了。"
+        redirect_to :back
+      end
     else
-      flash[:notice] = "该商品已经加入购物车了。"
+      flash[:warning] = "该商品已经没有库存了！"
       redirect_to :back
     end
   end
