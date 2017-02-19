961c574ae9f2aca6873946b13ed30ed6604cd2ee
linfgit <linfgg1@gmail.com>
2017-02-19 11:58:46 +0800

    add payment_method function

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -18,10 +18,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-   process resize_to_fit: [800, 800]
+   process resize_to_fit: [400, 400]
 
    version :thumb do
-     process resize_to_fill: [900,600]
+     process resize_to_fill: [400,500]
    end
 
    version :medium do
