624e425a8434432735f94d06c34b4471fc16595e
chentianwei <ctw.3@163.com>
2017-02-19 15:10:02 +0800

    add gem aasm

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -33,7 +33,7 @@ class OrdersController < ApplicationController
   def pay_with_alipay
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("alipay")
-    @order.make_payment
+    @order.make_payment!
 
     redirect_to order_path(@order.token), notice: "使用支付宝成功完成支付"
   end
@@ -41,7 +41,7 @@ class OrdersController < ApplicationController
   def pay_with_wechat
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("wechat")
-    @order.make_payment
+    @order.make_payment!
 
     redirect_to order_path(@order.token), notice: "使用微信成功完成支付"
   end
--- db/migrate/20170219063709_add_aasm_state_to_order.rb
+++ db/migrate/20170219063709_add_aasm_state_to_order.rb
@@ -3,6 +3,5 @@ class AddAasmStateToOrder < ActiveRecord::Migration[5.0]
     add_column :orders, :aasm_state, :string, default: "order_placed"
     add_index :orders, :aasm_state
     # index 是什么意思？
-    
   end
 end
