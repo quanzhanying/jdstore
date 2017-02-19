7ee1d4c14dae2df76faf5c7353d3cd66ce6c959d
zqz <71264737@qq.com>
2017-02-19 11:27:54 +0800

    admin see all orders

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,7 +2,7 @@ class Admin::OrdersController < ApplicationController
    layout "admin"
    
    before_action :authenticate_user!
-   before_action :admin_required!
+   before_action :admin_required
 
 
    def index
