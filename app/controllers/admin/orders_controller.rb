7068686583d2407eb74105dfe013085413524d8d
leichaofan <leichaofan2024@gmail.com>
2017-02-20 22:14:26 +0800

    add ship/shippen/cancel/return action to admin/orders_controller

---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,27 @@
+class Admin::OrdersController < ApplicationController
+  layout "admin"
+  before_action :authenticate_user!
+  before_action :admin_required
+  def index
+    @orders = Order.order("id DESC")
+  end
+  def ship
+    @order = Order.find(params[:id])
+    @order.ship!
+    redirect_to :back
+  end
+  def shipped
+    @order = Order.find(params[:id])
+    @order.deliver!
+    redirect_to :back
+  end
+  def cancel
+    @order = Order.find(params[:id])
+    @order.cancel_order!
+    redirect_to :back 
+  end
+  def show
+    @order = Order.find(params[:id])
+    @product_lists = @order.product_lists
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :products
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end 
   end
   resources :products do
     member do
