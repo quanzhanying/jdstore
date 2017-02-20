03dc19459c5d44502e3a22b65f21f7b9b99affb9
milesmao233 <milesmao233@milesmao233deMacBook-Pro.local>
2017-02-20 14:13:12 +0800

    implement add_product_to_cart function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,10 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    current_cart.add_product_to_cart(@product)
+    redirect_to :back
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,11 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
+
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
