7d855778b98427656b1b37db9da26c9e772535b2
tianshuai <157058166@qq.com>
2017-02-20 21:10:46 +0800

    add order model

---
+++ db/migrate/20170220130851_create_orders.rb
@@ -0,0 +1,12 @@
+class CreateOrders < ActiveRecord::Migration[5.0]
+  def change
+    create_table :orders do |t|
+       t.integer :total, default: 0
+       t.integer :user_id
+       t.string :billing_address
+       t.string :shipping_name
+       t.string :shipping_address
+      t.timestamps
+    end
+  end
+end
