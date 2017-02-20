d4699c1f32f3a9e71f28d31afcb550cb325f578d
milesmao233 <milesmao233@milesmao233deMacBook-Pro.local>
2017-02-20 17:22:51 +0800

    empty cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,25 +1,28 @@
 class Cart < ApplicationRecord
-  has_many :cart_items
-  has_many :products, through: :cart_items, source: :product
-
+    has_many :cart_items
+    has_many :products, through: :cart_items, source: :product
 
     def add_product_to_cart(product)
-      ci = cart_items.build
-      ci.product = product
-      ci.quantity = 1
-      ci.save
+        ci = cart_items.build
+        ci.product = product
+        ci.quantity = 1
+        ci.save
     end
     # 为什么这一段是放在cart.rb  而不是放在cart_item.rb?
     # 因为是当前的current_cart 添加。
 
     def total_price
-      sum = 0
-      cart_items.each do |cart_item|
-        if cart_item.product.price.present?
-          sum += cart_item.quantity * cart_item.product.price
+        sum = 0
+        cart_items.each do |cart_item|
+            if cart_item.product.price.present?
+                sum += cart_item.quantity * cart_item.product.price
 
+            end
         end
+        sum
     end
-    sum
-  end
+
+    def clean!
+      cart_item.destroy_all
+    end
 end
