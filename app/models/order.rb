4bf0b258adf17124ba6aaaa673379bb6829fe022
xieyubai <xieyubai17@gmail.coml>
2017-02-20 14:34:20 +0800

    add order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ApplicationRecord
+    belongs_to :user
+
+    validates :billing_name, presence: true
+    validates :billing_address, presence: true
+    validates :shipping_name, presence: true
+    validates :shipping_address, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    has_many :orders
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :trackable, :validatable
 
-  def admin?
-    is_admin
-  end
+    def admin?
+        is_admin
+    end
 end
