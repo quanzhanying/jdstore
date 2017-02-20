3aba18544574d8192b85dbf068fe59f9a482af99
范威 <fffw1989@gmail.com>
2017-02-21 00:30:14 +0800

    add welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,3 @@
 class WelcomeController < ApplicationController
-  def index
-    flash[:notice] = "你好！购物网站！"
-  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  root 'products#index'
+  root 'welcome#index'
   devise_for :users
   namespace :admin do
     resources :products
