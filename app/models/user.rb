a745b439550ebe3c57e4f2b161258fdbbfaea898
kaihe1989@gmail.com <hekai@HekaitekiMacBook-Pro.local>
2017-02-20 15:01:59 -0500

    add order controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
+  has_many :orders
 
   def admin?
     is_admin
   end
-       
+
 end
