2af1b7e580e832e9331e83ddcb675183c81810bf
leichaofan <leichaofan2024@gmail.com>
2017-02-20 17:16:19 +0800

    pull story6 from respont

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,6 +15,8 @@ class OrdersController < ApplicationController
         product_list.quantity = cart_item.quantity
         product_list.save
       end
+      current_cart.clean!
+      OrderMailer.notify_order_placed(@order).deliver!
       redirect_to order_path(@order.token)
     else
       render 'carts/checkout'
@@ -25,6 +27,18 @@ class OrdersController < ApplicationController
 
     @product_lists = @order.product_lists
 
+  end
+  def pay_with_alipay
+    @order = Order.find_by_token(params[:id])
+    @order.set_payment_with!("alipay")
+    @order.pay!
+    redirect_to order_path(@order.token), notice:"使用支付宝完成付款"
+  end
+  def pay_with_wechat
+    @order = Order.find_by_token(params[:id])
+    @order.set_payment_with!("wechat")
+    @order.pay!
+    redirect_to order_path(@order.token), notice: "使用微信支付成功完成付款"
   end
   private
 
