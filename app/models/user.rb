f7ad8e26f787f34ad20b5ad3c89dbff47a7f0e53
milesmao233 <milesmao233@milesmao233deMacBook-Pro.local>
2017-02-21 11:28:44 +0800

    touch app/views/carts/checkout.html.erb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   def admin?
-      is_admin  
+      is_admin
   end
+  has_many :orders
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,11 @@ Rails.application.routes.draw do
   resources :carts do
     collection do
       delete :clean
+      post :checkout
     end
   end
 
   resources :cart_items
+  resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
