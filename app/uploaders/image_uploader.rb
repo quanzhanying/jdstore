b12c0536e842a95f2260b544b6544ef36fe464cc
mike562661672 <mike562661672@gmail.com>
2017-02-21 15:35:48 +0800

    config qiniu

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
     # Choose what kind of storage to use for this uploader:
     # storage :file
-    # storage :fog
+    storage :qiniu
 
     def store_dir
         "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
