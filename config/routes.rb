be826f509ee32bbf212c7670107a9bbb98e13ee9
milesmao233 <milesmao233@milesmao233deMacBook-Pro.local>
2017-02-21 15:14:49 +0800

    add apply_cancel function

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,7 @@ Rails.application.routes.draw do
     member do
       post :pay_with_alipay
       post :pay_with_wechat
+      post :apply_to_cancel
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
