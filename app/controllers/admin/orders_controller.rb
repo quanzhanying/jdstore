25d554f394469a4a40a9173d78f5f519991d116c
kaihe1989@gmail.com <hekai@HekaitekiMacBook-Pro.local>
2017-02-21 16:17:36 -0500

    add apply_cancel function

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -19,24 +19,31 @@ class Admin::OrdersController < ApplicationController
    @order = Order.find(params[:id])
    @order.ship!
    redirect_to :back
- end
+  end
 
- def shipped
-   @order = Order.find(params[:id])
-   @order.deliver!
-   redirect_to :back
- end
+  def shipped
+    @order = Order.find(params[:id])
+    @order.deliver!
+    redirect_to :back
+  end
 
- def cancel
-   @order = Order.find(params[:id])
-   @order.cancel_order!
-   redirect_to :back
- end
+  def cancel
+    @order = Order.find(params[:id])
+    @order.cancel_order!
+    redirect_to :back
+  end
 
- def return
-   @order = Order.find(params[:id])
-   @order.return_good!
-   redirect_to :back
- end
- 
+  def return
+    @order = Order.find(params[:id])
+    @order.return_good!
+    redirect_to :back
+  end
+
+  def apply_to_cancel
+    @order = Order.find(params[:id])
+    OrderMailer.apply_cancel(@order).deliver!
+    flash[:notice] = "已提交申请"
+    redirect_to :back
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,14 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :products
-    resources :orders
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
   end
 
   resources :products do
@@ -29,19 +36,13 @@ Rails.application.routes.draw do
     member do
       post :pay_with_alipay
       post :pay_with_wechat
+      post :apply_to_cancel
     end
   end
 
 
   namespace :account do
-    resources :orders do
-      member do
-        post :cancel
-        post :ship
-        post :shipped
-        post :return
-      end
-    end 
+    resources :orders
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
