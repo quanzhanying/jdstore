72818f7970efad3551ad476918562440b7f84c8b
zqz <71264737@qq.com>
2017-02-21 14:45:54 +0800

    modify carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,16 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.fog_provider = 'fog'                  
+    config.storage :fog
     config.fog_credentials = {
-      provider:              'AWS',                        
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],         
+      provider:              'AWS',
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
 
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],        
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
 
-      region:                ENV["AWS_REGION"]    
+      region:                ENV["AWS_REGION"]
 
     }
-    config.fog_directory  = ENV["AWS_BUCKET_NAME"] 
+    config.fog_directory  = ENV["AWS_BUCKET_NAME"]
 
 
   else
