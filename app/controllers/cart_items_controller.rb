4ccb373242191930aa4f54234f2c40c710841d3e
milesmao233 <milesmao233@milesmao233deMacBook-Pro.local>
2017-02-21 11:19:54 +0800

    change quantity in cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -6,4 +6,29 @@ class CartItemsController < ApplicationController
     @cart_item.destroy
     redirect_to carts_path, alert: "成功将 #{@product.title} 移除"
   end
+
+  def update
+    p params
+    @cart = current_cart
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
+      if params[:add] == "1"
+        @cart_item.quantity += 1
+        @cart_item.save!
+      elsif params[:sub] =="1"
+        @cart_item.quantity -= 1
+        @cart_item.save!
+      end
+    elsif cart_item_params[:quantity].to_i < 0
+      redirect_to carts_path
+    end
+    redirect_to carts_path
+  end
+
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity)
+  end
 end
