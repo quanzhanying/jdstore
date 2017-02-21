2fed9b827b5d630c35e75aa2f656015ffc4c049b
“nataliu” <13636424061@163.com>
2017-02-21 11:29:27 +0800

    add ship/shipped/cancel/return action to admin/orders_controller

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,6 +8,36 @@ class Admin::OrdersController < ApplicationController
     @orders = Order.order("id DESC")
   end
 
+  def show
+    @order = Order.find(params[:id])
+    @product_lists = @order.product_lists
+  end
+
+  def ship
+    @order = Order.find(params[:id])
+    @order.ship!
+    redirect_to :back
+  end
+
+  def shipped
+    @order = Order.find(params[:id])
+    @order.shipped!
+    redirect_to :back
+  end
+
+  def cancel
+    @order = Order.find(params[:id])
+    @order.cancel_good!
+    redirect_to :back
+  end
+
+  def return
+    @order = Order.find(params[:id])
+    @order.return_good!
+    redirect_to :back
+  end
+
+
 
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 create_account = User.create([email: 'admin@test.com', password: '111111', password_confirmation: '111111', is_admin: 'true'])
 puts "Admin account created."
 create_account = User.create([email: 'admin2@test.com', password: '111111', password_confirmation: '111111', is_admin: 'true'])
@@ -23,13 +22,16 @@ end
 puts "10 products created."
 
 
-
 create_order = for i in 1..10 do
-  Order.create!([user_id: rand(1..10), total: rand(50..99)*100 , billing_name: "#{i}This is billing name.", billing_address: "#{i}This is billing address.", shipping_name: "#{i}This is shipping name.", shipping_address: "#{i}This is shipping address.", is_paid: true, payment_method: "wechat", aasm_state: "paid"])
+  Order.create!([user_id: rand(1..10), total: rand(50..99)*100 , billing_name: "#{i}This is billing name.", billing_address: "#{i}This is billing address.",
+    shipping_name: "#{i}This is shipping name.", shipping_address: "#{i}This is shipping address.",
+    is_paid: true, payment_method: "wechat", aasm_state: "paid"])
 end
 puts "10 orders paid with wechat."
 
 create_order = for i in 11..20 do
-  Order.create!([user_id: rand(1..10), total: rand(50..99)*100 , billing_name: "#{i}This is billing name.", billing_address: "#{i}This is billing address.", shipping_name: "#{i}This is shipping name.", shipping_address: "#{i}This is shipping address.", is_paid: true, payment_method: "alipay", aasm_state: "paid"])
+  Order.create!([user_id: rand(1..10), total: rand(50..99)*100 , billing_name: "#{i}This is billing name.", billing_address: "#{i}This is billing address.",
+    shipping_name: "#{i}This is shipping name.", shipping_address: "#{i}This is shipping address.",
+    is_paid: true, payment_method: "alipay", aasm_state: "paid"])
 end
 puts "10 orders paid with alipay."
