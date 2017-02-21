25fa03c50e4172bcf0d9e1229a099e529cc29bfb
caijia <caijia753@gmail.com>
2017-02-21 20:53:40 +0800

    test seed.rb for the first time

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+u1 = User.new
+u1.email = "xzhang@deloitte.com.cn"
+u1.password = "123456"
+u1.password_confirmation = "123456"
+u1.is_admin = true
+u1.save
+
+u2 = User.new
+u2.email = "caijia@b.c"
+u2.password = "123456"
+u2.password_confirmation = "123456"
+u2.is_admin = true
+u2.save
