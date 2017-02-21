a0189b111390503ec4c9ad3d23aef4e1b5c4dcd5
吴海铭 <freehuntman@gmail.com>
2017-02-22 00:04:47 +0800

    魔改 1.1：修改首页到 welcome 并美化

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    flash[:notice] = "早安！你好！"
+    flash[:notice] = " 🕹 这里是西元 1997 年 11 月 16 日，欢迎乘坐 fullstack-9527 号时光机，离 git 时间还有 5 分钟，请抓紧时间检票！！！"
   end
 end
