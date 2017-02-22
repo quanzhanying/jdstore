abe4d350b65265f253d540c8fa75d5dc26cb2e82
lihaihao <mashy1024@gmail.com>
2017-02-22 21:52:58 +0800

    add carousel to welcome index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'products#index'
+  root 'welcome#index'
 
   namespace :admin do
     resources :products
