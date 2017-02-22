7507edbc42fa36d5b105a65a12a89c976daf9574
Leo-Roil-Soleil <leokexin@gmail.com>
2017-02-22 20:37:33 +0600

    implement add_product_to_cart function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,8 +8,8 @@ class ProductsController < ApplicationController
     end
 
     def add_to_cart
-        @product = Product.find(params[:id])
-        redirect_to :back
-        flash[:notice] = '测试加入购物车'
+       @product = Product.find(params[:id])
+       current_cart.add_product_to_cart(@product)
+       redirect_to :back
      end
 end
