8cafdae706c19726a52ebcfe8e9f15958c697d71
zqz <71264737@qq.com>
2017-02-22 13:44:52 +0800

    config qiniu

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'carrierwave-qiniu'
+gem 'qiniu-rs'
 gem 'figaro'
 gem 'fog'
 gem 'aasm'
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,10 @@
 CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.storage :fog
-    config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-
-      region:                ENV["AWS_REGION"]
-
-    }
-    config.fog_directory  = ENV["AWS_BUCKET_NAME"]
-
-
-  else
-    config.storage :file
-  end
+  config.storage             = :qiniu
+  config.qiniu_access_key    = ENV["qiniu_access_key"]
+  config.qiniu_secret_key    = ENV['qiniu_secret_key']
+  config.qiniu_bucket        = ENV["qiniu_bucket"]
+  config.qiniu_bucket_domain = ENV["qiniu_bucket_domain"]
+  config.qiniu_block_size    = 4*1024*1024
+  config.qiniu_protocol      = "http"
+  config.qiniu_up_host       = 'http://up.qiniug.com'
 end
