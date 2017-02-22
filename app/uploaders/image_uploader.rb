1c0ce02de0613b8ee1180d3851e2c5bdca9754d4
yuyang <18516569687@163.com>
2017-02-22 19:49:35 +0800

    install fog

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,7 +6,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
+  # storage :fog
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
