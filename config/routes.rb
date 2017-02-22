064f10a458a0cf54535426bee700f1554b223e29
skyflyme <cszsxq@gmail.com>
2017-02-22 20:19:25 +0800

    implement cars index function

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :carts
+  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
