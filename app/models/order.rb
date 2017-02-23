8aa20a652334b750e4c2b70eedd96f66a0460795
Leo-Roil-Soleil <leokexin@gmail.com>
2017-02-23 19:33:17 +0600

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,14 @@
 class Order < ApplicationRecord
+    before_create :generate_token
+
+    def generate_token
+        self.token = SecureRandom.uuid
+    end
     belongs_to :user
 
     validates :billing_name, presence: true
     validates :billing_address, presence: true
     validates :shipping_name, presence: true
     validates :shipping_address, presence: true
-      has_many :product_lists
+    has_many :product_lists
 end
