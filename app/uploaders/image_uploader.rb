07853b06e1d45863f7553272b70fac30a527d8c8
molly <hulilzster@gmail.com>
2017-02-23 23:29:04 +0800

    implement image_uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
 if Rails.env.production?
