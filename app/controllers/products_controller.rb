d79ae0082d1d15b4e6aa9f5f4284c81756695c95
tuantuan <lisuji1986@gmail.com>
2017-02-24 00:23:31 +0800

    navbar category function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,28 @@ class ProductsController < ApplicationController
       if params[:search]
         @products = Product.search(params[:search]).order("created_at DESC")
       else
-        @products = Product.all.order("created_at DESC")
+        @products = case params[:cate]
+        when 'jujia'
+          @products = Product.all.where(:categories => '居家')
+        when 'canchu'
+          @products = Product.all.where(:categories => '餐厨')
+        when 'peijian'
+          @products = Product.all.where(:categories => '配件')
+        when 'fuzhuang'
+          @products = Product.all.where(:categories => '服装')
+        when 'xihu'
+          @products = Product.all.where(:categories => '洗护')
+        when 'yingtong'
+          @products = Product.all.where(:categories => '婴童')
+        when 'yinshi'
+          @products = Product.all.where(:categories => '饮食')
+        when 'zahuo'
+          @products = Product.all.where(:categories => '杂货')
+        when 'qita'
+          @products = Product.all.where(:categories => '其他')
+        else
+          @products = Product.all.order("created_at DESC")
+        end
       end
     end
 
