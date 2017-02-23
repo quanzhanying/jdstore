5d32d222173f27c1264fe5fae33d9a96c14885b1
zhoujin <jin100chow@hotmail.com>
2017-02-23 21:55:01 +0800

    change quanlity in cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,6 +1,20 @@
 class CartItemsController < ApplicationController
   before_action :authenticate_user!
 
+  def update
+    @cart = current_cart
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    @cart_item.update(cart_item_params)
+
+    redirect_to carts_path
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity)
+  end
+
    def destroy
      @cart = current_cart
      @cart_item = @cart.cart_items.find_by(product_id: params[:id])
