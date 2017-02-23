3a4a2001562c8055ae17aacfaa76ba383b29e3b2
tuheng <tuhruonral@hotmail.com>
2017-02-23 17:01:00 +0800

    add admin/orders

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'products#index'
   devise_for :users
   namespace :admin do
-    resources :products
+    resources :orders
   end
 
   resources :products do
