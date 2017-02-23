ee8ec3533f35ef5eeadc77ec9823e289604c6bd1
subinhuang <806519191@qq.com>
2017-02-23 13:01:44 +0800

    modified seed.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,9 +5,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
-  storage :fog
-
+  storage :file  #储存在本地
+  # storage :fog  # 储存在s3
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
