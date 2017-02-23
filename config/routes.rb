c251c22e118660375abadcb29d3f8a1721af4969
ranty <ranty720@gmail.com>
2017-02-23 21:14:10 +0800

    add products show&index

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :products
   end
   devise_for :users
-  root 'welcome#index'
+  root 'products#index'
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
