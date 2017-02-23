4f50473a038494adfec1f0fbdecb4ee8dedb3402
Leo-Roil-Soleil <leokexin@gmail.com>
2017-02-23 18:48:48 +0600

    delete an item from cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -17,4 +17,8 @@ class Cart < ApplicationRecord
         end
         sum
 end
+
+    def clean!
+        cart_items.destroy_all
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,10 @@ Rails.application.routes.draw do
     devise_for :users
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'welcome#index'
-    resources :carts
+    resources :carts do
+        collection do
+            delete :clean
+        end
+    end
+    resources :cart_items
 end
