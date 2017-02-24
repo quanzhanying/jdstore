b14f0a1c45faec5b870ff375b87eeac6fb4836a2
zhoujin <jin100chow@hotmail.com>
2017-02-25 00:01:27 +0800

    add token to order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,7 +16,7 @@ class OrdersController < ApplicationController
          product_list.save
        end
 
-      redirect_to order_path(@order)
+      redirect_to order_path(@order.token)
     else
       render 'carts/checkout'
     end
@@ -24,7 +24,7 @@ class OrdersController < ApplicationController
 
 
    def show
-     @order = Order.find(params[:id])
+     @order = Order.find_by_token(params[:id])
      @product_lists = @order.product_lists
    end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224051430) do
+ActiveRecord::Schema.define(version: 20170224155611) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
