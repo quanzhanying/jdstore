fd83d40ce8db71d00d460f4b2f93b7719d24a115
bob <bao309@163.com>
2017-02-24 22:48:27 +0800

    modify all

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,10 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
   if Rails.env.production?
+    config.storage :fog
     config.fog_provider = 'fog'
     config.fog_credentials = {
       provider:'AWS',
