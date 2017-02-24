880cfca903599f0ecd34cd30a97ce3d9f8215077
bob <bao309@163.com>
2017-02-24 22:21:17 +0800

    modify all

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,6 +7,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   # storage :file
   # storage :fog
+  if Rails.env.production?
+    storage :fog
+  elsif Rails.env.development?
+    storage :File
+   end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
