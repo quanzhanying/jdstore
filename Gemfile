09e3abf456ccb748219d779aedfb1527c3973c5c
bob <bao309@163.com>
2017-02-24 20:46:57 +0800

    modify add

--- Gemfile
+++ Gemfile
@@ -38,7 +38,7 @@ gem 'mini_magick'
 gem 'letter_opener', group: :development
 gem 'aasm'
 
-
+gem 'figaro'
 gem 'fog'
 
 group :development, :test do
@@ -46,7 +46,7 @@ group :development, :test do
   gem 'byebug', platform: :mri
   gem 'sqlite3'
 end
-gem 'figaro'
+
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console'
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,12 +2,12 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_provider = 'fog'
     config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+      provider:'AWS',
+      aws_access_key_id:ENV["AWS_ACCESS_KEY_ID"],
 
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      aws_secret_access_key:ENV["AWS_SECRET_ACCESS_KEY"],
 
-      region:               'ap-northeast-1'
+      region:'ap-northeast-1'
 
     }
     config.fog_directory  = ENV["AWS_BUCKET_NAME"]
