9db055b1f35e741584386404487fcdf1c55cdb20
qdlin <qdlin1314@gmail.com>
2017-02-25 10:55:25 +0800

    implement current_cart function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,7 +9,14 @@ class ProductsController < ApplicationController
 
   def add_to_cart
     @product = Product.find(params[:id])
+
+    if !current_cart.products.include?(@product)
+      current_cart.add_product_to_cart(@product)
+      flash[:notice] = "您已成将 #{@product.title} 加入购物车"
+    else
+      flash[:warning] = "您的购物车内已有此商品"
+    end
+
     redirect_to :back
-    flash[:notice] = "测试加入购物车"
   end
 end
