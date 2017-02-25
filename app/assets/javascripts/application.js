6ccf541da465fc8c36354aac6f71cb999e62b82d
linjiaxin <linjiaxin405@gmail.com>
2017-02-25 10:37:46 +0800

    导航栏

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -81,3 +81,76 @@ window.onload = function(){
 };
 
 /*----------------------------------功能分割线----------------------------------*/
+
+
+/*导航栏*/
+$(document).ready(function(){
+    $(".dropdown").hover(
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
+            $(this).toggleClass('open');
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
+            $(this).toggleClass('open');
+        }
+    );
+});
+
+$(document).ready(function()
+{
+
+	// Closes the sidebar menu on menu-close button click event
+	$("#menu-close").click(function(e)							//declare the element event ...'(e)' = event (shorthand)
+	{
+																// - will not work otherwise")
+		$("#sidebar-wrapper").toggleClass("active");			//instead on click event toggle active CSS element
+		e.preventDefault(); 									//prevent the default action ("Do not remove as the code
+
+		/*!
+		======================= Notes ===============================
+		* see: .sidebar-wrapper.active in: style.css
+		==================== END Notes ==============================
+		*/
+	});															//Close 'function()'
+
+	// Open the Sidebar-wrapper on Hover
+	$("#menu-toggle").hover(function(e)							//declare the element event ...'(e)' = event (shorthand)
+	{
+		$("#sidebar-wrapper").toggleClass("active",true);		//instead on click event toggle active CSS element
+		e.preventDefault();										//prevent the default action ("Do not remove as the code
+	});
+
+	$("#menu-toggle").bind('click',function(e)					//declare the element event ...'(e)' = event (shorthand)
+	{
+		$("#sidebar-wrapper").toggleClass("active",true);		//instead on click event toggle active CSS element
+		e.preventDefault();										//prevent the default action ("Do not remove as the code
+	});															//Close 'function()'
+
+	$('#sidebar-wrapper').mouseleave(function(e)				//declare the jQuery: mouseleave() event
+																// - see: ('//api.jquery.com/mouseleave/' for details)
+	{
+		/*! .toggleClass( className, state ) */
+		$('#sidebar-wrapper').toggleClass('active',false);		/* toggleClass: Add or remove one or more classes from each element
+																in the set of matched elements, depending on either the class's
+																presence or the value of the state argument */
+		e.stopPropagation();									//Prevents the event from bubbling up the DOM tree
+																// - see: ('//api.jquery.com/event.stopPropagation/' for details)
+
+		e.preventDefault();										// Prevent the default action of the event will not be triggered
+																// - see: ('//api.jquery.com/event.preventDefault/' for details)
+	});
+});
+// Closes the sidebar menu on menu-close button click event
+$("#menu-close").click(function(e)							//declare the element event ...'(e)' = event (shorthand)
+{
+															// - will not work otherwise")
+	$("#sidebar-wrapper").toggleClass("active");			//instead on click event toggle active CSS element
+	e.preventDefault(); 									//prevent the default action ("Do not remove as the code
+
+	/*!
+	======================= Notes ===============================
+	* see: .sidebar-wrapper.active in: style.css
+	==================== END Notes ==============================
+	*/
+});
