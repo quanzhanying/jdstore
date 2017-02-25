d57c70006b9ef6ab0e39bfd394a4c6e9b0ef1e43
ranty <ranty720@gmail.com>
2017-02-25 22:21:26 +0800

    some

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -5,6 +5,9 @@ class Admin::ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+  def show
+    @product = Product.find(params[:id])
+  end
   def new
     @product = Product.new
   end
