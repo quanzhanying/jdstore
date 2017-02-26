a32839dab54082f5e63ec35f9dc0a65aa4eac662
shijunfeng <johnshiluck@gmail.comgit>
2017-02-26 22:24:46 +0800

    add seed to project

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-u = User.new
-u.email = "admin@test.com"           # 可以改成自己的 email
-
-u.password = "123456"                # 最少要六码
-
-u.password_confirmation = "123456"   # 最少要六码
-
-u.is_admin = true
-u.save
+User.create(email: "test@gmail.com", password: "123456", password_confirmation: "123456", is_admin: false)
+User.create(email: "admin@gmail.com", password: "123456", password_confirmation: "123456", is_admin: true)
+Category.create(name: "文具")
+Category.create(name: "收纳")
+Category.create(name: "旅行")
