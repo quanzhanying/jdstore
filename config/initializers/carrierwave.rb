c416b8baef83035cd93ed85ea29305c73eb5901e
lihaihao <mashy1024@gmail.com>
2017-02-27 07:00:37 +0800

    redo AWS S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,13 +3,15 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog'
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV["AKIAJUSAPJJ6BJKTXQFA"],
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
 
-      aws_secret_access_key: ENV["pzL29CuFmq3vws2Rb/cE6r6Dfzs8CYdrVvIT1tHI"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
 
-      region:                ENV["ap-northeast-1"]
+      region:                ENV["AWS_REGION"]
+
     }
-    config.fog_directory = ENV["lihaihao"]
+    config.fog_directory  = ENV["AWS_BUCKET_NAME"]
+
 
   else
     config.storage :file
