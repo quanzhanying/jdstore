0d0d713c7d875797b8b5f1cbaa5d8abb40456791
freedomsky <hanwei_1989@126.com>
2017-02-26 11:28:06 +0800

    implement add_template_to_cart function

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,11 +1,19 @@
 class Cart < ApplicationRecord
   has_many :cart_items
+  has_many :cart_template_items
   has_many :products, through: :cart_items, source: :product
+  has_many :templates, through: :cart_template_items, source: :template
 
   def add_product_to_cart(product)
     ci = cart_items.build
     ci.product = product
     ci.quantity = 1
+    ci.save
+  end
+
+  def add_template_to_cart(template)
+    ci = cart_template_items.build
+    ci.template = template
     ci.save
   end
 
