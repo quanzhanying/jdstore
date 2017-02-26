c713afcb159aa5c1419a5ec85bcbbee074419626
RealAnalysis <detective_boys@hotmail.com>
2017-02-27 02:48:46 +0800

    fix yml pro

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.storage             = :qiniu
-  config.qiniu_access_key    = ENV["4dg8_gZJ_RwL4t847hd8NdAM0sP6ewKWoA80wtEx"]
-  config.qiniu_secret_key    = ENV["OJYnRHwHD7ndG8vwSRwEjO0weJRt-wUwHSqrLvIi"]
-  config.qiniu_bucket        = ENV["jdstore-demo"]
-  config.qiniu_bucket_domain = ENV["ols6vd1wm.bkt.clouddn.com"]
+  config.qiniu_access_key    = ENV["Sk-seimaTqUuB8iKOj2LWPEbBMODXpkrQgKH7oGy"]
+  config.qiniu_secret_key    = ENV["0dZ_O2yte6iamd3SzOsVcI0MnWR1k4bXoA-ZQ0yu"]
+  config.qiniu_bucket        = ENV["realanalysis"]
+  config.qiniu_bucket_domain = ENV["olzw7m80w.bkt.clouddn.com"]
   config.qiniu_block_size    = 4*1024*1024
   config.qiniu_protocol      = "http"
   config.qiniu_up_host       = "http://up.qiniug.com"  #选择不同的区域时，"up.qiniug.com" 不同
