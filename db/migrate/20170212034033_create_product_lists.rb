2cb773b2e4f7d5a1accbb2d488e5670d07f1fa79
Gh72 <cao.wang.work@foxmail.com>
2017-02-26 21:12:29 +0800

    null

--- db/migrate/20170212034033_create_product_lists.rb
+++ db/migrate/20170212034033_create_product_lists.rb
@@ -1,12 +1,12 @@
 class CreateProductLists < ActiveRecord::Migration[5.0]
-  def change
-    create_table :product_lists do |t|
-      t.integer :order_id
-      t.string  :product_name
-      t.integer :product_price
-      t.integer :quantity
+    def change
+        create_table :product_lists do |t|
+            t.integer :order_id
+            t.string  :product_name
+            t.integer :product_price
+            t.integer :quantity
 
-      t.timestamps
+            t.timestamps
+        end
     end
-  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,82 +10,80 @@ require 'json'
 
 puts 'seed 数据自动创建'
 
-User.create(email: "cw@test.com", password: "123456", password_confirmation: "123456", is_admin: 'false')
+User.create(email: 'cw@test.com', password: '123456', password_confirmation: '123456', is_admin: 'false')
 puts 'create user: cw@test.com,  not admin'
 
-User.create(email: "admin@test.com", password: "123456", password_confirmation: "123456", is_admin: 'true')
+User.create(email: 'admin@test.com', password: '123456', password_confirmation: '123456', is_admin: 'true')
 puts 'create user: admin@test.com,  is admin'
-=begin
-for i in 1..5 do
-  Product.create(title: "Product#{i}", description: "description#{i}", quantity: i*10, price: i*100)
-end
-puts 'create product : 5'
-=end
+# for i in 1..5 do
+#   Product.create(title: "Product#{i}", description: "description#{i}", quantity: i*10, price: i*100)
+# end
+# puts 'create product : 5'
 puts Dir.pwd
 puts '从josn文件导入趣玩网商品数据'
 
-#goods_base.json
-json = File.read("./db/seedjson/goods_base.json")
+# goods_base.json
+json = File.read('./db/seedjson/goods_base.json')
 obj = JSON.parse(json)
-count = obj["RECORDS"].count
+count = obj['RECORDS'].count
 puts "商品基本信息#{count}条导入中......"
 for i in 0...count do
-  item = obj["RECORDS"][i]
-  Product.create(
-  title: item["name"],
-  description: item["description"],
-  quantity: 50,
-  price: item["price"],
-  quwan_goodsid: item["id"],
-  page_idx: item["page_idx"],
-  logo: item["logo"],
-  brand: item["brand"]
-  )
+    item = obj['RECORDS'][i]
+    Product.create(
+        title: item['name'],
+        description: item['description'],
+        quantity: 50,
+        price: item['price'],
+        quwan_goodsid: item['id'],
+        page_idx: item['page_idx'],
+        logo: item['logo'],
+        brand: item['brand']
+    )
 end
-puts "商品基本信息记录导入完成"
+puts '商品基本信息记录导入完成'
 
-#goods_attr.json
-json = File.read("./db/seedjson/goods_attr.json")
+# goods_attr.json
+json = File.read('./db/seedjson/goods_attr.json')
 obj = JSON.parse(json)
-count = obj["RECORDS"].count
+count = obj['RECORDS'].count
 puts "商品属性记录#{count}条导入中......"
 for i in 0...count do
-  item = obj["RECORDS"][i]
-  GoodsAttr.create(
-  quwan_goodsid: item["goods_id"],
-  attr_name: item["attr_name"],
-  attr_value: item["attr_value"])
+    item = obj['RECORDS'][i]
+    GoodsAttr.create(
+        quwan_goodsid: item['goods_id'],
+        attr_name: item['attr_name'],
+        attr_value: item['attr_value']
+    )
 end
-puts "商品属性记录导入完成"
+puts '商品属性记录导入完成'
 
-#goods_img.json
-json = File.read("./db/seedjson/goods_img.json")
+# goods_img.json
+json = File.read('./db/seedjson/goods_img.json')
 obj = JSON.parse(json)
-count = obj["RECORDS"].count
+count = obj['RECORDS'].count
 puts "商品图片记录#{count}条导入中......"
 for i in 0...count do
-  item = obj["RECORDS"][i]
-  GoodsImage.create(
-  quwan_goodsid: item["goods_id"],
-  img_type: item["pic_type"],
-  url: item["url"],
-  idx: item["idx"]
-  )
+    item = obj['RECORDS'][i]
+    GoodsImage.create(
+        quwan_goodsid: item['goods_id'],
+        img_type: item['pic_type'],
+        url: item['url'],
+        idx: item['idx']
+    )
 end
-puts "商品图片记录导入完成"
+puts '商品图片记录导入完成'
 
-
-#page_goods.json
-json = File.read("./db/seedjson/page_goods.json")
+# page_goods.json
+json = File.read('./db/seedjson/page_goods.json')
 obj = JSON.parse(json)
-count = obj["RECORDS"].count
+count = obj['RECORDS'].count
 puts "商品页面索引记录#{count}条导入中......"
 for i in 0...count do
-  item = obj["RECORDS"][i]
-  GoodsPage.create(
-  quwan_goodsid: item["goods_id"],
-  page_idx: item["page_id"],
-  page_title: item["page_title"],
-  )
+    item = obj['RECORDS'][i]
+    GoodsPage.create(
+        quwan_goodsid: item['goods_id'],
+        page_idx: item['page_id'],
+        page_title: item['page_title']
+    )
 end
-puts "商品页面索引记录导入完成"
+puts '商品页面索引记录导入完成'
