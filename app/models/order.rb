a7d2a58cbd8f6928065d0ea71e8a76cd01d2ee5f
tianshuai <157058166@qq.com>
2017-02-26 11:54:40 +0800

    add order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,2 @@
 class Order < ApplicationRecord
-    belongs_to :user
-
-    validates :billing_name, presence: true
-    validates :billing_address, presence: true
-    validates :shipping_name, presence: true
-    validates :shipping_address, presence: true
 end
