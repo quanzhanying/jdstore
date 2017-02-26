a4db8387311de030caee03146182d09acce779d6
tianshuai <157058166@qq.com>
2017-02-26 12:41:10 +0800

    add order controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220130851) do
+ActiveRecord::Schema.define(version: 20170226035155) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170220130851) do
   create_table "orders", force: :cascade do |t|
     t.integer  "total",            default: 0
     t.integer  "user_id"
+    t.string   "billing_name"
     t.string   "billing_address"
     t.string   "shipping_name"
     t.string   "shipping_address"
