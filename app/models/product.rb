d576e60f854680a2935aca59da4e24786a89f0cd
Gh72 <cao.wang.work@foxmail.com>
2017-02-26 17:30:27 +0800

    首页显示商品图片

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,9 @@ class Product < ApplicationRecord
 
   has_many :goods_attrs, through: :goods_images, source: :goods_images
   has_many :goods_images, through: :goods_images, source: :goods_images
+
+
+  def is_quwan_goods?
+    quwan_goodsid.present?
+  end
 end
