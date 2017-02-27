446137648192882468b488e813711419dd4cc73c
huazai <470021389@qq.com>
2017-02-27 17:13:00 +0800

    implement category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170219083821) do
+ActiveRecord::Schema.define(version: 20170227075622) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20170219083821) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image"
+    t.string   "category"
   end
 
   create_table "users", force: :cascade do |t|
