f1fb8a1e5fc767155bd178f29ea1e68e8f5712d8
zengxiangrong <zengxiangrong5261435@gmail.com>
2017-02-27 11:17:39 +0800

    add apply_cancel function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -48,4 +48,11 @@ class OrdersController < ApplicationController
 
     redirect_to order_path(@order.token), notice: "使用微信完成付款"
   end
+
+  def apply_to_cancel
+    @order = Order.find(params[:id])
+    OrderMailer.apply_cancel(@order).deliver!
+    flash[:notice] = "commited appley cancel"
+    redirect_to :back 
+  end
 end
