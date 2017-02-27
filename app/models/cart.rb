f74770638d7db2885dda763c3a9f3e5295547fc6
ranty <ranty720@gmail.com>
2017-02-27 22:31:18 +0800

    implement carts index function

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,6 @@
 class Cart < ApplicationRecord
   has_many :cart_items
-  has_many :product, through: :cart_items, source: :product
+  has_many :products, through: :cart_items, source: :product
 
   def add_product_to_cart(product)
     ci = cart_items.build
