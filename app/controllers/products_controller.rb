c5e790cca5f283dff267d4f5ea76ae680992f46a
manxin2010 <manxin2010@163.com>
2017-02-27 21:03:14 +0800

    add add_to_cart button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,9 @@ end
 def show
   @product = Product.find(params[:id])
 end
+def add_to_cart
+   @product = Product.find(params[:id])
+   redirect_to :back
+   flash[:notice] = "测试加入购物车"
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ devise_for :users
   namespace :admin do
     resources :products
   end
-resources :products
+resources :products do
+  member do
+    post :add_to_cart
+  end
+end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
