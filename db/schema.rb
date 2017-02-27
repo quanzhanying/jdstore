e506d0ac56e5567168084070bb8b698972d62d17
freedomsky <hanwei_1989@126.com>
2017-02-27 22:28:34 +0800

    add template_list model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170226031834) do
+ActiveRecord::Schema.define(version: 20170227142458) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -65,6 +65,14 @@ ActiveRecord::Schema.define(version: 20170226031834) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image"
+  end
+
+  create_table "template_lists", force: :cascade do |t|
+    t.integer  "order_id"
+    t.string   "tempalte_name"
+    t.integer  "template_price"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "templatephotos", force: :cascade do |t|
