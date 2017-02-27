330d26a8d15a4a7260fe93cb8989b5e17212928d
noodlescnliu@gmail.com <noodlescnliu@gmail.com>
2017-02-27 22:04:44 +0800

    add search function

--- Gemfile
+++ Gemfile
@@ -43,6 +43,8 @@ gem 'qiniu-rs'
 gem 'figaro'
 gem 'rollbar'
 gem 'ransack'
+gem 'will_paginate'
+gem 'seo_helper'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,8 @@
 class ProductsController < ApplicationController
+  before_action :validate_search_key, only: [:search]
 
   def index
-    @q = Product.ransack(params[:q])
-    @products = @q.result(distinct: true)
+    @products = Product.all
   end
 
   def show
@@ -19,6 +19,24 @@ class ProductsController < ApplicationController
       flash[:warning] = "你的购物车内已有此物品"
     end
     redirect_to :back
+  end
+
+  def search
+      if @query_string.present?
+        search_result = Product.ransack(@search_criteria).result(:distinct => true)
+        @products = search_result.paginate(:page => params[:page], :per_page => 5)
+      end
+  end
+
+  protected
+
+  def validate_search_key
+    @query_string = params[:q].gsub(/\\|\'|\/|\?/, "") if params[:q].present?
+    @search_criteria = search_criteria(@query_string)
+  end
+
+  def search_criteria(query_string)
+    { :title_cont => query_string}
   end
 
 
