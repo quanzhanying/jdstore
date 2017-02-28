1033969a29bf04f485eca21ee8b5422aaa9bb4b5
qiaoyu <qiaoyu1111358@gmail.com>
2017-03-01 03:21:17 +0800

    add public/audios

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,17 @@
 module ProductsHelper
+# Returns an HTML audio tag for the +source+.
+# The +source+ can be full path or file that exists in
+# your public audios directory.
+#
+#   audio_tag("sound")
+#   # => <audio src="/audios/sound"></audio>
+#   audio_tag("sound.wav")
+#   # => <audio src="/audios/sound.wav"></audio>
+#   audio_tag("sound.wav", autoplay: true, controls: true)
+#   # => <audio autoplay="autoplay" controls="controls" src="/audios/sound.wav"></audio>
+#   audio_tag("sound.wav", "sound.mid")
+#   # => <audio><source src="/audios/sound.wav" /><source src="/audios/sound.mid" /></audio>
+  def audio_tag(*sources)
+    multiple_sources_tag('audio', sources)
+  end
 end
