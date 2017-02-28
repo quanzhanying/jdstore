ec6449509d926baea4b38160406ce38cfd41c34f
skyflyme <cszsxq@gmail.com>
2017-03-01 00:42:59 +0800

    add orders show function

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "建立一個admin账号"
+
 u = User.new
 u.email = "admin@test.com"   #可以改成自己的email
 
@@ -13,4 +15,4 @@ u.password = "123456"           #最少要六码
 u.password_confirmation = "123456"  #最少要六码
 
 u.is_admin = true
-s.save
+u.save
