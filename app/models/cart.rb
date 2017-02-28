7ad06605c6e3eea6bcacb8207670db2a6a8b4e1b
like <20783506632@qq.com>
2017-02-28 09:42:29 +0800

    implement add_product_to_cart function

---
+++ app/models/cart.rb
@@ -0,0 +1,18 @@
+class Cart < ApplicationRecord
+  has_many :cart_items
+  has_many :products, through: :cart_items, source: :product
+
+  def add_product_to_cart
+    ci = cart_items.build
+    ci.product = product
+    ci.quantity = 1
+    ci.save
+  end
+
+
+
+
+
+
+
+end
