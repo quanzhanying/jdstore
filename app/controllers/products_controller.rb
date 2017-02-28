45cd29e6260c32821c195184eb2784f5d287919a
like <20783506632@qq.com>
2017-02-28 09:29:39 +0800

    add_to_cart button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,5 +6,11 @@ end
 
 def show
   @product = Product.find(params[:id])
-end  
+end
+
+def add_to_cart
+  @product = Product.find(params[:id])
+   redirect_to :back
+ flash[:notice] = "测试加入购物车"
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,9 @@ resources :products
 
 
 
-resources :products
+resources :products do
+  member do
+    post :add_to_cart
+  end
+end
 end
