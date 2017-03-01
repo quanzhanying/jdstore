6cc71a04591c1a3cbda1878809b7f9d9d240b752
“将叫我将军” <d2194499406@gmail.com>
2017-03-01 12:08:25 +0800

    implement comments pagination with will_paginate

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    @comments = @product.comments.recent
+    @comments = @product.comments.recent.paginate(:page => params[:page], :per_page => 5)
   end
 
   def add_to_cart
