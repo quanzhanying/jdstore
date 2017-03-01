e98db0ffeab7bdd8c72f64efeee0e6adbd00802f
“将叫我将军” <d2194499406@gmail.com>
2017-03-01 10:46:35 +0800

    add comment and hook product/user relation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   acts_as_votable
+
+  belongs_to :user
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224102827) do
+ActiveRecord::Schema.define(version: 20170301024220) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20170224102827) do
   end
 
   create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "product_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
