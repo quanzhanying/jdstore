1567370c0144298040cd3942eefdc7fc828f0ef0
mayue <mayue@mayuedeMacBook-Air.local>
2017-03-03 23:14:22 +0800

    change the image_uploder.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,5 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
 
@@ -7,7 +8,13 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
+  # storage :fog
+  if Rails.env.production?
    storage :fog
+ else Rails.env.development?
+   storage :file
+ end
+
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
