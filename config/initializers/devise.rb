66c80387716269101f86ea94fc5202451c0e2fea
lihaihao <mashy1024@gmail.com>
2017-03-04 08:45:36 +0800

    change devise key and try to push heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  config.secret_key = '985718031f99c832853c833e5270e0c071b2b0176816a1270924a6ffebd8d37f2dc465e72e7512addc2a436486b572751c888c423de41078991efe0e0f39d346'
+  # config.secret_key = '985718031f99c832853c833e5270e0c071b2b0176816a1270924a6ffebd8d37f2dc465e72e7512addc2a436486b572751c888c423de41078991efe0e0f39d346'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
