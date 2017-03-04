3c93edcf2f5f1c09f5c1416983d9384d9ad8fb1f
linfgit <linfgg1@gmail.com>
2017-03-04 09:29:22 +0800

    add order controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,21 @@
+class OrdersController < ApplicationController
+    before_action :authenticate_user!, only: [:create]
+
+  def create
+    @order = Order.new(order_params)
+    @order.user = current_user
+    @order.total = current_cart.total_price
+
+    if @order.save
+      redirect_to order_path(@order)
+    else
+      render 'carts/checkout'
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:billing_name, :billing_address, :shipping_name, :shipping_address)
+  end
+end
