3d194bb7c4570a19f9922bafe5d3472a4420509a
lihaihao <mashy1024@gmail.com>
2017-03-04 10:39:08 +0800

    the last one

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery-3.1.0.min
 //= require jquery
-//= require jquery_ujs
 //= require turbolinks
 //= require main
 //= require masonry.pkgd.min
