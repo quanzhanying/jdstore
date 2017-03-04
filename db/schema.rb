2ff4e3270a810ef827807f060c3ef3e92f1736d6
michaeldon <michaeldon@126.com>
2017-03-04 19:43:06 +0800

    fix bug

--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 20170301230600) do
     t.datetime "updated_at", null: false
   end
 
-
   create_table "product_images", force: :cascade do |t|
     t.integer  "product_id"
     t.string   "big_image"
