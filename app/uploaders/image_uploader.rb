80cd877ecb0ff63926153b173bfab3caa186ca15
xyy601chenyan <xyy545688506@gmail.com>
2017-03-06 11:49:56 +0800

    debug seed

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,6 +9,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   storage :qiniu
   # storage :fog
 
+  
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
