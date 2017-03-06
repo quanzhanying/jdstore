46d835cb4dd0074e8277620295c51b9d20dbafa3
xyy601chenyan <xyy545688506@gmail.com>
2017-03-06 19:30:02 +0800

    config qiniu2

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,7 +9,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   storage :qiniu
   # storage :fog
 
-  
+
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
