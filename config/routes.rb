596148643747de02bf3b0994ee6d91b5dd63e8b5
like <20783506632@qq.com>
2017-03-07 11:18:13 +0800

    implement backend CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
    root 'welcome#index'
+
+   namespace :admin do
+      resources :products
+    end
+
+
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307030618) do
+ActiveRecord::Schema.define(version: 20170307031427) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "quantity"
+    t.integer  "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
