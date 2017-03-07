e4511905bc1a383f0488e7451594098132cb251d
like <20783506632@qq.com>
2017-03-07 11:34:48 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-   root 'welcome#index'
+   root 'products#index'
 
    namespace :admin do
       resources :products
     end
 
-
+resources :products
 
 
 end
