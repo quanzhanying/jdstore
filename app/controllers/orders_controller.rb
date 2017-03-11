ffe2dfbf254dafa7eecaafca742a38d8d4fa6b30
suilulu <m13869841793@gmail.com>
2017-03-11 14:54:24 +0800

    add checkout routing

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,7 +14,7 @@ class OrdersController < ApplicationController
     if @order.save
 
       current_cart.cart_items.each do |cart_item|
-        product_list = Productlist.new
+        product_list = ProductList.new
         product_list.order = @order
         product_list.product_name = cart_item.product.title
         product_list.product_price = cart_item.product.price
