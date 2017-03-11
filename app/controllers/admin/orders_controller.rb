52d65fc772b7dfddb061ae3388df3f80ef4107a6
manxin2010 <manxin2010@163.com>
2017-03-11 09:33:40 +0800

    add notify_cancel/ship function

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -15,6 +15,7 @@ end
 def ship
   @order = Order.find(params[:id])
   @order.ship!
+  OrderMailer.notify_ship(@order).deliver!
   redirect_to :back
 end
 def shipped
@@ -26,6 +27,7 @@ end
 def cancel
   @order =Order.find(params[:id])
   @order.cancel_order!
+  OrderMailer.notify_cancel(@order).deliver!
   redirect_to :back
 end
 def return
--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -6,6 +6,23 @@ class OrderMailer < ApplicationMailer
 
     mail(to: @user.email , subject: "[JDstore] 感谢您完成本次的下单，以下是您本次购物明细 #{order.token}")
   end
+
+  def notify_ship(order)
+    @order      = order
+    @user       = order.user
+    @product_lists = @order.product_lists
+
+    mail(to: @user.email, subject: "[JDStore] 您的订单 #{order.token}已发货")
+  end
+
+  def notify_cancel(order)
+    @order      = order
+    @user       = order.user
+    @product_lists = @order.product_lists
+
+    mail(to: @user.email, subject: "[JDStore] 您的订单 #{order.token}已取消")
+  end
+
   def apply_cancel(order)
     @order      = order
     @user       = order.user
