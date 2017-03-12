6bf21b7534c0b8d42b00252d57ca5c22414cae24
coco <cococardry@163.com>
2017-03-12 23:28:33 +0800

    add notify_cancel/ship function

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -15,12 +15,14 @@ class Admin::OrdersController < ApplicationController
   def cancel
     @order=Order.find(params[:id])
     @order.cancel_order!
+    OrderMailer.notify_cancel(@order).deliver!
     redirect_to :back
   end
 
   def ship
     @order=Order.find(params[:id])
     @order.ship!
+    OrderMailer.notify_ship(@order).deliver!
     redirect_to :back
   end
 
