9b555d02c84c54e07d933fd53eedafaeb366aba9
skyflyme <cszsxq@gmail.com>
2017-03-13 22:55:06 +0800

    add admin/orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -12,7 +12,7 @@ class Order < ApplicationRecord
 
   def pay!
     self.update_columns(is_paid: true )
-  end  
+  end
 
 
   validates :billing_name, presence: true
@@ -21,4 +21,35 @@ class Order < ApplicationRecord
   validates :shipping_address, presence: true
 
   has_many :product_lists
+
+  include AASM
+
+  aasm do
+    state :order_placed, initial: true
+    state :paid
+    state :shipping
+    state :shipped
+    state :order_cancelled
+    state :good_returned
+
+    event :make_payment, after_commit: :pay! do
+      transitions from: :order_placed, to: :paid
+    end
+
+    event :ship do
+      transitions from: :paid,         to: :shipping
+    end
+
+    event :deliver do
+      transitions from: :shipping,     to: :shipped
+    end
+
+    event :return_good do
+      transitions from: :shipped,      to: :good_returned
+    end
+
+    event :cancel_order do
+      transitions from: [:order_placed, :paid], to: :order_cancelled
+    end
+  end
 end
