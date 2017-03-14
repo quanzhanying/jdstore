b33bbc78e37bf4157886e848a62036337910b3dc
xyy601chenyan <xyy545688506@gmail.com>
2017-03-14 10:33:51 +0800

    implement rucaptcha

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   post '/rate' => 'rater#create', :as => 'rate'
   resources :comments
-  devise_for :users, :controllers => { :registrations => "users/registrations" }
+  devise_for :users, :controllers => { :registrations => "users/registrations",
+  passwords: 'users/passwords' }
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'welcome#index'
 
@@ -68,4 +69,5 @@ namespace :account do
 
 end
 
+mount RuCaptcha::Engine => "/rucaptcha"
 end
