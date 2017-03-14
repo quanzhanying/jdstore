1cc1f9a9c18ab1253ba3f53e8970eb65f4bb34ec
aliceguo2016reborn <aliceguo2016reborn@gmail.com>
2017-03-14 23:22:19 +0800

    implement carts index function

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -29,7 +29,7 @@ class Admin::ProductsController < ApplicationController
   def update
     @product = Product.find(params[:id])
 
-    if @product = Product.update(product_params)
+    if @product.update(product_params)
       redirect_to admin_products_path
     else
       render :edit
