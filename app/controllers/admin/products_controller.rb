b8cc16f1350d4a002a83edacaefbde1f8df45031
mavel <69673480@qq.com>
2017-03-15 14:14:16 +0800

    admin access and layout

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,18 +1,19 @@
 class Admin::ProductsController < ApplicationController
+before_action :authenticate_user!
+before_action :admin_required
+layout "admin"
+  before_action :find_product, only: [:edit,   :destroy,:update,:show]
   def index
      @products = Product.all
-
     end
   def new
       @product = Product.new
    end
     def show
-       @product = Product.find(params[:id])
     end
 
     def create
       @product = Product.new(product_params)
-
       if @product.save
         redirect_to admin_products_path
       else
@@ -21,18 +22,15 @@ class Admin::ProductsController < ApplicationController
     end
 
     def edit
-       @product = Product.find(params[:id])
      end
+
      def destroy
-       @product = Product.find(params[:id])
        flash[:alert] = @product.title+" 已经被删除"
        @product.destroy
-
        redirect_to admin_products_path
      end
-     def update
-       @product = Product.find(params[:id])
 
+     def update
        if @product.update(product_params)
          redirect_to admin_products_path
        else
@@ -42,7 +40,9 @@ class Admin::ProductsController < ApplicationController
 
 
     private
-
+    def find_product
+          @product = Product.find(params[:id])
+    end
     def product_params
       params.require(:product).permit(:title, :description, :quantity, :price)
     end
