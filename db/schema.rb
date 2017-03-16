c8537e2080b97bf737ca4580cfff6c5f01052813
mavel <69673480@qq.com>
2017-03-16 14:59:52 +0800

    add token to order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316064447) do
+ActiveRecord::Schema.define(version: 20170316065659) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170316064447) do
     t.string   "shipping_address"
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
+    t.string   "token"
   end
 
   create_table "product_lists", force: :cascade do |t|
