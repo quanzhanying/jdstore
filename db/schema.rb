a60b833c471708a44cafc8cc48785aa76cd95694
mavel <69673480@qq.com>
2017-03-16 14:46:24 +0800

    product_list model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316063419) do
+ActiveRecord::Schema.define(version: 20170316064447) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 20170316063419) do
     t.string   "shipping_address"
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
+  end
+
+  create_table "product_lists", force: :cascade do |t|
+    t.integer  "order_id"
+    t.string   "product_name"
+    t.integer  "product_price"
+    t.integer  "quantity"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "products", force: :cascade do |t|
