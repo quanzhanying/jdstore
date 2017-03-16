3953d3830aafd1b93972ba66d35b58f408fef775
mavel <69673480@qq.com>
2017-03-16 14:41:10 +0800

    order controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         def admin?
+  has_many :orders
+
+ def admin?
    is_admin
  end
+
+
 end
