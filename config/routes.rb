be602f7ec1aa5e4f4af412d6cc5ec06b0eaadc1d
ranty <ranty720@gmail.com>
2017-03-21 22:43:08 +0800

    touch checkout.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
         post :checkout
     end
   end
+  resources :orders
   resources :cart_items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
