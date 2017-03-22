695cd11aae87416569bb342573bec01e5b60dd10
aliceguo2016reborn <aliceguo2016reborn@gmail.com>
2017-03-22 19:28:14 +0800

    add product model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
+  has_many :orders
+
   def admin?
     is_admin
-  
+
   end
 
 end
