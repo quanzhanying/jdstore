e345ca956e942d38386aada896193ebebeac5faa
rockyjia <jrk1987@gmail.com>
2017-04-02 00:04:55 +0800

    add bootstrap flash function

--- config/application.rb
+++ config/application.rb
@@ -7,9 +7,9 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Jdstore
-  class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-  end
+    class Application < Rails::Application
+        # Settings in config/environments/* take precedence over those specified here.
+        # Application configuration should go into files in config/initializers
+        # -- all .rb files in that directory are automatically loaded.
+    end
 end
