b365edf667e3e9045b8373860d36924ebb5e8763
rockyjia <jrk1987@gmail.com>
2017-04-08 23:47:36 +0800

    implement cart_total_price function

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,5 +1,5 @@
 module CartsHelper
-    def render_cart_total_price
+    def render_cart_total_price(cart)
         cart.total_price
     end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -12,9 +12,11 @@ class Cart < ApplicationRecord
 
     def total_price
         sum = 0
-      cart_items.each do |cart_item|
-        if cart_item.product.price.present?
-            sum += cart_item.quantity * cart_item.product.price
+        cart_items.each do |cart_item|
+            if cart_item.product.price.present?
+                sum += cart_item.quantity * cart_item.product.price
+            end
+
         end
         sum
     end
