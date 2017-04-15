9b1d75403a796be6697f01ad32f1b990e4af24d2
rockyjia <jrk1987@gmail.com>
2017-04-15 23:20:34 +0800

    fix method post/get bug

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -33,7 +33,7 @@ class OrdersController < ApplicationController
     def pay_with_alipay
         @order = Order.find_by_token(params[:id])
         @order.set_payment_with!('alipay')
-        @order.nake_payment!
+        @order.make_payment!
 
         redirect_to order_path(@order.token), notice: '使用支付宝成功完成付款'
     end
@@ -44,6 +44,13 @@ class OrdersController < ApplicationController
         @order.make_payment!
 
         redirect_to order_path(@order.token), notice: '使用微信成功完成付款'
+    end
+
+    def apply_to_cancel
+        @order = Order.find(params[:id])
+        OrderMailer.apply_cancel(@order).deliver!
+        flash[:notice] = '已提交申请'
+        redirect_to :back
     end
 
     private
--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -2,7 +2,6 @@ module OrdersHelper
     def render_order_paid_state(order)
         if order.is_paid?
             '已付款'
-
         else
             '未付款'
         end
