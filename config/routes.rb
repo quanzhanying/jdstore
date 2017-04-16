2cd76cfee53dec28388b29b06d6c5e3ca0a0c8ac
you7588 <yo7588@gmail.com>
2017-04-16 11:41:24 +0800

    delete an item from cart

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
      end
    end
 
+   resources :cart_items
+
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
