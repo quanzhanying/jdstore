bfd159510e2cc4317b5f20e0f10b9c8fca230daf
hexiuwei <abchxw@gmail.com>
2017-04-18 07:29:08 +0800

    add assm_state to order

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'font-awesome-rails'
 gem 'mini_magick'
 gem 'carrierwave'
 gem 'letter_opener', group: :development
+gem 'aasm'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- db/migrate/20170417223310_add_is_paid_order.rb
+++ db/migrate/20170417223310_add_is_paid_order.rb
@@ -2,5 +2,7 @@ class AddIsPaidOrder < ActiveRecord::Migration[5.0]
   def change
     add_column :orders, :is_paid, :boolean, default: false
     add_column :orders, :payment_method, :string
+    add_column :orders, :aasm_state, :string, default: "order_placed"
+    add_index  :orders, :aasm_state
   end
 end
