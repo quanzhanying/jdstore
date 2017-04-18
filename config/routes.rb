19417abcf968b347499c382c16d5c1b3bf49c231
hexiuwei <abchxw@gmail.com>
2017-04-18 23:40:33 +0800

    finish assm function

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :admin do
     resources :products
+    resources :orders do
+     member do
+       post :cancel
+       post :ship
+       post :shipped
+       post :return
+     end
+   end
   end
 
   resources :products do
@@ -23,6 +31,7 @@ Rails.application.routes.draw do
      member do
        post :pay_with_alipay
        post :pay_with_wechat
+       post :apply_to_cancel
      end
    end
  namespace :account do
