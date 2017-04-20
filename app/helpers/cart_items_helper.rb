913327925135b485206726b4b54a2da5cb2c1e47
sam <morvazhu@gmail.com>
2017-04-20 14:12:19 +0800

    cart_item update action

--- app/helpers/cart_items_helper.rb
+++ app/helpers/cart_items_helper.rb
@@ -1,2 +1,14 @@
 module CartItemsHelper
+  def update
+    @cart = current_cart
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to carts_path
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity)
+  end
 end
