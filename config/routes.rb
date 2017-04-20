2b3e02eeb23481f7feba109600077feb73441787
ranty <ranty720@gmail.com>
2017-04-20 22:31:00 +0800

    add payment_method function

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,12 @@ Rails.application.routes.draw do
         post :checkout
     end
   end
-  resources :orders
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+    end
+  end
   resources :cart_items
   namespace :account do
     resources :orders
