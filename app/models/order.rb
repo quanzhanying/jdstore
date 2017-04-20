3cd32888e4b0115d8b1d7e0d4e0442bdf1fa90ca
sam <morvazhu@gmail.com>
2017-04-20 15:39:06 +0800

    def generate_token

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
+  before_create :generate_token
+
   belongs_to :user
 
   validates :billing_name, presence: true
@@ -7,4 +9,8 @@ class Order < ApplicationRecord
   validates :shipping_address, presence: true
 
   has_many :product_lists
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end  
 end
