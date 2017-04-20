4857b334f84707fd075ebae339b62b7de58c847c
ranty <ranty720@gmail.com>
2017-04-20 21:44:28 +0800

    add token to order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:create]
   def show
-    @order = Order.find(params[:id])
+    @order = Order.find_by_token(params[:id])
     @product_lists = @order.product_lists
   end
   def create
@@ -17,7 +17,7 @@ class OrdersController < ApplicationController
         product_list.quantity = cart_item.quantity
         product_list.save
       end
-      redirect_to order_path(@order)
+      redirect_to order_path(@order.token)
     else
       render 'carts/checkout'
     end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
+  before_create :generate_token
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
   belongs_to :user
   validates :billing_name, presence: true
   validates :billing_address, presence: true
