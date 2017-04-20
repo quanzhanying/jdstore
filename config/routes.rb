0911ef9364555ab64d279bc2c495afa693209403
sam <morvazhu@gmail.com>
2017-04-20 19:04:07 +0800

    add payment on view, controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,13 @@ Rails.application.routes.draw do
     end
   end
   resources :cart_items
-  resources :orders
   namespace :account do
     resources :orders
-  end  
+  end
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+    end
+  end   
 end
