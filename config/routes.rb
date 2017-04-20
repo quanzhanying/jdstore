8755f361690b81f89d7221b8d84ff0d09c53b961
zhubaocheng <imzhubaocheng@gmail.com>
2017-04-20 20:44:55 +0800

    empty cart

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
-resources :carts
+resources :carts do
+  collection do
+    delete :clean
+  end
+end
 resources :products do
   member do
     post :add_to_cart
