df7b650c8312bbecb81c82c3fe1a5b935079f9b0
sam <morvazhu@gmail.com>
2017-04-20 14:36:29 +0800

    debug helper to controller

--- app/helpers/cart_items_helper.rb
+++ app/helpers/cart_items_helper.rb
@@ -1,14 +1,2 @@
 module CartItemsHelper
-  def update
-    @cart = current_cart
-    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
-    @cart_item.update(cart_item_params)
-    redirect_to carts_path
-  end
-
-  private
-
-  def cart_item_params
-    params.require(:cart_item).permit(:quantity)
-  end
 end
