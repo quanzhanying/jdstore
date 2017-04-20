6165f2f8afa85ba5d8bb317123d51ec1c03effc9
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-04-20 10:11:48 +0800

    add is_admin function

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :require_is_admin
 
   def index
     @products = Product.all
