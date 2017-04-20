69319df970c658bc24f9384be1785553cefea1bd
sam <morvazhu@gmail.com>
2017-04-20 18:19:04 +0800

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420101535) do
+ActiveRecord::Schema.define(version: 20170420101739) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20170420101535) do
     t.datetime "updated_at",                       null: false
     t.string   "token"
     t.boolean  "is_paid",          default: false
+    t.string   "payment_method"
   end
 
   create_table "product_lists", force: :cascade do |t|
