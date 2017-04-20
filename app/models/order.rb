8cb9f2fc2892a8eb813a15f39e12092f274618bd
sam <morvazhu@gmail.com>
2017-04-20 15:12:03 +0800

    association between model order and product_list

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,5 +4,7 @@ class Order < ApplicationRecord
   validates :billing_name, presence: true
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
-  validates :shipping_address, presence: true  
+  validates :shipping_address, presence: true
+
+  has_many :product_lists
 end
