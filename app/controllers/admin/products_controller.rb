d157f15831431c5607300778c7fec2a394bf4678
hexiuwei <abchxw@gmail.com>
2017-04-24 00:24:41 +0800

    add category function 1

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -9,15 +9,17 @@ class Admin::ProductsController < ApplicationController
 
    def new
       @product = Product.new
+      @categories = Category.all.map { |c| [c.name, c.id] }
    end
 
    def edit
       @product = Product.find(params[:id])
+      @categories = Category.all.map { |c| [c.name, c.id] }
    end
 
    def update
      @product = Product.find(params[:id])
-
+     @product.category_id = params[:category_id]
      if @product.update(product_params)
        redirect_to admin_products_path
      else
@@ -27,7 +29,7 @@ class Admin::ProductsController < ApplicationController
 
    def create
       @product = Product.new(product_params)
-
+      @product.category_id = params[:category_id]
       if @product.save
         redirect_to admin_products_path
       else
@@ -38,6 +40,6 @@ class Admin::ProductsController < ApplicationController
    private
 
    def product_params
-     params.require(:product).permit(:title, :description, :quantity, :price, :image)
+     params.require(:product).permit(:title, :description, :quantity, :price, :image, :category_id)
    end
 end
