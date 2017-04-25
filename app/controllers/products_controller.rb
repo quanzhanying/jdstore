879cfa97a5f9595293114acd86440927e60746d3
hexiuwei <abchxw@gmail.com>
2017-04-25 11:18:30 +0800

    finish categroy function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,17 @@
 class ProductsController < ApplicationController
   def index
-   @products = Product.all
+  #   if params[:category].blank?
+  #     @products = Product
+  #   else
+  #     @category_id = Category.find_by(name:params[:category]).id
+  #     @products = Product.where(:category_id => @category_id).order("created_at DESC")
+  #   end
+  # @products = Product.all.recent
+    if params[:category].present?
+      @products = Product.all.where(category_id: params[:category]).recent
+    else
+      @products = Product.all.recent
+    end
   end
 
   def show
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :category
+
+  scope :recent, -> { order("created_at DESC")}
 end
