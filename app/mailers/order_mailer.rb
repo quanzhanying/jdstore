199f16d381ba2935418dd43414ded05158f53522
ranty <ranty720@gmail.com>
2017-04-25 22:01:54 +0800

    debug

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -5,7 +5,7 @@ class OrderMailer < ApplicationMailer
     @product_lists = @order.product_lists
     mail(to: @user.email, subject: "[JDstore] 感谢您完成本次的下单，以下是您这次购物明细 #{order.token}")
   end
-  def apply_canel(order)
+  def apply_cancel(order)
     @order = order
     @user = order.user
     @product_lists = @order.product_lists
