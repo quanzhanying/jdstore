7fd9bcce87c3ef041d90b7a18e36a198dd7cdc6f
zhubaocheng <imzhubaocheng@gmail.com>
2017-04-27 23:06:19 +0800

    已经加入购物车的商品，不能重复被加入

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,8 +9,12 @@ class ProductsController < ApplicationController
 
   def add_to_cart
     @product = Product.find(params[:id])
-    current_cart.add_product_to_cart(@product)
-    flash[:notice] = "成功加入购物车"
+    if !current_cart.products.include?(@product)
+      current_cart.add_product_to_cart(@product)
+      flash[:notice] = "你已成功将 #{@prduct.title}加入购物车"
+    else
+      flash[:warning] = "此商品已在购物车内"
+    end
     redirect_to :back
   end
 end
