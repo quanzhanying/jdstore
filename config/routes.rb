9de88f09771fb2a9af7df77e06f1cf6dba494c98
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-04-28 11:54:53 +0800

    implement carts index functin

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
 
   root 'products#index'
 
+  resources :carts
+
   resources :products do
     member do
       post :add_to_cart
-    end  
+    end
   end
 
   namespace :admin do
