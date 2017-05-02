114ee9d6dea5a7acc4e3d7d8324f770cea3bc39b
chanweiyan <chanweiyan007@gmail.com>
2017-05-02 18:42:37 +0800

    implement admin/products#edit/update/destroy/show

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -5,14 +5,36 @@ class Admin::ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+  
   def new
     @product = Product.new
+  end
+
+  def edit
+    @product = Product.find(params[:id])
   end
 
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to admin_products_path, notice: "产品新建成功！"
+      redirect_to admin_products_path, notice: "商品新建成功！"
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_products_path, notice: "商品修改成功！"
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    if @product.destroy
+      redirect_to admin_products_path, alert: "商品已经删除！"
     end
   end
 
