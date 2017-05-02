f4cd595fac679eb1ad97b8ac374e4fab6a2acead
superzrr <superzrr@gmail.com>
2017-05-02 23:27:53 +0800

    implement carrierwave install and  admin/product imge upload

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -50,7 +50,7 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description,:price, :quantity)
+    params.require(:product).permit(:title, :description,:price, :quantity, :image)
   end
 
   def require_is_admin
