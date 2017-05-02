d3b783248c5233016da5f5bea1108e4faa5dceda
seewhymoon <seewhymoon@gmail.com>
2017-05-02 16:37:03 +0800

    implement image upload

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -49,6 +49,6 @@ layout "admin"
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price)
+    params.require(:product).permit(:title, :description, :quantity, :price, :image)
   end
 end
