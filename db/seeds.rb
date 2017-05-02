95b05756fc7cbbb9659f72d593ae40cdd2a7d86a
Helen <helenmayanna@gmail.com>
2017-05-02 23:17:26 +0800

    move sqlite2 to dev group add pg to production group

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 u = User.new
-u.email = "admin@test.com"           # 可以改成自己的 email
+u.email = "275208067@qq.com"           # 可以改成自己的 email
 
 u.password = "123456"                # 最少要六码
 
