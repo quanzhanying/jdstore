4970abba7e381ae68d26390918c3ce6cf8aea49d
eagle111 <18645125206@sina.cn>
2017-05-02 18:10:04 +0800

    admin_CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502075857) do
+ActiveRecord::Schema.define(version: 20170502083918) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "price"
+    t.integer  "quantity"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
