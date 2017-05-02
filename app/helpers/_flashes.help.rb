72409f7a4b242e814fa5163d89144697820dde23
Lotus <lvdeshi2011@gmail.com>
2017-05-02 16:53:30 +0800

    implement sign in

--- app/helpers/_flashes.help.rb
+++
@@ -1,9 +0,0 @@
-moduel Flasheshelper
-  FLASH_CLASS = { alert: "danger", notice: "success", warning:"warning"}.freeze
-  def flash_class(key)
-    FLASH_CLASSES.fetch key.to_sym,key
-  end
-  def user_facing_flashes
-    flash.to_hash.slice "alert","notice","warning"
-  end
-end
