36485987ec368aaf3b4ef0db0bcd4c02f253fa49
黄永飞 <h739578083@gmail.com>
2017-05-03 15:44:00 +0800

    config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :admin do
-    resources :products
-  end
   root 'welcome#index'
 end
