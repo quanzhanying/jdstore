ebf7cb1ebacd7e120bbfd4149e13c222880cfc32
jonathan <zhanggongzifei@163.com>
2017-05-03 15:32:49 +0800

    change admin/product backend panel

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root "welcomes#index"
+root "products#index"
 
 namespace :admin do
    resources :products
 end
+
+
+resources :products
 
 end
