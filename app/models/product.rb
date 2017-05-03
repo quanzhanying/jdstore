4c734086c70fb712d3065d35570bde75cfcff4d2
jonathan <zhanggongzifei@163.com>
2017-05-03 13:07:40 +0800

    install annotate

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  price       :integer
+#  quantity    :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Product < ApplicationRecord
 end
