83b08eacb2ee3d9548f4c416ddb04f1c0af91b87
sfxg27@gmail.com <feixiguang@feixiguangdeMacBook-Pro.local>
2017-05-03 17:45:00 +0800

    change admin/product backend panel

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -13,9 +13,6 @@ class Admin::ProductsController < ApplicationController
     @product = Product.new
   end
 
-  def show
-    @product = Product.find(params[:id])
-  end
 
   def edit
     @product = Product.find(params[:id])
