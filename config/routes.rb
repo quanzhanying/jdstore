152e322382e4ad9cbbf317a9871618677e0d7188
emilesu <emilesu@qq.com>
2017-05-04 00:36:19 +0800

    add welcome pag and flashes alert

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :products       #后台上架网址 admin/products
   end
 
+  root 'welcome#index'
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
