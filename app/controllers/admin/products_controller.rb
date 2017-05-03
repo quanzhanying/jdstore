6f8ffbe34f8a88e4df5c67fa23ec73a6c5a55209
maxinlei <maxinlei_public@126.com>
2017-05-03 12:39:08 +0800

    devise2

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,5 @@
 class Admin::ProductsController < ApplicationController
   before_action :authenticate_user!
 
-  def index
-    @products = Product.all
-  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     resources :products
   end
 
-  root "welcome#index"
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
