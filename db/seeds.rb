a0b77d34858f6826729394f3a0fe94af12863fd0
superzrr <superzrr@gmail.com>
2017-05-03 20:36:47 +0800

    img-thumbnail set to center

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 u = User.new
+
 u.email = "bigmouthcrow@gmail.com"           # 可以改成自己的 email
 
 u.password = "123456"                # 最少要六码
 
 u.password_confirmation = "123456"   # 最少要六码
 
-u.is_admin = true
+u.is_admin = false
 
 u.save
