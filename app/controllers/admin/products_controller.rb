810c14037eac6fa23a35f624cc21ccfcd8092e7d
xiejianan <xiejianan86@gmail.com>
2017-05-04 00:33:22 +0800

    implement image upload

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,9 @@
 class Admin::ProductsController < ApplicationController
+  layout "admin"
   before_action :authenticate_user!
   before_action :admin_required
-  layout "admin"
+
+
   def new
     @product = Product.new
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 u = User.new
 u.email = "1@1.com"           # 可以改成自己的 email
 
-u.password = "11111"                # 最少要六码
+u.password = "111111"                # 最少要六码
 
 u.password_confirmation = "111111"   # 最少要六码
 
