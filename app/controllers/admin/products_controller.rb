3fec11293864fa44cbd9b4a2e26e97e39ee11473
like <20783506632@qq.com>
2017-05-03 16:04:03 +0800

    only admin can access bachend panel

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,8 @@
 class Admin::ProductsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :admin_required
+
+layout "admin"
 
   def new
     @product = Product.new
