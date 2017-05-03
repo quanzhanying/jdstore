f81135aa0b5eddc9dad933f10d8a10bd2afdce4b
emilesu <emilesu@qq.com>
2017-05-03 23:07:58 +0800

    add bootstrap and install simple_form

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -2,13 +2,59 @@ class Admin::ProductsController < ApplicationController
   before_action :authenticate_user! only: [:new, :create, :edit, :update, :destroy, :indext]
 
 
+  # ---CRUD---
+
   def index
     @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.user = current_user
+
+    if @product.save
+      redirect_to admin_products_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to admin_products_path
   end
 
 
 
 
+  # ---private---
 
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :quantity)
+  end
 
 end
