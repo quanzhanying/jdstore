82cd263f88412c4dc29f95caed4e7c81701ab10d
maxinlei <maxinlei_public@126.com>
2017-05-03 16:19:46 +0800

    products image

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
+  resources :products
 
   root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
