a723a1456470172b42c474bbccfe6d87e61ff732
two_app <wangyuliangf@gmail.com>
2017-05-04 23:22:53 +0800

    implement image upload

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'bootstrap-sass'
 gem 'devise'
 gem 'simple_form'
 gem 'font-awesome-rails'
+gem 'carrierwave'
+gem 'mini_magick'
 
 
 group :development do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504100005) do
+ActiveRecord::Schema.define(version: 20170504130508) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
