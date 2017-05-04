2115cfef83ed24140c92eeb515104cb2cd694c4b
jonathan <zhanggongzifei@163.com>
2017-05-04 16:21:17 +0800

    add one seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,14 @@ u.password_confirmation = "111111"   # 最少要六码
 
 u.is_admin = true
 u.save
+
+
+u = User.new
+u.email = "1092167387@qq.com"           # 可以改成自己的 email
+
+u.password = "111111"                # 最少要六码
+
+u.password_confirmation = "111111"   # 最少要六码
+
+u.is_admin = true
+u.save
