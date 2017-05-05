788bb41fa28134cf787f0bab2b841e82551c38f3
wuzhao <liwuzhao97@gmail.com>
2017-05-05 14:38:13 +0800

    controller products

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  root 'products#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :admin do
     resources :products
   end
+
+  resources :products
 end
