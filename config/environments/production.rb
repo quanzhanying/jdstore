24f8fba01a87a5adca16512890def60ab890d0e4
hexiuwei <abchxw@gmail.com>
2017-05-05 23:46:13 +0800

    add script-7

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,9 +23,11 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
-
+  config.assets.precompile += Ckeditor.assets
+  config.assets.precompile += %w(ckeditor/* )
+  config.autoload_paths += %W(/app/models/ckeditor)
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
