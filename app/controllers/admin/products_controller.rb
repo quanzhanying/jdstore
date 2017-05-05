6a3f4d19731680e9ad1ebd59f49f000bf44d9440
wangxing <18912540921@163.com>
2017-05-05 21:35:50 +0800

    implement product show

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -45,7 +45,7 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price. :image)
+    params.require(:product).permit(:title, :description, :quantity, :price, :image)
   end
 
 
