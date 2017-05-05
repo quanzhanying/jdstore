ea7b3cbe358bdf6c1c572dd7512562c7a0946021
hexiuwei <abchxw@gmail.com>
2017-05-05 23:33:03 +0800

    add script-6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,9 +25,6 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
-  config.assets.precompile += Ckeditor.assets
-  config.assets.precompile += %w(ckeditor/* )
-  config.autoload_paths += %W(#{config.root}/app/models/ckeditor) 
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
