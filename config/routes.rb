81368253fb83dce62c23892ea293e7eab535a8f4
nn70 <nn4work@gmail.com>
2017-05-05 23:42:38 +0800

    add icon to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
+  get 'products' => 'products#index'
+  get 'welcome' => 'welcome#index'
   get 'about' => 'welcome#about'
   get 'how' => 'welcome#how'
   get 'branch' => 'welcome#branch'
