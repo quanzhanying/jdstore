b0a577cd5954620bc8ccaae8d3752735c7a8e302
hexiuwei <abchxw@gmail.com>
2017-05-05 22:52:06 +0800

    add script-1

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
-config.autoload_paths += %W(#{config.root}/app/models/ckeditor) 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
