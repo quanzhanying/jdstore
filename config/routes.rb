75ec787fa232ea2e2894611c744e121d3f8907aa
emma <emmayuan@163.com>
2017-05-06 19:22:55 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  root 'products#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
 
   namespace :admin do
       resources :products
   end
-  
+
+  resources :products
+
 end
