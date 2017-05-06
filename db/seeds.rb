d02fedd0d53a6913371dccdc3d274de500ff073c
nn70 <nn4work@gmail.com>
2017-05-07 02:17:50 +0800

    small fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 u = User.new
-u.email = "b@a.com"           # 可以改成自己的 email
+u.email = "a@a.com"           # 可以改成自己的 email
 
 u.password = "123456"                # 最少要六码
 
