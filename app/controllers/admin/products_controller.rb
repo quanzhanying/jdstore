add19852f9ef60e3c84ec08a670b3e345c7b1c82
郑笑飞“git config --global user.email 13949836783@163.com” <zhengxiaofei@zhengxiaofeideMacBook-Air.local>
2017-05-06 11:44:47 +0800

    implement image upload

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -54,7 +54,7 @@ end
 private
 
 def product_params
-  params.require(:product).permit(:title, :description, :price, :quantity)
+  params.require(:product).permit(:title, :description, :price, :quantity, :image)
 end
 
 end
