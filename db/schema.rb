a2e038c38937bba8707eec773430b4e2da630930
Leo <leo@LeodeMacBook-Pro.local>
2017-05-07 22:48:57 +0800

    add login css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504170045) do
+ActiveRecord::Schema.define(version: 20170507102932) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20170504170045) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "is_admin",               default: false
+    t.string   "name"
+    t.integer  "user_type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
