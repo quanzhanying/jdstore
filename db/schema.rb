51164a6403c5fbf0f48231e08c9351dc12e4ab07
hubodashi <hubodashi@aliyun.com>
2017-05-07 15:32:37 +0800

    implement product  CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506235347) do
+ActiveRecord::Schema.define(version: 20170507061729) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "price"
+    t.string   "amount"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
