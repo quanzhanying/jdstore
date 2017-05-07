ba48f073d44b2e0695a21ae71d9b0160ccf60a9c
paulshu <paulshu18@gmail.com>
2017-05-07 12:03:53 +0800

    implement image upload

--- Gemfile
+++ Gemfile
@@ -33,6 +33,9 @@ gem 'bootstrap-sass'
 gem 'devise'
 gem 'simple_form'
 gem 'font-awesome-rails'
+gem 'carrierwave'
+gem 'mini_magick'
+gem 'annotate'
 
 
 group :development, :test do
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  quantity    :integer
+#  price       :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  image       :string
+#
+
 class Product < ApplicationRecord
   validates :title, presence: true
+
+  mount_uploader :image, ImageUploader
 end
