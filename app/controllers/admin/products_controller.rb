29fc00fc021da4487fbcb5bc56baecefda33b9e7
johsnow <johsnow08@gmail.com>
2017-05-08 22:31:32 +0800

    implement products CRUD

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,52 @@
+class Admin::ProductsController < ApplicationController
+  def idex
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to admin_products_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_products_path, alert: "update success!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to admin_products_path, alert: "product deleted"
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :quantity)
+  end
+
+
+
+
+end
