2c4b0b0ea97f92a4dc76ce9877caf6e0d848e121
like <20783506632@qq.com>
2017-05-08 21:58:55 +0800

    implement cart_total_price function

---
+++ app/helpers/carts_helper.rb
@@ -0,0 +1,5 @@
+module CartsHelper
+  def render_cart_total(cart)
+  cart.total_price
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ root 'products#index'
       post :add_to_cart
     end
   end
+
+  resources :carts
 end
