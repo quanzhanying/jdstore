0abbbdbaf83a677b9ce3eea3857fa05eb50dd555
陈彦锋 <chenyanfenghappy@163.com>
2017-05-08 22:59:28 +0800

    add add_to_cart button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,9 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    redirect_to :back    
+  end
 end
