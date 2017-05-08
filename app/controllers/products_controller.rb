d3adcc8ad60282eb295a0c08205f724698f5ac86
Yechti <yechti@gmail.com>
2017-05-09 03:32:14 +0800

    add add_to_cart button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,11 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    redirect_to :back
+    flash[:notice] = "測試加入購物車"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :products
-
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
+  
 end
