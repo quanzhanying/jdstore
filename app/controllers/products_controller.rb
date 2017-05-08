b4c5e4541f1c8e252e24495f8b19859440abc8e9
like <20783506632@qq.com>
2017-05-08 16:45:11 +0800

    add add_to_cart button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,14 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    redirect_to :back
+    flash[:notice] = "测试成功加入购物车"
+  end
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ root 'products#index'
     resources :products
   end
 
-  resources :products
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
 end
