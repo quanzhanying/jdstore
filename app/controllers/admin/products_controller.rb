ea13e117f948bccfcf0cd9601cff344caf967e01
zhuozhi12 <feibou@126.com>
2017-05-08 13:15:37 +0800

    install category

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -9,37 +9,35 @@ class Admin::ProductsController < ApplicationController
    @products = Product.all
  end
 
-  def new
-    @product = Product.new
-  end
+ def new
+ @product = Product.new
+ @categories = Category.all.map { |c| [c.name, c.id] }
+end
+def create
+ @product = Product.new(product_params)
+ @product.category_id = params[:category_id]
+ if @product.save
+   redirect_to admin_products_path
+ else
+   render :new
+ end
+end
+def edit
+ @product = Product.find(params[:id])
+ @categories = Category.all.map { |c| [c.name, c.id] } #这一行为加入的代码
+end
+def update
+ @product = Product.find(params[:id])
+ @product.category_id = params[:category_id]
+ if @product.update(product_params)
+   redirect_to admin_products_path
+ else
+   render :edit
+ end
+end
 
-  def edit
-     @product = Product.find(params[:id])
-   end
-
-   def update
-     @product = Product.find(params[:id])
-
-     if @product.update(product_params)
-       redirect_to admin_products_path
-     else
-       render :edit
-     end
-   end
-
-  def create
-    @product = Product.new(product_params)
-
-    if @product.save
-      redirect_to admin_products_path
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price, :address, :image)
+private
+def product_params
+ params.require(:product).permit(:title, :description, :quantity, :price, :address, :image, :category_id)
   end
 end
