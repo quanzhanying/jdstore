62a47f7ea7aa03339ca305ec913d6c7111914789
JUN MENG <smilingjun@MacBook-Pro.pavlov.gateway>
2017-05-08 08:20:53 -0400

    implement backend CRUD

---
+++ app/helpers/admin/products_helper.rb
@@ -0,0 +1,26 @@
+module Admin::ProductsHelper
+
+    class Admin::ProductsController < ApplicationController
+      def new
+        @product = Product.new
+      end
+
+      def create
+        @product = Product.new(product_params)
+
+        if @product.save
+          redirect_to admin_products_path
+        else
+          render :new
+        end
+      end
+
+      private
+
+      def product_params
+        params.require(:product).permit(:title, :description, :quantity, :price)
+      end
+    end
+
+
+end
