ee745939831338fbb2d03c428a26adb1d7e44bfb
dumpling001 <dumpling001@gmail.com>
2017-05-08 13:57:02 +0800

    implement product show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,11 @@
 class ProductsController < ApplicationController
- def index
-   @products = Product.all
- end
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
 end
