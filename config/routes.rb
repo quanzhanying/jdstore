ea5f07de13b312c4ad47173817f2a24307781f7a
zhuqi <ardentyouny>
2017-05-08 15:25:16 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
-
+  resources :products
   devise_for :users
-  root 'welcome#index'
+  root 'products#index'
 end
