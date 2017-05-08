be96bd0dcdc533f3eeadb7da7fd3c83c8608ced2
eagle111 <18645125206@sina.cn>
2017-05-08 18:25:03 +0800

    empty cart

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
       post :add_to_cart
     end
   end
-  resources :carts
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end
   root 'welcome#index'
 end
