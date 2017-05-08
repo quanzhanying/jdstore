e9e6d655c6a21feb421894dcba60354cd411da1d
zoudongdong <alpeveryday@gmail.com>
2017-05-08 19:54:09 +0800

    implement image upload

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,10 @@
 class Admin::ProductsController < ApplicationController
+
+  layout "admin"
+
+  before_action :authenticate_user!
+  before_action :admin_required
+
   def index
     @products = Product.all
   end
@@ -20,7 +26,7 @@ class Admin::ProductsController < ApplicationController
       render :edit
     end
   end
-  
+
   def create
     @product = Product.new(product_params)
 
@@ -34,6 +40,6 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price)
+    params.require(:product).permit(:title, :description, :quantity, :price, :image)
   end
 end
