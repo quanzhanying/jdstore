dc0f2da70091bc78df7d7dc360b6bda99d36fffb
csxgel <DonaldZhang@zhangsixudeMacBook-Pro.local>
2017-05-09 15:52:34 +0800

    implement admin/products CRUD

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,50 @@
+class Admin::ProductsController < ApplicationController
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "商品添加成功"
+      redirect_to admin_products_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to admin_products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :quantity, :price)
+  end
+
+end
