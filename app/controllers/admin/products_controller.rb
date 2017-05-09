0bc16af22f24569d98cf05f254cb0b676f20545f
zengjinlin <kinglinxinsen@gmail.com>
2017-05-09 21:56:31 +0800

    only admin can access backend panel

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,18 @@
 class Admin::ProductsController < ApplicationController
+  layout "admin"
+  before_action :authenticate_user!
+  before_action :admin_required
+
+  def index
+    @products = Product.all
+  end
+
   def new
     @product = Product.new
+  end
+
+  def edit
+    @product = Product.find(params[:id])
   end
 
   def create
@@ -10,13 +22,6 @@ class Admin::ProductsController < ApplicationController
     else
       render :new
     end
-  end
-  def index
-    @products = Product.all
-  end
-
-  def edit
-    @product = Product.find(params[:id])
   end
 
   def update
