e7c46a5e12a5236d0452f85177f34999a6a08ff3
anndoko <handcuffanndo@gmail.com>
2017-05-09 18:20:48 +0800

    only admin can access backend panel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def admin_required
+    if !current_user.admin?
+      redirect_to "/", alert: "You are not an admin."
+    end
+  end
 end
