9b273cea6d5a1ac53a621f8b8522d19f634b45a8
wuxiao <wudawuxiao1375@163.com>
2017-05-09 18:56:54 +0800

    delete an item from cart

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,18 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
+  
   resources :products do
     member do
       post :add_to_cart
     end
   end
+
   resources :carts do
     collection do
       delete :clean
     end
-  end 
+  end
+
+  resources :cart_items
 end
