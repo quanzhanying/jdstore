c3277f37d6aab34235400a8cd3f3a54c82e66065
havenwei <havenwei82@163.com>
2017-05-09 15:03:19 +0800

    implement cart_total_price function

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
+
+  resources :carts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
