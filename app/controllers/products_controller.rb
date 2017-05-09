87b1df2d28b1c8ad6eb69fe92800ab33327b1973
wuxiao <wudawuxiao1375@163.com>
2017-05-09 17:16:34 +0800

    implement current_cart function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -44,13 +44,29 @@ class ProductsController < ApplicationController
   def add_to_cart
      @product = Product.find(params[:id])
      current_cart.add_product_to_cart(@product)
+     flash[:notice] = "成功加入购物车"
      redirect_to :back
-     flash[:notice] = "测试加入购物车"
    end
+
+ helper_method :current_cart
+
+  def current_cart
+    @current_cart ||= find_cart
+  end
 
   private
 
   def product_params
     params.require(:product).permit(:title, :description, :price, :quantity, :is_hidden)
   end
+
+ def find_cart
+   cart = Cart.find_by(id: session[:cart_id])
+   if cart.blank?
+     cart = Cart.create
+   end
+   session[:cart_id] = cart.id
+   return cart
+ end
+
 end
