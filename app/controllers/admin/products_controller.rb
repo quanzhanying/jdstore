6985c263b8f68573821496368d505ad8086e7904
hubodashi <hubodashi@aliyun.com>
2017-05-09 23:07:23 +0800

    implement image upload

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -38,6 +38,6 @@ end
 private
 
 def product_params
-  params.require(:product).permit(:title, :description, :quantity, :price)
+  params.require(:product).permit(:title, :description, :quantity, :price, :image)
 end
 end
