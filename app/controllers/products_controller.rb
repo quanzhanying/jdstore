495bd9a7e880116341264c86be60ab8aec401fdb
陈彦锋 <chenyanfenghappy@163.com>
2017-05-09 18:18:21 +0800

    add add_to_cart button,and add flash

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,7 @@ class ProductsController < ApplicationController
 
   def add_to_cart
     @product = Product.find(params[:id])
-    redirect_to :back    
+    redirect_to :back
+    flash[:notice] = "测试加入购物车"    
   end
 end
