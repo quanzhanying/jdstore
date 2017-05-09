74d69bc41121be2acbab0a6c3e0ec37d101f24ea
blueming <wenyiff66@gmail.com>
2017-05-09 17:05:04 +0800

    empty cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -8,4 +8,19 @@ class Cart < ApplicationRecord
     ci.quantity = 1
     ci.save
   end
+
+  def clean!
+    cart_items.destroy_all
+  end
+
+  def total_price
+    sum = 0
+    cart_items.each do |cart_item|
+      if cart_item.product.price.present?
+        sum += cart_item.quantity * cart_item.product.price
+      end
+
+      end
+    sum
+  end
 end
