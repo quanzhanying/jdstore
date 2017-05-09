ec4223bd18e279585c1bbf93aa00b587ed1c0350
黄永飞 <h739578083@gmail.com>
2017-05-09 20:53:37 +0800

    empty cart

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,15 @@ Rails.application.routes.draw do
     resources :products
   end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products do
     member do
       post :add_to_cart
     end
   end
 
-  resources :carts
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end
 end
