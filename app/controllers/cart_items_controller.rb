1c60dacbcbb8cb574524b5831fc4792207d4e3dd
zhuozhi12 <feibou@126.com>
2017-05-09 13:29:43 +0800

    change quantity in cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -12,4 +12,19 @@ class CartItemsController < ApplicationController
     redirect_to :back
   end
 
+  def update
+      @cart = current_cart
+      @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+      @cart_item.update(cart_item_params)
+
+      redirect_to carts_path
+    end
+
+    private
+
+    def cart_item_params
+      params.require(:cart_item).permit(:quantity)
+    end
+  end
+
 end
