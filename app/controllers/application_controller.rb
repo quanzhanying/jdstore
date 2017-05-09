f46c63de79e56b4c49d9d9cd05ddc8811f1e78b9
maxinlei <maxinlei_public@126.com>
2017-05-09 08:49:02 +0800

    only admin can access backend panel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def admin_required
+    if !current_user.admin?
+      redirect_to "/", alert: "您没有权限"
+    end
+  end
 end
