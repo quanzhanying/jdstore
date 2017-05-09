c8a26669318129aa4d641d6067f5db76197cbeea
Hayson <heyson@sina.cn>
2017-05-10 01:35:21 +0800

    only admin can access backend panel

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,10 @@
 class Admin::ProductsController < ApplicationController
 
+  layout "admin"
+
+before_action :authenticate_user!
+before_action :admin_required
+
   def index
     @products = Product.all
   end
@@ -20,7 +25,7 @@ class Admin::ProductsController < ApplicationController
       render :edit
     end
   end
-  
+
   def create
     @product = Product.new(product_params)
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def admin_required
+    if !current_user.admin?
+      redirect_to "/", alert:"只有管理者才能登入"
+    end
+  end
 end
