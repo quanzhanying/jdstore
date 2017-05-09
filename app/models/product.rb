7e8933e67501a09d11548722da95505aa541d87b
wuxiao <wudawuxiao1375@163.com>
2017-05-09 18:52:32 +0800

    empty cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ApplicationRecord
   validates :price, presence: true
   validates :quantity, presence: true
   validates :price, numericality: { greater_than: 0}
-  validates :quantity, numericality: { greater_than: 0}
+  
 
   mount_uploader :image, ImageUploader
 end
