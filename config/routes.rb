bb3f0ffcc15e04ac17208f515ff14bcea804c9e5
xbearx1987 <xbearx1987@gmail.com>
2017-05-09 18:20:15 +0800

    setup products#index#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :products
+
   namespace :admin do
     resources :products
   end
 
-  root 'welcome#index'
+  root 'products#index'
 end
