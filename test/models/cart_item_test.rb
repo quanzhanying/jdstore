cd41469daf17e3ad47654ca8f6a6f5df24983f63
paulshu <paulshu18@gmail.com>
2017-05-09 20:24:53 +0800

    implement add_product_to_cart function

---
+++ test/models/cart_item_test.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: cart_items
+#
+#  id         :integer          not null, primary key
+#  cart_id    :integer
+#  product_id :integer
+#  quantity   :integer          default("1")
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'test_helper'
+
+class CartItemTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/cart_test.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: carts
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'test_helper'
+
+class CartTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
