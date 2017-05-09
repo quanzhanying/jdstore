7471e18bca2c14ad1646bb8a424e8f2330fc6cf4
anndoko <handcuffanndo@gmail.com>
2017-05-09 17:54:53 +0800

    implement backend CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  namespace :admin do
+    resources :products
+  end
+
   root 'welcome#index'
 
 end
