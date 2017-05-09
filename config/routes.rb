04945a6f484a157e92f8a461d1fc2ceeef7ac9fa
emilesu <emilesu@qq.com>
2017-05-10 00:59:58 +0800

    implement carts index funtion

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :carts            #购物车明细
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
