734f7a195489624c9b0f76cdbc9036471c7c10d5
Leo <leo@LeodeMacBook-Pro.local>
2017-05-09 23:13:59 +0800

    imlement clear cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,10 @@
 class CartsController < ApplicationController
+  def destroy
+    @cart = Cart.find(params[:id])
+    @cart.destroy
+
+    flash[:alert] = "购物车已清空"
+    redirect_to carts_path
+
+  end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,7 @@
 class Cart < ApplicationRecord
-  has_many :cart_items
+  has_many :cart_items, dependent: :destroy
   has_many :products, through: :cart_items, source: :product
+
 
   def add_product_to_cart(product)
     ci = cart_items.build
