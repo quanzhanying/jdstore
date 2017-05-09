cd8d5eb1d35b453452bd9ad9a9ff43bf78883ae0
xbearx1987 <xbearx1987@gmail.com>
2017-05-09 15:07:42 +0800

    setup admin/products#crud

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -4,4 +4,44 @@ class Admin::ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_products_path, notice: '商品新增成功'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+
+    if @product.save
+      redirect_to admin_products_path, notice: '商品修改成功'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+      redirect_to admin_products_path, notice: '商品删除成功'
+  end
+
+private
+
+  def product_params
+    params.require(:product).permit(:title,:description,:price,:quantity)
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+  validates :title, :description, :price, :quantity ,presence: true
+  validates :price, :quantity ,numericality: {greater_than:0}
 end
