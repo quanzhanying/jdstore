75d70df803dad1f35aab585002d780d811553242
paulshu <paulshu18@gmail.com>
2017-05-09 21:23:25 +0800

    implement current_cart function

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -12,7 +12,7 @@ class Cart < ApplicationRecord
   has_many :products, through: :cart_items, source: :product
 
 
-  def add_product_to_cart
+  def add_product_to_cart(product)
     ci = cart_items.build
     ci.product = product
     ci.quantity =  1
