0f544c4e5a05272977c315daa0566b49afcb7814
donker008 <richdonker@qq.com>
2017-05-09 23:09:22 +0800

    add to cart button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,17 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def add_to_cart
+    @product = Product.find(params[:id])
+    if !@product.blank?
+      redirect_to :back
+    else
+      flash[:warning] = "Product does't exist on store."
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :quantity)
+  end
 end
