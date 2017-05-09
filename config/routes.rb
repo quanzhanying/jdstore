42bff24d3e554af44d44df06d4d31d47c2a1d0f8
zoudongdong <alpeveryday@gmail.com>
2017-05-09 13:45:55 +0800

    add add_to_cart button

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
-  
-  resources :products
+
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
 
 end
