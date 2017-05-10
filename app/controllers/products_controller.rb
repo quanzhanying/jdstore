5ff3a760bc5fb8c02b3f83479de1fe0b327b94d7
House_Leung <House.liangshihao@gmail.com>
2017-05-10 20:38:58 +0800

    create products index

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,48 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path, notice: '商品信息更新成功！'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path, alert: '商品已移除'
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :stock)
+  end
+end
