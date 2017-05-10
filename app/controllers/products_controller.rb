0fad3977e2e2f0bf68b2fc58768471fbdaacab29
ankon <ankon@ankon-mac.local>
2017-05-11 07:46:04 +0800

    implement add_product_to cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,16 @@ class ProductsController < ApplicationController
    @product = Product.find(params[:id])
  end
 
+ def add_to_cart
+   @product = Product.find(params[:id])
+   redirect_to :back
+   flash[:notice] = "测试加入购物车"
+ end
+
+ private
+
+   def product_params
+     params.require(:product).permit(:title, :description, :price, :quantity, :image)
+   end
+
 end
