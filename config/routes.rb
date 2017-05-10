c9457b3520d9a237709046a39718047930dea0e1
陈彦锋 <chenyanfenghappy@163.com>
2017-05-10 22:06:08 +0800

    empty cart

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,11 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :carts
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end
 
   root "products#index"
 end
