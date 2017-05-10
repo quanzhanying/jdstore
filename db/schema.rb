6e19d1a139af27e9b355a56ffd7edd6ce08654b1
superzrr <superzrr@gmail.com>
2017-05-11 06:42:16 +0800

    modify the price datatype and show the correct index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509042951) do
+ActiveRecord::Schema.define(version: 20170510222016) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -28,10 +28,10 @@ ActiveRecord::Schema.define(version: 20170509042951) do
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.integer  "price"
+    t.decimal  "price",       precision: 10, scale: 2
     t.integer  "quantity"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
     t.string   "image"
   end
 
