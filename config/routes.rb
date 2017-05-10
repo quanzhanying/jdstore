1130129ac03a82699099211586318fd9cbf4b796
廖晓华“git config --global user.email “liaoxh@gmail.com <liaoxh1981@gmail.com>
2017-05-11 06:33:19 +0800

    add to cart

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
-resources :products
+resources :products do
+  membber do
+    post :add_to_cart
+  end
+end
 end
