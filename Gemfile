ee408d89a7434b12eebbb8e8dd959433b96e778d
sfxg27@gmail.com <feixiguang@feixiguangdeMacBook-Pro.local>
2017-05-10 14:15:28 +0800

    add gem annotate

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'annotate'
 gem 'bootstrap-sass'
 gem 'devise'
 gem 'simple_form'
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  quantity    :integer
+#  price       :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  image       :string
+#
+
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
