91ff7262765e1d2a5a1036cdbb3d86990e623df6
SegueX <free1989xj@hotmail.com>
2017-05-10 11:49:08 +0800

    add add_to_cart button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,11 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
 
   end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    redirect_to :back
+    flash[:notice] = "Added"
+
+  end
 end
