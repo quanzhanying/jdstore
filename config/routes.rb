ac0d1ec2aa98cc291ff318bec22ce0bd96c7016f
liyangbin <liyanbin98@gmail.com>
2017-05-10 21:24:50 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ namespace :admin do
  resources :products
 end
 
+resources :products
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'welcome#index'
+root 'products#index'
 
 end
