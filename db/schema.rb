a75f7eaf4b0892ffddb8d0af5288604a70b36909
郑笑飞“git config --global user.email 13949836783@163.com” <zhengxiaofei@zhengxiaofeideMacBook-Air.local>
2017-05-10 08:02:17 +0800

    change name as [通往财富自由之路]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170505162507) do
+ActiveRecord::Schema.define(version: 20170504231606) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -112,17 +112,6 @@ ActiveRecord::Schema.define(version: 20170505162507) do
     t.boolean  "is_admin",               default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "versions", force: :cascade do |t|
-    t.string   "item_type",                         null: false
-    t.integer  "item_id",                           null: false
-    t.string   "event",                             null: false
-    t.string   "whodunnit"
-    t.text     "object",         limit: 1073741823
-    t.datetime "created_at"
-    t.text     "object_changes", limit: 1073741823
-    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
 end
