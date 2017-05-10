4c47f3ad95e4bb4b0091eda4570e9deb372c5452
sfxg27@gmail.com <feixiguang@feixiguangdeMacBook-Pro.local>
2017-05-10 14:24:15 +0800

    delete annotate routes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,3 @@
-# == Schema Information
-#
-# Table name: products
-#
-#  id          :integer          not null, primary key
-#  title       :string
-#  description :text
-#  quantity    :integer
-#  price       :integer
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  image       :string
-#
-
 class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
 end
