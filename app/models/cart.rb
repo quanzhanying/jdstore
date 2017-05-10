58eb70837f3a2bf64b19afdb4adf49db8f308ee0
hubodashi <hubodashi@aliyun.com>
2017-05-10 18:41:45 +0800

    empty cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -7,4 +7,7 @@ class Cart < ApplicationRecord
     ci.quantity = 1
     ci.save
   end
+  def clean!
+   cart_items.destroy_all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
   namespace :admin do
    resources :products
   end
-  resources :carts
+ resources :carts do
+   collection do
+     delete :clean
+   end
+ end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
 end
