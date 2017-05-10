a62829a5b7ecce270db0c7b40a685421873de934
Yechti <yechti@gmail.com>
2017-05-10 11:51:50 +0800

    cannot add item with quantity 0

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -14,7 +14,13 @@ class CartItemsController < ApplicationController
   def update
     @cart = current_cart
     @cart_item = @cart.cart_items.find_by(product_id: params[:id])
-    @cart_item.update(cart_item_params)
+
+    if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
+       @cart_item.update(cart_item_params)
+       flash[:notice] = "成功變更數量"
+    else
+       flash[:warning] = "數量不足以加入購物車"
+    end
 
     redirect_to carts_path
   end
