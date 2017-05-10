d1dcff99d6f0f4f7d149570e1a38ea4a9630efe2
sww <sww@swwdeMacBook-Pro.local>
2017-05-10 19:08:44 +0800

    debug

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,7 @@ class ProductsController < ApplicationController
   def add_to_cart
     @product = Product.find(params[:id])
     if !current_cart.products.include?(@product)
+      current_cart.add_product_to_cart(@product)
       flash[:notice] = "你已成功将#{@product.title}加入购物车"
     else
       flash[:warning] = "你的购物车内已有此物品"
