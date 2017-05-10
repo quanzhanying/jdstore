d7020ca9864bd565ec3631c8c060d41d4f54fb19
yangyepeng <yangyepeng@gmail.com>
2017-05-10 22:45:22 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  root 'products#index'
+
   namespace :admin do
     resources :products
   end
-  root 'welcome#index'
+  resources :products
 end
