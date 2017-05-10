b703e103958af762ff5ea1902cfc2956c3a99a0f
liujia <ericrunner123456@yahoo.com>
2017-05-10 08:10:43 +0800

    admin/product CRUD

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,47 @@
+class Admin::ProductsController < ApplicationController
+  def index
+     @products=Product.all
+  end
+  def new
+    @product=Product.new
+  end
+  def edit
+    @product=Product.find(params[id])
+  end
+  def show
+    @product=Product.find(params[:id])
+  end
+  def create
+    @product=Product.new(product_params)
+    if @product.save
+       redirect_to admin_products_path
+       flash[:notice]="商品上架成功！"
+    else
+       render :new
+    end
+  end
+  def update
+    @product=Product.find(params[:id])
+    if @product.update(product_params)
+       redirect_to admin_products_path
+       flash[:notice]="商品更新成功！"
+    else
+       render :edit
+    end
+  end
+  def destroy
+    @product=Product.find(params[:id])
+    @product.destroy
+    redirect_to admin_products_path
+    flash[:alert]="商品已删除！"
+  end
+private
+def product_params
+  params.require(:product).permit(:title,:description,:price,:quantity)
+end
+
+
+
+
+
+end
