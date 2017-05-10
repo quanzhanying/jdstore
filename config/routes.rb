292ddb8c3ae5dd2c0c145fdb7eeb2b7d450981d1
peck liu <xiaoyang20170204@gmail.com>
2017-05-10 11:20:49 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
+
+  root 'products#index'
+  
   namespace :admin do
     resources :products
   end
+
+  resources :products
 end
