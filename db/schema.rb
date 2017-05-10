aa354e71f1d1194e5d3b463045c179d9e6023619
peck liu <xiaoyang20170204@gmail.com>
2017-05-10 12:03:20 +0800

    change admin/product backend panel

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170508134932) do
     t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
