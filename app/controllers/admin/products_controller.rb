a8c23f97b97e38e0878066574cacac464c1b9b34
liujia <ericrunner123456@yahoo.com>
2017-05-11 07:43:03 +0800

    admin/index,show,edit,new page

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,12 +1,15 @@
 class Admin::ProductsController < ApplicationController
+  before_action :authenticate_user!
+  #before_action :admin_required
   def index
+
      @products=Product.all
   end
   def new
     @product=Product.new
   end
   def edit
-    @product=Product.find(params[id])
+    @product=Product.find(params[:id])
   end
   def show
     @product=Product.find(params[:id])
