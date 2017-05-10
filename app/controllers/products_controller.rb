e820ed1ae3cdd33992df9deedd60a8d349b19313
谢育初 <524945638@qq.com>
2017-05-10 14:05:50 +0800

    add add_to_cart button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -41,6 +41,12 @@ class ProductsController < ApplicationController
     redirect_to products_path, alert: "Product deleted."
   end
 
+  def add_to_cart
+    @product = Product.find(params[:id])
+    redirect_to :back
+    flash[:notice] = "测试加入购物车"
+  end
+
   private
 
   def product_params
