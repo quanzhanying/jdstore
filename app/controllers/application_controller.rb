41f0fccf80037c3ba9dc02709ae8541b332ac30d
bixinxin81@gmail.com <bixinxin81@gmail.com>
2017-05-10 21:34:02 +0800

    only admin can access backend panel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+
+# For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  def admin_required
+    if !current_user.admin?
+      redirect_to "/", alert: "You are not admin."
+    end
+  end
 end
