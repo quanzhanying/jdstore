9a8f0030cdc67a40cb181f09e2b4c1a886f95f2e
廖晓华“git config --global user.email “liaoxh@gmail.com <liaoxh1981@gmail.com>
2017-05-11 07:06:34 +0800

    implement current_cart function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,7 +9,9 @@ class ProductsController < ApplicationController
 
   def add_to_cart
     @product = Product.find(params[:id])
-    redirect_to :back
-    flash[:notice] = "测试加入购物车"
+
+    current_cart.add_product_to_cart(@product)
+    flash[:notice] = "成功加入购物车"
+    redirect_to
   end
 end
