4c63f7ed93110b61aa9c619e622807ea6bc61049
House_Leung <House.liangshihao@gmail.com>
2017-05-10 22:33:48 +0800

    complish admin#CRUD & admin penal

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :products
+
+  namespace :admin do
+    resources :products 
+  end
+
   root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
