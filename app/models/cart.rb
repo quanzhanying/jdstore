6915456c644d38bd9983029a09fa9505867926e6
陈彦锋 <chenyanfenghappy@163.com>
2017-05-10 21:32:41 +0800

    carts index function be simple

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -6,6 +6,17 @@ class Cart < ApplicationRecord
     ci = cart_items.build
     ci.product = product
     ci.quantity = 1
-    ci.save    
+    ci.save
   end
+
+  def total_price
+    sum = 0
+    cart_items.each do |cart_item|
+      if cart_item.product.price.present?
+        sum += cart_item.quantity * cart_item.product.price
+      end
+    end
+    sum
+  end
+
 end
