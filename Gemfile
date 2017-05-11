9ae600d5ed29868b7dc77a0986882e047910b9d9
johsnow <johsnow08@gmail.com>
2017-05-11 17:12:47 +0800

    install font-awesome

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'devise'
 gem 'simple_form'
 gem 'carrierwave', '~> 1.0'
 gem 'mini_magick'
+gem 'font-awesome-rails'
 
 
 group :development, :test do
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -20,6 +20,7 @@ class Admin::ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
+    # @product.user = current_user
 
     if @product.save
       redirect_to admin_products_path
