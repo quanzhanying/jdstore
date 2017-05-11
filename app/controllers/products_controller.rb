ca52c3fa5c481629855b416a76946f5e7255da2a
paulshu <paulshu18@gmail.com>
2017-05-11 22:59:12 +0800

    delete an item from cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,6 +11,6 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     current_cart.add_product_to_cart(@product)
     flash[:notice] ="成功加入购物车"
-    redirect_to :back    
+    redirect_to :back
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,6 @@ Rails.application.routes.draw do
       delete :clean
     end
   end
+
+  resources :cart_items
 end
