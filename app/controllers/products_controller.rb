b83f2423411ca9ca741f47d5bb8f293d171b9f45
Tim <s6215s5216@gmail.com>
2017-05-11 13:34:52 +0800

    edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,12 +12,12 @@ class ProductsController < ApplicationController
     current_cart.add_product_to_cart(@product)
 
     if !current_cart.products.include?(@product)
-      current_cart.add_product_to_cart ( @product )
+      current_cart.add_product_to_cart(@product)
       flash[:notice]  =  "你已成功將#{ @product.title } 加入購物車"
     else
       flash[:warning]  =  "你的購物車內已有此物品"
     end
-    
+
     redirect_to :back
   end
 
