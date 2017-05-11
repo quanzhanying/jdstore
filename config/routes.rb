fc3956bf894f2ba8598376bea77daa5fcbbd0b02
amy <amy87326@163.com>
2017-05-11 23:32:45 +0800

    rename css_to_scss

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
    root 'welcome#index'
    namespace :admin do
-      resources :products
-   end
+   resources :products
+ end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
