e6c4f5a1b9ec405c1c921d08aeff16cec5fdf770
ym6 <yangml1988@gmail.com>
2017-05-11 22:19:59 +0800

    before part 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'admin/products#index'
+  root 'welcome#index'
+  # root 'admin/products#index'
   namespace :admin do
       resources :products
   end
