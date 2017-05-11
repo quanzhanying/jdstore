1b60a8a1a67db04b295134bd6e4a6a2992766442
ankon <ankon@ankon-mac.local>
2017-05-11 15:40:34 +0800

    cannot change quantity which exceed stock

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -10,4 +10,23 @@ class CartItemsController < ApplicationController
     flash[:warning] = "成功将 #{@product.title} 从购物车删除！"
     redirect_to :back
   end
+
+  def update
+    @cart = current_cart
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
+      @cart_item.update(cart_item_params)
+      flash[:notice] = "成功变更数量"
+    else
+      flash[:warning] = "数量不足，无法加入购物车"
+    end
+    redirect_to carts_path
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity)
+  end
+
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,9 +9,9 @@ class ProductsController < ApplicationController
 
  def add_to_cart
    @product = Product.find(params[:id])
-   if !current_cart.product.include?(@produc)
+   if !current_cart.products.include?(@product)
      current_cart.add_product_to_cart(@product)
-     flash[:notice] = "你已成功将 #{@produc.title} 加入购物车"
+     flash[:notice] = "你已成功将 #{ @product.title } 加入购物车"
    else
      flash[:warning] = "你的购物车内已有此物品"
    end
