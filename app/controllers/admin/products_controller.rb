0e467ab8182111d3c790b5046e00bbb0f39bb41d
Tim <s6215s5216@gmail.com>
2017-05-11 08:19:19 +0800

    finish 4-3

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,10 @@
 class Admin::ProductsController < ApplicationController
+
+  layout "admin"
+
+  before_action :authenticate_user!
+  before_action :admin_required
+
   def index
     @products = Product.all
   end
@@ -36,4 +42,5 @@ class Admin::ProductsController < ApplicationController
   def product_params
     params.require(:product).permit(:title, :description, :quantity, :price)
   end
+  
 end
