e4eded7b6ce3bc3bb17d7ae33f71f891a718cbbc
伯桐 <wuxiao@botongdeMacBook-Pro.local>
2017-05-11 17:00:59 +0800

    rake db:reset

--- db/schema.rb
+++ db/schema.rb
@@ -25,20 +25,6 @@ ActiveRecord::Schema.define(version: 20170509085956) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "collect_relationships", force: :cascade do |t|
-    t.integer  "product_id"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "photos", force: :cascade do |t|
-    t.integer  "product_id"
-    t.string   "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
