7c7c9148c5d28166718d0273927b685faffb2637
张瑞兰 <zhangruilan007@gmail.com>
2017-05-12 11:58:22 +0800

    implement delete action

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -36,6 +36,13 @@ before_action :admin_required
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+
+    @product.destroy
+    redirect_to admin_products_path
+  end
+
     private
 
     def product_params
