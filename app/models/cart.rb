faa7c8ee9c4666442b158ad14d02336150927b1c
hubodashi <hubodashi@aliyun.com>
2017-05-12 19:57:52 +0800

    implement cart_total_price function

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: carts
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Cart < ApplicationRecord
   has_many :cart_items
   has_many :products, through: :cart_items, source: :product
@@ -9,5 +18,14 @@ class Cart < ApplicationRecord
   end
   def clean!
    cart_items.destroy_all
+  end
+    def total_price
+    sum = 0
+    cart_items.each do |cart_item|
+      if cart_item.product.price.present?
+        sum += cart_item.quantity * cart_item.product.price
+      end
+    end
+    sum
   end
 end
