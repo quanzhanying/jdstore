cefe33421c152c7923f0e169926740ffdd6988fc
Xiying.Liu <xiyliu@gmail.com>
2017-05-12 15:04:23 +0200

    user can login/logout/signup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_bootstrap/alert
+//= require bootstrap/alert
+//= require bootstrap/dropdown
 //= require_tree .
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,5 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = "Hi！想看看有什么好玩儿的么？"
+  end
 end
