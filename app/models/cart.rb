6f52cbc7b4a1d4e8c2535c2f7a82648786e3f188
张伟 <zhangwei19890518@gmail.com>
2017-05-13 07:03:47 +0800

    添加当前购物车的功能

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -8,6 +8,9 @@
 #
 
 class Cart < ApplicationRecord
+  has_many :cart_items
+  has_many :products, through: :cart_items, source: :product
+
   def add_product_to_cart(product)
     ci = cart_items.build
     ci.product = product
