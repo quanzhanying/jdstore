cb00bb15dfa87fb4086a723f493353f2b192a315
linchunlong <learntofreedom@gmail.com>
2017-05-13 13:43:05 +0800

    add add_to_cart button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,9 @@ class ApplicationController < ActionController::Base
       redirect_to "/", alert: "You are not admin."
     end
   end
+  def add_to_cart
+     @product = Product.find(params[:id])
+     redirect_to :back
+     flash[:notice] = "测试加入购物车"
+   end
 end
