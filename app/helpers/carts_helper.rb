7c3281b650a41c0cb37358a36bab198a5d99e2e9
xuhangwu <xuhangwu5244@gmail.com>
2017-05-13 22:29:51 +0800

    implement cart_total_price function

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,2 +1,16 @@
 module CartsHelper
+  def render_cart_total_price(cart)
+    cart.total_price
+  end
+
+    def total_price
+      sum = 0
+      cart_items.each do |cart_item|
+        if cart_item.product.price.present?
+          sum = cart_item.quantity * cart_item.product.price
+        end
+      end
+      sum
+    end
+  
 end
