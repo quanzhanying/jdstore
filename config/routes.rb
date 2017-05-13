f3e2e7d651105d2207662eff5b4fe10e070321ec
Xiying.Liu <xiyliu@gmail.com>
2017-05-13 11:01:07 +0200

    add admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :products
+  end
   devise_for :users
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
