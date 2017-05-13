72989d7726bb6ff7eb6a1be5511239660f68fc5e
xiaaa <xiaenchanter@gmail.com>
2017-05-13 23:00:42 +0800

    empty cart

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,14 @@ Rails.application.routes.draw do
   resources :products do
     member do
     post :add_to_cart
+    end
   end
-end
 
-resources :carts
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
