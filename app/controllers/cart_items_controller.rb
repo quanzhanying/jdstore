7d322a3ec7b95c8945d68b7ccbaa86420cdc63f4
廖晓华“git config --global user.email “liaoxh@gmail.com <liaoxh1981@gmail.com>
2017-05-13 22:01:17 +0800

    sth

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,19 +1,20 @@
 class CartItemsController < ApplicationController
   before_action :authenticate_user!
 
-  def destory
+  def destroy
     @cart = current_cart
-    @cart_item =@cart.cart_items.find_by(product_id: params[:id])
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
     @product = @cart_item.product
-    @cart_item.destory
+    @cart_item.destroy
 
-    flash[warning] = "成功将 #{@product.title} 从购物车删除!"
+    flash[:warning] = "成功将 #{@product.title} 从购物车删除!"
     redirect_to :back
   end
+
   def update
     @cart = current_cart
     @cart_item = @cart.cart_items.find_by(product_id: params[:id])
-    
+
     if @cart_item.product.quantity >= cart_item_params[:quantity]. to_i
        @cart_item.update(cart_item_params)
         flash[:notice] = "成功变更数量"
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,6 @@
 class CartsController < ApplicationController
-  def clean_carts_path
-    current_cart.clean!
+  def clean
+    current_cart.clean
     flash[:warning] = "已清空购物车"
     redirect_to carts_path
   end
