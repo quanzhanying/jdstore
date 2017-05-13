a7223d3abe6f7376a05a01e94ef6a91a7f6aebc7
cl <cl_fighting@163.com>
2017-05-13 10:18:33 +0800

    complement clean_cart function

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,13 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :carts
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end
+
+
 
   resources :cart_items
 
