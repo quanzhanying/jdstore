2959753ae42459fbaf2a15b4795edb6578cb7ef0
梁信众 <liangxinzhong@liangxinzhongdeMacBook-Pro.local>
2017-05-13 11:48:20 +0800

    implement product show

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -11,6 +11,10 @@ class Admin::ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def create
     @product = Product.new(product_params)
     if @product.save
@@ -18,6 +22,25 @@ class Admin::ProductsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to admin_products_path
   end
 
   private
