d9ecad4b5180f59145f46a485dd701c2d9eb027e
xiaaa <xiaenchanter@gmail.com>
2017-05-13 22:14:10 +0800

    implement carts index function

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   end
 end
 
+resources :carts
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
