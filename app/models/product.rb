a19c5b1673701f0b43bf6f7b593a8bcd91a2a7a4
Xiying.Liu <xiyliu@gmail.com>
2017-05-13 22:26:54 +0200

    implement image upload

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,4 @@
 class Product < ApplicationRecord
   validates :title, presence: true
   mount_uploader :image, ImageUploader
-  has_many :photos, dependent: :destroy
-  accepts_nested_attributes_for :photos
 end
