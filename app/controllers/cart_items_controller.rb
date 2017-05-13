1aa5c59f214c7768668d609992b69a16eb1edfeb
Lucia <learningleadtorebirth@gmail.com>
2017-05-13 17:55:13 +0800

    change quantity in cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -12,4 +12,40 @@ class CartItemsController < ApplicationController
     redirect_to :back
   end
 
+  # def up
+  #   @cart = current_cart
+  #   @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+  #   @product = @cart_item.product
+  #   if @product.quantity > 0
+  #     @cart_item.quantity = @cart_item.quantity +1
+  #     @product.quantity = @product.quantity -1
+  #   else
+  #     flash[:warning] = "该商品已缺货，正在补货"
+  #   end
+  #   redirect_to :back
+  # end
+  #
+  # def down
+  #   @cart = current_cart
+  #   @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+  #   @product = @cart_item.product
+  #   @cart_item.quantity = @cart_item.quantity - 1
+  #   @product.quantity = @product.quantity + 1
+  #   redirect_to :back
+  # end
+
+  def update
+    @cart = current_cart
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to carts_path
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,11 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :cart_items
+  resources :cart_items do
+    member do
+      post :up
+      post :down
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
