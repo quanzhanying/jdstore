a9fac284454725ba6fd317b3b7c8cf9ce77c3ce0
kuangmin <qiangminmin520@gmail.com>
2017-05-14 08:48:32 +0800

    add_to_cart button

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -26,6 +26,15 @@ class Admin::ProductsController < ApplicationController
     end
   end
 
+  def destroy
+      @product = Product.find(params[:id])
+
+      @product.destroy
+
+      redirect_to admin_products_path
+    end
+
+
   def create
     @product = Product.new(product_params)
 
