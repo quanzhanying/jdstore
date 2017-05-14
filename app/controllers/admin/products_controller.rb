af8de3152032d1ef85408e1cc908c7db8fc1e243
NewMax <zhangpengmax@gmail.com>
2017-05-14 12:57:49 -0400

    implement backend CRUD

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,8 +1,7 @@
 class Admin::ProductsController < ApplicationController
-   def index
-     @products = Product.all
-   end
-
+  def index
+   @products = Product.all
+  end
   def new
     @product = Product.new
   end
