8b8b5f6b68dc699c24863b31157cf643f59a3966
zhuqi <ardentyouny>
2017-05-14 10:00:45 +0800

    empty cart

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,22 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
-    resources :products do
+
+
+  resources :products do
     member do
       post :add_to_cart
     end
   end
+
+
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end
+
+
   devise_for :users
   root 'products#index'
 end
