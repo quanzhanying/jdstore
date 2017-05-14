b826b3aabb9d563a85a819de1e7af3a4b4862918
wangxing <18912540921@163.com>
2017-05-14 14:57:17 +0800

    admin and user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parametes, if: :devise_controller?
   protect_from_forgery with: :exception
 
   def admin_reuqired
     if !current_user.admin?
-      redirect_to "/", alert: "You are not admin"
+      flash[:alert] = "你没有权限"
+      redirect_to root_path
     end
   end
 
@@ -22,6 +24,12 @@ class ApplicationController < ActionController::Base
     end
     session[:cart_id] = cart.id
     return cart
+  end
+
+  protected
+
+  def configure_permitted_parametes
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:is_admin,:name])
   end
 
 end
