8348d5411b2abe7adb15c3528b064aedaaa1d0fe
张伟 <zhangwei19890518@gmail.com>
2017-05-14 11:30:57 +0800

    清空购物车与教材不一致的地方添加注释

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,10 +1,12 @@
 class CartsController < ApplicationController
+  # 清空购物车方法与教材中不一致
   def destroy
     @products = current_cart.products
     @products.delete_all
     redirect_to :back
     flash[:alert] = "已清空购物车"
   end
+  # 清空购物车代码结束
 
   private
 
