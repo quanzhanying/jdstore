4e8b811086ee78bf18c71f539917618c5e0fa627
NewMax <zhangpengmax@gmail.com>
2017-05-14 18:29:25 -0400

    add add_to_cart button

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -38,13 +38,6 @@ class Admin::ProductsController < ApplicationController
     end
   end
 
-    def add_to_cart
-      @product = Product.find(params[:id])
-      redirect_to :back
-      flash[:notice] = "测试加入购物车"
-    end
-
-
   private
 
   def product_params
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,12 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :products
-
+    resources :products do
+      member do
+        post :add_to_cart
+      end
+    end
+    
   devise_for :users
 
   root 'products#index'
