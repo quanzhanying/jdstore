9a74287747ec027a244b4ecdb7a81150c5e76881
张伟 <zhangwei19890518@gmail.com>
2017-05-14 09:24:57 +0800

    添加购物车中更改商品数量的功能

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,4 +1,12 @@
 class CartItemsController < ApplicationController
+
+  def update
+    @cart = current_cart
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to carts_path
+  end
+
   def destroy
     @cart = current_cart
     @cart_item = @cart.cart_items.find_by(product_id: params[:id])
@@ -6,5 +14,11 @@ class CartItemsController < ApplicationController
     @cart_item.destroy
     flash[:alert] = "成功将 #{@product.name} 从购物车删除！"
     redirect_to :back
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity)
   end
 end
