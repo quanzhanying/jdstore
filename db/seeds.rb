5a74b85a068f834028fa1be0b4301ae636004cce
johsnow <johsnow08@gmail.com>
2017-05-14 22:35:51 +0800

    modify seed password

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,9 @@
 u = User.new
 u.email = "1243281264@qq.com"           # 可以改成自己的 email
 
-u.password = "111111"                # 最少要六码
+u.password = "11111111"                # 最少要八码
 
-u.password_confirmation = "111111"   # 最少要六码
+u.password_confirmation = "11111111"   # 最少要八码
 
 u.is_admin = true
 u.save
@@ -21,9 +21,9 @@ u.save
 u = User.new
 u.email = "1092167387@qq.com"           # 可以改成自己的 email
 
-u.password = "111111"                # 最少要六码
+u.password = "11111111"                # 最少要八码
 
-u.password_confirmation = "111111"   # 最少要六码
+u.password_confirmation = "11111111"   # 最少要八码
 
 u.is_admin = true
 u.save
