99d37d89272775974c201533bf75894a0f70aa72
廖晓华“git config --global user.email “liaoxh@gmail.com <liaoxh1981@gmail.com>
2017-05-14 21:38:46 +0800

    implement will_paginate

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.all.paginate(:page => params[:page], :per_page =>5)  
   end
 
   def show
