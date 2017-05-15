6a08ea304608d6ee5895af8a91aadf97436c986d
Xiying.Liu <xiyliu@gmail.com>
2017-05-15 11:00:35 +0200

    delete an item from cart

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,12 @@ Rails.application.routes.draw do
       post :add_to_cart
     end
   end
-  
+
   resources :carts do
     collection do
       delete :clean
     end
   end
+
+  resources :cart_items
 end
