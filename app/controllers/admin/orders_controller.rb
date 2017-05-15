7fc882589a73f27e3cb2c3e42b630af3327540cf
张伟 <zhangwei19890518@gmail.com>
2017-05-16 06:44:05 +0800

    添加后台显示订单状态界面

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,9 +2,14 @@ class Admin::OrdersController < ApplicationController
   layout "admin"
 
   before_action :authenticate_user!
-  before_action :admin_required
+  before_action :require_is_admin
 
   def index
     @orders = Order.order("id DESC")
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    @product_lists = @order.product_lists
   end
 end
