19dfe88760190fb2f0b2c1c81b5bbaecdee44a9f
李婉 <iwan16406783@gmail.com>
2017-05-15 15:45:26 +0800

    implement cart_total_price function

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   root 'products#index'
   devise_for :users
+  resources :carts
   namespace :admin do
   resources :products
 end
+
   resources :products do
     member do
       post :add_to_cart
-      resources :carts
+
     end
   end
 end
