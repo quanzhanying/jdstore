30d1d10c8263945a51700dbdd73e967080288b93
李婉 <iwan16406783@gmail.com>
2017-05-15 20:39:46 +0800

    empty cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'products#index'
   devise_for :users
-  resources :carts
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end
   namespace :admin do
   resources :products
 end
