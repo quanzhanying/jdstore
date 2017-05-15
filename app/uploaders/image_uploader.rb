669f31f9a62f2491368b9ecd70bd6d37ab35afee
ym6 <yangml1988@gmail.com>
2017-05-15 22:03:01 +0800

    first week

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -49,11 +49,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [800, 800]
 
   version :thumb do
-    process resize_to_fit: [200, 200]
+    process resize_to_fill: [200, 200]
   end
 
   version :medium do
-    process resize_to_fit: [400, 400]
+    process resize_to_fill: [400, 400]
   end
 
 end
