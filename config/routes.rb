ad7a47a1c0f0ff5b80c3167a062dcbfb4ff2d1c6
pengyu <chinakun2@sina.com>
2017-05-15 18:00:52 +0800

    add payment_method function

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,12 @@ Rails.application.routes.draw do
 
   resources :cart_items
 
-  resources :orders
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+    end
+  end
 
   namespace :account do
     resources :orders
