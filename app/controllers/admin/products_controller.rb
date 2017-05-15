49b11ffccdddae8f847b7b5ad29986be529f9a37
liujia <ericrunner123456@yahoo.com>
2017-05-16 07:58:59 +0800

    decorate langdingpage

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -2,7 +2,6 @@ class Admin::ProductsController < ApplicationController
   before_action :authenticate_user!
   before_action :admin_required
   def index
-
      @products=Product.all
   end
   def new
