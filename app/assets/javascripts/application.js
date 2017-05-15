606c16e4acbeddfc71b68a8df6ffd8c925737155
lichen <lichen19844@gmail.com>
2017-05-15 16:13:23 +0800

    solve conflicts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap/alert
-<<<<<<< HEAD
-=======
 //= require bootstrap/dropdown
->>>>>>> demo
 //= require_tree .
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,6 @@
 class WelcomeController < ApplicationController
-<<<<<<< HEAD
-=======
+
   def index
     flash[:notice] = "早安！你好！"
-  end  
->>>>>>> demo
+  end
 end
