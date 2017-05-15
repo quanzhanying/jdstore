054111825b4e38554e2631389b2587bbfa300215
chanweiyan <chanweiyan007@gmail.com>
2017-05-16 06:44:11 +0800

    add checkout routing

--- config/routes.rb
+++ config/routes.rb
@@ -14,11 +14,12 @@ Rails.application.routes.draw do
   resources :carts do
     collection do
       delete :clean
+      post :checkout
     end
   end
 
   resources :cart_items
-  
+
   root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
