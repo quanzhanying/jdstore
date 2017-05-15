f135de2a38c6c14783ca80e5dd5837852c875488
廖晓华“git config --global user.email “liaoxh@gmail.com <liaoxh1981@gmail.com>
2017-05-16 00:10:49 +0800

    add payment_method function

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,11 @@ Rails.application.routes.draw do
    resources :favorite
    resources :orders
    namespace :account do
-    resources :orders
+    resources :orders do
+      member do
+        post :pay_with_alipay
+        post :pay_with_wechat
+      end
+    end
   end
  end
