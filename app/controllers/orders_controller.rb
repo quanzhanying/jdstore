fb696c9b9884c7f98871f862ff27563215d0ae86
hubodashi <hubodashi@aliyun.com>
2017-05-15 21:19:39 +0800

    add orders show function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,5 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:create]
-
   def create
     @order = Order.new(order_params)
     @order.user = current_user
@@ -8,11 +7,22 @@ class OrdersController < ApplicationController
 
     if @order.save
       redirect_to order_path(@order)
+        current_cart.cart_items.each do |cart_item|
+        product_list = ProductList.new
+        product_list.order = @order
+        product_list.product_name = cart_item.product.title
+        product_list.product_price = cart_item.product.price
+        product_list.quantity = cart_item.quantity
+        product_list.save
+      end
     else
       render 'carts/checkout'
     end
   end
-
+  def show
+    @order = Order.find(params[:id])
+    @product_lists = @order.product_lists
+  end
   private
 
   def order_params
