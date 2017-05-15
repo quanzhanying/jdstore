fdcdc6c9d423a2a00785611b6fb5adee0227aa26
pengyu <chinakun2@sina.com>
2017-05-15 18:08:54 +0800

    add OrderMailer function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,6 +7,7 @@ class OrdersController < ApplicationController
     @order.total = current_cart.total_price
 
     if @order.save
+
       current_cart.cart_items.each do |cart_item|
         product_list = ProductList.new
         product_list.order = @order
@@ -15,6 +16,8 @@ class OrdersController < ApplicationController
         product_list.quantity = cart_item.quantity
         product_list.save
       end
+      current_cart.clean!
+      OrderMailer.notify_order_placed(@order).deliver!
 
       redirect_to order_path(@order.token)
     else
