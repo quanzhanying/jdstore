99c623b976520cd490ba0f87e523a8c398964a15
张伟 <zhangwei19890518@gmail.com>
2017-05-16 04:46:34 +0800

    增加订单model

---
+++ app/models/order.rb
@@ -0,0 +1,11 @@
+# == Schema Information
+#
+# Table name: orders
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Order < ApplicationRecord
+end
