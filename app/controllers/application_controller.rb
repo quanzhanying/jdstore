26b11f73569ecbf739282e71950b65d653cfcb5e
House_Leung <House.liangshihao@gmail.com>
2017-05-15 22:04:07 +0800

    implement current_cart function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,21 @@ class ApplicationController < ActionController::Base
       redirect_to "/"
     end
   end
+
+  helper_method :current_cart
+
+  def current_cart
+    @current_cart ||= find_cart
+  end
+
+  private
+
+  def find_cart
+    cart = Cart.find_by(id: session[:cart_id])
+    if cart.blank?
+      cart = Cart.create
+    end
+    session[:cart_id] = cart.id
+    return cart
+  end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,45 +10,8 @@ class ProductsController < ApplicationController
 
   def add_to_cart
     @product = Product.find(params[:id])
+    current_cart.add_product_to_cart(@product)
+    flash[:notice] = "成功加入购物车"
     redirect_to :back
-    flash[:notice] = "测试加入购物车"
   end
-
-  # def new
-  #   @product = Product.new
-  # end
-  #
-  # def create
-  #   @product = Product.new(product_params)
-  #   if @product.save
-  #     redirect_to products_path
-  #   else
-  #     render :new
-  #   end
-  # end
-  #
-  # def edit
-  #   @product = Product.find(params[:id])
-  # end
-  #
-  # def update
-  #   @product = Product.find(params[:id])
-  #   if @product.update(product_params)
-  #     redirect_to products_path, notice: '商品信息更新成功！'
-  #   else
-  #     render :edit
-  #   end
-  # end
-  #
-  # def destroy
-  #   @product = Product.find(params[:id])
-  #   @product.destroy
-  #   redirect_to products_path, alert: '商品已移除'
-  # end
-  #
-  # private
-  #
-  # def product_params
-  #   params.require(:product).permit(:title, :description, :price, :stock)
-  # end
 end
