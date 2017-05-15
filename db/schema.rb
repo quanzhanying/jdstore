1f220bda9a98a8fd8ab678cad9ba26c2a8646736
superzrr <superzrr@gmail.com>
2017-05-15 21:50:53 +0800

    rails g model order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170510222016) do
+ActiveRecord::Schema.define(version: 20170515134309) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -23,6 +23,18 @@ ActiveRecord::Schema.define(version: 20170510222016) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "total",            default: 0
+    t.integer  "user_id"
+    t.string   "shipping_name"
+    t.string   "shipping_address"
+    t.string   "shipping_number"
+    t.string   "shipping_by"
+    t.string   "paid_by"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
   end
 
   create_table "products", force: :cascade do |t|
