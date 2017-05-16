f6e2dc9b696d360099e4dd9aed33d0a7f0bd998f
马小舟 <maxiaozhou.kelly@gmail.com>
2017-05-16 16:39:25 +0800

    implement carts index function

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
+  root 'products#index'
+  devise_for :users
   namespace :admin do
-    resources :products do
-      member do
-        post :add_to_cart
-      end
+    resources :products
+  end
+
+  resources :products do
+    member do
+      post :add_to_cart
     end
   end
 
-  devise_for :users
-  root 'products#index'
-
-  resources :products
+  resources :carts
 end
