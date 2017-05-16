c7b45b3e772e42d550656cd877d723d7d8faa6db
伯桐 <wuxiao@botongdeMacBook-Pro.local>
2017-05-16 10:50:29 +0800

    add order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,9 @@
 class Order < ApplicationRecord
+  belongs_to user
+
+  validates :billing_name, presence: ture
+  validates :billing_address, presence: ture
+  validates :shipping_name, presence: ture
+  validates :shipping_address, presence: ture
+  
 end
