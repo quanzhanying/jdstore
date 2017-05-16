5e19f29353b20336f3a9aacf2caa6ebe530717ff
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-05-16 13:40:36 +0800

    def update find_by -> find in cart_items_controller to fix bug

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -11,7 +11,7 @@ class CartItemsController < ApplicationController
 
   def update
     @cart = current_cart
-    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    @cart_item = @cart.cart_items.find(params[:id])
     if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
       @cart_item.update(cart_item_params)
       flash[:notice] = "成功变更数量"
