20d384ee60943f19573c350afbd38d46beabd507
emilesu <emilesu@qq.com>
2017-05-16 19:53:29 +0800

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,14 @@
 class Order < ApplicationRecord
+
+  # 订单乱序token号
+  before_create :generate_token
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
+
+  # ------
+
   belongs_to :user
   has_many :product_lists
 
@@ -6,5 +16,7 @@ class Order < ApplicationRecord
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
+
+
 
 end
