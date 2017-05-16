500db2c81525c56cf0ec49e4538ad853a7d9f560
xuluchun8 <xuluchun8@gmail.com>
2017-05-16 18:06:33 +0800

    story5

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
-belongs_to :users
-validates :billing_name, persence:true
-validates :billing_address, prsence:true
-validates :shipping_name, presence:true
-validates :shipping_address, presence:true
+belongs_to :user
+validates :billing_name, presence: true
+validates :billing_address, presence: true
+validates :shipping_name, presence: true
+validates :shipping_address, presence: true
 end
