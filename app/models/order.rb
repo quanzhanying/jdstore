2ff9b207d31e8eb9e0f43ebd84e60adf324dcfd6
伯桐 <wuxiao@botongdeMacBook-Pro.local>
2017-05-16 11:10:17 +0800

    add product_list model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,10 @@
 class Order < ApplicationRecord
-  belongs_to user
+  belongs_to :user
+  has_many :product_lists
 
-  validates :billing_name, presence: ture
-  validates :billing_address, presence: ture
-  validates :shipping_name, presence: ture
-  validates :shipping_address, presence: ture
-  
+  validates :billing_name, presence: true
+  validates :billing_address, presence: true
+  validates :shipping_name, presence: true
+  validates :shipping_address, presence: true
+
 end
