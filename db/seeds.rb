baf5fe072f0c9ab723fce6feb9ebedfd1aa1b692
johsnow <johsnow08@gmail.com>
2017-05-16 20:37:58 +0800

    add product seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,20 @@ u.password_confirmation = "111111"   # 最少要六码
 
 u.is_admin = true
 u.save
+
+
+
+
+Product.create!(title: "斗笠杯",
+  description: "龙泉青瓷斗笠杯",
+  price: 280,
+  quantity: 50,
+  pic: open("http://ww2.sinaimg.cn/large/006tKfTcgy1ffn9ln4bxnj30m80m8aly.jpg")
+  )
+
+Product.create!(title: "侧把茶具套装",
+  description: "龙泉青瓷之侧把茶具套装",
+  price: 888,
+  quantity: 50,
+  pic: open("http://ww4.sinaimg.cn/large/006tNc79gy1ffnhfvvjfej30m80m9aig.jpg")
+  )
