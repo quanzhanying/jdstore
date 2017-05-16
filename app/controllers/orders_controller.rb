3974f7028fd1976dad14df9f3e1cb39c364fa66e
NinaWei1384 <NinaWei1384@foxmail.com>
2017-05-16 21:37:38 +0800

    add order controller II

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,6 +16,6 @@ class OrdersController < ApplicationController
    private
 
    def order_params
-     params.require(:order).permit(:billing_name, :billin_address, :shipping_address)
+     params.require(:order).permit(:billing_name, :billing_address, :shipping_name, :shipping_address)
    end
 end
