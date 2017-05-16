a7fa58f9f7514e6030145a487134ec0b1967f75a
eagle111 <18645125206@sina.cn>
2017-05-16 17:29:26 +0800

    add order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,7 @@
 class Order < ApplicationRecord
+  belongs_to :user
+  validates :billing_name, presence: true
+  validates :billing_address, presence: true
+  validates :shipping_name, presence: true
+  validates :shipping_address, presence: true
 end
