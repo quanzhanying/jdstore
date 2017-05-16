171ab5f8fdfe576d2adf2663cf287ab8e8dd8a9f
emilesu <emilesu@qq.com>
2017-05-16 20:25:10 +0800

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,10 @@
 class Order < ApplicationRecord
 
   # 订单乱序token号
-  before_create :generate_token
+  before_create :generate_token           # before_create 是rails model內建的callbacks（回调函数），目的是让资料储存前先执行某动作。
 
   def generate_token
-    self.token = SecureRandom.uuid
+    self.token = SecureRandom.uuid        # SecureRandom.uuid 是Ruby內建的随机生成器
   end
 
   # ------
