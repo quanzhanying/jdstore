7933f56aaaed095d7fc5d72296ec597b979fbb3f
blueming <wenyiff66@gmail.com>
2017-05-16 14:51:16 +0800

    touch app/views/carts/checkout.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,24 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
+  
   resources :products do
     member do
       post :add_to_cart
     end
   end
+
   devise_for :users
   root 'products#index'
   resources :cart_items
+
   resources :carts do
     collection do
       delete :clean
       post :checkout
     end
   end
+
+  resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
