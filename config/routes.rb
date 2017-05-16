aa7ff0a71fd46856ab839598b55db79d68237d28
niushuangmeng <niushuangmeng@gmail.com>
2017-05-16 11:04:51 +0800

    add payment_method function

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,12 @@ resources :products do
     end
   end
   resources :cart_items
-  resources :orders
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+    end
+  end
   namespace :account do
    resources :orders
   end
