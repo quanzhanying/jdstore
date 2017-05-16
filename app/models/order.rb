a3973da56d458ddaeac1b5d38853077822a30c99
anndoko <handcuffanndo@gmail.com>
2017-05-16 20:06:07 +0800

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,10 @@
 class Order < ApplicationRecord
+  before_create :generate_token
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
+
   validates :billing_name, presence: true
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
