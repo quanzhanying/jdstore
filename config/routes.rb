a4657efad766271692a699418ee36939a1c9b2af
yangyepeng <yangyepeng@gmail.com>
2017-05-16 21:41:04 +0800

    add ship/shipped/cancel/return action to admin/orders_controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,15 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   namespace :admin do
-    resources :products
-    resources :orders
+     resources :products
+     resources :orders do
+       member do
+         post :cancel
+         post :ship
+         post :shipped
+         post :return
+       end
+     end
   end
 
   resources :products do
