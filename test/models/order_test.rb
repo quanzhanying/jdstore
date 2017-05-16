931ab6df9604843ff065328b35861e12947a3394
paulshu <paulshu18@gmail.com>
2017-05-17 07:11:17 +0800

    add order model

---
+++ test/models/order_test.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: orders
+#
+#  id               :integer          not null, primary key
+#  total            :integer          default("0")
+#  user_id          :integer
+#  billing_name     :string
+#  billing_address  :string
+#  shipping_name    :string
+#  shipping_address :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
+require 'test_helper'
+
+class OrderTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
