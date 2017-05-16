b07283a29fb35555dcc0d9ca54c96e6abaadb1c7
zengjinlin <kinglinxinsen@gmail.com>
2017-05-16 10:10:05 +0800

    users can see their orders

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,10 @@ resources :carts do
    end
 end
 
+namespace :account do
+   resources :orders
+end
+
 resources :cart_items
 resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
