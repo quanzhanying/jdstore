274b7f325ccd440df5ba8b12c58b78af4417f487
Lucia <learningleadtorebirth@gmail.com>
2017-05-16 21:42:27 +0800

    add payment_method function

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,12 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :orders
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+    end
+  end
   namespace :account do
     resources :orders
   end
