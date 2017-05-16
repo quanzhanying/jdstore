bb207bd11f560dca31a37e8bbd04fb2da8156621
ym6 <yangml1988@gmail.com>
2017-05-16 22:37:49 +0800

    add add_to_cart button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,12 @@ class ProductsController < ApplicationController
         @products = Product.all
     end
 
+    def add_to_cart
+        @product = Product.find(params[:id])
+        redirect_to :back
+        flash[:notice] = "测试加入购物车"
+    end
+
     private
 
     def product_params
