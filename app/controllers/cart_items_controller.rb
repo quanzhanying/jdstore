3e6dc3b1d4b9fb719393d0129c634f465ffb1c00
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-05-16 14:07:31 +0800

    fix bug

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,7 +2,7 @@ class CartItemsController < ApplicationController
 
   def destroy
     @cart = current_cart
-    @cart_item = @cart.cart_items.find(params[:id])
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
     @product = @cart_item.product
     @cart_item.destroy
     flash[:warning] = "成功将 #{@product.title} 从购物车删除！"
@@ -11,7 +11,7 @@ class CartItemsController < ApplicationController
 
   def update
     @cart = current_cart
-    @cart_item = @cart.cart_items.find(params[:id])
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
     if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
       @cart_item.update(cart_item_params)
       flash[:notice] = "成功变更数量"
