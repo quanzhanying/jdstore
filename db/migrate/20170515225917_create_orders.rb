f6f711c922459adfcf5fb713918797c285e41d2b
zengjinlin <kinglinxinsen@gmail.com>
2017-05-16 08:08:57 +0800

    add order controller

--- db/migrate/20170515225917_create_orders.rb
+++ db/migrate/20170515225917_create_orders.rb
@@ -6,8 +6,8 @@ class CreateOrders < ActiveRecord::Migration[5.0]
       t.string :billing_name
       t.string :billing_address
       t.string :shipping_name
-      t.string :shiping_address
-    
+      t.string :shipping_address
+
       t.timestamps
     end
   end
