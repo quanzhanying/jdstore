665b63fe382357b749ab48aebc156acb2bd37ccb
chanweiyan <chanweiyan007@gmail.com>
2017-05-16 19:38:13 +0800

    fix payment error

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -42,7 +42,7 @@ class OrdersController < ApplicationController
   def pay_with_wechat
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("wechat")
-    @order.payment!
+    @order.make_payment!
 
     redirect_to order_path(@order.token), notice: "使用微信成功完成付款"
   end
