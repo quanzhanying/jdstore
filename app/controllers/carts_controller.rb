dd5e21af49404134ad4ade572b679aa3ca82b4f7
anndoko <handcuffanndo@gmail.com>
2017-05-16 16:55:57 +0800

    touch app/views/carts/checkout.html.erb

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,4 +4,8 @@ class CartsController < ApplicationController
     flash[:warning] = "已清空購物車"
     redirect_to carts_path
   end
+
+  def checkout
+    @order = Order.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,6 @@ Rails.application.routes.draw do
   end
 
   resources :cart_items
+  resources :orders
 
 end
