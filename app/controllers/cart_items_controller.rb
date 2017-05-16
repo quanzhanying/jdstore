f5d68d7fff814a12e5c3d407c7da10c3230c9078
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-05-16 13:33:48 +0800

    def destroy find_by -> find in cart_items_controller to fix bug

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,7 +2,7 @@ class CartItemsController < ApplicationController
 
   def destroy
     @cart = current_cart
-    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    @cart_item = @cart.cart_items.find(params[:id])
     @product = @cart_item.product
     @cart_item.destroy
     flash[:warning] = "成功将 #{@product.title} 从购物车删除！"
