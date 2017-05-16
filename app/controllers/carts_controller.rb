1864cc1094fb500a716c5000e22ad01a8c6db8e1
emilesu <emilesu@qq.com>
2017-05-16 18:11:47 +0800

    touch app/views/carts/checkout.html.erb

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,6 @@
 class CartsController < ApplicationController
 
   def destroy_all
-
   end
 
   def clean
@@ -9,4 +8,9 @@ class CartsController < ApplicationController
     flash[:warning] = "已清空购物车"
     redirect_to carts_path
   end
+
+  def checkout
+    @order = Order.new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,8 @@ Rails.application.routes.draw do
 
   resources :cart_items       #购物车列表
 
+  resources :orders
+
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
