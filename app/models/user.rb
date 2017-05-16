b76c5d41b65d44e3ebc16739eb36135f19f96738
emilesu <emilesu@qq.com>
2017-05-16 18:31:36 +0800

    add order controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+
+  # ---与order订单关系---
+
+  has_many :orders
 
 
   # ---is_admin判断式---
