1836f5e7ac820d79dd32ca35dc14545bc456a0e9
刘德政 <liudezheng@liudezhengdeMacBook-Pro.local>
2017-05-16 15:23:27 +0800

    add order controller

---
+++ app/controllers/order_controller.rb
@@ -0,0 +1,22 @@
+class OrderController < ApplicationController
+   before_action :authenticate_user!, only: [:create]
+
+   def create
+     @order = Order.new(order_params)
+     @order.user = current_user
+     @order.total = current_cart.total_price
+
+     if @order.save
+       redirect_to order_path
+     else
+       render 'carts/checkout'
+     end
+   end
+
+   private
+
+   def order_params
+     params.require(:order).permit(:billing_name, :billing_address, :shipping_name, :shipping_adress)
+   end
+
+end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,9 @@
 class Order < ApplicationRecord
+  belongs_to :user
+
+  validates :billing_name, presence: true
+  validates :billing_adress, presence: true
+  validates :shipping_name, presence: true
+  validates :shipping_adress, presence: true
+
 end
