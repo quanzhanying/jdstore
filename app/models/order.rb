75aca01205b7cc353ac8a8001a19799693967a73
fiona <13773907@qq.com>
2017-05-16 22:35:12 +0800

    add order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,9 @@
 class Order < ApplicationRecord
+  belongs_to :user
+
+  validateds :billing_name, presence: true
+  validateds :billing_address, presence: true
+  validateds :shipping_name, presence: true
+  validateds :shipping_address, presence:true
+  
 end
