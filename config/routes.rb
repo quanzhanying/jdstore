4e191a31c4b7accb32fb95136be38c9d1f6481dd
wangxing <18912540921@163.com>
2017-05-16 21:56:26 +0800

    add admin/orders

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
   namespace :admin do
+    resources :orders
     resources :products do
       member do
         patch :move_up
