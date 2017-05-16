830891a6640b57d55de4956f3e85599aafbc20cb
peterswj <peterswj@163.com>
2017-05-16 13:18:12 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
-  
-  root 'welcome#index'
+
+  #root 'welcome#index'
+  root 'products#index'
+
+  resources :products
 end
