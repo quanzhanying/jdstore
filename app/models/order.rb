986a54325af996d8304619aac5254ec60597b282
amy <amy87326@163.com>
2017-05-18 06:51:41 +0800

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,11 @@
 class Order < ApplicationRecord
+   before_create :generate_token
    belongs_to :user
+
+   def generate_token
+      self.token = SecureRandom.uuid
+   end
+
 
    validates :billing_name, presence: true
    validates :billing_address, presence: true
