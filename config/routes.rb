a2dd78b99970761149fda5bb46c743bb18feb16c
李婉 <iwan16406783@gmail.com>
2017-05-17 20:54:35 +0800

    touch app/views/carts/checkout.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'products#index'
   devise_for :users
   resources :cart_items
+  resources :orders
   resources :carts do
     collection do
       delete :clean
