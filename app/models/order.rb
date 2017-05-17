e9456a9e2c1aaba7444ad7179aae1d0a4ef2a35e
like <20783506632@qq.com>
2017-05-17 16:53:47 +0800

    add payment_method function

--- app/models/order.rb
+++ app/models/order.rb
@@ -12,4 +12,13 @@ has_many :product_lists
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
+
+def set_payment_with!(method)
+  self.update_columns(payment_method: method )
+end
+
+  def pay!
+    self.update_columns(is_paid: true )
+  end
+
 end
