c8202210f48873b742d17cc679fb24e3badabcb6
Helen <helenmayanna@gmail.com>
2017-05-17 22:28:52 +0800

    touch app/views/carts/checkout.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   end
 
   resources :cart_items
+  resources :orders
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
