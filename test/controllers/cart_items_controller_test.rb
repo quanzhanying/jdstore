2bd3c1a6f754118a30acb926519f2332d4301ac6
newevan <evan_bai@126.com>
2017-05-17 18:53:05 +0800

    delete an item from cart

--- test/controllers/cart_items_controller_test.rb
+++ test/controllers/cart_items_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CartItemControllerTest < ActionDispatch::IntegrationTest
+class CartItemsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
