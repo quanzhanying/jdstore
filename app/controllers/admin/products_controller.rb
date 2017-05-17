9080e701b8bcd7e7e141417d53974c14db8453e7
riverhome <liushuijiaxiang@gmail.com>
2017-05-17 23:25:31 +0800

    implement image upload

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -42,7 +42,7 @@ end
 private
 
 def product_params
-  params.require(:product).permit(:title, :description, :quantity, :price)
+  params.require(:product).permit(:title, :description, :quantity, :price, :image)
 end
 
 end
