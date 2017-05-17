64ab0d9cb34c30de4ed3a2150145527195d68f85
emilesu <emilesu@qq.com>
2017-05-17 11:23:18 +0800

    implement admin/orders show funthion

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,8 +3,16 @@ class Admin::OrdersController < ApplicationController
   before_action :admin_required
   layout "admin"
 
+
+  # ---CRUD---
+
   def index
     @orders = Order.order("id DESC")
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    @product_lists = @order.product_lists
   end
 
 
