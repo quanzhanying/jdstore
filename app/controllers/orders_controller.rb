552d5439ca1a3ab522413a8b75c7311bdf4708c3
zhaoxinxi <90zhaoxinxi@gmail.com>
2017-05-17 22:14:50 +0800

    add notify_cancel/ship function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,27 +25,33 @@ class OrdersController < ApplicationController
     end
   end
 
-    def show
+  def show
+    @order = Order.find_by_token(params[:id])
+    @product_lists = @order.product_lists
+  end
+
+  def pay_with_alipay
       @order = Order.find_by_token(params[:id])
-      @product_lists = @order.product_lists
-    end
-
-    def pay_with_alipay
-        @order = Order.find_by_token(params[:id])
-        @order.set_payment_with!("alipay")
-        @order.make_payment!
-
-        redirect_to order_path(@order.token), notice: "使用支付宝成功完成付款"
-    end
-
-    def pay_with_wechat
-      @order = Order.find_by_token(params[:id])
-      @order.set_payment_with!("wechat")
+      @order.set_payment_with!("alipay")
       @order.make_payment!
 
-      redirect_to order_path(@order.token), notice: "使用微信成功完成付款"
-    end
+      redirect_to order_path(@order.token), notice: "使用支付宝成功完成付款"
+  end
 
+  def pay_with_wechat
+    @order = Order.find_by_token(params[:id])
+    @order.set_payment_with!("wechat")
+    @order.make_payment!
+
+    redirect_to order_path(@order.token), notice: "使用微信成功完成付款"
+  end
+
+  def apply_to_cancel
+    @order = Order.find(params[:id])
+    OrderMailer.apply_cancel(@order).deliver!
+    flash[:notice] = "已提交申请"
+    redirect_to :back
+  end
 
   private
 
