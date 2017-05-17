68bdaaa33461c69666c80bdcc7b9fae5d44b0497
Archer Zhao <changlechangsheng@gmail.com>
2017-05-18 07:44:31 +0800

    Add admin/products controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def is_admin?
+    email == 'admin@jd.com'
+  end
+  
 end
