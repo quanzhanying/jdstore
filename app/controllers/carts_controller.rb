f463eba346c85cb829e0e4bf414ff92ebff8d20f
maxinlei <maxinlei_public@126.com>
2017-05-17 11:24:12 +0800

    empty cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,18 @@
 class CartsController < ApplicationController
 
+  def clean
+    current_cart.clean!
+    flash[:warning] = "已清空购物车"
+    redirect_to carts_path
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+  
+
+
 end
