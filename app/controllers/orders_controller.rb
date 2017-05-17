a9196bdd66a9e79e0a6a4ea1461f237411c481da
donker008 <richdonker@qq.com>
2017-05-17 21:08:49 +0800

    implement order list

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,10 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @orders = Order.where(:user_id => current_user.id).all
+  end
+
 
   def create
     @order =  Order.new(order_params)
