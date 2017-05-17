f23f7afd9df7d173a769ee7e84d5c6bb627a7f9f
like <20783506632@qq.com>
2017-05-17 10:30:00 +0800

    add product_list model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
+  has_many :orders
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
    def admin?
      is_admin
-   end     
+   end
 end
