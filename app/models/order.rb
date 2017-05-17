121dddff1c415613ab24389f39cc97b3643faeb9
Yechti <yechti@gmail.com>
2017-05-17 11:51:25 +0800

    add order controller re-adjustment

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,5 +4,5 @@ class Order < ApplicationRecord
   validates :billing_name, presence: true
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
-  validates :shipping_name, presence: true
+  validates :shipping_address, presence: true
 end
