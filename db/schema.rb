e89d0e96a524d8985ccd92cdbfbda0e937c9a97d
sww <sww@swwdeMacBook-Pro.local>
2017-05-17 21:15:31 +0800

    add orders show function

--- db/schema.rb
+++ db/schema.rb
@@ -38,8 +38,8 @@ ActiveRecord::Schema.define(version: 20170517121025) do
 
   create_table "product_lists", force: :cascade do |t|
     t.integer  "order_id"
-    t.integer  "product_name"
-    t.integer  "peoduct_price"
+    t.string   "product_name"
+    t.integer  "product_price"
     t.integer  "quantity"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
