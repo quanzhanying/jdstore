87e0e43dce6131a9786b0b9cf5dfad17c0128a72
superzrr <superzrr@gmail.com>
2017-05-18 07:26:39 +0800

    rails g controller admin::orders

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,18 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_is_admin
+  layout 'admin'
+
+  def index
+    @orders = Order.all.order("id DESC")
+  end
+
+  private
+  def require_is_admin
+    if !current_user.admin?
+      flash[:alert] = "您没有管理员权限，无法访问本页面"
+      redirect_to root_path
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :products
+    resources :orders
   end
 
   namespace :account do
