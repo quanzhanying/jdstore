2c5c448fcb8bde94c7c9e886c8c0be5a869a1e02
wangxing <18912540921@163.com>
2017-05-17 15:25:18 +0800

    reset gem 'annotate'

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: carts
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Cart < ApplicationRecord
   has_many :cart_items
   has_many :products, through: :cart_items, source: :product
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: orders
+#
+#  id               :integer          not null, primary key
+#  total            :integer          default("0")
+#  user_id          :integer
+#  billing_name     :string
+#  billing_address  :string
+#  shipping_name    :string
+#  shipping_address :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  token            :string
+#  is_paid          :boolean          default("f")
+#  payment_method   :string
+#  aasm_state       :string           default("order_placed")
+#
+
 class Order < ApplicationRecord
   before_create :generate_token
 
