fa8bb2ace6d65222ffeb2fbc632892627532020a
敖江帆 <enjoy_ror@yahoo.com>
2017-05-17 15:48:47 +0800

    add admin/orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :products
+    resources :orders
   end
 
   namespace :account do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517071504) do
+ActiveRecord::Schema.define(version: 20170517073928) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170517071504) do
     t.string   "token"
     t.boolean  "is_paid",          default: false
     t.string   "aasm_state",       default: "order_placed"
+    t.string   "payment_method"
     t.index ["aasm_state"], name: "index_orders_on_aasm_state"
   end
 
