0a81b2bf3f543aa349d4c819c7e98134a691f59f
donker008 <richdonker@qq.com>
2017-05-17 20:53:47 +0800

    new implement for for order detail

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
 
+
   def create
     @order =  Order.new(order_params)
     @order.user = current_user
@@ -14,18 +15,21 @@ class OrdersController < ApplicationController
         product_list.quantity  = cart_item.quantity
         product_list.save
       end
-      redirect_to order_path(@order)
+      redirect_to order_path(@order.token)
     else
       render 'carts/checkout'
     end
   end
 
   def show
-    @order = Order.find(params[:id])
+    @order = Order.find_by_token(params[:id])
     @product_lists = @order.product_lists
   end
 
+
   private
+
+
   def order_params
     params.require(:order).permit(:billing_name, :billing_address, :shipping_name, :shipping_address,);
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -5,5 +5,10 @@ class Order < ApplicationRecord
   validates :shipping_address, presence: true
   belongs_to :user
   has_many :product_lists
-  
+
+  before_create :generate_token
+
+  def generate_token
+      self.token = SecureRandom.uuid
+  end
 end
