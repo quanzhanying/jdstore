fee4b5908438a11f4a21517ee63eae383f64631f
fiona <13773907@qq.com>
2017-05-18 00:12:55 +0800

    users can see their orders

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # Prevent CSRF attacks by raising an exception.
 
+  # For APIs, you may want to use :null_session instead.
+
+
+  protect_from_forgery with: :exception
 
   def admin_required
     if !current_user.admin?
