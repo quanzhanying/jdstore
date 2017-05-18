a1348697ad27d72eb9916495f5b9ca23893683e6
“马静” <melindama@me.com>
2017-05-18 21:12:36 +0800

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,10 @@
 class Order < ApplicationRecord
+  before_create :generate_token
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
+
   belongs_to :user
   has_many :product_lists
 
