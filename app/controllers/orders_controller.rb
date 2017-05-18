95497e7060c4fa44dcf2016a94390ce027bf10cc
刘德政 <liudezheng@liudezhengdeMacBook-Pro.local>
2017-05-18 09:23:18 +0800

    add orders show function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,7 +11,7 @@ class OrdersController < ApplicationController
        product_list = ProductList.new
        product_list.order = @order
        product_list.product_name = cart_item.product.title
-       product_list.proudct_price = cart_item.product.price
+       product_list.product_price = cart_item.product.price
        product_list.quantity = cart_item.quantity
        product_list.save
      end
@@ -21,6 +21,12 @@ class OrdersController < ApplicationController
       render 'carts/checkout'
     end
   end
+
+      def show
+        @order = Order.find(params[:id])
+        @product_lists = @order.product_lists
+      end
+
     private
 
     def order_params
