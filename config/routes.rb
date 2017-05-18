a9bafb8db9f9a00eac9a2a9362250436ea2f86cc
lingboweibu1234 <qawzse656746@gmail.com>
2017-05-18 11:25:19 +0800

    empty cart

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,9 @@ Rails.application.routes.draw do
      end
    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   resources :carts
+   resources :carts do
+     collection do
+      delete :clean
+     end
+   end
 end
