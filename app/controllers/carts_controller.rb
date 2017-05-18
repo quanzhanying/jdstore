1aa01f8b79f5a9ad1319f27eced4a69f89370821
justin <justinng@Justins-MacBook-Air.local>
2017-05-18 21:29:59 +0800

    add orders show function

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,10 +3,11 @@ class CartsController < ApplicationController
    current_cart.clean!
    flash[:warning] = "已清空购物车"
    redirect_to carts_path
+  end
 
    def checkout
      @order = Order.new
    end
-   
- end
+
+
 end
