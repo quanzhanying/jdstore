eccd0fb1129c89de89dd74a2461228d9bd633f8d
“马静” <melindama@me.com>
2017-05-19 06:37:49 +0800

    add payment_method function

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,12 @@ Rails.application.routes.draw do
   end
 
   resources :cart_items
-  resources :orders
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+    end
+  end
 
   namespace :account do
     resources :orders
