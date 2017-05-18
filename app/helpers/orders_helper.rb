e91256d3ef0483d9baa54e5074f3bb9fa6dabdf3
黄永飞 <h739578083@gmail.com>
2017-05-18 16:18:49 +0800

    add ship/shipped/cancel/return action to admin/orders_controller

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,6 +1,6 @@
 module OrdersHelper
   def render_order_paid_state(order)
-    if order.is_padi?
+    if order.is_paid?
       "已付款"
     else
       "未付款"
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   root 'products#index'
   devise_for :users
+
   namespace :admin do
     resources :products
-    resources :orders
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return 
+      end
+    end
   end
 
   resources :products do
