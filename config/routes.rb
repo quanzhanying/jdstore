9c70471e6c147fa768f2eb8073ead42db8a2e86a
liujia <ericrunner123456@yahoo.com>
2017-05-18 08:08:04 +0800

    ready for shopping cart

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users,controllers:{sessions:'users/sessions',registrations:'users/registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-  resources :products
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
   namespace :admin do
     resources :products
   end
