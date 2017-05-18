202779b6342ad5077591b09f951d14d17c849067
blueming <wenyiff66@gmail.com>
2017-05-18 16:13:45 +0800

    add admin/order

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :products
+    resources :orders
   end
 
   resources :products do
@@ -29,7 +30,7 @@ Rails.application.routes.draw do
 
   namespace :account do
     resources :orders
-    
+
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
