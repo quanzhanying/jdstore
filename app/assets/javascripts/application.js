f10bda2d3e639367887f96950b5f534721032e2c
zhongxh <zhxhyz@gmail.com>
2017-05-18 23:11:04 +0800

    add float button to go top & qq service

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,30 @@
 //= require bootstrap/alert
 //= require bootstrap/dropdown
 //= require_tree .
+
+$(function() {
+    $("#usr_rmenu").each(function() {
+        $(this).find(".btn-top").click(function() {
+            $("html, body").animate({
+                "scroll-top": 0
+            }, 800)
+        })
+    });
+    var lastRmenuStatus = false;
+    $(window).scroll(function() {
+        var _top = $(window).scrollTop();
+        if (_top > 200) {
+            $("#usr_rmenu").data("expanded", true)
+        } else {
+            $("#usr_rmenu").data("expanded", false)
+        }
+        if ($("#usr_rmenu").data("expanded") != lastRmenuStatus) {
+            lastRmenuStatus = $("#usr_rmenu").data("expanded");
+            if (lastRmenuStatus) {
+                $("#usr_rmenu .btn-top").slideDown()
+            } else {
+                $("#usr_rmenu .btn-top").slideUp()
+            }
+        }
+    })
+});
