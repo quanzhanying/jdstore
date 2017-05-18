11208c46fd99221f562462a6beee8abdb4230ab5
吴昱 <wuyu1989610@gmail.com>
2017-05-18 14:37:20 +0800

    implement token

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,10 @@ class Order < ApplicationRecord
   validates :billing_name, :billing_address, :shipping_name, :shipping_address, presence: true
 
   has_many :product_lists
+
+  before_create :generate_token
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
 end
