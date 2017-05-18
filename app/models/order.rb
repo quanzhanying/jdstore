af4eb03054d98664213af79bb24a9eefb56ef39a
maxinlei <maxinlei_public@126.com>
2017-05-18 09:47:42 +0800

    add order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,9 @@
 class Order < ApplicationRecord
-  belongs_to :billing_name, presence: true
+  belongs_to :user
 
   validates :billing_name, presence: true
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
-  validates :shipping_address, presence: true 
+  validates :shipping_address, presence: true
 
 end
