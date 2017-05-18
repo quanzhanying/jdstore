e49deb1db9af07135295f2e32a6338335bcdf2e5
panyujin <panyujinyx@gmail.com>
2017-05-18 18:35:24 +0800

    add aasm_state to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -19,4 +19,36 @@ class Order < ApplicationRecord
   def pay!
     self.update_columns(is_paid: true )
   end
+
+  include AASM
+
+  aasm do
+    state :order_placed, initial: true
+    state :paid
+    state :shipping
+    state :shipped
+    state :order_cancelled
+    state :good_returned
+
+
+    event :make_payment, after_commit: :pay! do
+      transitions from: :order_placed, to: :paid
+    end
+
+    event :ship do
+      transitions from: :paid,         to: :shipping
+    end
+
+    event :deliver do
+      transitions from: :shipping,     to: :shipped
+    end
+
+    event :return_good do
+      transitions from: :shipped,      to: :good_returned
+    end
+
+    event :cancel_order do
+      transitions from: [:order_placed, :paid], to: :order_cancelled
+    end
+  end
 end
