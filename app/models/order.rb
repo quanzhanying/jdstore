88fd4a79714cf26aef8e4b4dc46f7699b83a69be
nathanye <khalil54781@gmail.com>
2017-05-18 11:06:52 +0800

    add OrderMailer function

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,4 @@
 class Order < ApplicationRecord
-  before_create :generate_token
-
-  def generate_token
-    self.token = SecureRandom.uuid
-  end
-
   belongs_to :user
 
   validates :billing_name, presence: true
@@ -13,4 +7,19 @@ class Order < ApplicationRecord
   validates :shipping_address, presence: true
 
   has_many :product_lists
+
+  before_create :generate_token
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
+
+  def set_payment_with!(method)
+    self.update_columns(payment_method: method)
+  end
+
+  def pay!
+    self.update_columns(is_paid: true)
+  end
+
 end
