69cc6859550d78719011ef4536c6ec0843cec1ce
xiaaa <xiaenchanter@gmail.com>
2017-05-19 23:12:14 +0800

    add checkout routing

--- Gemfile
+++ Gemfile
@@ -38,7 +38,9 @@ gem 'carrierwave'
 gem 'mini_magick'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platform: :mri
+  #gem 'byebug', platform: :mri
+  gem 'pry'
+  gem 'awesome_rails_console'
 end
 
 group :development do
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,11 +5,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
+  
   storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
