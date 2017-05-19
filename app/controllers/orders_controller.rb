937526d709ab6ddb28ffdc68db93bef8bf1beb0c
justin <justinng@Justins-MacBook-Air.local>
2017-05-19 20:48:00 +0800

    add apply_cancel function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -45,6 +45,14 @@ class OrdersController < ApplicationController
     redirect_to order_path(@order.token), notice: "Wechat pay successful"
   end
 
+  def apply_to_cancel
+    @order = Order.find(params[:id])
+    OrderMailer.apply_cancel(@order).deliver!
+    flash[:notice] = "已提交申请"
+    redirect_to :back
+  end
+
+
   private
 
   def order_params
