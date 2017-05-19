30ed57ceaa10348a5eca2306d8c5b387f57b8523
xiaaa <xiaenchanter@gmail.com>
2017-05-19 23:28:45 +0800

    touch app/views/carts/checkout.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
 
   resources :cart_items
 
+  resources :orders
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
