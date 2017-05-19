632a4bf5ce34a97573c0b2533098da41396e0c80
guomei <lengxuehx@gmail.com>
2017-05-19 11:33:16 +0800

    add product_list model

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:create]
   def create
-    @order = Order.nwe(order_params)
+    @order = Order.new(order_params)
     @order.user = current_user
     @order.total = current_cart.total_price
 
