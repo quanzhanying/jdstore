e6dd1bcdd31556980fcc370b7339505974a30937
songhao <songhao@songhaodeMacBook-Pro.local>
2017-05-19 13:45:09 +0800

    implement add_to_cart button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,10 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    flash[:notice] = "成功将#{@product.title}加入购物车"
+    redirect_to :back
+  end
 end
