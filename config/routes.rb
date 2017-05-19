7648673637ba7963f891c7bead9ebc45de6f2806
chanweiyan <chanweiyan007@gmail.com>
2017-05-19 17:06:58 +0800

    add category model/controllers/routes

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,9 @@ Rails.application.routes.draw do
   namespace :account do
     resources :orders
   end
+
+  resources :categories
+  
   root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
