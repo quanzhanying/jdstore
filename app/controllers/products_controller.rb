0b1e96457ce8c2e762e1514c9a099a8f07ab6167
wangxing <18912540921@163.com>
2017-05-19 20:03:03 +0800

    search function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :validate_search_key, only: [:search]
 
   def index
     @products = Product.all.order("position ASC")
@@ -20,6 +21,23 @@ class ProductsController < ApplicationController
 
   end
 
-  private
+  def search
+      if @query_string.present?
+        search_result = Product.ransack(@search_criteria).result(:distinct => true)
+        @products = search_result.paginate(:page => params[:page], :per_page => 10 )
+      end
+    end
 
+
+    protected
+
+    def validate_search_key
+      @query_string = params[:q].gsub(/\\|\'|\/|\?/, "") if params[:q].present?
+      @search_criteria = search_criteria(@query_string)
+    end
+
+
+    def search_criteria(query_string)
+      { title_or_description_cont: @query_string }
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,9 @@ Rails.application.routes.draw do
   end
 
   resources :products do
+    collection do
+      get :search
+    end
     member do
       post :add_to_cart
     end
