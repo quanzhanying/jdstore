380e67c5cfdd96223a7ea6018e6bdc6d822f9346
ankon <ankon@ankon-mac.local>
2017-05-19 11:19:35 +0800

    add payment method

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :orders
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+    end
+  end
 
   resources :carts do
     collection do
