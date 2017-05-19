d5ddba6d4ba78928283b7820caa5902669732c3a
ziud7rs <ziud7rs@SInG-2.local>
2017-05-19 17:30:49 +0800

    刪除衝突檔案＋加上landing page及主要css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,60 @@
 //= require turbolinks
 //= require bootstrap/alert
 //= require bootstrap/dropdown
+//= require bootstrap-sprockets
 //= require_tree .
+
+// --首頁圖片輪播--
+$(function(){
+    var $item = $('.carousel .item');
+    var $wHeight = $(window).height();
+    $item.height($wHeight);
+    $item.addClass('full-screen');
+
+    $('.carousel img').each(function() {
+      var $src = $(this).attr('src');
+      var $color = $(this).attr('data-color');
+      $(this).parent().css({
+        'background-image' : 'url(' + $src + ')',
+        'background-color' : $color
+      });
+      $(this).remove();
+    });
+
+    //下方自動加入控制圓鈕
+    var total = $('.carousel .carousel-inner div.item').size();
+    append_li();
+    function append_li()
+    {
+        var li = "";
+        var get_ac = $( ".carousel .active" );
+        var ac =  $( ".carousel .carousel-inner div" ).index( get_ac );
+
+        for (var i=0; i <= total-1; i++){
+            if(i == (ac)/2){
+                li += "<li data-target='#mycarousel' data-slide-to='"+i+"' class='active'></li>";
+            }else{
+                li += "<li data-target='#mycarousel' data-slide-to='"+i+"' class=''></li>";
+                }
+            }
+            $(".carousel-indicators").append(li);
+        }
+
+    //單則隱藏控制鈕
+    if ($('.carousel .carousel-inner div.item').length < 2 ) {
+            $('.carousel-indicators, .carousel-control').hide();
+    }
+
+    //縮放視窗調整視窗高度
+    $(window).on('resize', function (){
+      $wHeight = $(window).height();
+      $item.height($wHeight);
+    });
+
+    //輪播秒數與滑入停止
+    $('.carousel').carousel({
+      interval: 5000,
+      pause: "hover"
+    });
+});
+// --首頁圖片輪播--
--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,6 @@ Rails.application.routes.draw do
    resources :orders
    resources :products
   end
-  root 'products#index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
