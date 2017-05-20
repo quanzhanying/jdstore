038cb85943a47ddd4280faf1c26bb1881907b092
guomei <lengxuehx@gmail.com>
2017-05-20 15:04:26 +0800

    add use orders to navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170519055303) do
+ActiveRecord::Schema.define(version: 20170519064854) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20170519055303) do
     t.datetime "updated_at",                       null: false
     t.string   "token"
     t.boolean  "is_paid",          default: false
+    t.string   "payment_method"
   end
 
   create_table "product_lists", force: :cascade do |t|
