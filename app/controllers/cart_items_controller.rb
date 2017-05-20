1475c26a1a1126afa3834e528591a231072c1a38
afish <owengoal2008@163.com>
2017-05-20 09:27:10 +0800

    delete an item from cart

---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,12 @@
+class CartItemsController < ApplicationController
+  before_action :authenticate_user!
+
+  def destroy
+    @cart = current_cart
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    @product = @cart_item.product
+    @cart_item.destroy
+
+    redirect_to :back, warning:"成功将 #{@product.title} 从购物车删除！"
+  end
+end
