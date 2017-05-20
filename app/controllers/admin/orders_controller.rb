781c8a7828e56294f9c303f1a30ba265dca13a48
匡敏 <kuangmin@kuangmindeMacBook-Pro.local>
2017-05-20 20:34:02 +0800

    add ship/shipped/cancel/return action to admin/orders_controller

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,6 +4,30 @@ layout "admin"
 before_action :authenticate_user!
 before_action :admin_required
 
+def ship
+  @order = Order.find(params[:id])
+  @order.ship!
+  redirect_to :back
+end
+
+def shipped
+  @order = Order.find(params[:id])
+  @order.deliver!
+  redirect_to :back
+end
+
+def cancel
+  @order = Order.find(params[:id])
+  @order.cancel_order!
+  redirect_to :back
+end
+
+def return
+  @order = Order.find(params[:id])
+  @order.return_good!
+  redirect_to :back
+end
+
 
 def index
   @orders = Order.order("id DESC")
