9b8444bdd70f28949e811d8bd0899e35ff8178f4
Archer Zhao <changlechangsheng@gmail.com>
2017-05-20 18:46:22 +0800

    Add product index/show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :products
   end
   devise_for :users
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "welcome#index"
+  root "products#index"
 end
