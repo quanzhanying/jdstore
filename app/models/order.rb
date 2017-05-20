56e04c2a583bf0d8c579ead00f8cb98f7d737431
ypj <ypj2005@126.com>
2017-05-20 22:54:28 +0800

    add order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,9 @@
 class Order < ApplicationRecord
+  belongs_to :user
+
+  validates :billing_name, presence: true
+  validates :billing_address, presence: true
+  validates :shopping_name, presence: true
+  validates :shopping_address, presence: true
+  
 end
