16923b13b6b78f88e830c7b183d5141d5a3295c5
shimmy <397721260@qq.com>
2017-05-20 10:50:54 +0800

    cannot add item with quantity 0

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -9,4 +9,22 @@ def destroy
   redirect_to :back
 end
 
+def update
+  @cart = current_cart
+  @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
+  @cart_item.update(cart_item_params)
+flash[:notice] = "成功变更数量"
+else
+  flash[:warning]= "暂时无货，无法加入购物车"
+end
+  redirect_to carts_path
+end
+
+private
+
+def cart_item_params
+  params.require(:cart_item).permit(:quantity)
+end
+
 end
