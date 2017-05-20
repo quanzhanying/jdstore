e4833c169d25ad5cd8c29135a105762fae7ccff9
emilesu <emilesu@qq.com>
2017-05-20 14:17:19 +0800

    update seed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 
   # --CRUD--
   def index
-    @products = Product.all.order("position ASC")
+    @products = Product.all.order("position ASC").paginate(:page => params[:page], :per_page => 12)
   end
 
 end
