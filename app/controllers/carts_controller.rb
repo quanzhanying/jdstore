97e377609b1aa6245cb0e839e74d92358d4b779a
afish <owengoal2008@163.com>
2017-05-20 22:32:47 +0800

    touch app/views/carts/checkout.html.erb

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,4 +3,8 @@ class CartsController < ApplicationController
     current_cart.clean!
      redirect_to carts_path,  warning: "请清空购物车"
   end
+
+  def checkout
+    @order = Order.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,6 @@ Rails.application.routes.draw do
   end
 
   resources :cart_items
+  resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
