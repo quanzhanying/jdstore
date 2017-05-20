265d859558106b0a6edba6ab809c980b61d1c9a7
peterswj <peterswj@163.com>
2017-05-20 23:54:14 +0800

    生成订单跳转成功，但商品信息传不上来

--- app/models/order.rb
+++ app/models/order.rb
@@ -24,11 +24,5 @@ class Order < ApplicationRecord
      self.update_columns(is_paid: true )
    end
 
-  # def set_payment_with!(method)
-  #   self.update_columns(payment_method:method )
-  # end
-  #
-  # def pay!
-  #   self.update_columns(is_paid:true)
-  # end
+
 end
