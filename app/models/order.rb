2bcf4aba6bceeb24a5657d398787ffbe0a3c8f0d
House_Leung <House.liangshihao@gmail.com>
2017-05-21 16:29:51 +0800

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,12 @@
 class Order < ApplicationRecord
+  before_create :generate_token
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
+  
   belongs_to :user
-  has_many :product_lists 
+  has_many :product_lists
 
   validates :billing_name, presence: true
   validates :billing_address, presence: true
