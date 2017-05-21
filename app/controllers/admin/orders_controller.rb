2a978ae296f15f8060a41bece6a53efa874b7a0d
paulshu <paulshu18@gmail.com>
2017-05-21 11:44:33 +0800

    add admin/orders/state_option button

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -10,7 +10,7 @@ class Admin::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    @propdut_lists = @order.product_lists
+    @product_lists = @order.product_lists
   end
 
   def ship
