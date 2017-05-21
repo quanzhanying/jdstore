7814fe598931b0dbf6e002a831de6587f65e2662
riverhome <liushuijiaxiang@gmail.com>
2017-05-21 23:51:57 +0800

    add orders show function

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 20170521043028) do
 
   create_table "product_lists", force: :cascade do |t|
     t.integer  "order_id"
-    t.string   "prodcut_name"
+    t.string   "product_name"
     t.integer  "product_price"
     t.integer  "quantity"
     t.datetime "created_at",    null: false
