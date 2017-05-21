9e3af76c5afee8b515d6d8a0ada00df902bbdb7e
ethanyin89 <wei89.com@gmail.com>
2017-05-21 09:24:09 +0800

    cannot change quantity which exceed stock

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,7 +12,7 @@ class ProductsController < ApplicationController
     if !current_cart.products.include?(@product)
 
       current_cart.add_product_to_cart(@product)
-      flash[:notice] = "你已成功将 #{@product.title} 加入购物车"
+      flash[:notice] = "你已成功将 #{@product.title}加入购物车"
     else
       flash[:warning] = "你的购物车已有此物品"
     end
