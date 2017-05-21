abd3d881923c7f8af1001b684dd8a4b80cb7663b
zhuozhi <zhuozhi12@gmail.com>
2017-05-22 07:44:12 +0800

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,6 +7,10 @@ class Order < ApplicationRecord
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
 
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
+
   has_many :product_lists
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170521233235) do
+ActiveRecord::Schema.define(version: 20170521234201) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20170521233235) do
     t.string   "shipping_address"
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
+    t.string   "token"
   end
 
   create_table "product_lists", force: :cascade do |t|
