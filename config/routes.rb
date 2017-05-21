027984a5fd3286499d85de1653dd07d5b4013ce6
House_Leung <House.liangshihao@gmail.com>
2017-05-21 21:18:20 +0800

    users can see their orders

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,10 @@ Rails.application.routes.draw do
   end
 
   resources :cart_items
-  resources :orders 
+  resources :orders
+
+  namespace :account do
+    resources :orders 
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
