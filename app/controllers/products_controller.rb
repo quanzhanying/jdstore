1df2a2e578214dfb001adfb91d887ad4649e5f2d
imac <imac@imacdeiMac.local>
2017-05-22 06:30:23 +0800

    yixiu add category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,22 @@
 class ProductsController < ApplicationController
   def index
-    @products = case params[:order]
-              when 'down_product.price'
-                Product.published.order('price DESC').paginate(:page => params[:page], :per_page => 5)
-              when 'up_product.price'
-                Product.published.order('price ASC').paginate(:page => params[:page], :per_page => 5)
-              else
-                Product.published.recent.paginate(:page => params[:page], :per_page => 5)
-              end
+    if params[:category].present?
+       @category = params[:category]
+       if @category == '所有商品'
+         @products = Poduct.published.recent.paginate(:page => params[:page], :per_page => 5)
+       else
+         @products = Product.published.where(:category => @category).recent.paginate(:page => params[:page], :per_page => 5)
+       end
+    else
+        @products = case params[:order]
+                  when 'down_product.price'
+                    Product.published.order('price DESC').paginate(:page => params[:page], :per_page => 5)
+                  when 'up_product.price'
+                    Product.published.order('price ASC').paginate(:page => params[:page], :per_page => 5)
+                  else
+                    Product.published.recent.paginate(:page => params[:page], :per_page => 5)
+                  end
+    end
   end
 
   def show
