c65924f4d1a73fc127c1d76a48c7277faa5fd04f
Archer Zhao <changlechangsheng@gmail.com>
2017-05-21 16:04:08 +0800

    Add remove item from cart function

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
       post :clear
     end
   end
+  resources :cart_items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "products#index"
 end
