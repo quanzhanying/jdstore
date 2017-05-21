2eeefb1b9ea1ec929c969661349665218584072a
afish <owengoal2008@163.com>
2017-05-21 10:03:33 +0800

    add token to order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,36 +1,36 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:create]
 
-    def create
-      @order = Order.new(order_params)
-      @order.user = current_user
-      @order.total = current_cart.total_price
+  def create
+    @order = Order.new(order_params)
+    @order.user = current_user
+    @order.total = current_cart.total_price
 
-      if @order.save!
+    if @order.save
 
-       current_cart.cart_items.each do |cart_item|
-         product_list = ProductList.new
-         product_list.order = @order
-         product_list.product_name = cart_item.product.title
-         product_list.product_pricr = cart_item.product.price
-         product_list.quantity = cart_item.quantity
-         product_list.save
-       end
+        current_cart.cart_items.each do |cart_item|
+          product_list = ProductList.new
+          product_list.order = @order
+          product_list.product_name = cart_item.product.title
+          product_list.product_price = cart_item.product.price
+          product_list.quantity = cart_item.quantity
+          product_list.save
+        end
 
-       redirect_to order_path(@order)
-      else
-        render 'carts/checkout'
-      end
+      redirect_to order_path(@order.token)
+    else
+      render 'carts/checkout'
     end
+  end
 
-    def show
-      @order = Order.find(params[:id])
-      @product_list = @order.product_lists
-    end
+  def show
+    @order = Order.find_by_token(params[:id])
+    @product_lists = @order.product_lists
+  end
 
-    private
+  private
 
-    def order_params
-      params.require(:order).permit(:billing_name, :billing_address, :shipping_name, :shipping_address)
-    end
+  def order_params
+    params.require(:order).permit(:billing_name, :billing_address, :shipping_name, :shipping_address)
+  end
 end
