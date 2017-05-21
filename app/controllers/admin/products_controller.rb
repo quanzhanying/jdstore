3672a95a07db3ae7c57639a208dd11a9ea0975db
zehuama <zehua046@gmail.com>
2017-05-22 07:21:15 +0800

    implement backend CRUD

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,37 @@
+class Admin::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(products_params)
+      redirect_to admin_products_path
+    else
+      render :eidt
+    end
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :quantity)
+  end
+end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,42 +3,42 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
-  def show
-    @product = Product.find(params[:id])
-  end
-
-  def new
-    @product = Product.new
-  end
-
-  def create
-    @product = Product.new(product_params)
-    @product.save
-    redirect_to products_path
-  end
-
-  def edit
-    @product = Product.find(params[:id])
-  end
-
-  def update
-    @product = Product.find(params[:id])
-    if @product.update
-      redirect_to products_path
-    else
-      render :new
-    end
-  end
-
-  def delete
-    @product = Product.find(params[:id])
-    @product.destroy
-    redirect_to products_path
-  end
-
-  private
-
-  def product_params
-    params.require(:product).permit(:title, :description, :price, :quantity)
-  end
+  # def show
+  #   @product = Product.find(params[:id])
+  # end
+  #
+  # def new
+  #   @product = Product.new
+  # end
+  #
+  # def create
+  #   @product = Product.new(product_params)
+  #   @product.save
+  #   redirect_to products_path
+  # end
+  #
+  # def edit
+  #   @product = Product.find(params[:id])
+  # end
+  #
+  # def update
+  #   @product = Product.find(params[:id])
+  #   if @product.update
+  #     redirect_to products_path
+  #   else
+  #     render :new
+  #   end
+  # end
+  #
+  # def delete
+  #   @product = Product.find(params[:id])
+  #   @product.destroy
+  #   redirect_to products_path
+  # end
+  #
+  # private
+  #
+  # def product_params
+  #   params.require(:product).permit(:title, :description, :price, :quantity)
+  # end
 end
