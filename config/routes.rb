9f164ac4e3691dd9a4310e70a8431647cabf570f
heric <hericpeng@gmail.com>
2017-05-21 08:19:00 +0800

    move repo

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'products#index'
+  root 'welcome#index'
 
   resources :products do
     member do
