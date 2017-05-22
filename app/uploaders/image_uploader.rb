45d9d05f2835c651c3c3941190cb66fdf776eb5b
chen li <chenli@chendeMac-mini.local>
2017-05-22 22:11:29 +0800

    config qiniu

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   storage :qiniu
 
   # Override the directory where uploaded files will be stored.
