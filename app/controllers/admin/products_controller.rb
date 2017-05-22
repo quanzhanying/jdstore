c7ba148c1f5602de1743c86ee251c13555827c63
robote <2233628310@qq.com>
2017-05-22 18:45:22 +0800

    cannot   add   item  with   quantity  0

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -27,7 +27,7 @@ class Admin::ProductsController < ApplicationController
 
 
   def   update
-    @product  =  product.find(params[:id])
+    @product  =  Product.find(params[:id])
 
     if  @product.update(product_params)
       redirect_to   admin_products_path
