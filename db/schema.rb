a1484b31b2af6aa6d08b6a95da4f623c4e74ff73
Brittany <evlyn5@sina.com>
2017-05-22 09:38:56 -0700

    implement image upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522002730) do
+ActiveRecord::Schema.define(version: 20170522163143) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
