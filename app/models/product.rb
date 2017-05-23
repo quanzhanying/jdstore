b3c46713326ee7b2013d0d074b9051da502859b8
olive <peiwy1@gmail.com>
2017-05-23 12:58:13 +0800

    cannot be o

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :quantity, presence: true
+    validates :price, presence: true
+    validates :price, numericality: { greater_than: 0}
 end
