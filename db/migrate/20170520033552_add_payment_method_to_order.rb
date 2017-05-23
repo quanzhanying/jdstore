4c8124ffd3f36d25ed334a21faf4281cb4495f49
lyy <liangyunyu@gmail.com>
2017-05-24 01:55:09 +0800

    rebuild order model

--- db/migrate/20170520033552_add_payment_method_to_order.rb
+++
@@ -1,5 +0,0 @@
-class AddPaymentMethodToOrder < ActiveRecord::Migration[5.0]
-  def change
-    add_column :orders, :payment_method, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170520064827) do
+ActiveRecord::Schema.define(version: 20170523175011) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -39,15 +39,6 @@ ActiveRecord::Schema.define(version: 20170520064827) do
     t.string   "payment_method"
     t.string   "aasm_state",       default: "order_placed"
     t.index ["aasm_state"], name: "index_orders_on_aasm_state"
-  end
-
-  create_table "product_lists", force: :cascade do |t|
-    t.integer  "order_id"
-    t.string   "product_name"
-    t.integer  "product_price"
-    t.integer  "quantity"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
   end
 
   create_table "products", force: :cascade do |t|
