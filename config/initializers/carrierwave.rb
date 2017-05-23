de4e9057b48b5faa6282e77e7b06c2e067198913
Yechti <yechti@gmail.com>
2017-05-23 21:08:08 +0800

    adjust CarrierWave position

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -21,11 +21,12 @@ CarrierWave.configure do |config|
     config.storage :file
   end
 
+end
+
+
   require 'carrierwave/storage/abstract'
   require 'carrierwave/storage/file'
   require 'carrierwave/storage/fog'
   CarrierWave.configure do |config|
 
-end
-
-end
+  end
