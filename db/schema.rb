5227c301545dc0d73352ea8eec303268e3fe7410
敖江帆 <enjoy_ror@yahoo.com>
2017-05-23 15:13:57 +0800

    add category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522064753) do
+ActiveRecord::Schema.define(version: 20170523011314) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20170522064753) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image"
+    t.string   "category"
   end
 
   create_table "users", force: :cascade do |t|
