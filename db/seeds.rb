f000c6468a4517f5899c848b66aa06baa7233d3f
刘华佼 <liuhuajiao@liuhuajiaodeMacBook-Air.local>
2017-05-23 15:03:01 +0800

    add seedforfirdtproduct

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-u = User.new
-u.email = "test@jdstore.com" #可以改成自己的email
 
-u.password = "666666"
-u.password_confirmation = "666666"
+# Initialize Boss User
+if User.find_by(email: "boss@jdstore.com").nil?
+  u = User.new
+  u.email = "boss@jdstore.com" #可以改成自己的email
+  u.password = "666666"
+  u.password_confirmation = "666666"
+  u.is_admin = true
+  u.save
+  puts "Admin已建好，账号为#{u.email}，密码为#{u.password}"
+else
+  puts "Admin已存在，脚本跳过此步骤。"
+end
 
-u.is_admin = true
-u.save
+#Initialize Product
+
+Product.create!(title: "写给女生的五个择偶建议",
+  description: "建议",
+  price: 199,
+  quantity: 1000,
+  image: open("http://ww1.sinaimg.cn/large/006tNbRwgy1ffvayld8o1j30qo0f074q.jpg")
+  )
