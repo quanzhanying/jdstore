318f2c9d42806515626db41ff833456781d697fa
niushuangmeng <niushuangmeng@gmail.com>
2017-05-23 22:01:29 +0800

    add kefu

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,11 @@ Rails.application.routes.draw do
       end
     end
   end
-  devise_for :users
+  devise_for :users, :controllers => {
+    :sessions   =>"users/sessions",
+    :registrations =>"users/registrations",
+    :passwords     =>"users/passwords",
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'welcome#index'
 resources :products do
