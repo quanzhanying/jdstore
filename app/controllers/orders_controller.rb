7500f83e2e27c27a568e396e409d42547f02aece
lyy <liangyunyu@gmail.com>
2017-05-24 03:18:36 +0800

    fix product_list lost

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, only: [:create]
+  # before_action :authenticate_user!, only: [:create]
+  before_action :authenticate_user!
 
   def show
     # @order = Order.find(params[:id])
