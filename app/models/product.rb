e663b912a2419e4e15fbf5d4ca75e477dce7b862
chen li <chenli@chendeMac-mini.local>
2017-05-23 23:30:39 +0800

    navbar & search function

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
+
+  def self.search(searcher)
+    where("title LIKE ?", "%#{searcher}%").or(where("description LIKE ?", "%#{searcher}%"))
+  end
+
+
 end
