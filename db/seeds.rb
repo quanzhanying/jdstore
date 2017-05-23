2048e0e8222e8fc1360e679ffc2d33cdd54d9285
wang anna <wanganna@annas-MacBook-Pro.local>
2017-05-23 16:25:59 +0800

    move sqlite3 to dev group & add pg to production group

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # Initialize Admin User
-if User.find_by(email: "admin@test.com").nil?
-  u = User.new
-  u.email = "admin@test.com"           # 可以改成自己的 email
-  u.password = "123456"                # 最少要六码
-  u.password_confirmation = "123456"   # 最少要六码
-  u.is_admin = true
-  u.save
-  puts "Admin 已经建立好了，账号为＃{admin@test.com}, 密码为{123456}"
-else
-  puts "Admin 已经建立过了，脚本跳过该步骤。"
-end
+
 # Initialize product
 
 Product.create!(title: "Cherry Keyboard",
