c0c4822e94aacdff75f4bb8f38055058bb0c66bf
SegueX <free1989xj@hotmail.com>
2017-05-23 12:25:31 +0800

    bug fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {:host => https://sheltered-stream-52829.herokuapp.com/}
+  config.action_mailer.default_url_options = {:host => "https://sheltered-stream-52829.herokuapp.com/"}
 
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
