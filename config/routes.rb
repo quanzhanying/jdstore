f99bbd0d0e5a989e4d71e424cc1cedbba012525d
Tim <s6215s5216@gmail.com>
2017-05-24 16:44:13 +0800

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "users/registrations" }
 
   namespace :admin do
     resources :products
