93d8b4365a980d270820e8c8df1cb23edb690f30
fiona <13773907@qq.com>
2017-05-24 20:40:24 +0800

    add OrderMailer function

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,35 @@
 Rails.application.routes.draw do
     root 'products#index'
+
     devise_for :users
+
     namespace :admin do
       resources :products
     end
+
     resources :products do
       member do
         post :add_to_cart
       end
     end
-     resources :carts do
+
+    resources :carts do
        collection do
          delete :clean
          post :checkout
        end
-     end
-     resources :cart_items
+    end
+
+    resources :cart_items
+
     resources :orders do
       member do
         post :pay_with_alipay
         post :pay_with_wechat
       end
     end
+
     namespace :account do
-    resources :orders  
-   end
+      resources :orders
+    end
   end
