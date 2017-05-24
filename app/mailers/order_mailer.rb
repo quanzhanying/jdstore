4cb855c7d809bfc96c0ad3cfb5b6b7aac7524d99
emilesu <emilesu@qq.com>
2017-05-24 21:16:00 +0800

    sendcloud settings

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,7 +4,7 @@ class OrderMailer < ApplicationMailer
     @user       = order.user
     @product_lists = @order.product_lists
 
-    mail(to: @user.email , subject: "[简屋] 感谢您完成本次的下单，以下是您这次购物明细 #{order.token}")
+    mail(to: @user.email , subject: "[极简生活法] 感谢您完成本次的下单，以下是您这次购物明细 #{order.token}")
   end
 
   def apply_cancel(order)
@@ -12,7 +12,7 @@ class OrderMailer < ApplicationMailer
     @user       = order.user
     @product_lists = @order.product_lists
 
-    mail(to: "admin@123.com" , subject: "[简屋] 用户#{order.user.email}申请取消订单 #{order.token}")
+    mail(to: "admin@123.com" , subject: "[极简生活法] 用户#{order.user.email}申请取消订单 #{order.token}")
   end
 
   def notify_ship(order)
@@ -20,7 +20,7 @@ class OrderMailer < ApplicationMailer
     @user       = order.user
     @product_lists = @order.product_lists
 
-    mail(to: "admin@123.com" , subject: "[简屋] 用户#{order.user.email}，您的订单 #{order.token}已发货")
+    mail(to: "admin@123.com" , subject: "[极简生活法] 用户#{order.user.email}，您的订单 #{order.token}已发货")
   end
 
   def notify_cancel(order)
@@ -28,6 +28,6 @@ class OrderMailer < ApplicationMailer
     @user       = order.user
     @product_lists = @order.product_lists
 
-    mail(to: "admin@123.com" , subject: "[简屋] 用户#{order.user.email}，您的订单 #{order.token}已取消")
+    mail(to: "admin@123.com" , subject: "[极简生活法] 用户#{order.user.email}，您的订单 #{order.token}已取消")
   end
 end
