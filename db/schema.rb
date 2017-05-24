286a4a4ef2c21d56faa8ae8436240b0eabc49bb1
wangxing <18912540921@163.com>
2017-05-25 01:03:00 +0800

    add stars to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -87,14 +87,6 @@ ActiveRecord::Schema.define(version: 20170524093748) do
     t.integer  "price"
   end
 
-  create_table "starposts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "look"
-    t.integer  "freshness"
-    t.integer  "price"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
