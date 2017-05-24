5805cee277e3f1b46ef7d40c416b047f18c530ff
罐头 <zhangwei@guantoudeMacBook-Pro.local>
2017-05-25 05:27:37 +0800

    合并每个品牌介绍分支

--- db/schema.rb
+++ db/schema.rb
@@ -46,14 +46,14 @@ ActiveRecord::Schema.define(version: 20170522085437) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.datetime "created_at",                                null: false
-    t.datetime "updated_at",                                null: false
     t.integer  "total",            default: 0
     t.integer  "user_id"
     t.string   "billing_name"
     t.string   "billing_address"
     t.string   "shipping_name"
     t.string   "shipping_address"
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
     t.string   "token"
     t.boolean  "is_paid",          default: false
     t.string   "payment_method"
