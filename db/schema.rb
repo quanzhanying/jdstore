27d5c34ccb29c4b9375eb7b159c806c57adbda02
lyy <liangyunyu@gmail.com>
2017-05-24 22:22:45 +0800

    add position to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524135234) do
+ActiveRecord::Schema.define(version: 20170524141925) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -58,12 +58,12 @@ ActiveRecord::Schema.define(version: 20170524135234) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.string   "image"
-    t.integer  "position"
     t.string   "pricd_unit",  default: "元"
     t.string   "content"
     t.string   "category"
     t.string   "location"
     t.string   "remark"
+    t.string   "position"
   end
 
   create_table "users", force: :cascade do |t|
