6bc3d69a3cba46cb20ac09a561614bcd2440c855
Yechti <yechti@gmail.com>
2017-05-24 22:51:10 +0800

    adjust carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,6 @@
 CarrierWave.configure do |config|
-  if Rails.env.production? or Rails.env.development?
-
-
+  if Rails.env.production?
+    config.fog_provider = 'fog'
     config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],      # 你的 key
