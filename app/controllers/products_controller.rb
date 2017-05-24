6e2ffcdb51fd48a04fa17c7fa1595b8bb2e623f2
songhao <songhao@songhaodeMacBook-Pro.local>
2017-05-24 19:48:47 +0800

    implement repeat product can't add to cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,8 +12,13 @@ class ProductsController < ApplicationController
 
   def add_to_cart
     @product = Product.find(params[:id])
-    current_cart.add_product_to_cart(@product)
-    flash[:notice] = "成功将#{@product.title}加入购物车"
+    if !current_cart.products.include?(@product)
+      current_cart.add_product_to_cart(@product)
+      flash[:notice] = "成功将#{@product.title}加入购物车"
+    else
+      flash[:warning] = "你已经将此商品加入过购物车!"
+    end
     redirect_to :back
+
   end
 end
