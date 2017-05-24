dd9f5438e974453e05bd09023ed3d04ef93cd8ca
韦月炎 <weiyueyan@MBA.local>
2017-05-24 15:19:49 +0800

    chenge deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:liaoxh1981/jdstore.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 set :branch, "step5"
 # Default deploy_to directory is /var/www/my_app_name
- set :deploy_to, "/home/apps/JDstore-mogai"
+ set :deploy_to, "/home/apps/JDstore-mogai-2"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
