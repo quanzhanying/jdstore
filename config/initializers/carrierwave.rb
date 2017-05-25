a4079e2a889ad7534bba1ae2a3dca634bf6213f2
justin <justinng@Justins-MacBook-Air.local>
2017-05-26 01:28:13 +0800

    change aws4

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,14 +3,14 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog'
     config.fog_credentials = {
   provider: 'AWS',
-      aws_access_key_id:     ENV["AKIAIAXRUSY6PENWBMMA"],
+      aws_access_key_id:     ENV["AKIAJB62X4N3ZN6ZFJEA"],
 
-      aws_secret_access_key: ENV["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"],
+      aws_secret_access_key: ENV["eXUmaynFjhzRfliBlqjryanRFlQyMvAejUQ8Z0Py"],
 
       region:                ENV["ap-southeast-1"]
 }
 
-config.fog_directory  = ENV["fullstack-justin"]
+config.fog_directory  = ENV["jdstore-justin"]
 
 
   else
