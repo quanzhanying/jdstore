279533947a8419f36f5c9a12563a5c98c8887863
xiaowu <xiaohuiwu521@gmail.com>
2017-05-26 00:53:39 +0800

    add column phone order

--- db/migrate/20170524153359_add_columns_to_user.rb
+++ db/migrate/20170524153359_add_columns_to_user.rb
@@ -1,6 +1,6 @@
 class AddColumnsToUser < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :name , :string #添加姓名栏位
-    add_column :users, :address, :string #添加地址栏位 
+    add_column :users, :address, :string #添加地址栏位
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524153359) do
+ActiveRecord::Schema.define(version: 20170525161229) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20170524153359) do
     t.boolean  "is_paid",          default: false
     t.string   "payment_method"
     t.string   "aasm_state",       default: "order_placed"
+    t.integer  "phone"
     t.index ["aasm_state"], name: "index_orders_on_aasm_state"
   end
 
