ce804a2c88c0190c8a4de66768eb20a18b4567fc
justin <justinng@Justins-MacBook-Air.local>
2017-05-26 00:23:41 +0800

    change aws access

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,9 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog'
     config.fog_credentials = {
   provider: 'AWS',
-      aws_access_key_id:     ENV["AKIAJOBFSCAOYGYKVVBA"],
+      aws_access_key_id:     ENV["AKIAJP2YTSEUWKB7UASQ"],
 
-      aws_secret_access_key: ENV["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"],
+      aws_secret_access_key: ENV["oytyH2AJFS/Axl4ZTYk9x/tJk5IE2cT6M+IoDe57"],
 
       region:                ENV["ap-southeast-1"]
 }
