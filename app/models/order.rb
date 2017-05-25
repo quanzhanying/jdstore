0204f509d8682b99344506f094dd646ca60e355d
郭姝彤 <Lynsmiling@outlook.com>
2017-05-25 12:29:12 +0800

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,13 @@
 class Order < ApplicationRecord
-  belongs_to :user_id
+  before_create :generate_token
+  belongs_to :user
   validates :billing_name, presence: true
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
   has_many :product_lists
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
 end
