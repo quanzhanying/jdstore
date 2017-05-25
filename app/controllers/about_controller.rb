75695038eca4a5fd61ca405e90c35df291d233f6
wayson <wayson@waysondeMacBook-Pro.local>
2017-05-26 00:17:08 +0800

    Landing page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,8 @@
 class AboutController < ApplicationController
   def about
   end
+
+  def index
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -34,5 +34,5 @@ Rails.application.routes.draw do
     namespace :account do
       resources :orders
     end
-    get 'about/index'
+    resources :about
  end
