fd7a21d40878e4514e76c068ba79785247f814f9
刘华佼 <liuhuajiao@liuhuajiaodeMacBook-Air.local>
2017-05-25 15:48:36 +0800

    add category_id and link to table:products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517062106) do
+ActiveRecord::Schema.define(version: 20170525074736) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(version: 20170517062106) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image"
+    t.integer  "category_id"
+    t.string   "link"
   end
 
   create_table "users", force: :cascade do |t|
