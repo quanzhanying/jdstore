c26fa21a459071e66f9b1d6f8fdd1b3cad78160e
Ellen Yu <ellenyu@Ellens-MacBook-Pro.local>
2017-05-25 22:37:37 +0800

    install polyamorous

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,35 +20,35 @@ else
 end
 
 # Intialize Product
-
- Product.create!(title: "HYBRID中西融合系列 骨瓷菜盘",
-    description: "深度：2.5CM 盘口直径：26CM 盘底直径：16CM",
-    price: 298,
-    quantity: 5,
-    image: open("http://imgs-qn.iliangcang.com/ware/upload/orig/2/354/354800.jpg"),
-    category_id: 1
-    )
-
-  Product.create!(title: "gogo family-漫步云端 鸡年纪念盘",
-    description: "深度：2.5CM 盘口直径：26CM 盘底直径：16CM",
-    price: 299,
-    quantity: 5,
-    image: open("http://imgs-qn.iliangcang.com/ware/upload/orig/2/373/373935.jpg"),
-    category_id: 1
-    )
-
-  Product.create!(title: "礼盒“福”字款",
-    description: "小蓝边碗",
-    price: 699,
-    quantity: 5,
-    image: open("http://imgs-qn.iliangcang.com/ware/upload/orig/2/306/306268.jpg"),
-    category_id: 1
-    )
-
-  Product.create!(title: "迷你便携式果汁榨汁机",
-    description: "MR9200-A果汁料理机",
-    price: 328,
-    quantity: 5,
-    image: open("https://ws3.sinaimg.cn/large/006tNc79gy1ffvr0obs1nj30m80m83yt.jpg"),
-    category_id: 2
-    )
+ #
+ # Product.create!(title: "HYBRID中西融合系列 骨瓷菜盘",
+ #    description: "深度：2.5CM 盘口直径：26CM 盘底直径：16CM",
+ #    price: 298,
+ #    quantity: 5,
+ #    image: open("http://imgs-qn.iliangcang.com/ware/upload/orig/2/354/354800.jpg"),
+ #    category_id: 1
+ #    )
+ #
+ #  Product.create!(title: "gogo family-漫步云端 鸡年纪念盘",
+ #    description: "深度：2.5CM 盘口直径：26CM 盘底直径：16CM",
+ #    price: 299,
+ #    quantity: 5,
+ #    image: open("http://imgs-qn.iliangcang.com/ware/upload/orig/2/373/373935.jpg"),
+ #    category_id: 1
+ #    )
+ #
+ #  Product.create!(title: "礼盒“福”字款",
+ #    description: "小蓝边碗",
+ #    price: 699,
+ #    quantity: 5,
+ #    image: open("http://imgs-qn.iliangcang.com/ware/upload/orig/2/306/306268.jpg"),
+ #    category_id: 1
+ #    )
+ #
+ #  Product.create!(title: "迷你便携式果汁榨汁机",
+ #    description: "MR9200-A果汁料理机",
+ #    price: 328,
+ #    quantity: 5,
+ #    image: open("https://ws3.sinaimg.cn/large/006tNc79gy1ffvr0obs1nj30m80m83yt.jpg"),
+ #    category_id: 2
+ #    )
