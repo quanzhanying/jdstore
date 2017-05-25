9326ab699613406f02deea44fc0a0b28e85807b5
lyy <liangyunyu@gmail.com>
2017-05-25 16:40:58 +0800

    add search method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
 
+  before_action :validate_search_key, only: [:search]
+
   def index
     @products = Product.all.order("position ASC")
   end
@@ -21,6 +23,22 @@ class ProductsController < ApplicationController
       flash[:warning] = "你的购物车内已有此物品"
     end
     redirect_to :back
+  end
+
+  def search
+    if @query_string.present?
+      search_result = Product.ransack(@search_criteria).result(:distinct => true)
+      @products = search_result.paginate(:page => params[:page], :per_page => 8 )
+    end
+  end
+
+  def validate_search_key
+    @query_string = params[:q].gsub(/\\|\'|\/|\?/, "") if params[:q].present?
+    @search_criteria = search_criteria(@query_string)
+  end
+
+  def search_criteria(query_string)
+    { :name_or_category_or_category1_or_category2_or_keyword_or_location_cont => query_string }
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,11 @@ Rails.application.routes.draw do
     member do
       post :add_to_cart
     end
+
+    collection do
+      get :search
+    end
+    
   end
 
   resources :carts do
