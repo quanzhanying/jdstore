e2295f998ce993f6ef2ff8a75a3623db9b6d1bac
SAI <sai@SAIdeMacBook-Pro.local>
2017-05-25 13:52:13 +0800

    qiniu update01

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,7 +6,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   #storage :file
-  storage :qiniu
+   storage :qiniu
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
