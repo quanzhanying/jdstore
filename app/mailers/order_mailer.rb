ae3054ed6ae354a3170dc906678e8ee52d13d07d
郭姝彤 <Lynsmiling@outlook.com>
2017-05-25 13:30:49 +0800

    add OrderMailer function

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,9 @@
+class OrderMailer < ApplicationMailer
+  def notify_order_placed(order)
+    @order = order
+    @user = order.user
+    @product_lists = @order.product_lists
+
+    mail(to: @user.email, subject: "[JDstore] Thank you for your order, these are details #{order.token}")
+  end
+end
