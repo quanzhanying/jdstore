81b49a92c686d89f0df58456ebfe241d10cb775e
xiaaa <xiaenchanter@gmail.com>
2017-05-26 20:31:38 +0800

    welcome

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'products#index'
+  root 'welcome#index'
 
   namespace :account do
     resources :orders
