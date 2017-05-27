469cb1bec4f50a4e5f2236f1d42ca410b17b5871
emilesu <emilesu@qq.com>
2017-05-28 00:42:29 +0800

    complement course_order

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@ class WelcomeController < ApplicationController
   # --CRUD--
   def index
     @products = Product.all.order("position ASC").paginate(:page => params[:page], :per_page => 12)
+    @courses = Course.all.order("position ASC").paginate(:page => params[:page], :per_page => 3)
   end
 
 end
