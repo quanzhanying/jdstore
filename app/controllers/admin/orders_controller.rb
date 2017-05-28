c615c49ada01a8ff76b99df2016d2aaa3e14dd63
马小舟 <maxiaozhou.kelly@gmail.com>
2017-05-28 20:15:44 +0800

    config qiniu

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -39,10 +39,4 @@ class Admin::OrdersController < ApplicationController
     redirect_to :back
   end
 
-  def apply_to_cancel
-    @order = Order.find(params[:id])
-    OrderMailer.apply_cancel(@order).deliver!
-    flash[:notice] = "已提交申请"
-    redirect_to :back
-  end
 end
