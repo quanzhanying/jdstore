f812780c926c14e5288b850a4950569d0193cdd1
jiangxfeng <fengxjiang@163.com>
2017-05-29 07:31:44 +0800

    add product_list model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,10 @@
 class Order < ApplicationRecord
+  belongs_to :order
   belongs_to :user
 
   validates :billing_name, presence: true
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
-  
+
 end
