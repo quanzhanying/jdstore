5f5491ed6b9511b9ee50fa7c90a7cd3f78d3da38
yangxiaodan <yangxiaodanpo@gmail.com>
2017-05-28 23:41:46 +0800

    sendcloud settings

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,7 +7,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   # storage :file
 
-  storage :qiniu
+  if Rails.env.production?
+    storage :qiniu
+  else
+    storage :file
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
