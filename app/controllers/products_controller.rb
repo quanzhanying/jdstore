005f04184de36c310b30f80bf166cd4cfac3a255
韦月炎 <weiyueyan@MBA.local>
2017-05-29 04:23:45 +0800

    change login

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -91,7 +91,7 @@ class ProductsController < ApplicationController
    #render :js =>  params[:num]
    @num = params[:num].to_i
    @product = Product.find(params[:id])
-   
+
   end
 
   def upvote
