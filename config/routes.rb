7bf197c15d2f9c5103dcff2a960edbb5549a37fd
jensen liang <liangjianjiannick@126.com>
2017-05-28 13:13:22 +0800

    加了滚轮广告

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'products#index'
+  #root 'products#index'
+  root 'welcome#index'
+  
   devise_for :users
 
   namespace :account do
