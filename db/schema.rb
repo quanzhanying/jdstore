b6e584eeb6ed6209b2d9ae8d13533e16a1e085df
amy <amy87326@163.com>
2017-05-29 23:14:56 +0800

    add column is_fitting to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529071751) do
+ActiveRecord::Schema.define(version: 20170529151239) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 20170529071751) do
     t.boolean  "is_admin",               default: false
     t.string   "username"
     t.string   "gender"
+    t.boolean  "is_fitting"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
