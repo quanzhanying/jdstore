b2cf7c85b923cd0e26204b2e4c956b36742e1c34
amy <amy87326@163.com>
2017-05-29 15:39:26 +0800

    add category to product part1

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -37,7 +37,7 @@ class Admin::ProductsController < ApplicationController
    private
 
    def product_params
-      params.require(:product).permit(:title, :description, :quantity, :price, :image)
+      params.require(:product).permit(:title, :description, :quantity, :price, :image, :category_id)
    end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529023353) do
+ActiveRecord::Schema.define(version: 20170529071751) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20170529023353) do
   end
 
   create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -66,6 +72,8 @@ ActiveRecord::Schema.define(version: 20170529023353) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image"
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
