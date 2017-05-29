8d1189875fd8e6846138475571b515727c38e3c0
刘华佼 <liuhuajiao@172-13-3-27.lightspeed.mssnks.sbcglobal.net>
2017-05-29 15:49:24 +0800

    product#index beautify

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,11 @@
 module ProductsHelper
+  def trancate(string, length = 20)
+	  string.size > length+5 ? [string[0,length],string[-5,5]].join("...") : string
+	end
+
+	def last_item(index)
+		if index > 0 && (index + 1) % 5 == 0
+		  "productList-lastItem"
+		end
+	end  
 end
