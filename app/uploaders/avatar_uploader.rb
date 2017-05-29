79d2d1bc37a96a07dafc0b70609d91e1295963da
罐头 <zhangwei@guantoudeMacBook-Pro.local>
2017-05-29 21:43:46 +0800

    修改图片失效的问题

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -6,7 +6,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.production?
+    storage :qiniu
+  else
+    storage :file
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,7 @@ Product.create!(
   description: "尺寸：宽57 x深47 x高73 /高高44.5厘米",
   price: 2020,
   stock: 5,
-  image: open("https://ws4.sinaimg.cn/large/006tKfTcgy1ffug7u3qq1j30b40gomxq.jpg")
+  image: open("https://ws4.sinaimg.cn/large/006tKfTcgy1ffug7u3qq1j30b40gomxq.jpg"),
   category_id: 1,
   can_sell: true
   )
@@ -38,7 +38,7 @@ Product.create!(
   description: "尺寸：直径45 x高35.5厘米",
   price: 1795,
   stock: 5,
-  image: open("https://ws1.sinaimg.cn/large/006tKfTcgy1ffug88zz8qj30m80eu3z3.jpg")
+  image: open("https://ws1.sinaimg.cn/large/006tKfTcgy1ffug88zz8qj30m80eu3z3.jpg"),
   category_id: 2,
   can_sell: true
   )
@@ -49,7 +49,7 @@ Product.create!(
   description: "高型：直径34×高76厘米",
   price: 2020,
   stock: 5,
-  image: open("https://ws4.sinaimg.cn/large/006tKfTcgy1ffug8g1srej30m80et0tl.jpg")
+  image: open("https://ws4.sinaimg.cn/large/006tKfTcgy1ffug8g1srej30m80et0tl.jpg"),
   category_id: 3,
   can_sell: true
   )
@@ -60,7 +60,7 @@ Product.create!(
   description: "尺寸：宽173 x深90 x高82 /座高40厘米",
   price: 16895,
   stock: 5,
-  image: open("https://ws2.sinaimg.cn/large/006tKfTcgy1ffug8o7gltj30m80eujsv.jpg")
+  image: open("https://ws2.sinaimg.cn/large/006tKfTcgy1ffug8o7gltj30m80eujsv.jpg"),
   category_id: 4,
   can_sell: true
   )
@@ -71,7 +71,7 @@ Product.create!(
   description: "尺寸：宽72 x深63 x高82 /座高36厘米",
   price: 13450,
   stock: 5,
-  image: open("https://ws1.sinaimg.cn/large/006tKfTcgy1ffug8zwccdj30ci0gogm0.jpg")
+  image: open("https://ws1.sinaimg.cn/large/006tKfTcgy1ffug8zwccdj30ci0gogm0.jpg"),
   category_id: 1,
   can_sell: true
   )
@@ -83,7 +83,7 @@ Product.create!(
   大桌：40 x 48 x高39厘米",
   price: 4450,
   stock: 5,
-  image: open("https://ws2.sinaimg.cn/large/006tKfTcgy1ffug97f5kvj30bo0goq34.jpg")
+  image: open("https://ws2.sinaimg.cn/large/006tKfTcgy1ffug97f5kvj30bo0goq34.jpg"),
   category_id: 2,
   can_sell: true
   )
@@ -94,7 +94,7 @@ Product.create!(
   description: "尺寸：34 x 39 x高74厘米/坐高72厘米",
   price: 4125,
   stock: 5,
-  image: open("https://ws3.sinaimg.cn/large/006tKfTcgy1ffug9g55qbj30bj0go0sy.jpg")
+  image: open("https://ws3.sinaimg.cn/large/006tKfTcgy1ffug9g55qbj30bj0go0sy.jpg"),
   category_id: 3,
   can_sell: true
   )
@@ -105,7 +105,7 @@ Product.create!(
   description: "尺寸：宽43 x深54 x高77 /坐高46厘米",
   price: 4250,
   stock: 5,
-  image: open("https://ws3.sinaimg.cn/large/006tKfTcgy1ffug9q8z40j30ci0btglv.jpg")
+  image: open("https://ws3.sinaimg.cn/large/006tKfTcgy1ffug9q8z40j30ci0btglv.jpg"),
   category_id: 1,
   can_sell: true
   )
@@ -122,7 +122,7 @@ Product.create!(
 
   stock: 5,
 
-  image: open("https://ws4.sinaimg.cn/large/006tKfTcgy1ffugbfsausj30m80euaaw.jpg")
+  image: open("https://ws4.sinaimg.cn/large/006tKfTcgy1ffugbfsausj30m80euaaw.jpg"),
 
   category_id: 2,
   can_sell: true
@@ -140,7 +140,7 @@ Product.create!(
 
   stock: 5,
 
-  image: open("https://ws4.sinaimg.cn/large/006tKfTcgy1ffugbrqtq2j30m80goab4.jpg")
+  image: open("https://ws4.sinaimg.cn/large/006tKfTcgy1ffugbrqtq2j30m80goab4.jpg"),
 
   category_id: 1,
   can_sell: true
@@ -158,7 +158,7 @@ Product.create!(
 
   stock: 5,
 
-  image: open("https://ws2.sinaimg.cn/large/006tKfTcgy1ffugc1uw71j30fx0goab3.jpg")
+  image: open("https://ws2.sinaimg.cn/large/006tKfTcgy1ffugc1uw71j30fx0goab3.jpg"),
 
   category_id: 3,
   can_sell: true
@@ -178,7 +178,7 @@ Product.create!(
 
   stock: 5,
 
-  image: open("https://ws1.sinaimg.cn/large/006tKfTcgy1ffugceazrsj30f20cq75a.jpg")
+  image: open("https://ws1.sinaimg.cn/large/006tKfTcgy1ffugceazrsj30f20cq75a.jpg"),
 
   category_id: 4,
   can_sell: true
@@ -196,7 +196,7 @@ Product.create!(
 
   stock: 5,
 
-  image: open("https://ws2.sinaimg.cn/large/006tKfTcgy1ffugcmit59j30m80ettbf.jpg")
+  image: open("https://ws2.sinaimg.cn/large/006tKfTcgy1ffugcmit59j30m80ettbf.jpg"),
 
   category_id: 1,
   can_sell: true
@@ -214,7 +214,7 @@ Product.create!(
 
   stock: 5,
 
-  image: open("https://ws2.sinaimg.cn/large/006tKfTcgy1ffugcxl8vdj30m80etgoa.jpg")
+  image: open("https://ws2.sinaimg.cn/large/006tKfTcgy1ffugcxl8vdj30m80etgoa.jpg"),
 
   category_id: 3,
   can_sell: true
@@ -232,7 +232,7 @@ Product.create!(
 
   stock: 5,
 
-  image: open("https://ws4.sinaimg.cn/large/006tKfTcgy1ffugd6h2nrj30m80etjv3.jpg")
+  image: open("https://ws4.sinaimg.cn/large/006tKfTcgy1ffugd6h2nrj30m80etjv3.jpg"),
 
   category_id: 4,
   can_sell: true
@@ -250,7 +250,7 @@ Product.create!(
 
   stock: 5,
 
-  image: open("https://ws1.sinaimg.cn/large/006tKfTcgy1ffugdfvofvj30b80c7407.jpg")
+  image: open("https://ws1.sinaimg.cn/large/006tKfTcgy1ffugdfvofvj30b80c7407.jpg"),
 
   category_id: 1,
   can_sell: true
@@ -268,7 +268,7 @@ Product.create!(
 
   stock: 5,
 
-  image: open("https://ws2.sinaimg.cn/large/006tKfTcgy1ffugdq0w1aj30ab0avjsp.jpg")
+  image: open("https://ws2.sinaimg.cn/large/006tKfTcgy1ffugdq0w1aj30ab0avjsp.jpg"),
 
   category_id: 3,
   can_sell: true
@@ -286,7 +286,7 @@ Product.create!(
 
   stock: 5,
 
-  image: open("https://ws1.sinaimg.cn/large/006tKfTcgy1ffuge0fdx2j30ci0an3za.jpg")
+  image: open("https://ws1.sinaimg.cn/large/006tKfTcgy1ffuge0fdx2j30ci0an3za.jpg"),
 
   category_id: 4,
   can_sell: true
