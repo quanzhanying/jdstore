d2bd8e913f36688286c381e67bca98ff977b7580
amy <amy87326@163.com>
2017-05-29 21:42:03 +0800

    devise gender setup-succeed

--- app/controllers/account/users_controller.rb
+++ app/controllers/account/users_controller.rb
@@ -1,25 +1,28 @@
 class Account::UsersController < ApplicationController
-   before_action :authenticate_user!
+  before_action :authenticate_user!
 
-   def edit
-      @user = current_user
-   end
+  def index
+   @users = User.all
+  end
 
-   def update
-      @user = current_user
-      if @user.update(user_params)
-         flash[:notice] = "信息修改成功"
-         redirect_to account_users_path
-      else
-         render :edit
-      end
-   end
+  def edit
+    @user = current_user
+  end
 
-   private
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      flash[:notice] = "信息更新成功"
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
 
-   def user_params
-      params.require(:user).permit(:username, :email)
-   end
+  private
 
+  def user_params
+    params.require(:user).permit(:username, :email)
+  end
 
 end
