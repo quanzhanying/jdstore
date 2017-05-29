221434e22b1179088186be6cd295e2172919056e
lihantao <htl00001111@163.com>
2017-05-29 21:54:28 +0800

    cannot change quantity which exceed stock

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -17,7 +17,7 @@ class CartItemsController < ApplicationController
 
     if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
       @cart_item.update(cart_item_params)
-      falsh[:notice] = "成功变更数量"
+      flash[:notice] = "成功变更数量"
     else
       flash[:warning] = "数量不足以加入购物车"
     end
