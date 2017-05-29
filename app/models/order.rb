32b0e0f4ea534b86a8839d09bc1f0cd736443cee
Archer Zhao <changlechangsheng@gmail.com>
2017-05-29 11:13:47 +0800

    Add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,10 @@ class Order < ApplicationRecord
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
 
+  before_create :generate_token
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
+
 end
