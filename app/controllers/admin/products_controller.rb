b78dcdda4eace49b12aff120294afb6f72f79b1e
alice_guo2016 <alice_guo2016@MacdeiMac-2.local>
2017-05-30 22:54:45 +0800

    product_delete_heroku

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -36,6 +36,12 @@ class Admin::ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to admin_products_path
+  end
+
   private
 
   def product_params
