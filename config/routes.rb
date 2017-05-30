9e70d7998c15a064748870f67ebf4eae1a049cc0
amy <amy87326@163.com>
2017-05-30 22:33:15 +0800

    cannot fix element-routes-bug

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,6 @@ Rails.application.routes.draw do
 
    resources :cart_items
    resources :category
-   resources :elements
    resources :orders do
       member do
          post :pay_with_alipay
