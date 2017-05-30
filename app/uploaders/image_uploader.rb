52953d828a163d56a88bb615a545c8deda6e0933
alice_guo2016 <alice_guo2016@MacdeiMac-2.local>
2017-05-31 03:34:35 +0800

    fix_upload_image_to_heroku_thirdly

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,10 +9,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # storage :fog
   if Rails.env.production? #远端
       storage :qiniu
-    elsif Rails.env.development? #本地
+    else Rails.env.development? #本地
       storage :file
     end
-    
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
