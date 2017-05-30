b3287cb5babc20ae1a6b41b9a423c8c0e18e1f0f
lucky <fengzongzhixin@gmail.com>
2017-05-30 10:36:34 +0800

    update product model with image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,9 +5,14 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  #storage :file
   # storage :fog
-
+  if Rails.env.production? #远端
+      storage :qiniu
+    elsif Rails.env.development? #本地
+      storage :file
+    end
+    
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
