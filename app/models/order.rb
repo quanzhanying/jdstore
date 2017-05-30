b9cc0944267a98fdacac174fd06addbbabab7617
lihantao <htl00001111@163.com>
2017-05-30 09:54:36 +0800

    add order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ApplicationRecord
+  belongs_to :user
+
+  validates :billing_name, presence: true
+  validates :billing_address, presence: true
+  validates :shiping_name, presence: true
+  validates :shiping_address, presence: true
 end
