5e2905c56ba0108560fca96e5c580a6df57c37c9
Calvin <xcalvin.zhao@gmail.com>
2017-05-30 17:41:53 +0800

    storage file & qiniu

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,7 +6,14 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :qiniu
+  # storage :qiniu
+
+  if Rails.env.development?
+    storage :file
+  else  
+    storage :qiniu
+  end
+
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
