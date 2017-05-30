768ffd5225958b9637b9090168e3965f71f55cc4
zhangxingzhi <zhangxingzhizailushang@gmail.com>
2017-05-30 20:41:25 +0800

    empty cart

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,9 @@ end
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :carts
+resources :carts do
+  collection do
+    delete :clean
+  end
+end
 end
