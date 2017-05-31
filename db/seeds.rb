5d80e3bbe0535f8049dd3e351296c414644c3b37
donker008 <richdonker@qq.com>
2017-05-31 22:28:49 +0800

    update footer css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@ end
 
 for i in 1..10 do
   Product.create("title"=>"飞傲（FiiO）i1 苹果闪电接口耳放", "description"=>"飞傲（FiiO）i1 苹果闪电接口耳放", "price"=>100, "quantity"=>20, "image"=>"http://oqex30cqv.bkt.clouddn.com/uploads/product/image/4/5.png", "category"=>"寝居玩具")
-end 
+end
+
+puts "10 products created!"
 
 ProductCategory.create("name"=>"婴幼儿服饰")
 ProductCategory.create("name"=>"儿童服饰")
@@ -14,3 +16,5 @@ ProductCategory.create("name"=>"妈咪")
 ProductCategory.create("name"=>"寝居玩具")
 ProductCategory.create("name"=>"婴童")
 ProductCategory.create("name"=>"喂养")
+
+puts "product catetoy created!"
