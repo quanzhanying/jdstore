af76d752af2606714b4be71b2f759cb6879a72c3
zhangxingzhi <zhangxingzhizailushang@gmail.com>
2017-05-31 21:25:50 +0800

    add OrderMailer function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,6 +15,10 @@ class OrdersController < ApplicationController
         product_list.quantity = cart_item.quantity
         product_list.save
       end
+      current_cart.clean!
+      OrderMailer.notify_order_placed(@order).deliver!
+
+
         redirect_to order_path(@order.token)
       else
         render 'carts/checkout'
@@ -24,7 +28,7 @@ class OrdersController < ApplicationController
     @order = Order.find_by_token(params[:id])
     @product_lists = @order.product_lists
     end
-    
+
     def pay_with_alipay
       @order = Order.find_by_token(params[:id])
       @order.set_payment_with!("alipay")
