813cead5ca640cda24d09428c8809957b95da331
jiaowagn <wangjiaohere@gmail.com>
2017-06-01 01:16:29 +0800

    adjust category form  of admin product new & edit

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -61,7 +61,7 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :price, :quantity, :image)
+    params.require(:product).permit(:title, :description, :price, :quantity, :image, :category_id)
   end
 
 end
