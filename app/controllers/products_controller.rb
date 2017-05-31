58ec5917a3c1a7c1b8fea409a67b8a8253cdbcf5
刘华佼 <liuhuajiao@liuhuajiaodeMacBook-Air.local>
2017-05-31 16:06:46 +0800

    fixed product#show beautify

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
 
+  respond_to :js
 
   def index
     @products = Product.all
@@ -13,30 +14,40 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
    if !current_cart.products.include?(@product)
      current_cart.add_product_to_cart(@product)
-     flash[:notice] = "你已成功将 #{@product.title} 加入购物车"
+     @product.quantity -= @product.buying_quantity
+     @product.buying_quantity = 1
+     @product.save
+     flash[:notice] = "你已成功将 《#{@product.title}》 加入购物车"
    else
-     flash[:warning] = "你的购物车内已有此物品"
+     flash[:warning] = "你的购物车内已有 《#{@product.title}》"
    end
     redirect_to :back
+    # respond_to do |format|
+    #   format.js   { render :layout => false }
+    # end
   end
 
   def add_buying_quantity
+    @product = Product.find(params[:id])
     if @product.buying_quantity <= @product.quantity
-      binding.pry
       @product.buying_quantity +=1
       @product.save
-      redirect_to :back
-
+      # redirect_to :back
+      respond_to do |format|
+        format.js   { render :layout => false }
+      end
     end
   end
 
   def remove_buying_quantity
+    @product = Product.find(params[:id])
     if @product.buying_quantity > 1
       @product.buying_quantity -= 1
       @product.save
-      redirect_to :back
+      # redirect_to :back
+      respond_to do |format|
+        format.js   { render :layout => false }
+      end
     end
-  end
-
-
 end
+  end
