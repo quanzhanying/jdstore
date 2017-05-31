d4d9adfcadc11e473d4a9615c283a4626df5984c
donker008 <richdonker@qq.com>
2017-05-31 19:26:32 +0800

    export product, category to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
 
-User.create("email"=>"admin@test.com", "is_admin"=>true, "password"=>"111111", "password_confirmation"=>"111111")
+if User.where(:email => "admin@test.com").all.count == 0
+  User.create("email"=>"admin@test.com", "is_admin"=>true, "password"=>"111111", "password_confirmation"=>"111111")
+end
