3a8710f1ed1571c8ea41d838a5ef4c54f6e04727
jiaowagn <wangjiaohere@gmail.com>
2017-06-02 00:37:28 +0800

    add author to product and add some yml words

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530164815) do
+ActiveRecord::Schema.define(version: 20170601161241) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20170530164815) do
     t.string   "image"
     t.integer  "position"
     t.integer  "category_id"
+    t.string   "author"
   end
 
   create_table "users", force: :cascade do |t|
