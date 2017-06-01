f16bf65f85c19f8a335e3e71c4d3fc16e5982bcf
张家琛 <zjc834723074@gmail.com>
2017-06-02 03:46:36 +0800

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+create_account = User.create([email: 'zjc834723074@gmail.com', password:'123456', password_confirmation: '123456', is_admin: 'true'])
+ puts "Admin account created."
