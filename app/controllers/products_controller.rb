5dc48c67a0169512437dc6059724f4f4eda6d285
Mason <q791862937@126.com>
2017-06-01 15:50:00 +0800

    implement search title and description

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,4 +18,14 @@ class ProductsController < ApplicationController
     end
     redirect_to :back
   end
+
+  def index
+    @products = Product.all
+    if params[:search]
+      @products = Product.search(params[:search]).order("created_at DESC")
+    else
+      @products = Product.all.order("created_at DESC")
+    end
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
+
+  def self.search(search)
+    where("title LIKE ? OR description LIKE ?", "%#{search}%", "%#{search}%")
+  end
 end
