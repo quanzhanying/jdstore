aa956b615640ed75880aa2afab988587d304b51a
tangjia <scarletttang89@163.com>
2017-06-01 19:34:00 +0800

    add assm_state to order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,6 +16,8 @@ class OrdersController < ApplicationController
         product_list.quantity = cart_item.quantity
         product_list.save
       end
+      current_cart.clean!
+      OrderMailer.notify_order_placed(@order).deliver!
 
       redirect_to order_path(@order.token)
     else
@@ -31,7 +33,7 @@ class OrdersController < ApplicationController
   def pay_with_alipay
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("alipay")
-    @order.pay!
+    @order.make_payment!
 
     redirect_to order_path(@order.token), notice: "使用支付宝成功完成付款"
   end
@@ -39,7 +41,7 @@ class OrdersController < ApplicationController
   def pay_with_wechat
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("wechat")
-    @order.pay!
+    @order.make_payment!
 
     redirect_to order_path(@order.token), notice: "使用微信成功完成付款"
   end
