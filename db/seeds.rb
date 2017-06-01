5311674ceb9810eca94dde6f300f9e0a3ea6922f
donker008 <richdonker@qq.com>
2017-06-01 20:25:30 +0800

    add seeds for new products with image

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,26 @@ if User.where(:email => "admin@test.com").all.count == 0
   User.create("email"=>"admin@test.com", "is_admin"=>true, "password"=>"111111", "password_confirmation"=>"111111")
 end
 
-for i in 1..10 do
-  Product.create("title"=>"飞傲（FiiO）i1 苹果闪电接口耳放", "description"=>"飞傲（FiiO）i1 苹果闪电接口耳放", "price"=>100, "quantity"=>20, "image"=>"http://oqex30cqv.bkt.clouddn.com/uploads/product/image/4/5.png", "category"=>"寝居玩具")
-end
+products_info = [
+ {title: 'a', catetoy: "婴幼儿服饰", description: '4',image: 'https://ws3.sinaimg.cn/large/006tNc79gy1fg5ylhho85j30by0byq3m.jpg'},
+ {title: 'b', catetoy: "儿童服饰", description: '3',image: 'https://ws3.sinaimg.cn/large/006tNc79gy1fg5ylhho85j30by0byq3m.jpg'},
+ {title: 'c', catetoy: "配搭", description: '2',image: 'https://ws3.sinaimg.cn/large/006tNc79gy1fg5ylhho85j30by0byq3m.jpg'},
+ {title: 'd', catetoy: "妈咪", description: '1',image: 'https://ws3.sinaimg.cn/large/006tNc79gy1fg5ylhho85j30by0byq3m.jpg'},
+ {title: 'e', catetoy: "寝居玩具", description: '1',image: 'https://ws3.sinaimg.cn/large/006tNc79gy1fg5ylhho85j30by0byq3m.jpg'},
+ {title: 'f', catetoy: "婴童", description: '1',image: 'https://ws3.sinaimg.cn/large/006tNc79gy1fg5ylhho85j30by0byq3m.jpg'},
+ {title: 'g', catetoy: "喂养", description: '1',image: 'https://ws3.sinaimg.cn/large/006tNc79gy1fg5ylhho85j30by0byq3m.jpg'},
+]
 
-puts "10 products created!"
+products_info.each do |info|
+	 Product.create!(
+		 title: info[:title],
+     category: info[:catetoy],
+		 description: info[:description],
+		 quantity: rand(5..6),
+		 price: rand(5..6),
+		 image: open(info[:image])
+		 )
+ end
 
 ProductCategory.create("name"=>"婴幼儿服饰")
 ProductCategory.create("name"=>"儿童服饰")
