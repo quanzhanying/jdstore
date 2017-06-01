80ddb8d125d54fb4c82decc7c2868e74f623572d
alice_guo2016 <alice_guo2016@MacdeiMac-2.local>
2017-06-02 00:58:23 +0800

    change key and heroku_app

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.storage             = :qiniu
-  config.qiniu_access_key    = ENV["04fWYM1u7ZsFZCMMa34XOzGbmmUvUa1iRxxHhLtD"]
-  config.qiniu_secret_key    = ENV["72ULQ3TJr6Mfs3S_Q9ZMvMSfPECCfS_-P77D4edw"]
-  config.qiniu_bucket        = ENV["aliceguo2017-1"]
-  config.qiniu_bucket_domain = ENV["onbqi9gro.bkt.clouddn.com"]
+  config.qiniu_access_key    = ENV["-kWYVXiAAbnt5y9JFYHfmnmSwLSMAS3CyHbBpA7X"]
+  config.qiniu_secret_key    = ENV["QMUwH7J2OmkmjVlxg6FGB35H836mzYmSGkFOOG6a"]
+  config.qiniu_bucket        = ENV["jdstore666"]
+  config.qiniu_bucket_domain = ENV["oqvoq8jf8.bkt.clouddn.com"]
   config.qiniu_block_size    = 4*1024*1024
   config.qiniu_protocol      = "http"
   config.qiniu_up_host       = "http://up-z2.qiniu.com"  #华南
