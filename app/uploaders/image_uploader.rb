825ff51286090ecc8bf68df5dc723462b43067a2
周文耿 <412582902@qq.com>
2017-06-01 12:07:49 +0800

    uploader changed

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,12 +7,12 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   #storage :file
-  storage :qiniu
-  # if Rails.env.production? #远端
-  #   storage :qiniu
-  # elsif Rails.env.development? #本地
-  #   storage :file
-  # end
+  # storage :qiniu
+  if Rails.env.production? #远端
+    storage :qiniu
+  elsif Rails.env.development? #本地
+    storage :file
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
