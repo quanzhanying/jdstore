24cdee15aabf502a4610a227b2947cb39bb08ca5
huangpuxiang <hpx0809@outlook.com>
2017-06-02 21:31:36 +0800

    review

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,7 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :likes
   has_many :liked_users, through: :likes, source: :user
+  has_many :reviews
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170601170724) do
+ActiveRecord::Schema.define(version: 20170602050952) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -75,6 +75,14 @@ ActiveRecord::Schema.define(version: 20170601170724) do
     t.integer  "category_id"
     t.integer  "sum"
     t.string   "author"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "user_id"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
