727db3993b913151ef5e2caa373e793bbc43093b
xinshengdaxue <shenquanzhen18@126.com>
2017-06-02 13:52:25 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+
+
+    devise_for :users
+    root 'products#index'
+    
    namespace :admin do
    resources :products
  end
+  resources :products
 
-  devise_for :users
-  root 'welcome#index'
 end
