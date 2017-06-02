882a95faa9ccf8c68c90589694d085e14718bc80
zhongdaoyou <18942371859@163.com>
2017-06-02 09:25:01 +0800

    implement image upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  root 'products#index'
+
   namespace :admin do
     resources :products
   end
-  
-  devise_for :users
-  root 'welcome#index'
+
+  resources :products
 
 end
