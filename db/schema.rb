ad971b4eca880aa5519d21ad04bbaacb526d0242
wang anna <wanganna@annas-MacBook-Pro.local>
2017-06-03 21:56:43 +0800

    Jdstore-v1

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20170603064112) do
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
     t.integer  "product_id"
-    t.integer  "quantity",   default: 0
+    t.integer  "quantity",   default: 1
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
@@ -55,13 +55,6 @@ ActiveRecord::Schema.define(version: 20170603064112) do
     t.string   "payment_method"
     t.string   "aasm_state",       default: "order_placed"
     t.index ["aasm_state"], name: "index_orders_on_aasm_state"
-  end
-
-  create_table "photos", force: :cascade do |t|
-    t.integer  "product_id"
-    t.string   "avatar"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "product_lists", force: :cascade do |t|
