cf07bac3508f78b7f1f93f96c7478b1d22cc68e8
alice_guo2016 <alice_guo2016@MacdeiMac-2.local>
2017-06-03 21:07:38 +0800

    login

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
 //= require bootstrap/alert
 //= require bootstrap/dropdown
 //= require bootstrap/modal
-//= require bootstrap
+
 //= require_tree .
