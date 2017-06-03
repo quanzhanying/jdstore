394d94c1309ad892d7b572a52ecc9f7a446cd495
shuibo <sbjyemail@gmail.com>
2017-06-03 22:14:39 +0800

    add apply_cancel function

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
         post :shipped
         post :return
       end
-    end 
+    end
   end
 
   resources :products do
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
     member do
       post :pay_with_alipay
       post :pay_with_wechat
+      post :apply_to_cancel
     end
   end
 
