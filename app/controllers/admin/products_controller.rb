8edd6aef9fff0eb388dc400fde3eb961d2c0cfa0
zhuozhi <zhuozhi12@gmail.com>
2017-06-03 23:29:01 +0800

    save

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -13,6 +13,7 @@ class Admin::ProductsController < ApplicationController
  @product = Product.new
  @categories = Category.all.map { |c| [c.name, c.id] }
 end
+
 def create
  @product = Product.new(product_params)
  @product.category_id = params[:category_id]
@@ -22,10 +23,12 @@ def create
    render :new
  end
 end
+
 def edit
  @product = Product.find(params[:id])
  @categories = Category.all.map { |c| [c.name, c.id] } #这一行为加入的代码
 end
+
 def update
  @product = Product.find(params[:id])
  @product.category_id = params[:category_id]
