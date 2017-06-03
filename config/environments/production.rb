d501c47fccdc0903e5ec8d6a135db8ee95761d04
Mason <q791862937@126.com>
2017-06-04 00:27:37 +0800

    Implement login/signin popup,adjust the popup position

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'https://salty-headland-33423.herokuapp.com'}
+  config.action_mailer.default_url_options = { :host => 'https://zhendu.herokuapp.com'}
 
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
