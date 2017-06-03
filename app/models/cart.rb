7c3410b75011890296b658b8752ff275436dcd26
haozan <haozan@qinglion.com>
2017-06-03 11:22:50 +0800

    empty cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -10,11 +10,15 @@ class Cart < ApplicationRecord
     ci.save
   end
 
+   def clean!
+     cart_items.destroy_all
+   end
+
     def total_price
       sum = 0
       cart_items.each do |cart_item|
         if cart_item.product.price.present?
-          sum += cart_item.quantity * cart_item.product.price
+          sum = cart_item.quantity * cart_item.product.price
         end
       end
       sum
