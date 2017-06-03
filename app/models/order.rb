0ed803ff8a861586331c0b1fccdb83f667a25ac2
zhongdaoyou <18942371859@163.com>
2017-06-03 10:31:35 +0800

    add aasm to project

--- app/models/order.rb
+++ app/models/order.rb
@@ -18,9 +18,40 @@ class Order < ApplicationRecord
   def set_payment_with!(method)
     self.update_columns(payment_method: method)
   end
-  
+
   def pay!
     self.update_columns(is_paid: true)
+  end
+
+  include AASM
+
+  aasm do
+    state :order_placed, initial: true
+    state :paid
+    state :shipping
+    state :shipped
+    state :order_cancelled
+    state :good_returned
+
+    event :make_payment, after_commit: :pay! do
+      transitions form: :order_placed, to: :paid
+    end
+
+    event :ship do
+      transitions form: :paid, to: :shipping
+    end
+
+    event :deliver do
+      transitions form: :shipping, to: :shipped
+    end
+
+    event :return_good do
+      transitions form: :shipped, to: :goog_returned
+    end
+
+    event :cancel_order do
+      transitions form: [:order_placed, :paid], to: :order_canceled
+    end
   end
 
 end
