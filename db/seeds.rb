9bc57d0e310049d1120e8d777ce9d375a1e8b788
eggegg <eggegg2016@163.com>
2017-06-03 15:55:56 +0800

    finish seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,7 @@ u.save
 Category.create(:name => "男性健康餐")
 Category.create(:name => "女性健康餐")
 Category.create(:name => "随心健康餐")
+
+Product.create!(title:"男性健身餐",description:"专为男性设计的健身午餐",quantity:100,price:150,image: open("http://oqmcovki6.bkt.clouddn.com/uploads/product/image/1/medium_man-gym-muscle-fitness-38908.jpeg"))
+Product.create!(title:"女性健身餐",description:"专为女性设计的健身午餐",quantity:100,price:150,image: open("http://oqmcovki6.bkt.clouddn.com/uploads/product/image/2/medium_pexels-photo-196640.jpeg"))
+Product.create!(title:"随心餐",description:"随意吃，开心就好",quantity:100,price:150,image: open("https://ws4.sinaimg.cn/large/006tKfTcgy1fg7z491709j30hs0budhf.jpg"))
