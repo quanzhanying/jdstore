0ac9ea8237a3fff19480941e5cb9608c48a6670e
Tim <s6215s5216@gmail.com>
2017-06-04 11:59:43 +0800

    edit checkout bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "jdstore_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { :host => 'https://calm-waters-26689.herokuapp.com/'}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
