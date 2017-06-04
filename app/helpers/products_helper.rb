90192a577ecf2c7a56ad6b2c8df95ee02ac7a0ae
ankon <ankon@ankon-mac.local>
2017-06-04 12:58:14 +0800

    highlight search result

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,6 @@
 module ProductsHelper
+  def render_highlight_content(product,query_string)
+    excerpt_cont = excerpt(product.title, query_string, radius: 500)
+    highlight(excerpt_cont, query_string)
+  end
 end
