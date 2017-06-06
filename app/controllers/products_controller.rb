8330f79f59e06bbe3310a1c67663144ef84bb592
jensen liang <liangjianjiannick@126.com>
2017-06-06 22:27:58 +0800

    newintro

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,10 @@ class ProductsController < ApplicationController
   def index
     if params[:category].blank?
     @products = Product.all
-  else
+    else
     @category_id = Category.find_by(name: params[:category]).id
     @products = Product.where(:category_id => @category_id)
-  end
+    end
   end
 
   def show
