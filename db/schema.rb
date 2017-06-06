2a7638e9215cf0b2af2734bb22a0e02e473b0a2f
alice_guo2016 <alice_guo2016@MacdeiMac-2.local>
2017-06-06 17:04:54 +0800

    favorites function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170603121115) do
+ActiveRecord::Schema.define(version: 20170606082121) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -27,6 +27,21 @@ ActiveRecord::Schema.define(version: 20170603121115) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "details", force: :cascade do |t|
+    t.integer  "product_id"
+    t.string   "avatar"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "graph"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer  "group_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -50,6 +65,12 @@ ActiveRecord::Schema.define(version: 20170603121115) do
   create_table "photos", force: :cascade do |t|
     t.integer  "product_id"
     t.string   "avatar"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "detail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
