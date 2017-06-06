ae56df2ea3e74c0d786c97b4a8851b3ae4346751
eggegg <eggegg2016@163.com>
2017-06-07 00:51:31 +0800

    category finish

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,13 @@
 class ProductsController < ApplicationController
    def index
-      @products = Product.all
+     if params[:category].blank?
+       @products = Product.all
+     else
+       @category_id = Category.find_by(name: params[:category]).id
+       @products = Product.where(:category_id => @category_id)
+     end
    end
+  
 
    def show
       @product = Product.find(params[:id])
