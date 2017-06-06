74b2b39896686969a8fa10b48f156d33d19f9172
xinshengdaxue <shenquanzhen18@126.com>
2017-06-06 21:23:27 +0800

    rake db:migrate

--- db/migrate/20170605133529_create_carts.rb
+++ db/migrate/20170605133529_create_carts.rb
@@ -1,9 +1,7 @@
 class CreateCarts < ActiveRecord::Migration[5.0]
   def change
     create_table :carts do |t|
-        has_many :cart_items
-        has_many :products, through: :cart_items, source: :product
-
+        
       t.timestamps
     end
   end
