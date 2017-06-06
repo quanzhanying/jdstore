54531d5669465ede7ec9188fff346243891ab1e8
Bitwildfox <z252045847@163.com>
2017-06-06 10:13:39 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
+
+  root 'products#index'
+  devise_for :users
+  namespace :admin do
+    resources :products
+  end
+
+  resources :products
+
+  
   namespace :admin do
    resources :products
   end
 
 
   devise_for :users
-  root 'welcome#index'
+
 end
