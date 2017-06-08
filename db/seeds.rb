96e8b93a0378cdfe9511bc9a9c67b27bade380ee
周文耿 <412582902@qq.com>
2017-06-09 00:22:30 +0800

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,106 +34,105 @@ Category.create!(
 )
 
 Product.create!(
-  id: 1,
   category_id: 1,
   title: "澳洲羊排素材沙拉",
   description: "每天限量供应",
   price: 56,
   quantity: 100,
-  image: MiniMagick::Image.open("#{Rails.root}/app/public/images/野兽yangpai.jpg")
+  image: MiniMagick::Image.open("#{Rails.root}/public/images/yangpai2.jpg")
   )
 
 
 Product.create!(
-id: 2,
 category_id: 1,
 title: "烟熏三文鱼",
 description: "每天限量供应",
 price: 56,
 quantity: 100,
-image: MiniMagick::Image.open("#{Rails.root}/app/public/images/野兽烟熏三文鱼.jpg")
+image: MiniMagick::Image.open("#{Rails.root}/public/images/sanwenyu.jpg")
 )
 Product.create!(
-id: 3,
 category_id: 1,
 title: "新西兰黑椒牛排",
 description: "每天限量供应",
 price: 56,
 quantity: 100,
-image: MiniMagick::Image.open("#{Rails.root}/app/public/images/野兽新西兰黑椒牛排.jpg")
+image: MiniMagick::Image.open("#{Rails.root}/public/images/niupai.jpg")
 )
 
-# Product.create!(title: "野兽生活 防弹咖啡 中度烘焙 低霉菌咖啡豆227g2袋 生酮饮食 包邮",
-# description: "防弹咖啡专用，极低霉菌度咖啡豆，多重精选，帮助你随时保持最佳状态。",
-# price: 380,
-# quantity: 100,
-# category_id: 2,
-#
-# )
-#
-# Product.create!(title: "防弹咖啡必备套装 野兽生活咖啡豆227g椰子油450ml黄油2块 包邮",
-# description: "  ",
-# price: 369,
-# quantity: 100,
-# category_id: 2,
-#
-# )
-#
-# Product.create!(title: "防弹咖啡 六合一制作套装 手冲器具 生酮减脂 野兽生活 全国包邮",
-# description: "来自硅谷的超级创新饮品，野兽防弹咖啡套装，破解体重问题的终极解决方案。",
-# price: 899,
-# quantity: 100,
-# category_id: 2,
-#
-# )
-#
-# Product.create!(title: "野兽生活 防弹咖啡 中度烘焙 低霉菌咖啡豆227g2袋 生酮饮食 包邮",
-# description: "防弹咖啡专用，极低霉菌度咖啡豆，多重精选，帮助你随时保持最佳状态。",
-# price: 380,
-# quantity: 100,
-# category_id: 3,
-#
-# )
-#
-# Product.create!(title: "防弹咖啡专用 金凯利草饲无盐黄油爱尔兰200g1块 野兽生活包邮",
-# description: " ",
-# price: 36,
-# quantity: 100,
-# category_id: 4,
-#
-# )
-#
-# Product.create!(title: "防弹咖啡专用 泰国进口椰子油 gre3n天然初榨冷压 生酮 野兽生活
-# ",
-# description: "富含中链脂肪酸MCT，帮助生酮，制作防弹咖啡专用，瞬间唤醒全天能量。",
-# price: 168,
-# quantity: 100,
-# category_id: 4,
-#
-# )
-#
-# Product.create!(title: "防弹咖啡 HARIO磨豆机 手动手摇 日本原装进口 野兽生活
-# ",
-# description: "日本品牌 品质保障 陶瓷磨芯 手动磨豆机 研磨机。",
-# price: 188,
-# quantity: 100,
-# category_id: 5,
-#
-# )
-#
-# Product.create!(title: "泰摩小黑3.0法压壶 防弹咖啡冲泡家用小型350ml 野兽生活包邮
-# ",
-# description: "3.0升级版",
-# price: 69,
-# quantity: 100,
-# category_id: 5,
-#
-# )
-#
-# Product.create!(title: "野兽生活 防弹咖啡 OSTER/奥士达 BLSTPB-WGN-073 搅拌机家用自动
-# ",
-#   description: "",
-#   price: 299,
-#   quantity: 100,
-#   category_id: 5,
-# )
+Product.create!(title: "野兽生活 防弹咖啡 中度烘焙 低霉菌咖啡豆227g2袋 生酮饮食 包邮",
+description: "防弹咖啡专用，极低霉菌度咖啡豆，多重精选，帮助你随时保持最佳状态。",
+price: 380,
+quantity: 100,
+category_id: 2,
+image: MiniMagick::Image.open("#{Rails.root}/public/images/kafei1.gif")
+
+)
+
+Product.create!(title: "防弹咖啡必备套装 野兽生活咖啡豆227g椰子油450ml黄油2块 包邮",
+description: "  ",
+price: 369,
+quantity: 100,
+category_id: 2,
+image: MiniMagick::Image.open("#{Rails.root}/public/images/fangdantz1.jpg")
+
+)
+
+Product.create!(title: "防弹咖啡 六合一制作套装 手冲器具 生酮减脂 野兽生活 全国包邮",
+description: "来自硅谷的超级创新饮品，野兽防弹咖啡套装，破解体重问题的终极解决方案。",
+price: 899,
+quantity: 100,
+category_id: 2,
+image: MiniMagick::Image.open("#{Rails.root}/public/images/liuheyi.jpg")
+
+)
+
+
+Product.create!(title: "防弹咖啡专用 金凯利草饲无盐黄油爱尔兰200g1块 野兽生活包邮",
+description: " ",
+price: 36,
+quantity: 100,
+category_id: 4,
+image: MiniMagick::Image.open("#{Rails.root}/public/images/huangyou.jpg")
+
+)
+
+Product.create!(title: "防弹咖啡专用 泰国进口椰子油 gre3n天然初榨冷压 生酮 野兽生活
+",
+description: "富含中链脂肪酸MCT，帮助生酮，制作防弹咖啡专用，瞬间唤醒全天能量。",
+price: 168,
+quantity: 100,
+category_id: 4,
+image: MiniMagick::Image.open("#{Rails.root}/public/images/yeziyou.jpg")
+
+)
+
+Product.create!(title: "防弹咖啡 HARIO磨豆机 手动手摇 日本原装进口 野兽生活
+",
+description: "日本品牌 品质保障 陶瓷磨芯 手动磨豆机 研磨机。",
+price: 188,
+quantity: 100,
+category_id: 5,
+image: MiniMagick::Image.open("#{Rails.root}/public/images/yanmoji.jpg")
+
+)
+
+Product.create!(title: "泰摩小黑3.0法压壶 防弹咖啡冲泡家用小型350ml 野兽生活包邮
+",
+description: "3.0升级版",
+price: 69,
+quantity: 100,
+category_id: 5,
+image: MiniMagick::Image.open("#{Rails.root}/public/images/kafeibei.jpg")
+
+)
+
+Product.create!(title: "野兽生活 防弹咖啡 OSTER/奥士达 BLSTPB-WGN-073 搅拌机家用自动
+",
+  description: "",
+  price: 299,
+  quantity: 100,
+  category_id: 5,
+  image: MiniMagick::Image.open("#{Rails.root}/public/images/jiaobanji.jpg")
+
+)
