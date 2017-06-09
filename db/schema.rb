ca83bd0452867c2c326884ba0d8f9191bfdc9d17
ziud7rs <ziud7rs@SInG-2.local>
2017-06-09 16:20:42 +0800

    調整商品詳情區圖片寬度隨視屏變化＋修正商品評論跑版問題

--- db/schema.rb
+++ db/schema.rb
@@ -56,14 +56,14 @@ ActiveRecord::Schema.define(version: 20170607223208) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.datetime "created_at",                                null: false
-    t.datetime "updated_at",                                null: false
     t.integer  "total",            default: 0
     t.integer  "user_id"
     t.string   "billing_name"
     t.string   "billing_address"
     t.string   "shipping_name"
     t.string   "shipping_address"
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
     t.string   "token"
     t.boolean  "is_paid",          default: false
     t.string   "payment_method"
