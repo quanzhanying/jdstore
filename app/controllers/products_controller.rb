b3770e621d507a24341964fd7c060d461f5f3d28
duzilong <122517637@qq.com>
2017-06-09 15:55:35 +0800

    order checkout

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,7 +69,7 @@ class ProductsController < ApplicationController
 
  private
    def product_params
-     params.require(:product).permit(:title, :description, :price, :quantity, :image)
+     params.require(:product).permit(:title, :description, :price, :quantity, :image, :author)
    end
 
 end
