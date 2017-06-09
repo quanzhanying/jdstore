a55f3b8557ffd3cc75fc557ee84ba8b11cf9ebbb
lh850718 <lh850718@gmail.com>
2017-06-09 21:42:00 +0800

    change product quantity in shoping cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -14,16 +14,19 @@ class CartItemsController < ApplicationController
     @cart = current_cart
     @cart_item = @cart.cart_items.find_by(product_id: params[:id])
 
-
-    if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
-       @cart_item.update(cart_item_params)
-            flash[:notice] = "成功变更数量"
-   else
-     flash[:warning] = "数量不足以加入购物车"
-   end
-
-    redirect_to carts_path
+    @cart_item.quantity = params[:quantity]
+  #   if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
+  #      @cart_item.update(cart_item_params)
+  #           flash[:notice] = "成功变更数量"
+  #  else
+  #    flash[:warning] = "数量不足以加入购物车"
+  #  end
+@cart_item.save
+redirect_to :back
+    
   end
+
+
 
   private
 
