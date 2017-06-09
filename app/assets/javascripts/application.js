36314c8ee2aa9d82cf95c872cba3b60db786edbd
周文耿 <412582902@qq.com>
2017-06-09 23:50:02 +0800

    js wufu shuoming

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,3 +27,20 @@ $(function(){
       });
     });
   });
+
+
+  // Tabbed Menu
+  function openMenu(evt, menuName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("menu");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+       tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+    }
+    document.getElementById(menuName).style.display = "block";
+    evt.currentTarget.firstElementChild.className += " w3-red";
+  }
+  document.getElementById("myLink").click();
