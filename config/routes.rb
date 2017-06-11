e18c01b082f2629b14c2c957878c6966af19d090
amy <amy87326@163.com>
2017-06-11 15:15:59 +0800

    modified Gerenzhongxin

--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,8 @@ Rails.application.routes.draw do
       resources :users
    end
 
+   resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
