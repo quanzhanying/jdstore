5c8181fdc2cfd0c02b04173ed76e9eb839200b86
bixinxin81@gmail.com <bixinxin81@gmail.com>
2017-06-21 12:26:57 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-   namespace :admin do
-     resources :products
-   end
+  root 'products#index'
   devise_for :users
-  root 'welcome#index'
+  namespace :admin do
+    resources :products
+  end
+
+  resources :products
+
 end
