7479c9a5c41fcc28436b90e435f6675df6ad3f11
匡敏 <kuangmin@kuangmindeMacBook-Pro.local>
2017-06-27 07:28:38 +0800

    gaizheng seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,9 +23,9 @@ p1 = Product.create(title: "青海湖5日深度休闲游",
                    description: "青海湖又名“库库淖尔”，即蒙语“青色的海”之意。由祁连山的大通山、日月山与青海南山之间的断层陷落形成。",
                    price: 12000,
                    quantity: 10,
-                  
+
                     )
-p1.photos.create!(image: open("http://oqjdhoq9e.bkt.clouddn.com/%E9%9D%92%E6%B5%B7%E6%B9%961.jpg"))
+p1.image.create!(image: open("http://oqjdhoq9e.bkt.clouddn.com/%E9%9D%92%E6%B5%B7%E6%B9%961.jpg"))
 # p1.photos.create!(image: open("http://oqjdhoq9e.bkt.clouddn.com/%E9%9D%92%E6%B5%B7%E6%B9%962.jpg"))
 # p1.photos.create!(image: open("http://oqjdhoq9e.bkt.clouddn.com/%E9%9D%92%E6%B5%B7%E6%B9%963.jpg"))
 # p1.photos.create!(image: open("http://oqjdhoq9e.bkt.clouddn.com/%E9%9D%92%E6%B5%B7%E6%B9%964.jpg"))
