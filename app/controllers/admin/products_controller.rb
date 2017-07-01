e718c7b37cea41ea2cf41557d9c73497e8954f0a
李阳 <admin@admindeMacBook-Air.local>
2017-07-01 11:07:22 +0800

    implement backend CRUD

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -7,6 +7,20 @@ class Admin::ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
+  end
+
   def create
     @product = Product.new(product_params)
 
