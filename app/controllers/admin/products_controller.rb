c7294266053e5244579e122605f91bc4280d12ec
李阳 <admin@admindeMacBook-Air.local>
2017-07-01 10:41:33 +0800

    install simpleform with bootstrap

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,25 @@
+class Admin::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @prouct.save
+      redirect_to admin_products_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :quantity, :price)
+  end
+end
