1b176a6eca9426aaafe2a6bc4d8cb1b48acaaca9
wuzhao <liwuzhao97@gmail.com>
2017-07-03 22:50:43 +0800

    image

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -50,7 +50,7 @@ class Admin::ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name,:description,:price,:quantity)
+    params.require(:product).permit(:name,:description,:price,:quantity,:image)
   end
 
 end
