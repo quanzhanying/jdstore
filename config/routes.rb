9656e33a3010e40afbadf124434161540ce224e3
dreamrobot <chingchih.wu@gmail.com>
2017-07-12 20:03:32 +0800

    implement prodcut show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
-  root 'welcome#index'
+  root 'products#index'
   devise_for :users
+
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
