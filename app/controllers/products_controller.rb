998bc58b700d75b04bdc66d336cdef7682cb9e2b
dreamrobot <chingchih.wu@gmail.com>
2017-07-14 18:18:03 +0800

    implement carts index function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,6 +12,6 @@ class ProductsController < ApplicationController
 
    current_cart.add_product_to_cart(@product)
    flash[:notice] = "成功加入購物車"
-   redirect_to :backs
+   redirect_to :back
  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
       post :add_to_cart
     end
   end
+  resources :carts
 end
