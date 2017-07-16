2a18cbc682a56afe551d787580c147dcfd0ae1b4
zsmhy <zsm@example.com>
2017-07-16 18:27:46 +0800

    add payment_method function

--- config/routes.rb
+++ config/routes.rb
@@ -19,8 +19,17 @@ end
   end
 
   resources :cart_items
-  resources :orders
-  namespace :account do
-    resources :orders
+
+    resources :orders do
+      member do
+        post :pay_with_alipay
+        post :pay_with_wechat
+      end
+    end
+
+    namespace :account do
+      resources :orders
+
+
   end
 end
