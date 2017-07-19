c0ebde1ce2b5647dafb6ed0f1e0786eed80caf80
dreamrobot <chingchih.wu@gmail.com>
2017-07-19 21:25:22 +0800

    add aasm_state to order

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,7 +1,7 @@
 class OrderMailer < ApplicationMailer
   def notify_order_placed(order)
     @order        = order
-    @order        = order.user
+    @user       = order.user
     @product_lists = @order.product_lists
 
     mail(to: @user.email, subject: "[JDstore] 感謝您完成本次的下單，以下是您這次購物明細＃{order.token}")
