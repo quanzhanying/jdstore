46b8343728f50b39628d71cafe9fc485f2077aaf
xuedong <xuedong6624@gmail.com>
2017-08-06 18:00:19 +0800

    only admin can access backend panel

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u = User.new
+u.email = "admin@test.com"           # 可以改成自己的 email
+
+u.password = "123456"                # 最少要六码
+
+u.password_confirmation = "123456"   # 最少要六码
+
+u.is_admin = true
+u.save
