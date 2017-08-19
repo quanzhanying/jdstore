66d3b449fccb414732c34d61257c8401cea082e2
Your Nmae <you@example.com>
2017-08-19 17:34:22 +0800

    cannot add same item in cart

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,5 @@ Rails.application.routes.draw do
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :cart_items
 end
