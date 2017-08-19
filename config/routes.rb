a04f6f406336fcff91deba18aaf12ea464042221
Your Nmae <you@example.com>
2017-08-20 04:30:02 +0800

    touch app/views/carts/checkout.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,5 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cart_items
+  resources :orders
 end
