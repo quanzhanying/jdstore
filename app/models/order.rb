92a6d43b71f548b13c08050974975572893e13f5
silvia <gitz1594@icloud.com>
2017-08-20 12:27:40 +0800

    add order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ApplicationRecord
+  belongs_to :user
+
+  validatables :billing_name, presence: true
+  validatables :billing_address, presence: true
+  validatables :shipping_name, presence: true
+  validatables :shipping_address, presence: true
 end
