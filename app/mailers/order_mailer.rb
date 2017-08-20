edfca2f2e392c047dac5a26384c99fcc5ecdbc32
silvia <gitz1594@icloud.com>
2017-08-20 20:33:43 +0800

    add OrderMailer function

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,6 +4,6 @@ class OrderMailer < ApplicationMailer
     @user        = order.user
     @product_lists = @order.product_lists
 
-    mail(to: @user.email , subject: "[JDstore] 感謝您完成本次的下單, 以下是您這次購物明細 #{order.token}" )
-  end
+   mail(to: @user.email , subject: "[JDstore] 感謝您完成本次下單, 以下是您這次購物明細 #{order.token}")
+ end
 end
