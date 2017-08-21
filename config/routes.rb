5f8fecc30384663b753045af9b3fe2f3dc2fabdd
Your Nmae <you@example.com>
2017-08-21 22:38:40 +0800

    users can see their orders

--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cart_items
   resources :orders
+
+  namespace :account do
+    resources :orders
+  end
+
 end
