072fdd2134cc1b5e500133b26d7459529d390ca5
zhubaocheng <imzhubaocheng@gmail.com>
2017-08-21 17:02:42 +0800

    implement current_cart function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,21 @@ class ApplicationController < ActionController::Base
       flash[:alert] = "你不是管理员，没有这个权限！"
     end
   end
+
+  helper_method :current_cart
+
+  def current_cart
+    @current_cart ||= find_cart
+  end
+
+  private
+
+  def find_cart
+    cart = Cart.fing_by(id: session[:cart_id])
+    if cart.blank?
+      cart = Cart.create
+    end
+    session[:cart_id] = cart.id
+    return cart
+  end
 end
