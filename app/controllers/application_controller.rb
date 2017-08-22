24b0706c462050a61b28d91f938160315dc06af7
dreamrobot <chingchih.wu@gmail.com>
2017-08-22 14:48:33 +0800

    add english/chinese and time_zone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
+
+  def set_locale
+    if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
+      session[:locale] = params[:locale]
+    end
+
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
 
   def admin_required
     if !current_user.admin?
