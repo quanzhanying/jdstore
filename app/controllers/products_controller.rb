eaf8aa20ca9cee0f8ab0fe09804b47624ab9a683
sarah <smile-wu@hotmail.com>
2017-08-24 20:20:10 +0800

    can't add product repeatedy to cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -46,8 +46,12 @@ class ProductsController < ApplicationController
 
   def add_to_cart
     @product = Product.find(params[:id])
-    current_cart.add_product_to_cart(@product)
-    flash[:notice] = "成功加入购物车"
+    unless @product.present?
+      current_cart.add_product_to_cart(@product)
+      flash[:notice] = "成功加入购物车"
+    else
+      flash[:warning] = "购物车已有该商品，无需再加入。"
+    end
     redirect_to :back
   end
 
