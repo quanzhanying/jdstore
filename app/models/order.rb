37dbb75841d7262c42c51ab3f52dec745ad1dc3b
zhubaocheng <imzhubaocheng@gmail.com>
2017-08-24 09:27:16 +0800

    billing&shipping info must be writed

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ApplicationRecord
+  belongs_to :user
+
+  validates_presence_of :billing_name
+  validates_presence_of :billing_address
+  validates_presence_of :shipping_name
+  validates_presence_of :shipping_address  
 end
