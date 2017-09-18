8e04207ffa9fc2cff58ae55de56fbe2296f7ea27
Celery8911 <celery8911@gmail.com>
2017-09-18 16:34:39 +0800

    empty cart

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,12 @@ Rails.application.routes.draw do
        post :add_to_cart
      end
    end
- resources :carts
+
+    resources :carts do
+      collection do
+        delete :clean
+      end
+    end
+
+
 end
