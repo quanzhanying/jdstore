5b5efdc54b0af13720ef37e3bc15cdaac97fabc4
sarah <smile-wu@hotmail.com>
2017-09-25 12:38:38 +0800

    can see pic both at dev and produ env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,15 @@
-CarrierWave.cofigure do |config|
-  if Rails.evn.production?
+CarrierWave.configure do |config|
+  if Rails.env.production?
     config.fog_provider = "fog"
     config.fog_credentials = {
       provider:               "AWS",
-      aws_access_key_id:      EVN["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key:         EVN["AWS_SECRET_ACCESS_KEY"],
-      region:                 EVN["AWS_REGION"]
+      aws_access_key_id:      ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key:         ENV["AWS_SECRET_ACCESS_KEY"],
+      region:                 ENV["AWS_REGION"]
     }
-    config.fog_directory = EVN["AWS_BUCKET_NAME"]
+    config.fog_directory = ENV["AWS_BUCKET_NAME"]
   else
-    config.storeage :file
+    config.storage :file
   end
 
 end
