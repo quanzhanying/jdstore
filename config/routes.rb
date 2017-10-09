4cb09a08f6b96cb2b9fc7a2fa15a9203c4d07bd8
chun <yiguo5880@gmail.com>
2017-10-09 21:51:20 +0800

    empty cart

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
-  resources :carts
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end
   resources :products do
     member do
       post :add_to_cart
