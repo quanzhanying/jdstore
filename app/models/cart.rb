a55d16cd29f20bd2300f3afb64bcb804a45b396e
gaomingzhen <jingmosigui@sina.com>
2017-10-30 21:08:01 +0800

    implement cart_total_price function

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -8,4 +8,14 @@ class Cart < ApplicationRecord
     ci.quantity = 1
     ci.save
   end
+
+  def total_price
+    sum = 0
+    cart_items.each do |cart_item|
+      if cart_item.product.price.present?
+        sum += cart_item.quantity * cart_item.product.price
+      end
+    end
+    sum
+  end
 end
