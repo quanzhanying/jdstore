baea51414e03c64347fb7654552bd13cf54abe20
gaomingzhen <jingmosigui@sina.com>
2017-11-04 11:51:52 +0800

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,10 @@ class Order < ApplicationRecord
   validates :shipping_address, presence: true
 
   has_many :product_lists
+
+  before_create :generate_token
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
 end
