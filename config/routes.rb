5e948c85ccb449889773794b1f05d5d3b72d62ad
cpbabyblue <cpbabyblue@gmail.com>
2017-11-26 15:15:36 +0800

    change admin/product backend panel

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   end
 
   root 'products#index'
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
