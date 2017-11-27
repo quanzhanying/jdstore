09ea113c9c63767332d1b54062da01e0aee26ee8
cpbabyblue <cpbabyblue@gmail.com>
2017-11-27 15:04:58 +0800

    delete an item from cart

---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,14 @@
+class CartItemsController < ApplicationController
+  before_action :authenticate_user!
+
+  def destroy
+    @cart = current_cart
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    @product = @cart_item.product
+    @cart_item.destroy
+
+    flash[:warning] = "empty the #{@product.title} from your cart"
+    redirect_to :back
+  end
+  
+end
