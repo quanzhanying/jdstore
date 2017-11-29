c0ffec4559261944b87c4d50cdf0df90218c00d7
cpbabyblue <cpbabyblue@gmail.com>
2017-11-29 15:20:58 +0800

    implement admin/orders show function

--- app/helpers/admin/orders_helper.rb
+++ app/helpers/admin/orders_helper.rb
@@ -1,10 +1,5 @@
 module Admin::OrdersHelper
-  layout "admin"
-  before_action :authenticate_user!
-  before_action :admin_required
 
-  def index
-    @orders = Order.order("id DESC")
-  end
-  
+
+
 end
--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,10 @@
 module OrdersHelper
+  def render_order_paid_state(order)
+    if order.is_paid?
+      "Paid"
+    else
+      "not Paid"
+    end
+  end
+  
 end
