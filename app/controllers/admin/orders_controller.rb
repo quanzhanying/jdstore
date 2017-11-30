a6adf82b7ab63c451b92989000e963754661dec9
cpbabyblue <cpbabyblue@gmail.com>
2017-11-30 11:34:12 +0800

    add notify_cancel/ship function

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -16,7 +16,9 @@ class Admin::OrdersController < ApplicationController
   def ship
     @order = Order.find(params[:id])
     @order.ship!
+    OrderMailer.notify_ship(@order).deliver!
     redirect_to :back
+
   end
 
 
@@ -29,6 +31,7 @@ class Admin::OrdersController < ApplicationController
   def cancel
     @order = Order.find(params[:id])
     @order.cancel_order!
+    OrderMailer.notify_cancel(@order).deliver!
     redirect_to :back
   end
 
--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,11 +1,11 @@
 class OrderMailer < ApplicationMailer
   def notify_order_placed(order)
 
-    @order = order
-    @user = order.user
-    @product_lists = @order.product_lists
+      @order = order
+      @user = order.user
+      @product_lists = @order.product_lists
 
-    mail(to: @user.email, subject: "[JDstore] Thank you for your order, the following is your purchasing details #{order.token}")
+      mail(to: @user.email, subject: "[JDstore] Thank you for your order, the following is your purchasing details #{order.token}")
   end
 
   def apply_cancel(order)
@@ -14,6 +14,21 @@ class OrderMailer < ApplicationMailer
     @product_lists = @order.product_lists
 
     mail(to: "admin@test.com", subject: "[JDstore] Purchaser #{order.user.email} apply to cancel order #{order.token}")
+  end
+
+  def notify_ship(order)
+      @order = order
+      @user = order.user
+      @product_lists = @order.product_lists
+
+      mail(to: @user.email, subject: "[JDstore] your order #{order.token} has been shipped")
+  end
+
+  def notify_cancel(order)
+      @order = order
+      @user = order.user
+      @product_lists = @order.product_lists
+      mail(to: @user.email, subject: "[JDstore] your order #{order.token} has been cancelled")
   end
 
 
