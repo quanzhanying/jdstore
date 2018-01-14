abc241d343a8f5d3550356d1d526f4a698f039fb
uuurs <zhongrenju2016@gmail.com>
2018-01-14 16:45:04 +0800

    add_to_cart button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,11 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    redirect_to :back
+    flash[:notice] = "test mv to cart"
+  end
+
 end
