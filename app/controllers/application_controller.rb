2ad8580879eeb6880368ad6894b60e1b1b9bd0e1
uuurs <zhongrenju2016@gmail.com>
2018-01-18 22:59:43 +0800

    implement current_cart function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,12 +17,12 @@ class ApplicationController < ActionController::Base
   private
 
   def find_cart
-    cart = Cart.find_by(:id session[:cart_id])
+    cart = Cart.find_by(id: session[:cart_id])
     if cart.blank?
       cart = Cart.create
     end
     session[:cart_id] = cart.id
     return cart
   end
-  
+
 end
