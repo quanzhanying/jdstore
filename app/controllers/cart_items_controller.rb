a3a6d4b58f0d7ccc5e4466b49e5a58a1e400611f
uuurs <zhongrenju2016@gmail.com>
2018-01-28 21:02:42 +0800

    delete an item from cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -3,7 +3,7 @@ class CartItemsController < ApplicationController
 
   def destroy
     @cart  = current_cart
-    @cart_item = @cart.cart_items.find_by(product_id: parmas[:id])
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
     @product = @cart_item.product
     @cart_item.destroy
 
