ed089461dbc756eef029ed5e4bde768365d01fef
uuurs <zhongrenju2016@gmail.com>
2018-01-28 20:42:51 +0800

    correct clean error

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,7 @@
 class CartsController < ApplicationController
-  def clean!
-    cart_items.destroy_all
+  def clean
+    current_cart.clean!
+    flash[:warning] = "已清空购物车"
+    redirect_to carts_path
   end
 end
