cf9c623ad878e086e59e84a4254ab831f80b5426
xueshaojie <xueshaojie@otcbtc.com>
2019-03-12 20:16:24 -0700

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,10 @@ class Order < ApplicationRecord
 
   belongs_to :user
   has_many :product_lists
+
+  before_create :generate_token
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
 end
