73d71e9127dbe480d1273ffeb0bcb129a5121def
xueshaojie <xueshaojie@otcbtc.com>
2019-03-12 20:05:57 -0700

    add product_list model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ApplicationRecord
-  belongs_to :user
 
   validates :billing_name, :billing_address, :shipping_name, :shipping_address, presence: true
+
+  belongs_to :user
+  has_many :product_lists
 end
