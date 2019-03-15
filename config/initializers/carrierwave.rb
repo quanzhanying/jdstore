ca42487c362e915641a881977eb4a670d1f7a5ed
张昱雯 <1612896130@qq.com>
2019-03-15 15:25:10 +0800

    config qiniu

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,11 @@
+CarrierWave.configure do |config|
+  config.storage             = :qiniu
+  config.qiniu_access_key    = ENV["qiniu_access_key"]
+  config.qiniu_secret_key    = ENV["qiniu_secret_key"]
+  config.qiniu_bucket        = ENV["qiniu_bucket"]
+  config.qiniu_bucket_domain = ENV["qiniu_bucket_domain"]
+  config.qiniu_block_size    = 4*1024*1024
+  config.qiniu_protocol      = "http"
+  config.qiniu_up_host       = "http://up.qiniu.com"  #选择不同的区域时，"up.qiniu.com" 不同
+
+end
