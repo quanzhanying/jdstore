d823c6c0b9c79d6be004dc59da47c15b1afe0cbd
张昱雯 <1612896130@qq.com>
2019-03-15 13:54:07 +0800

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,15 @@
 class Order < ApplicationRecord
   has_many :product_lists
   belongs_to :user
-
+  before_create :generate_token
   validates :billing_name, presence: true
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
+
+
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
 end
