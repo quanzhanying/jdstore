c87baea44f0248e29999d96df18307f424a32164
张昱雯 <1612896130@qq.com>
2019-03-15 14:27:34 +0800

    add admin/orders

---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,10 @@
+class Admin::OrdersController < ApplicationController
+  layout "admin"
+
+  before_action :authenticate_user!
+  before_action :admin_required
+
+  def index
+    @orders = Order.order("id DESC")
+  end
+end
