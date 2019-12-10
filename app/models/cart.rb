3251f2ca01fd9e54aad748ac84f9d829c811c885
laijay <jack1011tw@gmail.com>
2019-12-10 11:09:30 +0800

    implement current_cart function

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,6 @@
 class Cart < ApplicationRecord
   has_many :cart_items
-  has_many :products, through: :cart_items, sources: :product
+  has_many :products, through: :cart_items, source: :product
 
   def add_product_to_cart(product)
     ci = cart_items.build
@@ -8,5 +8,5 @@ class Cart < ApplicationRecord
     ci.quantity = 1
     ci.save
   end
-  
+
 end
