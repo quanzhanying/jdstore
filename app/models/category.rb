f9c87cf149a525af2feb52d0f116ee901881ec67
laijay <jack1011tw@gmail.com>
2020-01-05 23:19:13 +0800

    finish proodcut categorization function

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,17 @@
 class Category < ApplicationRecord
-  has_ancestry
+
+  before_validation :correct_ancestry
+
+  validates :title, presence: { message: "名稱不得為空" }
+  validates :title, uniqueness: { message: "名稱不得重複"}
+
+  has_ancestry orphan_strategy: :destroy
+
   has_many :products, dependent: :destroy
+
+  private
+  def correct_ancestry
+    self.ancestry = nil if self.ancestry.blank?
+  end
+
 end
