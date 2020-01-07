09f57a67f295ff98e46da009529ac015498f77ce
laijay <jack1011tw@gmail.com>
2020-01-07 23:34:43 +0800

    add search function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,4 +21,20 @@ class ProductsController < ApplicationController
     redirect_to :back
   end
 
+    def search
+
+    #在全栈商品中搜索
+    @products = Product.where("title like :title", title: "%#{params[:w]}%")
+      .order("id desc").page(params[:page] || 1).per_page(params[:per_page] || 12)
+      .includes(:main_product_image)
+
+    #在二级分类商品中搜索
+    unless params[:category_id].blank?
+      @products = @products.where(category_id: params[:category_id])
+    end
+
+    render file: 'products/index'
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products do
+    get :search, on: :collection
     member do
       post :add_to_cart
     end
