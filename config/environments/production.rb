52c767b990b914b7f84ca88d9a0723176e2e9c76
laijay <jack1011tw@gmail.com>
2020-01-18 18:44:08 +0800

    reconnect new cloudinary and heroku app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'https://desolate-spire-48958.herokuapp.com/'}
+  config.action_mailer.default_url_options = { :host => 'https://secret-lowlands-24556.herokuapp.com/'}
 
   # 加上這段 SMTP 設定
   config.action_mailer.delivery_method = :smtp
